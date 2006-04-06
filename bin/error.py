25629f18446e4e626646652c18bc49fab53ed5a0
Andy Whitcroft <apw@shadowen.org>
2006-04-06 18:21:16 +0000

    commands: ensure we get hard errors from system
    Update the system routine which will run the specified command
    and check its exit status.  Any non-zero exit is converted into
    CmdError(TestError) exception ensuring that any uncaught command
    errors will result in test failure.
    
    Signed-off-by: Andy Whitcroft <apw@shadowen.org>

--- bin/error.py
+++ bin/error.py
@@ -2,3 +2,7 @@ class JobError(Exception):
 	pass
 class TestError(Exception):
 	pass
+class CmdError(TestError):
+	def __str__(self):
+		return "Command <" + self.args[0] + "> failed, rc=%d" % (
+			self.args[1])
