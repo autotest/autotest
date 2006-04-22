59b91d68a3b6f7ce376fdb2a1918a1dc0672d0a7
Martin J. Bligh <mbligh@google.com>
2006-04-22 18:12:35 +0000

    Fix up bonnie.setup()

--- bin/tests/bonnie/bonnie.py
+++ bin/tests/bonnie/bonnie.py
@@ -3,14 +3,21 @@ from autotest_utils import *
 
 class bonnie(test.test):
 	def setup(self)
+		self.tarball = self.bindir + 'bonnie++-1.03a.tgz'
+		# http://www.coker.com.au/bonnie++/bonnie++-1.03a.tgz
+		os.chdir(self.srcdir . '/..')
+		system_raise('tar ztf ' + self.tarball)
+		os.rename('bonnie++-1.03a/', self.srcdir)
+		os.chdir(self.srcdir)
 
+		system_raise('./configure')
+		system_raise('make')
+		
 	def execute(self, iterations = 1, extra_args = None, user = 'root');
-
 		args = ['-d ' + self.tmp_dir]
 		args.append('-u ' + user)
 		args.append(extra_args)
-		os.system('./bonnie++ -d ' + ' '.join(args))
+		system_raise(self.srcdir + '/bonnie++ ' + ' '.join(args))
 
 		for i in range(1, iterations+1):
-			
 			kernel.build_timed(threads, '../log/time.%d' % i)
