80a10d106105406a5754bdf18344bb2ec80858c8
apw <apw@592f7852-d20e-0410-864c-8624ca9c26a4>
2006-04-22 10:30:33 +0000

    errors: make errors hierachical
    In order to make it possible to catch and handle all Autotest errors
    separatly from unexpected uncaught ones we need a handle for them.
    To this end add the AutotestError object and make the TestError
    and JobError objects children of it.
    
    Signed-off-by: Andy Whitcroft <apw@shadowen.org>
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@36 592f7852-d20e-0410-864c-8624ca9c26a4

--- bin/error.py
+++ bin/error.py
@@ -1,7 +1,18 @@
-class JobError(Exception):
+# AutotestError: the parent of all errors deliberatly thrown
+# within the client code.
+class AutotestError(Exception):
 	pass
-class TestError(Exception):
+
+# JobError: indicates an error which terminates and fails the whole job.
+class JobError(AutotestError):
 	pass
+
+# TestError: indicates an error which terminates and fails the test.
+class TestError(AutotestError):
+	pass
+
+# CmdError: indicates that a command failed, is fatal to the test
+# unless caught.
 class CmdError(TestError):
 	def __str__(self):
 		return "Command <" + self.args[0] + "> failed, rc=%d" % (
