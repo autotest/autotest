dc879e6e9327a3d17ee76935201e5a4d1fd10636
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2006-04-22 22:11:09 +0000

    make ltp use unmap_potential_url
    git-svn-id: svn://test.kernel.org/autotest/trunk@58 592f7852-d20e-0410-864c-8624ca9c26a4

--- bin/tests/ltp/ltp.py
+++ bin/tests/ltp/ltp.py
@@ -1,18 +1,13 @@
 class ltp(test.test):
 	version = 1
 
-	def setup(self, url=None)
-		if (url):
-			self.tarball = self.tmpdir + os.path.basename(url)
-			get_file (url, self.tarball)
-		else:
-			self.tarball = self.bindir + 'ltp-full-20060412.tgz'
-		# http://prdownloads.sourceforge.net/ltp/ltp-full-20060412.tgz
+	# http://prdownloads.sourceforge.net/ltp/ltp-full-20060412.tgz
+	def setup(self, tarball = self.bindir+'ltp-full-20060412.tgz');
+		self.tarball = unmap_potential_url(url, self.tmpdir)
 		extract_tarball_to_dir(self.tarball, self.srcdir)
 		os.chdir(self.srcdir)
 
-		cpus = count_cpus()
-		system('make -j' + cpus)
+		system('make -j' + count_cpus())
 		system('make install')
 		
 	def execute(self, args = None);
