3eb39498fe3f56e191fdef2f5dedf961e98f062e
Andy Whitcroft <apw@shadowen.org>
2006-04-22 12:40:39 +0000

    errors: fix sub process exception handling
    Ensure that we pick up the full stack from any test errors from Python.
    We now classify any failed error as an UnhandledError.  This allows
    us to store the stack trace in that error and emit it more cleanly
    into the logs.
    
    Also adds an unused non-fork version if we decided to remove the
    fork here.
    
    Signed-off-by: Andy Whitcroft <apw@shadowen.org>

--- bin/error.py
+++ bin/error.py
@@ -1,3 +1,6 @@
+import sys
+from traceback import format_exception
+
 # AutotestError: the parent of all errors deliberatly thrown
 # within the client code.
 class AutotestError(Exception):
@@ -17,3 +20,18 @@ class CmdError(TestError):
 	def __str__(self):
 		return "Command <" + self.args[0] + "> failed, rc=%d" % (
 			self.args[1])
+
+# UnhandledError: indicates an unhandled exception in a test.
+class UnhandledError(TestError):
+	def __init__(self, prefix):
+		t, o, tb = sys.exc_info()
+		trace = format_exception(t, o, tb)
+		# Clear the backtrace to prevent a circular reference
+		# in the heap -- as per tutorial
+		tb = ''
+
+		msg = prefix
+		for line in trace:
+			msg = msg + line
+
+		self.args = msg
