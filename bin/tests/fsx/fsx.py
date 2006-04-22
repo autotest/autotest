a49adb0049ef4ae62a718f8f405358406ff99713
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2006-04-22 23:33:10 +0000

    add fsx as a test
    git-svn-id: svn://test.kernel.org/autotest/trunk@64 592f7852-d20e-0410-864c-8624ca9c26a4

---
+++ bin/tests/fsx/fsx.py
@@ -0,0 +1,20 @@
+# This requires aio headers to build.
+# On ubuntu: "apt-get install libaio libaio-dev"
+
+# NOTE - this should also have the ability to mount a filesystem, 
+# run the tests, unmount it, then fsck the filesystem
+
+class fsx(test.test):
+	version = 1
+
+	# http://www.zip.com.au/~akpm/linux/patches/stuff/ext3-tools.tar.gz
+	def setup(self, tarball = self.bindir + 'ext3-tools.tar.gz')
+		self.tarball = unmap_potential_url(tarball, self.tmpdir)
+		extract_tarball_to_dir(self.tarball, self.srcdir)
+		os.chdir(self.srcdir)
+
+		system('make fsx-linux')
+		
+	def execute(self, repeat = '100000');
+		args = '-N ' + repeat
+		system(self.srcdir + '/fsx-linux ' + args)
