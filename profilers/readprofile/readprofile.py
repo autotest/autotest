3fec33abac4ab1f71c5aaeb8f9e4bd8b689d1267
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2006-04-23 00:40:06 +0000

    add readprofile profiler
    git-svn-id: svn://test.kernel.org/autotest/trunk@70 592f7852-d20e-0410-864c-8624ca9c26a4

---
+++ profilers/readprofile/readprofile.py
@@ -0,0 +1,35 @@
+import shutil
+
+class readprofile(profiler.profiler):
+	version = 1
+
+# http://www.kernel.org/pub/linux/utils/util-linux/util-linux-2.12r.tar.bz2
+	def setup(self, tarball = self.bindir + 'util-linux-2.12r.tar.bz2'):
+		system('grep -iq " profile=" /proc/cmdline')
+
+		self.tarball = unmap_potential_url(tarball, self.tmpdir)
+		extract_tarball_to_dir(self.tarball, self.srcdir)
+		os.chdir(self.srcdir)
+
+		system('./configure')
+		os.chdir('sys-utils')
+		system('make readprofile')
+		self.cmd = os.getcwd() + '/readprofile'
+
+
+	def start(self):
+		system(self.cmd + ' -r')
+
+
+	def stop(self):
+		self.rawprofile = self.resultsdir + '/results/profile.raw'
+		shutil.copyfile('/proc/profile', rawprofile)
+
+
+	def report(self)
+		args = ' -n'
+		args = args + ' -m /boot/System.map'
+		args = args + ' -r ' + self.rawprofile
+		self.txtprofile = self.resultsdir + '/results/profile.text'
+		system(self.cmd + args + ' | sort -nr > ' + self.txtprofile)
+
