1b76965cb45423b963a5557b3cd630b24f191847
Martin J. Bligh <mbligh@google.com>
2006-05-03 16:38:15 +0000

    Fix up references to kernel to self.kernel so they're preserved from setup to run

--- tests/kernbench/kernbench.py
+++ tests/kernbench/kernbench.py
@@ -8,16 +8,16 @@ class kernbench(test.test):
   		   config = os.environ['AUTODIR'] + "/tests/kernbench/config"):
 
 		self.top_dir = self.job.tmpdir+'/kernbench'
-		kernel = self.job.kernel(self.top_dir, kernelver)
-		kernel.config(config)
+		self.kernel = self.job.kernel(self.top_dir, kernelver)
+		self.kernel.config(config)
 
 
 	def execute(self, iterations = 1, threads = 2 * count_cpus()):
 		print "kernbench x %d: %d threads" % (iterations, threads)
 
-		kernel.build_timed(threads)         # warmup run
+		self.kernel.build_timed(threads)         # warmup run
 		for i in range(1, iterations+1):
-			kernel.build_timed(threads, '../log/time.%d' % i)
+			self.kernel.build_timed(threads, '../log/time.%d' % i)
 
 		os.chdir(top_dir + '/log')
 		system("grep elapsed time.* > time")
