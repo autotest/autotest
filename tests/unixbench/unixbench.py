3a0b5310d8ba69829a6460ca8c9fa752c559b158
Martin J. Bligh <mbligh@google.com>
2006-05-17 21:32:55 +0000

    add unixbench

---
+++ tests/unixbench/unixbench.py
@@ -0,0 +1,19 @@
+import test
+from autotest_utils import *
+
+class unixbench(test.test):
+	version = 1
+
+	# http://www.tux.org/pub/tux/niemi/unixbench/unixbench-4.1.0.tgz
+	def setup(self, tarball = 'unixbench-4.1.0.tar.bz2'):
+		tarball = unmap_url(self.bindir, tarball, self.tmpdir)
+		extract_tarball_to_dir(tarball, self.srcdir)
+		os.chdir(self.srcdir)
+
+		system('make')
+		
+	def execute(self, iterations = 1, args = ''):
+		for i in range(1, iterations+1):
+			os.chdir(self.srcdir)
+			vars = 'TMPDIR=\"%s\" RESULTDIR=\"%s\"' % (self.tmpdir, self.resultsdir)
+			system(vars + ' ./Run ' + args)
