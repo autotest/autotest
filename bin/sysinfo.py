d10b05bf80278276f57ac87e5ea5a2f9382061be
Martin J. Bligh <mbligh@google.com>
2006-05-18 18:27:31 +0000

    add sysinfo script

---
+++ bin/sysinfo.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import os,os.path,shutil
+from autotest_utils import *
+
+files = ['/proc/pci', '/proc/meminfo', '/proc/slabinfo', '/proc/version', 
+	'/proc/cpuinfo']
+commands = ['lshw', 'uname -a']
+path = ['/usr/bin', '/bin']
+
+def run_command(command, output):
+	for dir in path:
+		pathname = dir + '/' + command
+		if (os.path.exists(pathname)):
+			system(pathname + ' > ' + output)
+
+for command in commands:
+	run_command(command, os.path.basename(command))
+
+for file in files:
+	shutil.copyfile(file, os.path.basename(file))
+
