ec67511d246ca2f11b8350e41055ea55454f1a75
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2006-05-31 17:23:19 +0000

    add profiler.py
    git-svn-id: svn://test.kernel.org/autotest/trunk@145 592f7852-d20e-0410-864c-8624ca9c26a4

---
+++ bin/profiler.py
@@ -0,0 +1,23 @@
+class profiler:
+	def __init__(self, job):
+		self.job = job
+
+	def setup(self):
+		return
+
+
+	def initialize(self, *args):
+		return
+
+
+	def start(self, test):
+		return
+
+
+	def stop(self, test):
+		return
+
+
+	def report(self, test):
+		return
+
