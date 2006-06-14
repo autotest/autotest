037afb370069befac5574052f4de4e202d064b94
Martin J. Bligh <mbligh@google.com>
2006-06-14 22:34:58 +0000

    Add hooks to setup deps in main job structure and fsx.

--- tests/fsx/fsx.py
+++ tests/fsx/fsx.py
@@ -1,5 +1,5 @@
 # This requires aio headers to build.
-# On ubuntu: "apt-get install libaio libaio-dev"
+# Should work automagically out of deps now.
 
 # NOTE - this should also have the ability to mount a filesystem, 
 # run the tests, unmount it, then fsck the filesystem
@@ -8,26 +8,38 @@ import test
 from autotest_utils import *
 
 class fsx(test.test):
-	version = 1
+	version = 2
 
+	def initialize(self):
+		self.job.setup_dep(['libaio'])
+		ldflags = '-L ' + self.autodir + '/deps/libaio/lib'
+		cflags = '-I ' + self.autodir + '/deps/libaio/include'
+		var_ldflags = 'LDFLAGS="' + self.ldflags + '"'
+		var_cflags  = 'CFLAGS="' + self.cflags + '"'
+		self.make_flags = var_ldflags + ' ' + var_cflags
+	
+	
 	# http://www.zip.com.au/~akpm/linux/patches/stuff/ext3-tools.tar.gz
 	def setup(self, tarball = 'ext3-tools.tar.gz'):
 		self.tarball = unmap_url(self.bindir, tarball, self.tmpdir)
 		extract_tarball_to_dir(self.tarball, self.srcdir)
 		os.chdir(self.srcdir)
-
-		system('make fsx-linux')
+		system(self.make_flags + ' make fsx-linux')
 
 
 	def execute(self, repeat = '100000'):
 		args = '-N ' + repeat
 		os.chdir(self.tmpdir)
-		system(self.srcdir + '/fsx-linux ' + args + ' poo')
+		libs = self.autodir+'/deps/libaio/lib/'
+		ld_path = prepend_path(libs, environ('LD_LIBRARY_PATH'))
+		var_ld_path = 'LD_LIBRARY_PATH=' + ld_path
+		cmd = self.srcdir + '/fsx-linux ' + args + ' poo'
+		system(var_ld_path + ' ' + cmd)
 
 		# Do a profiling run if necessary
 		profilers = self.job.profilers
 		if profilers.present():
 			profilers.start(self)
-			system(self.srcdir + '/fsx-linux ' + args + ' poo')
+			system(var_ld_path + ' ' + cmd)
 			profilers.stop(self)
 			profilers.report(self)
