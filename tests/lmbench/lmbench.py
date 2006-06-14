22f302edec1fbb310f486865fbe2708b748c13a0
Martin J. Bligh <mbligh@google.com>
2006-06-14 21:55:29 +0000

    update version for lmbench

--- tests/lmbench/lmbench.py
+++ tests/lmbench/lmbench.py
@@ -3,7 +3,7 @@ import test
 from autotest_utils import *
 
 class lmbench(test.test):
-	version = 1
+	version = 2
 
 	def setup(self, tarball = 'lmbench3.tar.bz2'):
 		tarball = unmap_url(self.bindir, tarball, self.tmpdir)
