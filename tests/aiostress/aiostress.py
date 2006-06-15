4da57b07c8845600a083d11c7c714d1797cb513d
Martin J. Bligh <mbligh@google.com>
2006-06-15 22:03:06 +0000

    From: masouds@google.com
    Hello,
    The following tarball adds aiostress test to the tests collection.
    Please consider for inclusion; All feedback is welcome.
    cheers,
    Masoud

---
+++ tests/aiostress/aiostress.py
@@ -0,0 +1,45 @@
+# This requires aio headers to build.
+# Should work automagically out of deps now.
+
+# NOTE - this should also have the ability to mount a filesystem, 
+# run the tests, unmount it, then fsck the filesystem
+
+import test
+from autotest_utils import *
+
+class aiostress(test.test):
+	version = 2
+
+	def initialize(self):
+		self.job.setup_dep(['libaio'])
+		ldflags = '-L ' + self.autodir + '/deps/libaio/lib'
+		cflags = '-I ' + self.autodir + '/deps/libaio/include'
+		self.gcc_flags = ldflags + ' ' + cflags
+
+
+	# ftp://ftp.suse.com/pub/people/mason/utils/aio-stress.c
+	def setup(self, tarball = None):
+		print self.srcdir, self.bindir, self.tmpdir
+		os.mkdir(self.srcdir)
+		os.chdir(self.srcdir)
+		system('cp ' + self.bindir+'/aio-stress.c .')
+		os.chdir(self.srcdir)
+		self.gcc_flags += ' -Wall -lpthread -laio'
+		system('gcc ' + self.gcc_flags + ' aio-stress.c -o aio-stress')
+
+
+	def execute(self, args = ''):
+		os.chdir(self.tmpdir)
+		libs = self.autodir+'/deps/libaio/lib/'
+		ld_path = prepend_path(libs, environ('LD_LIBRARY_PATH'))
+		var_ld_path = 'LD_LIBRARY_PATH=' + ld_path
+		cmd = self.srcdir + '/aio-stress ' + args + ' poo'
+		system(var_ld_path + ' ' + cmd)
+
+		# Do a profiling run if necessary
+		profilers = self.job.profilers
+		if profilers.present():
+			profilers.start(self)
+			system(var_ld_path + ' ' + cmd)
+			profilers.stop(self)
+			profilers.report(self)
