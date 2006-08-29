a29900067db286292a150eaf8e90adc93a368a40
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2006-08-29 04:59:17 +0000

    Fix several interbench issues
    - add space before args so they work
    - remove . in profiler run path, this breaks the run.  Oops.
    - use tmpdir and add -c to supress the log.  tee_redirect now grabs the results.
    Signed-off-by: Brandon Philips <brandon@ifup.org>
    
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@167 592f7852-d20e-0410-864c-8624ca9c26a4

--- tests/interbench/interbench.py
+++ tests/interbench/interbench.py
@@ -13,16 +13,18 @@ class interbench(test.test):
 		system('make')
 		
 	def execute(self, iterations = 1, args = ''):
-		os.chdir(self.resultsdir)
+		os.chdir(self.tmpdir)
+		args += " -c"
+
 		for i in range(1, iterations+1):
-			system(self.srcdir + '/interbench -m \'run #%s\'' % i \
-				+ args)
+			system("%s/interbench -m 'run #%s' %s" % \
+				(self.srcdir, i, args))
 
 		# Do a profiling run if necessary
 		profilers = self.job.profilers
 		if profilers.present():
 			profilers.start(self)
-			system(self.srcdir + './interbench \'profile run\'' \
-				+ args)
+			system("%s/interbench -m 'profile run' %s" % \
+				(self.srcdir, args))
 			profilers.stop(self)
 			profilers.report(self)
