77412b979e0befd88abebd1e5b42ed3ba41c72b9
Yi Yang <yang.y.yi@gmail.com>
2006-08-30 14:32:07 +0000

    From: Yi Yang <yang.y.yi@gmail.com>
    This patch adds stress test case, please review.

---
+++ tests/stress/stress.py
@@ -0,0 +1,31 @@
+import test
+from autotest_utils import *
+
+class stress(test.test):
+	version = 1
+
+	# http://weather.ou.edu/~apw/projects/stress/stress-0.18.8.tar.gz
+	def setup(self, tarball = 'stress-0.18.8.tar.gz'):
+		tarball = unmap_url(self.bindir, tarball, self.tmpdir)
+		extract_tarball_to_dir(tarball, self.srcdir)
+		os.chdir(self.srcdir)
+
+		system('./configure')
+		system('make')
+		
+	def execute(self, iterations = 1, args_list = ''):
+		if len(args_list) != 0:
+			args = '' + args_list
+		else:
+			args = '-c 4 -t 50 -v'
+
+		for i in range(1, iterations+1):
+			system(self.srcdir + '/src/stress ' + args)
+
+		# Do a profiling run if necessary
+		profilers = self.job.profilers
+		if profilers.present():
+			profilers.start(self)
+			system(self.srcdir + '/src/stress ' + args)
+			profilers.stop(self)
+			profilers.report(self)
