b78dfb5242af6095fd1e49926f2b55da82194687
Martin J. Bligh <mbligh@google.com>
2006-09-23 23:34:16 +0000

    Fix up reaim, part 2.
    For the record, I hate libtool. Oh, and autoconf. Oh, and reaim.

--- tests/reaim/reaim.py
+++ tests/reaim/reaim.py
@@ -11,30 +11,51 @@ class reaim(test.test):
 		extract_tarball_to_dir(tarball, self.srcdir)
 
 		self.job.setup_dep(['libaio'])
-		ldflags = '-L ' + self.autodir + '/deps/libaio/lib'
+		libs = '-L' + self.autodir + '/deps/libaio/lib -laio'
 		cflags = '-I ' + self.autodir + '/deps/libaio/include'
-		var_ldflags = 'LDFLAGS="' + ldflags + '"'
+		var_libs = 'LIBS="' + libs + '"'
 		var_cflags  = 'CFLAGS="' + cflags + '"'
-		self.make_flags = var_ldflags + ' ' + var_cflags
+		self.make_flags = var_libs + ' ' + var_cflags
 
 		os.chdir(self.srcdir)
 		system('./bootstrap')
 		system('./configure')
+		system('patch -p1 < ../reaim.diff')
 		system(self.make_flags + ' make')
-		
+		os.rename('src/reaim', 'reaim')
+
+
+	def initialize(self):
+		self.ldlib = 'LD_LIBRARY_PATH=%s/deps/libaio/lib'%(self.autodir)
+
+
 	def execute(self, iterations = 1, workfile = 'workfile.short', 
-			start = '1', end = '10', increment = '2',
-			tmpdir = None):
+			start = 1, end = 10, increment = 2,
+			extra_args = '', tmpdir = None):
 		if not tmpdir:
 			tmpdir = self.tmpdir
-		args = '-f ' + ' '.join((workfile,start,end,increment))
+		
+		# -f workfile
+		# -s <number of users to start with>
+		# -e <number of users to end with>
+		# -i <number of users to increment>
+		workfile = os.path.join('data', workfile)
+		args = "-f %s -s %d -e %d -i %d" %(workfile,start,end,increment)
+		config = os.path.join(self.srcdir, 'reaim.config')
+		system('cp -f %s/reaim.config %s' % (self.bindir, config))
+		args += ' -c ./reaim.config'
+		open(config, 'a+').write("DISKDIR %s\n" % (tmpdir))
+		os.chdir(self.srcdir)
+		print os.getcwd()
+		cmd = self.ldlib + ' ./reaim ' + args + ' ' + extra_args
+
 		for i in range(1, iterations+1):
-			system(self.srcdir + '/reaim ' + args)
+			system(cmd)
 
 		# Do a profiling run if necessary
 		profilers = self.job.profilers
 		if profilers.present():
 			profilers.start(self)
-			system(self.srcdir + '/reaim ' + args)
+			system(cmd)
 			profilers.stop(self)
 			profilers.report(self)
