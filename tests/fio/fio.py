8ccf5f51525835ed89196c1b8d17221a88952dcd
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2006-09-27 06:13:30 +0000

    Make fio used the embedded libaio stuff
    git-svn-id: svn://test.kernel.org/autotest/trunk@230 592f7852-d20e-0410-864c-8624ca9c26a4

--- tests/fio/fio.py
+++ tests/fio/fio.py
@@ -8,14 +8,21 @@ class fio(test.test):
 	def setup(self, tarball = 'fio-1.6.tar.gz'):
 		tarball = unmap_url(self.bindir, tarball, self.tmpdir)
 		extract_tarball_to_dir(tarball, self.srcdir)
-		os.chdir(self.srcdir)
 
-		system('make')
+		self.job.setup_dep(['libaio'])
+		ldflags = '-L' + self.autodir + '/deps/libaio/lib'
+		cflags = '-I' + self.autodir + '/deps/libaio/include'
+		var_ldflags = 'LDFLAGS="' + ldflags + '"'
+		var_cflags  = 'CFLAGS="' + cflags + '"'
+
+		os.chdir(self.srcdir)
+		system('patch -p1 < ../fio.diff')
+		system('%s %s make' % (var_ldflags, var_cflags))
 
 	def execute(self, args = '', user = 'root'):
 		os.chdir(self.srcdir)
 		##vars = 'TMPDIR=\"%s\" RESULTDIR=\"%s\"' % (self.tmpdir, self.resultsdir)
-		vars = ''
+		vars = 'LD_LIBRARY_PATH="' + self.autodir + '/deps/libaio/lib"'
 		##args = '-m -o ' + self.resultsdir + '/fio-tio.log ' + self.srcdir + '/examples/tiobench-example';
 		args = '-m -o ' + self.resultsdir + '/fio-mixed.log ' + self.bindir + '/fio-mixed.job';
 		system(vars + ' ./fio ' + args)
