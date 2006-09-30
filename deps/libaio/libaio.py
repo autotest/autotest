382bcb36eda1136d6f60e3f84b68009c9722dce3
Martin J. Bligh <mbligh@google.com>
2006-09-30 20:17:05 +0000

    Clean up logic in setup of libaio

--- deps/libaio/libaio.py
+++ deps/libaio/libaio.py
@@ -3,22 +3,21 @@ import os
 from autotest_utils import *
 
 version = 1
-def do_actual_extract(): 
-	extract_tarball_to_dir(tarball, 'src')
 
-dir = os.getcwd()
+def setup(tarball, topdir): 
+	srcdir = os.path.join(topdir, 'src')
+	extract_tarball_to_dir(tarball, srcdir)
+	os.chdir(srcdir)
+	system ('make')
+	system ('make prefix=%s install' % topdir)
+
 
 # old source was
 # http://www.kernel.org/pub/linux/kernel/people/bcrl/aio/libaio-0.3.92.tar.bz2
 # now grabbing from debian
 # http://ftp.debian.org/debian/pool/main/liba/libaio/libaio_0.3.106.orig.tar.gz
-tarball = 'libaio_0.3.106.orig.tar.gz'
 
-tmpdir = 'tmp'
-tarball = unmap_url(dir, tarball, tmpdir)
-update_version(dir+'/src', version, do_actual_extract)
-
-os.chdir('src')
-system ('make')
-system ('make prefix=%s install' % (dir))
+pwd = os.getcwd()
+tarball = os.path.join(pwd, 'libaio_0.3.106.orig.tar.gz')
+update_version(pwd+'/src', version, setup, tarball, pwd)
 
