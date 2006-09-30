c2c75ecf009cd5afab2b033c3ad62a8150fcd65e
Mark Wong <markw@osdl.org>
2006-09-30 21:56:37 +0000

    This patch adds PostgreSQL 8.1.4 to deps.  I hope it's useful.  I'm planning on using it for our database systems workloads.
    Signed-off-by: Mark Wong <markw@osdl.org>

---
+++ deps/pgsql/pgsql.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import os
+from autotest_utils import *
+
+version = 1
+
+def setup(tarball, topdir): 
+	srcdir = os.path.join(topdir, 'src')
+	if not os.path.exists(tarball):
+		get_file('ftp://ftp.us.postgresql.org/pub/mirrors/postgresql/v8.1.4/postgresql-8.1.4.tar.bz2', tarball)
+	extract_tarball_to_dir(tarball, 'src')
+	os.chdir(srcdir)
+	system ('./configure --enable-debug --prefix=%s/pgsql' % topdir)
+	system('make -j %d' % count_cpus())
+	system('make install')
+	
+	os.chdir(topdir)
+	
+pwd = os.getcwd()
+tarball = os.path.join(pwd, 'postgresql-8.1.4.tar.bz2')
+update_version(pwd+'/src', version, setup, tarball, pwd)
+
+
