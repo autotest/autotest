fcfbbb1498b80a757725b4bd9cd47540c014469b
Martin J. Bligh <mbligh@google.com>
2006-10-01 19:57:02 +0000

    Make boottool use the local instance of the boottool command, and fix up various errors.

--- bin/boottool.py
+++ bin/boottool.py
@@ -1,22 +1,17 @@
-import shutil
-import re
-import os 
-import os.path
-import string
+import shutil, re, os, os.path, string
+from autotest_utils import *
 
 class boottool:
 	def __init__(self, boottool_exec=None):
 		if boottool_exec:
 			self.boottool_exec = boottool_exec
 		else:
-                        # TODO:  detect boottool from PATH
-			self.boottool_exec = '/usr/bin/boottool'
+			autodir = os.environ['AUTODIR']
+			self.boottool_exec = autodir + '/tools/boottool'
 
 
         def run_boottool(self, params):
-                # TODO:  What is ignorestatus?
-                # TODO:  Should system_output be used instead?
-                return system('%s %s' % (self.boottool_exec, params), ignorestatus=1)
+                return system_output('%s %s' % (self.boottool_exec, params))
 
 	def bootloader(self):
                 return self.run_boottool('--bootloader-probe')
@@ -30,15 +25,20 @@ class boottool:
 	def print_entry(self, index):
                 print self.run_boottool('--info=%s' % index)
 
+	def get_default(self):
+                self.run_boottool('--default')
+
 	def set_default(self, index):
-                self.run_boottool('--set-default= %s' % (index) )
+                self.run_boottool('--set-default=%s' % index)
 
         # 'kernel' can be an position number or a title
         def add_args(self, kernel, args):
-                self.run_boottool('--update-kernel=%s --args=%s' % (kernel, args) )
+                self.run_boottool('--update-kernel=%s --args=%s' % \
+							(kernel, args) )
 
         def remove_args(self, kernel, args):
-                self.run_boottool('--update-kernel=%s --remove-args=%s' % (kernel, args) )
+                self.run_boottool('--update-kernel=%s --remove-args=%s' % \
+							(kernel, args) )
 
         def add_kernel(self, path):
                 self.run_boottool('--add-kernel=%s' % path)
@@ -49,8 +49,8 @@ class boottool:
         def boot_once(self, title):
                 self.run_boottool('--boot-once --title=%s' % title)
 
-        def info(self):
-                self.run_boottool('--info' % )
+        def info(self, index):
+                self.run_boottool('--info %d' % index)
 
 
 # TODO:  backup()
