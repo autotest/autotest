c6c540297b0179710188cc19a9b4822264923ca0
Michal Piotrowski <michal.k.k.piotrowski@gmail.com>
2006-10-04 04:10:38 +0000

    Please consider an upgrade to the latest LTP version.
    Problems with LTP 20060412:
    - it doesn't build on Fedora Core 5
    - it doesn't build on Fedora Core 6-test3+
    
    getdents01.c: In function main:
    getdents01.c:97: warning: implicit declaration of function _syscall3
    getdents01.c:97: error: expected expression before int
    getdents01.c:139: warning: implicit declaration of function getdents
    
    From: Michal Piotrowski <michal.k.k.piotrowski@gmail.com>

--- tests/ltp/ltp.py
+++ tests/ltp/ltp.py
@@ -3,10 +3,10 @@ import test
 from autotest_utils import *
 
 class ltp(test.test):
-	version = 1
+	version = 2
 
-	# http://prdownloads.sourceforge.net/ltp/ltp-full-20060412.tgz
-	def setup(self, tarball = 'ltp-full-20060412.tgz'):
+	# http://prdownloads.sourceforge.net/ltp/ltp-full-20060918.tgz
+	def setup(self, tarball = 'ltp-full-20060918.tar.bz2'):
 		tarball = unmap_url(self.bindir, tarball, self.tmpdir)
 		extract_tarball_to_dir(tarball, self.srcdir)
 		os.chdir(self.srcdir)
