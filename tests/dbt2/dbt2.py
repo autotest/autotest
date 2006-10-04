889419e47775a6931109539335a8fdcc832449d5
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2006-10-04 04:40:03 +0000

    This patch updates the dbt-2 kit and modifies the test so that it runs against MySQL in addition to PostgreSQL.
    Signed-off-by: Mark Wong <markw@osdl.org>
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@279 592f7852-d20e-0410-864c-8624ca9c26a4

--- tests/dbt2/dbt2.py
+++ tests/dbt2/dbt2.py
@@ -5,13 +5,13 @@ from autotest_utils import *
 # currently hardcoded to use PostgreSQL but the kit also supports MySQL.
 
 class dbt2(test.test):
-	version = 1
+	version = 2
 
-	# http://osdn.dl.sourceforge.net/sourceforge/osdldbt/dbt2-0.38.tar.gz
-	def setup(self, tarball = 'dbt2-0.38.tar.gz'):
+	# http://osdn.dl.sourceforge.net/sourceforge/osdldbt/dbt2-0.39.tar.gz
+	def setup(self, tarball = 'dbt2-0.39.tar.bz2'):
 		tarball = unmap_url(self.bindir, tarball, self.tmpdir)
 		extract_tarball_to_dir(tarball, self.srcdir)
-		self.job.setup_dep(['pgsql'])
+		self.job.setup_dep(['pgsql', 'mysql'])
 
 		# Create symlink to autotest's results directory from dbt-2's
 		# preferred results directory to self.resultsdir
@@ -22,11 +22,22 @@ class dbt2(test.test):
 		logfile = self.resultsdir + '/dbt2.log'
 
 		os.chdir(self.srcdir)
+		system('make clean')
+		system('./configure --with-mysql=%s/deps/mysql/mysql' \
+				% self.autodir)
+		system('make')
+		os.chdir(self.srcdir + '/scripts')
+		system('mysql/build_db.sh -g -w 1')
+		args = args + ' -u root'
+		system(self.srcdir + '/scripts/run_workload.sh ' + args)
+
+		os.chdir(self.srcdir)
+		system('make clean')
 		system('./configure --with-postgresql=%s/deps/pgsql/pgsql' \
 				% self.autodir)
 		system('make')
 		os.chdir(self.srcdir + '/scripts')
-		system('pgsql/build_db.sh -g')
+		system('pgsql/build_db.sh -g -w 1')
 		system(self.srcdir + '/scripts/run_workload.sh ' + args)
 		#
 		# Clean up by dropping the database after the test.
