16dbc6acbb3449949a3b21bb0e5d16308d5e9c09
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2006-10-04 04:02:33 +0000

    This patch adds MySQL-5.0.24a to deps.  I'm planning on using it for our database systems workloads, specifically for dbt-2 (TPC-C fair-use implementation) next.
    Signed-off-by: Mark Wong <markw@osdl.org>
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@277 592f7852-d20e-0410-864c-8624ca9c26a4

---
+++ deps/mysql/mysql.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+import os
+from autotest_utils import *
+
+version = 1
+
+def setup(tarball, topdir): 
+	srcdir = os.path.join(topdir, 'src')
+	if not os.path.exists(tarball):
+		get_file('http://mirror.x10.com/mirror/mysql/Downloads/MySQL-5.0/mysql-5.0.24a.tar.gz', tarball)
+	extract_tarball_to_dir(tarball, 'src')
+	os.chdir(srcdir)
+	system ('./configure --prefix=%s/mysql --enable-thread-safe-client' \
+			% topdir)
+	system('make -j %d' % count_cpus())
+	system('make install')
+
+	#
+	# MySQL doesn't create this directory on it's own.  The server process
+	# needs it for writing temporary files to.
+	#
+	os.mkdir(topdir + '/mysql/var')
+	#
+	# Initialize the database.
+	#
+	system('%s/mysql/bin/mysql_install_db' % topdir)
+	
+	os.chdir(topdir)
+	
+pwd = os.getcwd()
+tarball = os.path.join(pwd, 'mysql-5.0.24a.tar.gz')
+update_version(pwd+'/src', version, setup, tarball, pwd)
+
+
