e41ca59fb9e612e366b76fb9ab8b656e18400992
Mark Wong <markw@osdl.org>
2006-10-06 03:51:57 +0000

    Fix source extraction of dbt2
    I don't have 'make install' really doing anything useful so I opted to try this; I copied the source to two separate directories: src.mysql and src.pgsql.  I originally tried to set extract_tarball_to_dir() to separate directories but it seems to not recognize that it's already extracted the source in the second call to job.runtest().  From there I build each src for mysql and pgsql respectively.
    
    From: Mark Wong <markw@osdl.org>

--- tests/dbt2/dbt2.py
+++ tests/dbt2/dbt2.py
@@ -13,35 +13,50 @@ class dbt2(test.test):
 		extract_tarball_to_dir(tarball, self.srcdir)
 		self.job.setup_dep(['pgsql', 'mysql'])
 
-		# Create symlink to autotest's results directory from dbt-2's
-		# preferred results directory to self.resultsdir
-		system('ln -s %s %s' % (self.resultsdir, \
-				self.srcdir + '/scripts/output'))
-
-	def execute(self, args = ''):
-		logfile = self.resultsdir + '/dbt2.log'
-
-		os.chdir(self.srcdir)
-		system('make clean')
+		#
+		# Extract one copy of the kit for MySQL.
+		#
+		system('cp -pR ' + self.srcdir + ' ' + self.srcdir + '.mysql')
+		os.chdir(self.srcdir + '.mysql')
 		system('./configure --with-mysql=%s/deps/mysql/mysql' \
 				% self.autodir)
 		system('make')
-		os.chdir(self.srcdir + '/scripts')
-		system('mysql/build_db.sh -g -w 1')
-		args = args + ' -u root'
-		system(self.srcdir + '/scripts/run_workload.sh ' + args)
 
-		os.chdir(self.srcdir)
-		system('make clean')
+		#
+		# Extract one copy of the kit for PostgreSQL.
+		#
+		system('cp -pR ' + self.srcdir + ' ' + self.srcdir + '.pgsql')
+		os.chdir(self.srcdir + '.pgsql')
 		system('./configure --with-postgresql=%s/deps/pgsql/pgsql' \
 				% self.autodir)
 		system('make')
-		os.chdir(self.srcdir + '/scripts')
-		system('pgsql/build_db.sh -g -w 1')
-		system(self.srcdir + '/scripts/run_workload.sh ' + args)
+
+		# Create symlinks to autotest's results directory from dbt-2's
+		# preferred results directory to self.resultsdir
+		system('ln -s %s %s' % (self.resultsdir, \
+				self.srcdir + '.mysql/scripts/output'))
+		system('ln -s %s %s' % (self.resultsdir, \
+				self.srcdir + '.pgsql/scripts/output'))
+
+	def execute(self, db_type, args = ''):
+		logfile = self.resultsdir + '/dbt2.log'
+
+		if (db_type == "mysql"):
+			self.execute_mysql(args)
+		elif (db_type == "pgsql"):
+			self.execute_pgsql(args)
+
+	def execute_mysql(self, args = ''):
+		args = args
+		system(self.srcdir + '.mysql/scripts/mysql/build_db.sh -g -w 1')
+		system(self.srcdir + '.mysql/scripts/run_workload.sh ' + args)
+
+	def execute_pgsql(self, args = ''):
+		system(self.srcdir + '.pgsql/scripts/pgsql/build_db.sh -g -w 1')
+		system(self.srcdir + '.pgsql/scripts/run_workload.sh ' + args)
 		#
 		# Clean up by dropping the database after the test.
 		#
-		system('pgsql/start_db.sh')
-		system('pgsql/drop_db.sh')
-		system('pgsql/stop_db.sh')
+		system(self.srcdir + '.pgsql/scripts/pgsql/start_db.sh')
+		system(self.srcdir + '.pgsql/scripts/pgsql/drop_db.sh')
+		system(self.srcdir + '.pgsql/scripts/pgsql/stop_db.sh')
