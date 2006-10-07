2366769e60de8048cd9174cdd0e03af2ac601a0c
Mark Wong <markw at osdl.org>
2006-10-07 05:05:02 +0000

    This patch adds pgpool-II-1.0.1 to deps.  pgpool does connection pooling, load balancing and more for PostgreSQL.  I'm planning on using it for our database systems workloads, specifically for dbt-2 (TPC-C fair-use implementation) next.
    Signed-off-by: Mark Wong <markw at osdl.org>

---
+++ deps/pgpool/pgpool.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import os
+from autotest_utils import *
+
+version = 1
+
+def setup(tarball, topdir): 
+	# FIXME - Waiting to be able to specify dependency.
+	#self.job.setup_dep(['pgsql'])
+	srcdir = os.path.join(topdir, 'src')
+	if not os.path.exists(tarball):
+		get_file('http://pgfoundry.org/frs/download.php/1083/pgpool-II-1.0.1.tar.gz', tarball)
+	extract_tarball_to_dir(tarball, 'src')
+	os.chdir(srcdir)
+	# FIXEME - Waiting to be able to use self.autodir instead of
+	# os.environ['AUTODIR']
+	system('./configure --prefix=%s/pgpool --with-pgsql=%s/deps/pgpool/pgpool' \
+			% (topdir, os.environ['AUTODIR']))
+	system('make -j %d' % count_cpus())
+	system('make install')
+
+	os.chdir(topdir)
+	
+pwd = os.getcwd()
+tarball = os.path.join(pwd, 'pgpool-II-1.0.1.tar.gz')
+update_version(pwd+'/src', version, setup, tarball, pwd)
