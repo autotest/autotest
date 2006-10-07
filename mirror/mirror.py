d5272620768ab33a53d8db5f774ded40e730a99c
Martin J. Bligh <mbligh@google.com>
2006-10-07 22:08:47 +0000

    Add mirror functions, intended to mirror and monitor kernel.org

---
+++ mirror/mirror.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python2.4
+import os, rsync, trigger
+
+excludes = ('2.6.0-test*/', '*.sign', '*.gz')
+
+if 'MIRROR_DIR' in os.environ:
+	target = os.environ['MIRROR_DIR']
+else: 
+	target = "/home/mirror"
+
+source = 'rsync://rsync.kernel.org/pub/linux/kernel'
+mirror = rsync.rsync(source, target, excludes)
+
+mirror.sync('v2.6/patch-2.6.*.bz2', 'kernel/v2.6')
+# for some reason 'linux-2.6.[0-9]*.tar.bz2' doesn't work
+mirror.sync('v2.6/linux-2.6.[0-9].tar.bz2', 'kernel/v2.6')
+mirror.sync('v2.6/linux-2.6.[0-9][0-9].tar.bz2', 'kernel/v2.6')
+mirror.sync('v2.6/snapshots/*.bz2', 'kernel/v2.6/snapshots')
+mirror.sync('people/akpm/patches/2.6/*', 'akpm')
+
+for t in trigger.scan('mirror.tmpfile'):
+	print t
