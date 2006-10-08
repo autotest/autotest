d6b4250f881c2f072e9fb7dcded390c4727066dc
Martin J. Bligh <mbligh@google.com>
2006-10-08 22:31:10 +0000

    Make better use of the db

--- tko/db.py
+++ tko/db.py
@@ -7,6 +7,22 @@ class db:
 
 
 	def select(self, cmd):
+		print 'select ' + cmd
 		self.cur.execute('select ' + cmd)
 		return self.cur.fetchall()
 
+
+	def insert_job(self, tag, job):
+		command = 'insert into jobs ' + \
+			'(job, version, status, reason, machine, kernel) ' +\
+			'values (%s, %s, %s, %s, %s, %s) '
+		values = (tag, job.kernel, job.status_num, job.reason, \
+			 job.machine, job.kernel)
+		self.cur.execute(command, values)
+		self.con.commit();
+
+
+	def find_job(self, tag):
+		command = 'select * from jobs where job = %s'
+		self.cur.execute(command, tag)
+		return self.cur.fetchall()
