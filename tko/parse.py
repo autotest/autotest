8a79b9ade835768fcc0932a979b933cb6cf1371a
Martin J. Bligh <mbligh@google.com>
2006-10-08 21:34:16 +0000

    Add very basic database stuff

--- tko/parse.py
+++ tko/parse.py
@@ -6,6 +6,23 @@ build_stock = re.compile('build generic stock (2\.\S+)')
 build_url   = re.compile('build generic url \S*/linux-(2\.\d\.\d+(\.\d+)?(-rc\d+)?).tar')	
 valid_kernel= re.compile('2\.\d\.\d+(\.\d+)?(-rc\d+)?(-(git|bk))\d+')
 
+statuses = ['NOSTATUS', 'ERROR', 'ABORT', 'FAIL', 'WARN', 'GOOD']
+status_num = {}
+for x in range(0, len(statuses)):
+	status_num[statuses[x]] = x
+
+munge_reasons = (
+	(r', job abort.*', ''),
+	(r'autobench reported job failed, aborting', 'harness failed job'),
+	(r'machine did not return from reboot.*', 'reboot failed'),
+	(r'machine reboot failed', 'reboot failed'),
+	(r'machine reached login', 'reached login'),
+	(r'lost contact with run', 'lost contact'),
+	(r'\(machine panic.*', '(panic)'),
+	(r'.*build generic url.*', 'kernel build failed'),
+	(r'^.fs .*', 'fs operation failed'),
+		)
+
 
 def shorten_patch(long):
 	short = os.path.basename(long)
@@ -16,15 +33,22 @@ def shorten_patch(long):
 	return short
 
 
+
+
 class parse:
-	def __init__(self, topdir):
+	def __init__(self, topdir, type):
 		self.topdir = topdir
-		self.control = "%s/autobench.dat" % topdir
+		self.type = type
+		self.control = os.path.join(topdir, "autobench.dat")
+		self.set_status('NOSTATUS')
+		self.reason = ''
 		self.variables = {}
 		self.kernel = None
 
-		if os.path.exists(self.control):
-			self.grope_datfile()
+		if not os.path.exists(self.control):
+			return
+		self.grope_datfile()
+		self.grope_status()
 
 
 	def grope_datfile(self):
@@ -79,4 +103,33 @@ class parse:
 		match = valid_kernel.match(self.patches_short[0])
 		if match:
 			self.kernel = match.group()
-		
+
+
+	def grope_status(self):
+		status_file = os.path.join(self.topdir, "status")
+		try:
+			line = open(status_file, 'r').readline().rstrip()
+			(status, reason) = line.split(' ', 1)
+		except:
+			return
+
+		for (a, b) in munge_reasons:
+			reason = re.sub(a, b, reason)
+
+		if reason.count('run completed unexpectedly'):
+			try:
+				if os.system('head $resultsdir{$job}/debug/test.log.0 | grep "autobench already running, exiting" > /dev/null'):
+					status = 'FAIL'
+					reason = 'autobench already running'
+			except:
+				pass
+
+		self.set_status(status)
+		self.reason = reason
+
+
+	def set_status(self, status):
+		if status not in status_num:
+			return
+		self.status = status
+		self.status_num = status_num[status]
