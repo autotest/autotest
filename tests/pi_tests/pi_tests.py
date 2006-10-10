ac37283f4f937a8c9fc516429129fe8eb3b228a6
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2006-10-10 16:06:31 +0000

    Add pi_tests
     The basic premise here is to set up a deadlock scenario and confirm that PI
     mutexes resolve the situation. Three worker threads will be created from the
     main thread: low, medium and high priority threads that use SCHED_FIFO as
     their scheduling policy. The low priority thread claims a mutex and then
     starts "working". The medium priority thread starts and preempts the low
     priority thread. Then the high priority thread runs and attempts to claim
     the mutex owned by the low priority thread. Without priority inheritance,
     this will deadlock the program. With priority inheritance, the low priority
     thread receives a priority boost, finishes it's "work" and releases the mutex,
     which allows the high priority thread to run and finish and then the medium
     priority thread finishes.
    
     That's the theory, anyway..."
    
    From: Michal Piotrowski <michal.k.k.piotrowski@gmail.com>
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@328 592f7852-d20e-0410-864c-8624ca9c26a4

---
+++ tests/pi_tests/pi_tests.py
@@ -0,0 +1,18 @@
+import test
+from autotest_utils import *
+
+class pi_tests(test.test):
+	version = 1
+
+	# http://www.stardust.webpages.pl/files/patches/autotest/pi_tests.tar.bz2
+
+	def setup(self, tarball = 'pi_tests.tar.bz2'):
+		tarball = unmap_url(self.bindir, tarball, self.tmpdir)
+		extract_tarball_to_dir(tarball, self.srcdir)
+		os.chdir(self.srcdir)
+
+		system('make')
+
+	def execute(self, args = '1 300'):
+		os.chdir(self.srcdir)
+		system('./start.sh ' + args)
