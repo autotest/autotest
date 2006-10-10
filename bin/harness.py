9e6252555188605e77ebbd63e0013f2d729f5d13
apw <apw@592f7852-d20e-0410-864c-8624ca9c26a4>
2006-10-10 08:34:21 +0000

    harness: add the bones of a server harness and noop harness
    Add a basic server harness object.  This object provides general
    server specific hooks allowing such a harness to detect transitions
    and handle them as required.  The noop harness object is loaded
    by default and overridden as below:
    
    	job.harness_select('NAME')
    
    Signed-off-by: Andy Whitcroft <apw@shadowen.org>
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@326 592f7852-d20e-0410-864c-8624ca9c26a4

---
+++ bin/harness.py
@@ -0,0 +1,63 @@
+"""The harness interface
+
+The interface between the client and the server when hosted.
+"""
+
+__author__ = """Copyright Andy Whitcroft 2006"""
+
+from autotest_utils import *
+import os, sys
+
+class harness:
+	"""The NULL server harness
+
+	Properties:
+		job
+			The job object for this job
+	"""
+
+	def __init__(self, job):
+		"""
+			job
+				The job object for this job
+		"""
+		self.job = job
+
+
+	def run_start(self):
+		"""A run within this job is starting"""
+		pass
+
+
+	def run_pause(self):
+		"""A run within this job is completing (expect continue)"""
+		pass
+
+
+	def run_reboot(self):
+		"""A run within this job is performing a reboot
+		   (expect continue following reboot)
+		"""
+		pass
+
+
+	def run_complete(self, status):
+		"""A run within this job is completing (all done)"""
+		pass
+
+
+	def test_status(self, status):
+		"""A test within this job is completing"""
+		pass
+
+
+def select(which, job):
+	if which:
+		exec "import harness_%s" % (which)
+		exec "myharness = harness_%s.harness_%s(job)" % (which, which)
+	else:
+		myharness = harness(job)
+
+	myharness.run_start()
+
+	return myharness
