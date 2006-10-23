3a9243cd95b215344f2bd022c1576520b60914e5
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2006-10-23 21:59:42 +0000

    Add fsfuzzer test.
    Signed-off-by: mbligh@google.com
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@360 592f7852-d20e-0410-864c-8624ca9c26a4

---
+++ tests/fsfuzzer/fsfuzzer.py
@@ -0,0 +1,26 @@
+import test
+from autotest_utils import *
+
+class fsfuzzer(test.test):
+	version = 1
+
+	# http://people.redhat.com/sgrubb/files/fsfuzzer-0.6.tar.gz
+	def setup(self, tarball = 'fsfuzzer-0.6.tar.gz'):
+		tarball = unmap_url(self.bindir, tarball, self.tmpdir)
+		extract_tarball_to_dir(tarball, self.srcdir)
+		os.chdir(self.srcdir)
+
+		system('make')
+		
+	def execute(self, iterations = 1, fstype = 'iso9660'):
+		for i in range(1, iterations+1):
+			args = fstype + ' 1'
+			system(self.srcdir + '/run_test ' + args)
+
+		# Do a profiling run if necessary
+		profilers = self.job.profilers
+		if profilers.present():
+			profilers.start(self)
+			system(self.srcdir + '/run_test ' + args)
+			profilers.stop(self)
+			profilers.report(self)
