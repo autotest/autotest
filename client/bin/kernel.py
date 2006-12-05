409c2cd0720b03b7660f814935e5573a13b5fae8
Andy Whitcroft <apw@shadowen.org>
2006-12-05 12:16:39 +0000

    kernel: make the default config a default parameter
    Currently you configure a kernel using the kernel config method.
    This allows you to supply a base config file.  If this file is ''
    then a defconfig is generated and used.  This leads to the following
    construction:
    
    	testkernel = job.kernel('2.6.18')
    	testkernel.config('')
    
    It seems senseless to mandate the parameter and yet take '' as
    meaning default.  This patch makes this parameter default to ''.
    Allowing the following:
    
    	testkernel = job.kernel('2.6.18')
    	testkernel.config()
    
    Signed-off-by: Andy Whitcroft <apw@shadowen.org>

--- client/bin/kernel.py
+++ client/bin/kernel.py
@@ -109,7 +109,7 @@ class kernel:
 		# self.job.stdout.restore()
 
 
-	def config(self, config_file, config_list = None):
+	def config(self, config_file = '', config_list = None):
 		self.job.stdout.redirect(os.path.join(self.log_dir, 'stdout'))
 		config = kernel_config.kernel_config(self.job, self.build_dir, self.config_dir, config_file, config_list)
 		self.job.stdout.restore()
