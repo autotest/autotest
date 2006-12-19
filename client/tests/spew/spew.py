50003fa935c3313cac264356f9d7e9b4052218fe
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2006-12-19 20:30:15 +0000

    Add spew test
    Signed-off-by: Martin J. Bligh <mbligh@google.com>
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@436 592f7852-d20e-0410-864c-8624ca9c26a4

---
+++ client/tests/spew/spew.py
@@ -0,0 +1,39 @@
+import test
+from autotest_utils import *
+
+class spew(test.test):
+	version = 1
+
+	# ftp://ftp.berlios.de/pub/spew/1.0.5/spew-1.0.5.tgz
+	def setup(self, tarball = 'spew-1.0.5.tgz'):
+		self.tarball = unmap_url(self.bindir, tarball, self.tmpdir)
+		extract_tarball_to_dir(self.tarball, self.srcdir)
+
+		os.chdir(self.srcdir)
+		system('./configure')
+		system('make')
+
+
+	def execute(self, testdir = None, iterations = 1, filesize='200G'):
+		cmd = os.path.join(self.srcdir + 'src/spew')
+		if not testdir:
+			testdir = self.testdir
+		tmpfile = os.path.join(testdir, 'spew-test.%d' % os.getpid())
+		results = os.path.join(self.resultsdir, tmpfile)
+		args = '-q -i %d -p random -b 2k -B 2M %s %s' % \
+						(iterations, filesize, tmpfile)
+
+		# Do a profiling run if necessary
+		profilers = self.job.profilers
+		if profilers.present():
+			profilers.start(self)
+
+		self.job.stdout.redirect(results)
+		try:
+			system(cmd + ' ' + args)
+		finally:
+			self.job.stdout.restore()
+
+		if profilers.present():
+			profilers.stop(self)
+			profilers.report(self)
