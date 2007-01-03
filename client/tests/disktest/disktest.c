699d7a709c3329703ab75cc6a434b22e07004c5b
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2007-01-03 00:10:38 +0000

    update disktest
    git-svn-id: svn://test.kernel.org/autotest/trunk@456 592f7852-d20e-0410-864c-8624ca9c26a4

--- client/tests/disktest/disktest.c
+++ client/tests/disktest/disktest.c
@@ -2,7 +2,7 @@
 // New Year's Eve, 2006 
 // Released under the GPL v2.
 //
-// Compile with -D_FILE_OFFSET_BITS=64
+// Compile with -D_FILE_OFFSET_BITS=64 -D _GNU_SOURCE
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,12 +15,13 @@
 
 #define SECTOR_SIZE 512
 #define UINT_PER_SECTOR  (SECTOR_SIZE / sizeof(unsigned int))
+#define VERIFY_BLOCKS 16
 
 char *filename = "testfile";
 unsigned int megabytes = 1;
 unsigned int blocksize = 4096;
 unsigned int seconds = 15;
-unsigned int linear_tasks = 4;
+unsigned int linear_tasks = 1;
 unsigned int random_tasks = 4;
 unsigned int blocks;
 unsigned int sectors_per_block;
@@ -29,6 +30,7 @@ int fd;
 void die(char *error)
 {
 	fprintf(stderr, error);
+	fprintf(stderr, "\n");
 	exit(1);
 }
 
@@ -36,12 +38,14 @@ void die(char *error)
  * Fill a block with it's own sector number
  * buf must be at least blocksize
  */
-void write_block(unsigned int block, unsigned int *buf)
+void write_blocks(unsigned int block, unsigned int *buf, unsigned int blocks)
 {
 	unsigned int i, sec_offset, sector;
 	off_t offset;
+	unsigned int writesize = blocksize * blocks;
+	unsigned int sectors = writesize / SECTOR_SIZE;
 
-	for (sec_offset = 0; sec_offset < sectors_per_block; sec_offset++) {
+	for (sec_offset = 0; sec_offset < sectors; sec_offset++) {
 		sector = (block * sectors_per_block) + sec_offset;
 
 		for (i = 0; i < SECTOR_SIZE / sizeof(unsigned int); i++)
@@ -50,7 +54,7 @@ void write_block(unsigned int block, unsigned int *buf)
 
 	offset = block; offset *= blocksize;   // careful of overflow
 	lseek(fd, offset, SEEK_SET);
-	if (write(fd, buf, blocksize) != blocksize)
+	if (write(fd, buf, writesize) != writesize)
 		die("write failed");
 }
 
@@ -60,17 +64,19 @@ void write_block(unsigned int block, unsigned int *buf)
  * 
  * We only check the first number - the rest is pretty pointless
  */
-void verify_block(unsigned int block, unsigned int *buf)
+void verify_blocks(unsigned int block, unsigned int *buf, unsigned int blocks)
 {
 	unsigned int sec_offset, sector;
 	off_t offset;
+	unsigned int readsize = blocksize * blocks;
+	unsigned int sectors = readsize / SECTOR_SIZE;
 
 	offset = block; offset *= blocksize;   // careful of overflow
 	lseek(fd, offset, SEEK_SET);
-	if (read(fd, buf, blocksize) != blocksize)
+	if (read(fd, buf, readsize) != readsize)
 		die("read failed");
 
-	for (sec_offset = 0; sec_offset < sectors_per_block; sec_offset++) {
+	for (sec_offset = 0; sec_offset < sectors; sec_offset++) {
 		sector = (block * sectors_per_block) + sec_offset;
 
 		if (buf[sec_offset * UINT_PER_SECTOR] != sector) {
@@ -78,6 +84,69 @@ void verify_block(unsigned int block, unsigned int *buf)
 					buf[sec_offset * UINT_PER_SECTOR]);
 		}
 	}
+}
+
+void write_file(int fd, unsigned int end_time, int random_access)
+{
+	int pid = fork();
+	unsigned int block;
+	void *buffer;
+
+	if (pid < 0)
+		die ("error forking child");
+	if (pid)			// parent
+		return;
+
+	buffer = malloc(blocksize);
+
+	if (random_access) {
+		srandom(time(NULL) - getpid());
+		while(time(NULL) < end_time) {
+			block = (unsigned int) (random() % blocks);
+			write_blocks(block, buffer, 1);
+		}
+	} else {
+		while(time(NULL) < end_time)
+			for (block = 0; block < blocks; block++)
+				write_blocks(block, buffer, 1);
+	}
+	free(buffer);
+	exit(0);
+}
+
+void verify_file(char *filename, unsigned int end_time, int random_access, 
+								int direct)
+{
+	int fd;
+	unsigned int block;
+	int pid = fork();
+	void *buffer;
+
+	if (pid < 0)
+		die ("error forking child");
+	if (pid)			// parent
+		return;
+
+	if (direct)
+		fd = open(filename, O_RDONLY | O_TRUNC | O_CREAT | O_DIRECT);
+	else
+		fd = open(filename, O_RDONLY | O_TRUNC | O_CREAT | O_DIRECT);
+		
+	buffer = malloc(blocksize * VERIFY_BLOCKS);
+
+	if (random_access) {
+		srandom(time(NULL) - getpid());
+		while(time(NULL) < end_time) {
+			block = (unsigned int) (random() % blocks);
+			verify_blocks(block, buffer, 1);
+		}
+	} else {
+		while(time(NULL) < end_time)
+			for (block = 0; block < blocks; block++)
+				verify_blocks(block, buffer, VERIFY_BLOCKS);
+	}
+	free(buffer);
+	exit(0);
 }
 
 void usage(void)
@@ -95,9 +164,9 @@ void usage(void)
 int main(int argc, char *argv[])
 {
 	unsigned int block;
-	time_t end_time;
-	int tasks, pid, opt;
-	void *buffer;
+	time_t start_time, end_time;
+	int tasks, opt;
+	void *init_buffer;
 
 	/* Parse all input options */
 	while ((opt = getopt(argc, argv, "f:s:m:b:l:r:")) != -1) {
@@ -130,58 +199,40 @@ int main(int argc, char *argv[])
 
 	/* blocksize must be < 1MB, and a divisor. Tough */
 	blocks = megabytes * (1024 * 1024 / blocksize);
-	buffer = malloc(blocksize);
+	sectors_per_block = blocksize / SECTOR_SIZE;
 
 	/* Initialise file */
 	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0666);
 	if (fd < 0)
 		die("open failed");
 
+	start_time = time(NULL);
+
 	/* Initialise all file data to correct blocks */
+	init_buffer = malloc(blocksize * VERIFY_BLOCKS);
 	for (block = 0; block < blocks; block++)
-		write_block(block, buffer);
+		write_blocks(block, init_buffer, VERIFY_BLOCKS);
+	if(fsync(fd) != 0)
+		die("fsync failed");
+	free(init_buffer);
+	
+	printf("Wrote %d MB to %s (%d seconds)\n", megabytes, filename, (int) (time(NULL) - start_time));
 
 	end_time = time(NULL) + seconds;
 
 	/* Fork off all linear access pattern tasks */
-	for (tasks = 0; tasks < linear_tasks; tasks++) {
-		pid = fork();
-		if (!pid) {                         // child
-			unsigned int block;
-			void *buf = malloc(blocksize);
-
-			while(time(NULL) < end_time)
-				for (block = 0; block < blocks; block++)
-					write_block(block, buf);
-
-			free(buf);
-			exit(0);
-		}
-	}
+	for (tasks = 0; tasks < linear_tasks; tasks++)
+		write_file(fd, end_time, 0);
 
 	/* Fork off all random access pattern tasks */
-	for (tasks = 0; tasks < random_tasks; tasks++) {
-		pid = fork();
-		if (!pid) {                         // child
-			void *buf = malloc(blocksize);
-			unsigned int block;
+	for (tasks = 0; tasks < random_tasks; tasks++)
+		write_file(fd, end_time, 1);
 
-			srandom(time(NULL) - getpid());
-			while(time(NULL) < end_time) {
-				block = (unsigned int) (random() % blocks);
-				write_block(block, buf);
-			}
+	/* Verify in all four possible ways */
+	verify_file(filename, end_time, 0, 0);
+	verify_file(filename, end_time, 0, 1);
+	verify_file(filename, end_time, 1, 0);
+	verify_file(filename, end_time, 1, 1);
 
-			free(buf);
-			exit(0);
-		}
-	}
-
-	/* The parent task's job is to verify the data */
-	while(time(NULL) < end_time)
-		for (block = 0; block < blocks; block++)
-			verify_block(block, buffer);
-
-	free(buffer);
 	return 0;
 }
