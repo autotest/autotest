5bb8b6866e13aa875e1716741428a26f70c32ba4
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2007-01-09 22:24:38 +0000

    Updates to disktest and wrapper
    git-svn-id: svn://test.kernel.org/autotest/trunk@464 592f7852-d20e-0410-864c-8624ca9c26a4

--- client/tests/disktest/disktest.py
+++ client/tests/disktest/disktest.py
@@ -1,5 +1,7 @@
 import test
 from autotest_utils import *
+import os, sys
+from subprocess import *
 
 class disktest(test.test):
 	version = 1
@@ -9,9 +11,40 @@ class disktest(test.test):
 		os.chdir(self.bindir)
 		system('cp disktest.c src/')
 		os.chdir(self.srcdir)
-		cflags = '-D_FILE_OFFSET_BITS=64 -D _GNU_SOURCE -Wall'
+		cflags = '-D_FILE_OFFSET_BITS=64 -D _GNU_SOURCE -static -Wall'
 		system('cc disktest.c ' + cflags + ' -o disktest')
 
 
-	def execute(self, iterations = 1):
-		system(self.srcdir + '/disktest')
+	def test_one_disk_chunk(self, disk, chunk):
+		print "testing %d MB files on %s in %d MB memory" % \
+					(self.chunk_mb, disk, self.memory_mb)
+		cmd = "~/disktest -m %d -f %s/testfile.%d -i -S" % \
+					(self.chunk_mb, disk, chunk)
+		p = Popen(cmd, shell=True)
+		return(p.pid)
+
+
+	def execute(self, disks, gigabytes = 100, chunk_mb = 1024):
+		os.chdir(self.srcdir + '/disktest')
+
+		self.chunk_mb = chunk_mb
+		self.memory_mb = memtotal()/1024
+		if self.memory_mb > chunk_mb/2:
+			raise "Too much RAM (%dMB) for this test to work" % \
+								self.memory_mb
+
+		chunks = (1024 * gigabytes) / chunk_mb
+
+		for i in range(chunks):
+			pids = []
+			for disk in disks:
+				pid = test_one_disk_chunk(chunk_mb, disk, chunk)
+				pids.append(pid)
+			errors = []
+			for pid in pids:
+				(junk, retval) = os.waitpid(pid, 0)
+				if (retval != 0):
+					errors.append(retval)
+			if errors:
+				raise "Errors from children: %s" % errors
+		
