e489d7620c7b5c07262a0620315fb6d5cd1af0c3
Martin J. Bligh <mbligh@google.com>
2007-01-28 20:08:45 +0000

    Fix up sysinfo commands that take arguments, remove lshw

--- client/bin/sysinfo.py
+++ client/bin/sysinfo.py
@@ -2,22 +2,32 @@
 from check_version import check_python_version
 check_python_version()
 
-import os,os.path,shutil
+import os,os.path,shutil,re
 from autotest_utils import *
 
 files = ['/proc/pci', '/proc/meminfo', '/proc/slabinfo', '/proc/version', 
 	'/proc/cpuinfo', '/proc/cmdline']
-commands = ['lshw', 'uname -a', 'lspci -vvn']
+# commands = ['lshw']        # this causes problems triggering CDROM drives
+commands = ['uname -a', 'lspci -vvn']
 path = ['/usr/bin', '/bin']
 
+
 def run_command(command, output):
+	parts = command.split(None, 1)
+	cmd = parts[0]
+	if len(parts) > 1:
+		args = parts[1]
+	else:
+		args = ''
 	for dir in path:
-		pathname = dir + '/' + command
+		pathname = dir + '/' + cmd
 		if (os.path.exists(pathname)):
-			system(pathname + ' > ' + output + ' 2> /dev/null')
+			system("%s %s > %s 2> /dev/null" % (pathname, args, output))
+
 
 for command in commands:
-	run_command(command, os.path.basename(command))
+	run_command(command, re.sub(r'\s', '_', command))
+
 
 for file in files:
 	if (os.path.exists(file)):
