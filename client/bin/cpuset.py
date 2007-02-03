c302297a5ff0288a76815944528b5e42cf8c6433
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2007-02-03 00:26:35 +0000

    broken ;-)
    git-svn-id: svn://test.kernel.org/autotest/trunk@474 592f7852-d20e-0410-864c-8624ca9c26a4

--- client/bin/cpuset.py
+++ client/bin/cpuset.py
@@ -1,28 +1,46 @@
-import os, sys
+import os, sys, re
+from autotest_utils import *
 
 class cpuset:
-	def __init__(self, job_name, memory_size, numa_fake, job_size, job_pid, cpu_number):
+	def available_cpus():
+		available = set(range(
+
+	def __init__(self, job_name, job_size, job_pid, cpus):
+		# Create a cpuset container and move job_pid into it
+		# Allocate the list "cpus" of cpus to that container
+
+		# job name = arbitrary string tag
+		# job size = reqested memory for job in bytes
+		# job pid = pid of job we're putting into the container
+
 		if not os.path.exists('/dev/cpuset'):
 			os.mkdir('/dev/cpuset')
 		# if not grep('/dev/cpuset', '/etc/mtab')
 			system('mount -t cpuset none /dev/cpuset')
 
-	self.cpudir = "/dev/cpuset/%s" % job_name
-	os.mkdir(self.cpudir)
-	write_one_line("%d" % cpu_number, '/dev/cpuset/%s/cpus' % job_name)
-  	node_size = memory_size / numa_fake
-	num_nodes = int(math.ceil(job_size / node_size))
-	if cpu_number == 0:
-		m_start = 3
-		m_end = m_start + num_nodes
-	else:
-		m_end = numa_fake - 1
-		m_start = m_end - num_nodes
-	alloc_size = (m_end - m_start + 1) * node_size
-	mems = os.path.join(self.cpudir, 'mems')
-	write_one_line("%d-%d" % (m_start, m_end), self.cpudir + '/mems')
-	write_one_line("%d" % job_pid, self.cpudir + 'tasks')
-	print "Cpuset: pid %d, cpu %d, memory %s --> nodes %d-%d --> %s" % (job_pid, cpu_number, int2KMG(job_size), m_start, m_end, int2KMG(alloc_size))
+		cmdline = read_one_line('/proc/cmdline')
+		fake_numa_nodes = re.search('numa=fake=(\d+)', cmdline).group(1)
+
+		# Bind the specificed cpus to the cpuset
+		self.cpudir = "/dev/cpuset/%s" % job_name
+		os.mkdir(self.cpudir)
+		cpu_spec = ','.join(['%d' % x for x in cpus])
+		write_one_line(cpu_spec, '/dev/cpuset/%s/cpus' % job_name)
+
+	  	node_size = memtotal() * 1024 / fake_numa_nodes
+		num_nodes = int(math.ceil(job_size / node_size))
+
+		if cpu_number == 0: # First cpuset
+			m_start = 3
+			m_end = m_start + num_nodes
+		else:
+			m_end = num_fake_numa_nodes - 1
+			m_start = m_end - num_nodes
+		alloc_size = (m_end - m_start + 1) * node_size
+		mems = os.path.join(self.cpudir, 'mems')
+		write_one_line("%d-%d" % (m_start, m_end), self.cpudir + '/mems')
+		write_one_line("%d" % job_pid, self.cpudir + 'tasks')
+		print "Cpuset: pid %d, cpu %d, memory %s --> nodes %d-%d --> %s" % (job_pid, cpu_number, int2KMG(job_size), m_start, m_end, int2KMG(alloc_size))
 
 
 	def release(self):
