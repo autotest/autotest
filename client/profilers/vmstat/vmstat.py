33dac245f42609660129c44a65ca73e492c86e53
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2007-02-05 23:50:55 +0000

    add vmstat profiler
    Signed-off-by: Martin J. Bligh <mbligh@google.com>
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@478 592f7852-d20e-0410-864c-8624ca9c26a4

---
+++ client/profilers/vmstat/vmstat.py
@@ -0,0 +1,22 @@
+# sets up a subprocess to cat a file on a specified interval
+# really, really ought to autoswitch on a list of files or individual file
+import profiler,time,os,subprocess
+
+class vmstat(profiler.profiler):
+	version = 1
+
+	def start(self, test):
+		cmd = "/usr/bin/vmstat %d" % 1
+		logfile = open(os.path.join(test.profdir, "vmstat"), 'w')
+		p = subprocess.Popen(cmd, shell=True, stdout=logfile, \
+						stderr=subprocess.STDOUT)
+		self.pid = p.pid
+
+
+	def stop(self, test):
+		os.kill(self.pid, 15)
+
+
+	def report(self, test):
+		return None
+
