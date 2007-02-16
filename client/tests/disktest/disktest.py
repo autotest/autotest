6d36c93217690c43df20b69f32ad964f5fe488a1
Martin J. Bligh <mbligh@google.com>
2007-02-16 00:27:26 +0000

    fix disktest
    Signed-off-by: Martin J. Bligh <mbligh@google.com>

--- client/tests/disktest/disktest.py
+++ client/tests/disktest/disktest.py
@@ -18,18 +18,18 @@ class disktest(test.test):
 	def test_one_disk_chunk(self, disk, chunk):
 		print "testing %d MB files on %s in %d MB memory" % \
 					(self.chunk_mb, disk, self.memory_mb)
-		cmd = "~/disktest -m %d -f %s/testfile.%d -i -S" % \
-					(self.chunk_mb, disk, chunk)
+		cmd = "%s/disktest -m %d -f %s/testfile.%d -i -S" % \
+				(self.srcdir, self.chunk_mb, disk, chunk)
 		p = Popen(cmd, shell=True)
 		return(p.pid)
 
 
-	def execute(self, disks, gigabytes = 100, chunk_mb = 1024):
-		os.chdir(self.srcdir + '/disktest')
+	def execute(self, disks, gigabytes = 100, chunk_mb = memtotal()/1024):
+		os.chdir(self.srcdir)
 
 		self.chunk_mb = chunk_mb
 		self.memory_mb = memtotal()/1024
-		if self.memory_mb > chunk_mb/2:
+		if self.memory_mb > chunk_mb:
 			raise "Too much RAM (%dMB) for this test to work" % \
 								self.memory_mb
 
@@ -38,7 +38,7 @@ class disktest(test.test):
 		for i in range(chunks):
 			pids = []
 			for disk in disks:
-				pid = test_one_disk_chunk(chunk_mb, disk, chunk)
+				pid = self.test_one_disk_chunk(disk, i)
 				pids.append(pid)
 			errors = []
 			for pid in pids:
