a4bfd5e08679d90fae380a683830014a71559e4a
Andy Whitcroft <apw@shadowen.org>
2007-03-06 19:23:23 +0000

    client: move to unbuffered io for stdio
    We want the output and error from the client to be as up to date
    as possible as we are potentially triggering destructive testing.
    To this end we want the output and error to be unbuffered.  Request
    this on the she-bang line and when we cross exec versions of python.
    
    Signed-off-by: Andy Whitcroft <apw@shadowen.org>

--- client/bin/check_version.py
+++ client/bin/check_version.py
@@ -7,6 +7,7 @@ def check_python_version():
 	if version < [2, 4]:
                 for new in ('/usr/bin/python2.4', '/usr/local/bin/python2.4'):
                         if os.path.exists(new):
+                                sys.argv.insert(0, '-u')
                                 sys.argv.insert(0, new)
                                 os.execv(sys.argv[0], sys.argv)
 		raise "Python 2.4 or newer is needed"
