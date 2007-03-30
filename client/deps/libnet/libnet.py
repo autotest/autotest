40abe740fcce1c789d150b0755b32c1e8237a2ed
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2007-03-30 19:31:00 +0000

    This patch adds libnet to deps/ dir.
    From: michal.k.k.piotrowski@gmail.com
    
    I noticed that isic doesn't work out of box on Fedora 6
    (on other systems without libnet-devel package too).
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@516 592f7852-d20e-0410-864c-8624ca9c26a4

---
+++ client/deps/libnet/libnet.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+from check_version import check_python_version
+check_python_version()
+
+import os
+from autotest_utils import *
+
+version = 1
+
+def setup(tarball, topdir): 
+	srcdir = os.path.join(topdir, 'src')
+	if not os.path.exists(tarball):
+		get_file('http://www.packetfactory.net/libnet/dist/libnet.tar.gz', tarball)
+	extract_tarball_to_dir(tarball, 'src')
+	os.chdir(srcdir)
+	system ('./configure --prefix=%s/libnet' % topdir)
+	system('make')
+	system('make install')
+
+	os.chdir(topdir)
+	
+pwd = os.getcwd()
+tarball = os.path.join(pwd, 'libnet.tar.gz')
+update_version(pwd+'/src', False, version, setup, tarball, pwd)
