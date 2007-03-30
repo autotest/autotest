e528789bbfc95b3a61c830b94e72b4f5c8935330
Michal Piotrowski <michal.k.k.piotrowski@gmail.com>
2007-03-30 19:28:08 +0000

    Fio 1.6 doesn't work on new distributions due to kernel headers changes.
    From: michal.k.k.piotrowski@gmail.com
    
    Please consider an upgrade to 1.15 version.

--- client/tests/fio/fio.py
+++ client/tests/fio/fio.py
@@ -2,10 +2,10 @@ import test
 from autotest_utils import *
 
 class fio(test.test):
-	version = 1
+	version = 2
 
-	# http://brick.kernel.dk/snaps/fio-1.6.tar.gz
-	def setup(self, tarball = 'fio-1.6.tar.gz'):
+	# http://brick.kernel.dk/snaps/fio-1.15.tar.bz2
+	def setup(self, tarball = 'fio-1.15.tar.bz2'):
 		tarball = unmap_url(self.bindir, tarball, self.tmpdir)
 		extract_tarball_to_dir(tarball, self.srcdir)
 
@@ -16,7 +16,7 @@ class fio(test.test):
 		var_cflags  = 'CFLAGS="' + cflags + '"'
 
 		os.chdir(self.srcdir)
-		system('patch -p1 < ../fio.diff')
+		system('patch -p1 < ../Makefile.patch')
 		system('%s %s make' % (var_ldflags, var_cflags))
 
 	def execute(self, args = '', user = 'root'):
@@ -24,7 +24,7 @@ class fio(test.test):
 		##vars = 'TMPDIR=\"%s\" RESULTDIR=\"%s\"' % (self.tmpdir, self.resultsdir)
 		vars = 'LD_LIBRARY_PATH="' + self.autodir + '/deps/libaio/lib"'
 		##args = '-m -o ' + self.resultsdir + '/fio-tio.log ' + self.srcdir + '/examples/tiobench-example';
-		args = '-m -o ' + self.resultsdir + '/fio-mixed.log ' + self.bindir + '/fio-mixed.job';
+		args = '--output ' + self.resultsdir + '/fio-mixed.log ' + self.bindir + '/fio-mixed.job';
 		system(vars + ' ./fio ' + args)
 
 		# Do a profiling run if necessary
