79f13edb91a1a9668f49308f642963aa76392476
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2007-04-01 16:06:54 +0000

    This patch adds support for libnet in deps/ dir to the isic test
    From: michal.k.k.piotrowski@gmail.com
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@521 592f7852-d20e-0410-864c-8624ca9c26a4

--- client/tests/isic/isic.py
+++ client/tests/isic/isic.py
@@ -2,18 +2,21 @@ import test, os_dep
 from autotest_utils import *
 
 class isic(test.test):
-	version = 1
+	version = 2
 
 	# http://www.packetfactory.net/Projects/ISIC/isic-0.06.tgz
 	# + http://www.stardust.webpages.pl/files/crap/isic-gcc41-fix.patch
+
+        def initialize(self):
+		self.job.setup_dep(['libnet'])
 
 	def setup(self, tarball = 'isic-0.06.tar.bz2'):
 		tarball = unmap_url(self.bindir, tarball, self.tmpdir)
 		extract_tarball_to_dir(tarball, self.srcdir)
 		os.chdir(self.srcdir)
 
-		os_dep.library('libnet.so')
-		system('./configure')
+		system('patch -p1 < ../build-fixes.patch')
+		system('PREFIX=' + self.autodir + '/deps/libnet/libnet/ ./configure')
 		system('make')
 
 	def execute(self, args = '-s rand -d 127.0.0.1 -p 10000000'):
