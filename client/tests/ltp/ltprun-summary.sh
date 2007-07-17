b27155bc1794018af7b24c8691f0261ac4ff7049
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2007-07-17 21:09:54 +0000

    Add LTP scoring script
    From: Randy Dunlap <rdunlap@xenotime.net>
    
    I run a script to calculate a SCORE value.  Example output:
    
    Using logfile=ltp0.log
    build errors:  0 lines
    PASS:          2223
    WARNing:       1
    BROKen:        1
    RETiRed:       0
    FAIL:          0
    CONFig error:  11
    SCORE.ltp:        99.5
    
    then I look for changes in that summary.
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@562 592f7852-d20e-0410-864c-8624ca9c26a4

---
+++ client/tests/ltp/ltprun-summary.sh
@@ -0,0 +1,44 @@
+#! /bin/sh
+# summary of LTP suite 'logfile' results:
+# count "execution: PASS|FAIL|WARN|BROK|RETR|CONF(config error)";
+# count "\.c:.*error:" for build errors (lines);
+
+logfile=${1-logfile}
+echo "Using logfile=$logfile"
+
+TMPFILE="$logfile.$$"
+prtag2tag.pl test_output execution_status $logfile >$TMPFILE
+
+##build_failed=`grep "build: FAILED" $TMPFILE | wc -l`
+build_errors=`grep "\.c:.*error:"  $TMPFILE | wc -l`
+
+run_passed=`grep "^\w* *[[:digit:]]* *PASS *:"  $TMPFILE | wc -l`
+run_warned=`grep "^\w* *[[:digit:]]* *WARN *:"  $TMPFILE | wc -l`
+run_broken=`grep "^\w* *[[:digit:]]* *BROK *:"  $TMPFILE | wc -l`
+run_failed=`grep "^\w* *[[:digit:]]* *FAIL *:"  $TMPFILE | wc -l`
+run_retire=`grep "^\w* *[[:digit:]]* *RETR *:"  $TMPFILE | wc -l`
+run_cfgerr=`grep "^\w* *[[:digit:]]* *CONF *:"  $TMPFILE | wc -l`
+# ignore INFO lines
+
+printf "build errors:  %s lines\n" $build_errors
+printf "PASS:          %s\n" $run_passed
+printf "WARNing:       %s\n" $run_warned
+printf "BROKen:        %s\n" $run_broken
+printf "RETiRed:       %s\n" $run_retire	## NB: not counted in any totals
+printf "FAIL:          %s\n" $run_failed
+printf "CONFig error:  %s\n" $run_cfgerr
+
+passing=$((run_passed + run_warned))
+notpass=$((run_broken + run_cfgerr + run_failed))
+total=$((notpass + passing))
+score=$((1000 * passing))	# convert to percent.decimal
+score=$((score + total / 2))	# rounding
+if [ $total -eq 0 ]; then
+	score=0
+else
+	score=$((score / total))
+fi
+fraction=$((score % 10))
+score=$((score / 10))
+printf "SCORE.ltp:        %d.%d\n" $score $fraction
+rm -f $TMPFILE
