f76eecd6a05c909badac0b4df5f13d4f4dbeeedb
Martin J. Bligh <mbligh@google.com>
2007-07-26 00:56:37 +0000

    Time output in MB/s and time raw reads/writes as well

--- client/tests/parallel_dd/parallel_dd.py
+++ client/tests/parallel_dd/parallel_dd.py
@@ -6,7 +6,26 @@ from subprocess import *
 class parallel_dd(test.test):
 	version = 1
 
-	def write_out(self):
+
+	def raw_write(self):
+		print "Timing raw write of %d megabytes" % self.megabytes
+		sys.stdout.flush()
+		dd = 'dd if=/dev/zero of=%s bs=4K count=%d' % \
+						(self.fs.device, self.blocks)
+		print dd
+		system(dd + ' > /dev/null')
+
+
+	def raw_read(self):
+		print "Timing raw read of %d megabytes" % self.megabytes
+		sys.stdout.flush()
+		dd = 'dd if=%s of=/dev/null bs=4K count=%d' % \
+						(self.fs.device, self.blocks)
+		print dd
+		system(dd + ' > /dev/null')
+
+
+	def fs_write(self):
 		p = []
 		# Write out 'streams' files in parallel background tasks
 		for i in range(self.streams):
@@ -15,68 +34,80 @@ class parallel_dd(test.test):
 			dd = 'dd if=/dev/zero of=%s bs=4K count=%d' % \
 						(file, self.blocks_per_file)
 			print dd
-			p.append(Popen(dd, shell=True))
+			p.append(Popen(dd + ' > /dev/null', shell=True))
 		print "Waiting for %d streams" % self.streams
 		# Wait for everyone to complete
 		for i in range(self.streams):
 			print "Waiting for %d" % p[i].pid
 			sys.stdout.flush()
-			sys.stderr.flush()
 			os.waitpid(p[i].pid, 0)
 		sys.stdout.flush()
 		sys.stderr.flush()
 
 
-	def read_in(self):
+	def fs_read(self):
 		for i in range(self.streams):
 			file = os.path.join(self.job.tmpdir, 'poo%d' % (i+1))
-			system('cat %s > /dev/null' % file)
+			dd = 'dd if=%s of=/dev/null bs=4K count=%d' % \
+						(file, self.blocks_per_file)
+			system(dd + ' > /dev/null')
 
 
 	def test(self, tag):
-		self.fs.mount()
-		t1 = time.time()
+		start = time.time()
+		self.raw_write()
+		self.raw_write_rate = self.megabytes / (time.time() - start)
 
+		start = time.time()
+		self.raw_read()
+		self.raw_read_rate = self.megabytes / (time.time() - start)
+
+		self.fs.mkfs(self.fstype)
+		self.fs.mount()
+		start = time.time()
 		try:
-			self.write_out()
+			self.fs_write()
 		except:
 			try:
 				self.fs.unmount()
 			finally:
 				raise
 		self.fs.unmount()
-
-		t2 = time.time()
+		self.fs_write_rate = self.megabytes / (time.time() - start)
 
 		self.fs.mount()
+		start = time.time()
 		try:
-			self.read_in()
+			self.fs_read()
 		except:
 			try:
 				self.fs.unmount()
 			finally:
 				raise
 			read_in()
-
-		t3 = time.time()
+		self.fs_read_rate = self.megabytes / (time.time() - start)
 		self.fs.unmount()
 
-		return (t2 - t1, t3 - t2)
-		
 
 	def execute(self, fs, fstype = 'ext2', iterations = 2, megabytes = 1000, streams = 2):
-		self.blocks_per_file = (megabytes * 256) / streams
+		self.megabytes = megabytes
+		self.blocks = megabytes * 256
+		self.blocks_per_file = self.blocks / streams
 		self.fs = fs
+		self.fstype = fstype
 		self.streams = streams
 		
 		print "Dumping %d megabytes across %d streams, %d times" % \
 						(megabytes, streams, iterations)
-		# fs.mkfs(fstype)
 
 		keyval = open(os.path.join(self.resultsdir, 'keyval'), 'w')
 		for i in range(iterations):
-			(out_time, in_time) = self.test('%d' % i)
-			t = "out=%d\nin=%d\n\n" % (out_time, in_time)
+			self.test('%d' % i)
+			t = "raw_write=%d\n" % self.raw_write_rate
+			t += "raw_read=%d\n" % self.raw_read_rate
+			t += "fs_write=%d\n" % self.fs_write_rate
+			t += "fs_read=%d\n" % self.fs_read_rate
+			t += "\n"
 			print t
 			keyval.write(t)
 		keyval.close()
