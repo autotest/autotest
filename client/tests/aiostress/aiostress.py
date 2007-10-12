fc92f754cd9854f9fceac68d9b578a5ce5b85660
Martin J. Bligh <mbligh@google.com>
2007-10-12 23:34:37 +0000

    add parsing of keyvals to aiostress and unixbench

--- client/tests/aiostress/aiostress.py
+++ client/tests/aiostress/aiostress.py
@@ -35,8 +35,12 @@ class aiostress(test.test):
 		var_ld_path = 'LD_LIBRARY_PATH=' + ld_path
 		cmd = self.srcdir + '/aio-stress ' + args + ' poo'
 		profilers = self.job.profilers
+
 		if not profilers.only():
 			system(var_ld_path + ' ' + cmd)
+			report = open(self.debugdir + '/stderr')
+			keyval = open(self.resultsdir + '/keyval', 'w')
+			_format_results(report, keyval)
 
 		# Do a profiling run if necessary
 		if profilers.present():
@@ -44,3 +48,38 @@ class aiostress(test.test):
 			system(var_ld_path + ' ' + cmd)
 			profilers.stop(self)
 			profilers.report(self)
+			if profilers.only():
+				report = open(self.debugdir + '/stderr')
+				keyval = open(self.resultsdir + '/keyval', 'w')
+				_format_results(report, keyval)
+
+
+
+def _format_results(report, keyval):
+	for line in report:
+		if 'threads' in line:
+			if 'files' in line:
+				if 'contexts' in line:
+					break
+
+	for line in report:
+		line = line.split(')')[0]
+		key, value = line.split('(')
+		key = key.strip().replace(' ', '_')
+		value = value.split()[0]
+		print >> keyval, '%s=%s' % (key, value)
+
+
+"""
+file size 1024MB, record size 64KB, depth 64, ios per iteration 8
+max io_submit 8, buffer alignment set to 4KB
+threads 1 files 1 contexts 1 context offset 2MB verification off
+write on poo (245.77 MB/s) 1024.00 MB in 4.17s
+thread 0 write totals (55.86 MB/s) 1024.00 MB in 18.33s
+read on poo (1311.54 MB/s) 1024.00 MB in 0.78s
+thread 0 read totals (1307.66 MB/s) 1024.00 MB in 0.78s
+random write on poo (895.47 MB/s) 1024.00 MB in 1.14s
+thread 0 random write totals (18.42 MB/s) 1024.00 MB in 55.59s
+random read on poo (1502.89 MB/s) 1024.00 MB in 0.68s
+thread 0 random read totals (1474.36 MB/s) 1024.00 MB in 0.69s
+"""
