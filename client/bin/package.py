4f50b10bbcb5a3a5a7cd9b1ca880843d37b366b7
Lucas Meneghel Rodrigues <lmr@linux.vnet.ibm.com>
2007-11-09 17:04:04 +0000

    Remove need for specified package_type in package_installed
    The logic of the new patch checks if we have the binary anywhere in the
    path, if so, it will query that particular package manager database
    looking for the package version.
    
    This won't fall apart if we have different package managers installed.
    If a package version is installed on any of the package managers, the
    function will return True, the correct result. 
    
    In a corner case where the exact same version string of a package is
    installed under different package managers (unlikely), it's also very
    likely that we have very serious file install conflicts and therefore
    bad system administration practices. Not worth considering IMO.
    
    So in my opinion there's no need to check /etc/issue in this particular
    case. 
    
    From: Lucas Meneghel Rodrigues <lmr@linux.vnet.ibm.com>

--- client/bin/package.py
+++ client/bin/package.py
@@ -4,28 +4,30 @@ generic, with implementations that deal with different package managers, such
 as dpkg and rpm.
 """
 
-import os, re
+import os, os_dep, re
 from error import *
 from autotest_utils import *
 
 
-def package_installed(package, package_type):
-       # Verify if a package file from a known package manager is intstalled.
-       if package_type == 'dpkg':
-               status = system_output('dpkg -s %s | grep Status' % package)
-               result = re.search('not-installed', status, re.IGNORECASE)
-               if result:
-                       return False
-               else:
+def package_installed(package_version):
+# Verify if package from a known package manager is intstalled given the package 
+# version.
+       known_package_managers = ['rpm', 'dpkg']
+       for package_manager in known_package_managers:
+               if package_manager == 'dpkg' and os_dep.command('dpkg'):
+                       status = \
+                       system_output('dpkg -s %s | grep Status' % package)
+                       result = \
+                       re.search('not-installed', status, re.IGNORECASE)
+                       if not result:
+                               return True
+               if package_manager == 'rpm' and os_dep.command('rpm'):
+                       try:
+                               system('rpm -q ' + package)
+                       except:
+                               pass
                        return True
-       elif package_type == 'rpm':
-               try:
-                       system('rpm -q ' + package)
-               except:
-                       return False
-               return True
-       else:
-               raise 'PackageError', 'Package method not implemented'
+       return False
 
 
 def install_package(package):
@@ -41,12 +43,13 @@ def install_package(package):
                        package_pattern = 'RPM'
                        install_command = 'rpm -Uvh ' + package
                elif package_manager == 'dpkg':
-                       package_version = system_output('dpkg -f %s Package' % package)
+                       package_version = \ 
+                       system_output('dpkg -f %s Package' % package)
                        package_pattern = 'Debian'
                        install_command = 'dpkg -i ' + package
                if package_type.__contains__(package_pattern):
                        known_package = True
-                       if not package_installed(package_version, package_manager):
+                       if not package_installed(package_version):
                                return system(install_command)
        if not known_package:
-               raise 'PackageError', 'Package method not implemented'
+               raise PackageError, 'Package method not implemented' 
