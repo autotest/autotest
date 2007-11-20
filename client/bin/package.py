978b2be1a4af2268f99b4af8ae0e8d24bba74610
Lucas Meneghel Rodrigues <lucasmr@br.ibm.com>
2007-11-20 16:26:23 +0000

    Improve package.py by adding other package utility functions: package_type - returns the package type of a file package_version - returns a version string for a package file
    The introduction of package_type simplified the code of the other
    functions, making them easier to read and cleaner. 
    
    Signed-off-by: Lucas Meneghel Rodrigues <lucasmr@br.ibm.com>

--- client/bin/package.py
+++ client/bin/package.py
@@ -4,52 +4,90 @@ generic, with implementations that deal with different package managers, such
 as dpkg and rpm.
 """
 
+__author__ = 'lucasmr@br.ibm.com (Lucas Meneghel Rodrigues)'
+
 import os, os_dep, re
 from error import *
 from autotest_utils import *
 
+def package_type(package):
+	"""\
+	Returns the package type (if any) of a given file.
+	Implemented package types:
+	- 'dpkg' - dpkg (debian, ubuntu) package files
+	- 'rpm' - rpm (red hat, suse) package files
+	Raises an exception if the package type is not one of the implemented
+	package types.
+	"""
+	if not os.path.isfile(package):
+		raise ValueError, 'invalid file %s to verify' % package
+	# Use file and libmagic to determine the actual package file type.
+	file_result = system_output('file ' + package)
+	known_package_managers = ['rpm', 'dpkg']
+	for package_manager in known_package_managers:
+		if package_manager == 'rpm':
+			package_pattern = 'RPM'
+		elif package_manager == 'dpkg':
+			package_pattern = 'Debian'
 
-def package_installed(package_version):
-# Verify if package from a known package manager is intstalled given the package 
-# version.
-       known_package_managers = ['rpm', 'dpkg']
-       for package_manager in known_package_managers:
-               if package_manager == 'dpkg' and os_dep.command('dpkg'):
-                       status = \
-                       system_output('dpkg -s %s | grep Status' % package)
-                       result = \
-                       re.search('not-installed', status, re.IGNORECASE)
-                       if not result:
-                               return True
-               if package_manager == 'rpm' and os_dep.command('rpm'):
-                       try:
-                               system('rpm -q ' + package)
-                       except:
-                               pass
-                       return True
-       return False
+		result = re.search(package_pattern, file_result, re.IGNORECASE)
+		if result:
+			return package_manager
+	# If it's not one of the implemented package manager methods, there's
+	# not much that can be done, hence we throw an exception.
+	raise PackageError, 'Unknown package type %s' % file_result
+
+
+def package_version(package):
+	"""Returns the version string for a package file."""
+	my_package_type = package_type(package)
+
+	if my_package_type == 'rpm' and os_dep.command('rpm'):
+		return system_output('rpm -qp ' + package)
+	elif my_package_type == 'dpkg' and os_dep.command('dpkg'):
+		return system_output('dpkg -f %s Package' % package)
+
+
+def package_installed(package):
+	"""Returns whether a given package is installed in the system"""
+	my_package_type = package_type(package)
+
+	if my_package_type == 'rpm' and os_dep.command('rpm'):
+		try:
+			system('rpm -q ' + package)
+		except:
+			return False
+		return True
+	elif my_package_type == 'dpkg' and os_dep.command('dpkg'):
+		my_package_version = package_version(package)
+		status = \
+		system_output('dpkg -s %s | grep Status' % my_package_version)
+		result = re.search('not-installed', status, re.IGNORECASE)
+		if result:
+			return False
+		return True
+
+	return False
 
 
 def install_package(package):
-       if not os.path.isfile(package):
-               raise ValueError, 'invalid file %s to install' % package
-       # Use file and libmagic to determine the actual package file type.
-       package_type = system_output('file ' + package)
-       known_package = False
-       known_package_managers = ['rpm', 'dpkg']
-       for package_manager in known_package_managers:
-               if package_manager == 'rpm':
-                       package_version = system_output('rpm -qp ' + package)
-                       package_pattern = 'RPM'
-                       install_command = 'rpm -Uvh ' + package
-               elif package_manager == 'dpkg':
-                       package_version = \ 
-                       system_output('dpkg -f %s Package' % package)
-                       package_pattern = 'Debian'
-                       install_command = 'dpkg -i ' + package
-               if package_type.__contains__(package_pattern):
-                       known_package = True
-                       if not package_installed(package_version):
-                               return system(install_command)
-       if not known_package:
-               raise PackageError, 'Package method not implemented' 
+	"""\
+	Tries to install a package file. If the package is already installed,
+	it prints a message to the user and ends gracefully.
+	"""
+	my_package_type = package_type(package)
+
+	if my_package_type == 'rpm' and os_dep.command('rpm'):
+		install_command = 'rpm -Uvh ' + package
+	elif my_package_type == 'dpkg' and os_dep.command('dpkg'):
+		install_command = 'dpkg -i ' + package
+
+	if package_installed(package):
+		return 'Package %s is already installed' % package
+
+	# At this point, the most likely thing to go wrong is that there are 
+	# unmet dependencies for the package. We won't cover this case, at 
+	# least for now.
+	system(install_command)
+	return 'Package %s was installed successfuly' % package
+
