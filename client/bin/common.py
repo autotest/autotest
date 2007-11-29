c84f8ad6eb1acff9fd5ea177723a52b20d713c19
John Admanski <jadmanski@google.com>
2007-11-29 18:14:43 +0000

    Change common.py'>common.py so that it inserts all of the libraries it loads into sys.modules, so that we can import specific submodules directly (e.g. "import common.logging") or specific contents of submodules (e.g. "from common.logging import record") instead of only being able to import the top-level common module.
    Signed-off-by: John Admanski <jadmanski@google.com>

--- client/bin/common.py
+++ client/bin/common.py
@@ -10,5 +10,13 @@ dirname = os.path.dirname(__file__)
 top_level_dir = os.path.abspath(os.path.join(dirname, '..'))
 
 sys.path.insert(0, top_level_dir)
+import common_lib
 from common_lib import *
 del sys.path[0]
+
+for library in common_lib.__all__:
+        sys.modules['common.%s' % library] = eval(library)
+
+# clean up the namespace
+del dirname, top_level_dir, library
+del os, sys, common_lib
