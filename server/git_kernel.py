a5ea4476648a3612cb5b3670fec55430a8f87580
Ryan Harper <ryanh@us.ibm.com>
2007-12-04 22:45:03 +0000

    Add git_kernel class for building kernels from git repositories.
    This class uses a gitrepo and source_kernel to build and install kernels from a
    git repo.
    
    Signed-off-by: Ryan Harper <ryanh@us.ibm.com>

---
+++ server/git_kernel.py
@@ -0,0 +1,76 @@
+#
+# Copyright 2007 IBM Corp. Released under the GPL v2
+# Authors: Ryan Harper <ryanh@us.ibm.com>
+#
+
+"""
+This module defines the GitKernel class
+"""
+
+__author__ = """
+ryanh@us.ibm.com (Ryan Harper)
+"""
+
+
+import os
+import urllib2
+
+import git
+import source_kernel
+from utils import *
+
+
+class GitKernel(git.GitRepo):
+	"""
+	This class represents a git kernel repo.
+
+	It is used to pull down a local copy of a git repo, check if the local repo
+	is up-to-date, if not update and then build the kernel from the git repo.
+
+	"""
+	def __init__(self, repodir, giturl, weburl):
+		git.GitRepo.__init__(self, repodir, giturl, weburl)
+		self.__patches = []
+		self.__config = None
+		self.__build = None
+
+
+	def configure(self, config):
+		self.__config = config
+
+
+	def patch(self, patch):
+		self.__patches.append(patch)
+
+
+	def install(self, host, build=True, builddir=None):
+		# use tmpdir if no builddir specified
+		# NB: pass a builddir to install() method if you
+		# need to ensure the build remains after the completion
+		# of a job 
+		if not builddir:
+			self.__build = os.path.join(host.get_tmp_dir(),"build")
+			print 'warning: builddir %s is not persistent' %(self.__build)
+
+		# push source to host for install
+		print 'pushing %s to host' %(self.source_material)
+		host.send_file(self.source_material, self.__build)
+		remote_source_material= os.path.join(self.__build,
+                                os.path.basename(self.source_material))
+
+		# use a source_kernel to configure, patch, build and install.
+		sk = source_kernel.SourceKernel(remote_source_material)
+
+		if build:
+			# apply patches
+			for p in self.__patches:
+				sk.patch(p)
+
+			# configure
+			sk.configure(self.__config)
+
+			# build
+			sk.build(host)
+			
+		# install
+		sk.install(host)
