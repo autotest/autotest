a3263d4265d833230920d02cc5e1d94b1594db2d
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2007-12-04 22:44:08 +0000

    Refactor server/source_kernel.py
    Rewrote a bit of source_kernel.py to provide both a build and install method to
    be used.  Keep a per-object instance of Autotest for installing to be used by
    either build or install methods.  Removed step_init() portion of the control
    file generation as it didn't seem needed.
    
    Signed-off-by: Ryan Harper <ryanh@us.ibm.com>
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@1029 592f7852-d20e-0410-864c-8624ca9c26a4

--- server/source_kernel.py
+++ server/source_kernel.py
@@ -16,6 +16,7 @@ stutsman@google.com (Ryan Stutsman)
 
 
 import kernel
+import autotest
 
 
 
@@ -30,10 +31,12 @@ class SourceKernel(kernel.Kernel):
 	This is a leaf class in an abstract class hierarchy, it must 
 	implement the unimplemented methods in parent classes.
 	"""
-	def __init__(self):
+	def __init__(self, k):
 		super(kernel.Kernel, self).__init__()
+		self.__kernel = k
 		self.__patch_list = []
 		self.__config_file = None
+		self.__autotest = autotest.Autotest()
 
 
 	def configure(self, configFile):
@@ -45,28 +48,32 @@ class SourceKernel(kernel.Kernel):
 
 
 	def build(self, host):
-		at = autotest.Autotest()
-		at.install(host)
-		ctlfile = self.control_file(self.__kernel, self.__patch_list,
+		ctlfile = self.__control_file(self.__kernel, self.__patch_list,
 					    self.__config_file)
-		at.run(ctlfile, host.get_tmp_dir(), host)
+		self.__autotest.run(ctlfile, host.get_tmp_dir(), host)
 
+
+	def install(self, host):
+		self.__autotest.install(host)
+		ctlfile = ("testkernel = job.kernel('%s')\n"
+			   "testkernel.install()\n"
+			   "testkernel.add_to_bootloader()\n" %(self.__kernel))
+		self.__autotest.run(ctlfile, host.get_tmp_dir(), host)
+		
 
 	def __control_file(self, kernel, patch_list, config):
-		ctl = ("def step_init():\n"
-		       "\tjob.next_step([step_test])\n"
-		       "\ttestkernel = job.kernel('%s')\n" % kernel)
+		ctl = ("testkernel = job.kernel('%s')\n" % kernel)
 
 		if len(patch_list):
-			 patches = ', '.join(["'%s'" % x for x in patch_list])
-			 ctl += "\ttestkernel.patch(%s)" % patches
+			patches = ', '.join(["'%s'" % x for x in patch_list])
+			ctl += "testkernel.patch(%s)\n" % patches
 
 		if config:
-			ctl += "\ttestkernel.config('%s')" % config
+			ctl += "testkernel.config('%s')\n" % config
 		else:
-			ctl += "\ttestkernel.config('', None, True)"
+			ctl += "testkernel.config('', None, True)\n"
 
-		ctl += "\ttestkernel.build()"
+		ctl += "testkernel.build()\n"
 
 		# copy back to server
 
