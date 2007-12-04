3f44a4aeb4cd58c8327bb9fd5578340ae21d9b83
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2007-12-04 20:46:01 +0000

    To avoid hard coding paths and variables inside a test, it's useful to support standard python INI configuration files. The configuration files help to make things cleaner and more organized.
    Example:
    
    Control file:
    
    {{{
    config_file = 'mytest2.cfg'
    tests = ['test1', 'test2', 'test3']
    
    for test in tests:
           job.run_test('mytest', test, config_file, tag = test)
    }}}
    
    Config file mytest.cfg, inside the test class dir
    (client/tests/mytest/mytest2.cfg):
    
    {{{
    [session1]
    parameter1 = foo
    parameter2 = bar
    
    [session2]
    parameter1 = baz
    
    [session3]
    parameter1 = spam
    parameter2 = eggs
    }}}
    
    Test class file mytest.py (client/tests/mytest/mytest.py)
    
    {{{
    import test
    from test_config import config_loader
    
    class mytest(test):
           def execute(self, test = 'test1', cfg = 'mytest.cfg'):
                   # Load the test configuration file
                   config_file = os.path.join(self.bindir, cfg)
                   my_config = config_loader(filename = config_file)
                   ...
                   parameter = my_config.get('session2', 'parameter1') # retrieves baz
                   my_config.set('session3', 'parameter2', 'sausage') # exchanges eggs for sausage
                   my_config.remove('session1', 'parameter2') # removes bar
                   my_config.save() # saves the changes made to the file
    }}}
    
    This patch adds a wrapper around standard ConfigParser to handle such
    files. We're sending the patch because we think it might be useful.
    
    From: Ricardo Salveti de Araujo <rsalveti@br.ibm.com>
    Signed-off-by: Lucas Meneghel Rodrigues <lucasmr@br.ibm.com>
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@1026 592f7852-d20e-0410-864c-8624ca9c26a4

---
+++ client/bin/test_config.py
@@ -0,0 +1,57 @@
+"""
+Wrapper around ConfigParser to manage testcases configuration.
+"""
+
+__author__ = 'rsalveti@linux.vnet.ibm.com (Ricardo Salveti de Araujo)'
+
+from ConfigParser import ConfigParser
+from os import path
+
+__all__ = ['config']
+
+class config_loader:
+	"""Base class of the configuration parser"""
+	def __init__(self, filename="test.conf"):
+		self.filename = filename
+		if not path.isfile(self.filename):
+			raise IOError, "File '%s' not found" % (self.filename)
+		self.parser = ConfigParser()
+		self.parser.read(self.filename)
+
+
+	def get(self, section, name, default=None):
+		"""Get the value of a option.
+
+		Section of the config file and the option name.
+		You can pass a default value if the option doesn't exist.
+		"""
+		if not self.parser.has_option(section, name):
+			return default
+		return self.parser.get(section, name)
+
+
+	def set(self, section, option, value):
+		"""Set an option.
+
+		This change is not persistent unless saved with 'save()'.
+		"""
+		if not self.parser.has_section(section):
+			self.parser.add_section(section)
+		return self.parser.set(section, name, value)
+
+
+	def remove(self, section, name):
+		"""Remove an option."""
+		if self.parser.has_section(section):
+			self.parser.remove_option(section, name)
+
+
+	def save(self):
+		"""Save the configuration file with all modifications"""
+		if not self.filename:
+			return
+		fileobj = file(self.filename, 'w')
+		try:
+			self.parser.write(fileobj)
+		finally:
+			fileobj.close()
