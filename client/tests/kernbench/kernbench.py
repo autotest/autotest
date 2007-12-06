5faa1afe6df6ff15230dcce90b60d3e32e46dca8
Andy Whitcroft <apw@shadowen.org>
2007-12-06 17:51:48 +0000

    kernbench: allow specification of the built tree
    Some machines cannot build specific versions, so we need to be able to
    specify the version for those machines.  This patch allows the version
    to be specified caching the tree for the latest one.
    
    Signed-off-by: Andy Whitcroft <apw@shadowen.org>

--- client/tests/kernbench/kernbench.py
+++ client/tests/kernbench/kernbench.py
@@ -8,6 +8,10 @@ class kernbench(test.test):
 	def setup(self, build_dir = None):
 		if not build_dir:
 			build_dir = self.srcdir
+		os.mkdir(build_dir)
+
+
+	def __init_tree(self, build_dir, version = None):
 		#
 		# If we have a local copy of the 2.6.14 tarball use that
 		# else let the kernel object use the defined mirrors
@@ -16,11 +20,28 @@ class kernbench(test.test):
 		# http://kernel.org/pub/linux/kernel/v2.6/linux-2.6.14.tar.bz2
 		#
 		# On ia64, we default to 2.6.20, as it can't compile 2.6.14.
-
-		if get_current_kernel_arch() == 'ia64':
+		if version:
+			default_ver = version
+		elif get_current_kernel_arch() == 'ia64':
 			default_ver = '2.6.20'
 		else:
 			default_ver = '2.6.14'
+
+		kversionfile = os.path.join(build_dir, ".kversion")
+		install_needed = True
+		if os.path.exists(kversionfile):
+			old_version = pickle.load(open(kversionfile, 'r'))
+			if (old_version == default_ver):
+				install_needed = False
+
+		if not install_needed:
+			return
+
+		# Clear out the old version
+		system("echo rm -rf '" + build_dir + "/*'")
+
+		pickle.dump(default_ver, open(kversionfile, 'w'))
+
 		tarball = None
 		for dir in (self.bindir, '/usr/local/src'):
 			tar = 'linux-%s.tar.bz2' % default_ver
@@ -36,12 +57,13 @@ class kernbench(test.test):
 		kernel.config(defconfig=True, logged=False)
 
 
-	def execute(self, iterations = 1, threads = 2*count_cpus(), dir = None):
+	def execute(self, iterations = 1, threads = 2*count_cpus(), dir = None, version = None):
 		if dir:
 			build_dir = dir
-			self.setup(build_dir)
 		else:
 			build_dir = self.srcdir
+
+		self.__init_tree(build_dir, version)
 
 		kernel = self.job.kernel(build_dir, self.tmpdir, build_dir,
 								leave = True)
