4ea64027fc48324c7fded3e7c3cb41ff0dba30c7
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2007-12-19 00:49:21 +0000

    check_python_version: change to be more readable
    Taking a quick look at the code I saw that a minor change in the
    check_python_version function would let the code more readable.
    This patch only changes sys.version call to sys.version_info, that
    returns the needed data for this verification more directly.
    
    Signed-off-by: Ursula Junque <ursinha@gmail.com>
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@1083 592f7852-d20e-0410-864c-8624ca9c26a4

--- client/bin/check_version.py
+++ client/bin/check_version.py
@@ -1,10 +1,8 @@
 import sys, string, os
 
 def check_python_version():
-	py_version = string.split(sys.version, ' ')[0]
-	version = string.split(py_version, '.')[0:2]
-	version = map(int, version)
-	if version < [2, 4]:
+	version = sys.version_info[0:2]
+	if version < (2, 4):
                 for new in ('/usr/bin/python2.4', '/usr/local/bin/python2.4'):
                         if os.path.exists(new):
                                 sys.argv.insert(0, '-u')
