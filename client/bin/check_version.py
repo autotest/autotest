e814f598dce9642f745c9d6e7b13c8735bbe939d
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2007-12-20 01:33:08 +0000

    Fix check_version for REALLY old versions of Python
    Signed-off-by: John Admanski <jadmanski@google.com>
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@1092 592f7852-d20e-0410-864c-8624ca9c26a4

--- client/bin/check_version.py
+++ client/bin/check_version.py
@@ -1,8 +1,12 @@
 import sys, string, os
 
 def check_python_version():
-	version = sys.version_info[0:2]
-	if version < (2, 4):
+	version = None
+	try:
+		version = sys.version_info[0:2]
+	except AttributeError:
+		pass # pre 2.0, no neat way to get the exact number
+	if version and version < (2, 4):
                 for new in ('/usr/bin/python2.4', '/usr/local/bin/python2.4'):
                         if os.path.exists(new):
                                 sys.argv.insert(0, '-u')
