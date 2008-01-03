43da422a841b4c00ab4b75b23b7d266ecf9f7b52
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2008-01-03 01:58:17 +0000

    Added the start_profilers command from server. Profilers sync locally after GO is sent.
    Signed-off-by: Colby Ranger <cranger@google.com>
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@1098 592f7852-d20e-0410-864c-8624ca9c26a4

--- client/tests/barriertest/barriertest.py
+++ client/tests/barriertest/barriertest.py
@@ -9,14 +9,19 @@ class barriertest(test.test):
 			hostid, masterid, all_ids):
 		profilers = self.job.profilers
 
-		b1 = self.job.barrier(hostid, "sync_profilers",
+		b0 = self.job.barrier(hostid, "sync_profilers",
+			timeout_start, port=63100)
+		b0.rendevous_servers(masterid, hostid)
+
+		b1 = self.job.barrier(hostid, "start_profilers",
+			timeout_start, port=63100)
+		b1.rendevous_servers(masterid, hostid)
+
+		b2 = self.job.barrier(hostid, "local_sync_profilers",
 			timeout_sync)
-		b1.rendevous(*all_ids)
+		b2.rendevous(*all_ids)
 
 		profilers.start(self)
-		b2 = self.job.barrier(hostid, "start_profilers",
-			timeout_start, port=63100)
-		b2.rendevous_servers(masterid, hostid)
 
 		b3 = self.job.barrier(hostid, "stop_profilers",
 			timeout_stop, port=63100)
