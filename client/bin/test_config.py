4524f03092aac16f997bec897600198da205ce41
Lucas Meneghel Rodrigues <lucasmr@br.ibm.com>
2008-02-01 17:41:39 +0000

    In order to make test_config.config_loader() more flexible, implemented a mechanism that allows us to choose between the following options:  * Specify the configuration as a remote file (http or ftp);  * Specify the configuration as a filesystem path to a file (the only method previously supported  * Specify the configuration file as a string object that will be later transformed in a file like object. So it's possible to use a config string inside the control file like this (config file trimmed down):
    config = '''
    [dacapo]
    tarball_url =
    http://easynews.dl.sourceforge.net/sourceforge/dacapobench/dacapo-2006-10-MR2.jar
    tarball_url_alt =
    http://myhost.foo.com/packages/dacapobench/dacapo-2006-10-MR2.jar
    override_default_url = no
    package_md5 = 35cf23b26ac1b4fb83f11445300f492f
    '''
    
    This way, we still can use a config file format, keeping configuration
    data organized and separated from test logic and still use one control
    file. 
    
    Signed-off-by: Lucas Meneghel Rodrigues <lucasmr@br.ibm.com>

--- client/bin/test_config.py
+++ client/bin/test_config.py
@@ -5,18 +5,52 @@ Wrapper around ConfigParser to manage testcases configuration.
 __author__ = 'rsalveti@linux.vnet.ibm.com (Ricardo Salveti de Araujo)'
 
 from ConfigParser import ConfigParser
+from StringIO import StringIO
+from urllib import urlretrieve
 from os import path
+import types
 
 __all__ = ['config_loader']
 
 class config_loader:
 	"""Base class of the configuration parser"""
-	def __init__(self, filename="test.conf"):
-		self.filename = filename
-		if not path.isfile(self.filename):
-			raise IOError("File %s not found" % (self.filename))
+	def __init__(self, cfg, tmpdir = '/tmp'):
+		"""\
+		Instantiate ConfigParser and provide the file like object that we'll 
+		use to read configuration data from.
+		Args:
+			* cfg: Where we'll get configuration data. It can be either:
+				* A URL containing the file
+				* A valid file path inside the filesystem
+				* A string containing configuration data
+			* tmpdir: Where we'll dump the temporary conf files. The default
+			is the /tmp directory.
+		"""
+		# Base Parser
 		self.parser = ConfigParser()
-		self.parser.read(self.filename)
+		# File is already a file like object
+		if hasattr(cfg, 'read'):
+			self.cfg = cfg
+			self.parser.readfp(self.cfg)
+		elif isinstance(cfg, types.StringTypes):
+			# Config file is a URL. Download it to a temp dir
+			if cfg.startswith('http') or cfg.startswith('ftp'):
+				self.cfg = path.join(tmpdir, path.basename(cfg))
+				urlretrieve(cfg, self.cfg)
+				self.parser.read(self.cfg)
+			# Config is a valid filesystem path to a file.
+			elif path.exists(path.abspath(cfg)):
+				if path.isfile(cfg):
+					self.cfg = path.abspath(cfg)
+					self.parser.read(self.cfg)
+				else:
+					e_msg = 'Invalid config file path: %s' % cfg
+					raise IOError(e_msg)
+			# Config file is just a string, convert it to a python file like
+			# object using StringIO
+			else:
+				self.cfg = StringIO(cfg)
+				self.parser.readfp(self.cfg)
 
 
 	def get(self, section, name, default=None):
