8bd6f575b5c5ef68b7eb780f8446314846815e50
Jeremy Orlow <jorlow@google.com>
2008-02-01 18:08:53 +0000

    Move check_version into the common utils and make sure we run it for any autotest binaries which require 2.4.
    Signed-off-by: Jeremy Orlow <jorlow@google.com>

--- client/common_lib/__init__.py
+++ client/common_lib/__init__.py
@@ -1,4 +1,11 @@
-__all__ = ['error', 'logging', 'barrier']
+__all__ = ['error', 'logging', 'barrier', 'check_version']
 
 import site_libraries
-__all__ += site_libraries.libraries
+__all__.extend(site_libraries.libraries)
+
+# This is a dirty, dirty hack...but it must be here to avoid changing a bunch
+# of code to be python 1.5 compatible.  To be fair, anything using this library
+# should be mandating that it runs the correct version of python, so it's
+# fairly safe.
+import check_version
+check_version.check_python_version()
