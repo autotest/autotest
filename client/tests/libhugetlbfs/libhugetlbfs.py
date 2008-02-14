a518464263b70a93f184d0aecb1c3a0e6c200296
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2008-02-14 16:19:12 +0000

    Since libhuge v1.1 was giving problems during compile stage on power 6 machines, and since there's a new upstream version (v1.2), I thought it'd be useful to update the tarball version on the upstream wrapper version. Also, if no directories are specified to the test, choose a sensible default directory (inside autotest tree) where to mount hugetlbfs.
    Signed-off-by: Lucas Meneghel Rodrigues <lucasmr@br.ibm.com>
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@1240 592f7852-d20e-0410-864c-8624ca9c26a4

--- client/tests/libhugetlbfs/libhugetlbfs.py
+++ client/tests/libhugetlbfs/libhugetlbfs.py
@@ -1,20 +1,20 @@
-import test, re
+import test, re, os
 from autotest_utils import *
 
 class libhugetlbfs(test.test):
-	version = 2
+	version = 3
 
-	# http://downloads.sourceforge.net/libhugetlbfs/libhugetlbfs-1.1.tar.gz
-	def setup(self, tarball = 'libhugetlbfs-1.1.tar.gz'):
+	# http://downloads.sourceforge.net/libhugetlbfs/libhugetlbfs-1.2.tar.gz
+	def setup(self, tarball = 'libhugetlbfs-1.2.tar.gz'):
 		tarball = unmap_url(self.bindir, tarball, self.tmpdir)
 		extract_tarball_to_dir(tarball, self.srcdir)
 		os.chdir(self.srcdir)
 
 		system('make')
-		
-	def execute(self, dir, pages_requested = 20):
+
+	def execute(self, dir = None, pages_requested = 20):
 		check_kernel_ver("2.6.16")
-		
+
 		# Check huge page number
 		pages_available = 0
 		if os.path.exists('/proc/sys/vm/nr_hugepages'):
@@ -25,11 +25,14 @@ class libhugetlbfs(test.test):
 			raise TestError('Kernel does not support hugepages')
 		if pages_available < pages_requested:
 			raise TestError('%d huge pages available, < %d pages requested' % (pages_available, pages_requested))
-		
+
 		# Check if hugetlbfs has been mounted
 		if not file_contains_pattern('/proc/mounts', 'hugetlbfs'):
+			if not dir:
+				dir = os.path.join(self.tmpdir, 'hugetlbfs')
+				os.makedirs(dir)
 			system('mount -t hugetlbfs none %s' % dir)
-		
+
 		os.chdir(self.srcdir)
 
 		profilers = self.job.profilers
