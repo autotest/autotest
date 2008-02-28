864ad977b95afae2e18d7658badfdcfc1362e2ce
Scott Zawalski <scottz@google.com>
2008-02-28 16:38:53 +0000

    Fix a few misses in the global_config patch  global_config.py import sys  common.py change import from top_level_dir to client
    From: Scott Zawalski <scottz@google.com>
    Signed-off-by: John Admanski <jadmanski@google.com>

--- server/common.py
+++ server/common.py
@@ -7,9 +7,9 @@ __author__ = 'showard@google.com (Steve Howard)'
 import os, sys
 
 dirname = os.path.dirname(sys.modules[__name__].__file__)
-top_level_dir = os.path.abspath(os.path.join(dirname, '..', 'client'))
+client_dir = os.path.abspath(os.path.join(dirname, '..', 'client'))
 
-sys.path.insert(0, top_level_dir)
+sys.path.insert(0, client_dir)
 import common_lib
 from common_lib import *
 del sys.path[0]
@@ -18,5 +18,5 @@ for library in common_lib.__all__:
         sys.modules['common.%s' % library] = eval(library)
 
 # clean up the namespace
-del dirname, top_level_dir, library
+del dirname, client_dir, library
 del os, sys, common_lib
