2bb8950324501a1ca822b841d2bd530a9d655ed1
Anton Blanchard <anton@samba.org>
2008-03-04 16:12:02 +0000

    ftp.us.postgresql.org no longer resolves. Fix the hostname and update to version 8.3.0.
    When testing this on a fresh install I noticed readline-dev and zlib-dev
    weren't installed. It turns out we don't need them - readline is only
    useful for interactive usage and zlib is used for dump/restore. Lets
    disable them and get rid of a bit of dependency hell.
    
    Signed-off-by: Anton Blanchard <anton@samba.org>

--- client/deps/pgsql/pgsql.py
+++ client/deps/pgsql/pgsql.py
@@ -5,22 +5,22 @@ check_python_version()
 import os
 from autotest_utils import *
 
-version = 2
+version = 3
 
 def setup(tarball, topdir): 
 	srcdir = os.path.join(topdir, 'src')
 	if not os.path.exists(tarball):
-		get_file('ftp://ftp.us.postgresql.org/pub/mirrors/postgresql/v8.2.5/postgresql-8.2.5.tar.bz2', tarball)
+		get_file('ftp://ftp.postgresql.org/pub/v8.3.0/postgresql-8.3.0.tar.bz2', tarball)
 	extract_tarball_to_dir(tarball, 'src')
 	os.chdir(srcdir)
-	system ('./configure --enable-debug --prefix=%s/pgsql' % topdir)
+	system ('./configure --without-readline --without-zlib --enable-debug --prefix=%s/pgsql' % topdir)
 	system('make -j %d' % count_cpus())
 	system('make install')
 	
 	os.chdir(topdir)
 	
 pwd = os.getcwd()
-tarball = os.path.join(pwd, 'postgresql-8.2.5.tar.bz2')
+tarball = os.path.join(pwd, 'postgresql-8.3.0.tar.bz2')
 update_version(pwd+'/src', False, version, setup, tarball, pwd)
 
 
