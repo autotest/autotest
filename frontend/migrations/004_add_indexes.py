e21c273011575cc8cc87b68e0403c8dca80802c2
showard <showard@592f7852-d20e-0410-864c-8624ca9c26a4>
2008-03-31 23:33:26 +0000

    Add indexes to frontend DB.
    git-svn-id: svn://test.kernel.org/autotest/trunk@1386 592f7852-d20e-0410-864c-8624ca9c26a4

---
+++ frontend/migrations/004_add_indexes.py
@@ -0,0 +1,23 @@
+INDEXES = (
+    ('ineligible_host_queues', 'job_id'),
+    ('ineligible_host_queues', 'host_id'),
+    ('host_queue_entries', 'job_id'),
+    ('host_queue_entries', 'host_id'),
+    ('host_queue_entries', 'meta_host'),
+    ('hosts_labels', 'label_id'),
+)
+
+def get_index_name(table, field):
+	return table + '_' + field
+
+
+def migrate_up(manager):
+	for table, field in INDEXES:
+		manager.execute('CREATE INDEX %s ON %s (%s)' %
+				(get_index_name(table, field), table, field))
+
+
+def migrate_down(manager):
+	for table, field in INDEXES:
+		manager.execute('DROP INDEX %s ON %s' %
+				(get_index_name(table, field), table))
