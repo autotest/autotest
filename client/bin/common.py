4e396db3d19d7a8427035f294f880e829cf57709
John Admanski <jadmanski@google.com>
2008-04-03 22:29:48 +0000

    Modify all the common.py to set up an autotest_lib.* namespace as well as all the common.* module setup that it normally does.
    This should make it easier to replace any funky sys.path code with
    code that actually does the right thing, and imports modules using
    absolute paths; as long as our entry points import common, code should
    be able to count on imports like "import autotest_lib.tko.parse" or
    "import autotest_lib.server.autotest" working, without having to
    fiddle with sys.path themselves.
    
    The client/bin/common.py is an exception; we can't assume anything
    other than the client/ directory is present, so it only adds the
    autotest_lib.client.* namespace. So code within the client
    itself should only do absolute imports from the client directory
    downward, not from the top-level dir downward.
    
    We'll probably need to add __init__.py files to some directories to
    allow us to import from them as packages, but that can be done on
    an as-needed basis when we write import statements that require them.
    
    Signed-off-by: John Admanski <jadmanski@google.com>

--- client/bin/common.py
+++ client/bin/common.py
@@ -1,22 +1,8 @@
-"""\
-Hook into common libraries.
-"""
-
-__author__ = 'showard@google.com (Steve Howard)'
-
 import os, sys
-
 dirname = os.path.dirname(sys.modules[__name__].__file__)
-client_dir = os.path.abspath(os.path.join(dirname, '..'))
-
+client_dir = os.path.abspath(os.path.join(dirname, ".."))
 sys.path.insert(0, client_dir)
-import common_lib
-from common_lib import *
-del sys.path[0]
-
-for library in common_lib.__all__:
-        sys.modules['common.%s' % library] = eval(library)
-
-# clean up the namespace
-del dirname, client_dir, library
-del os, sys, common_lib
+import setup_modules
+sys.path.pop(0)
+setup_modules.setup(base_path=client_dir,
+		    root_module_name="autotest_lib.client")
--- frontend/common.py
+++ frontend/common.py
@@ -1,26 +1,8 @@
-"""\
-This abortion of a file is here so we can access stuff in
-client/common_lib
-"""
-
-__author__ = "raphtee@google.com (Travis Miller)"
-
 import os, sys
-
 dirname = os.path.dirname(sys.modules[__name__].__file__)
-client_dir = os.path.abspath(os.path.join(dirname, "..", 'client'))
-
-# insert client into top of path
+autotest_dir = os.path.abspath(os.path.join(dirname, ".."))
+client_dir = os.path.join(autotest_dir, "client")
 sys.path.insert(0, client_dir)
-import common_lib
-from common_lib import *
-
-# remove top of path
-del sys.path[0]
-
-for library in common_lib.__all__:
-    sys.modules['common.%s' % library] = eval(library)
-
-# clean up the namespace
-del dirname, client_dir, library
-del os, sys, common_lib
+import setup_modules
+sys.path.pop(0)
+setup_modules.setup(base_path=autotest_dir, root_module_name="autotest_lib")
