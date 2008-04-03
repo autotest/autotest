b652446467f70eda741ab25c299b464885bf24ba
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2008-04-03 22:29:48 +0000

    Modify all the common.py to set up an autotest_lib.* namespace as well as all the common.* module setup that it normally does.
    This should make it easier to replace any funky sys.path code with
    code that actually does the right thing, and imports modules using
    absolute paths; as long as our entry points import common, code should
    be able to count on imports like "import autotest_lib.tko.parse" or
    "import autotest_lib.server.autotest" working, without having to
    fiddle with sys.path themselves.
    
    The client/bin/common.py is an exception; we can't assume anything
    other than the client/ directory is present, so it only adds the
    autotest_lib.client.* namespace. So code within the client
    itself should only do absolute imports from the client directory
    downward, not from the top-level dir downward.
    
    We'll probably need to add __init__.py files to some directories to
    allow us to import from them as packages, but that can be done on
    an as-needed basis when we write import statements that require them.
    
    Signed-off-by: John Admanski <jadmanski@google.com>
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@1397 592f7852-d20e-0410-864c-8624ca9c26a4

--- client/common_lib/__init__.py
+++ client/common_lib/__init__.py
@@ -3,10 +3,3 @@ __all__ = ['error', 'logging', 'barrier', 'check_version', 'test', 'utils',
 
 import site_libraries
 __all__.extend(site_libraries.libraries)
-
-# This is a dirty, dirty hack...but it must be here to avoid changing a bunch
-# of code to be python 1.5 compatible.  To be fair, anything using this library
-# should be mandating that it runs the correct version of python, so it's
-# fairly safe.
-import check_version
-check_version.check_python_version()
