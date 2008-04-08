11dd26468d31fd57aba460166e6847290089cb36
showard <showard@592f7852-d20e-0410-864c-8624ca9c26a4>
2008-04-08 19:43:21 +0000

    -don't run pylint as part of the frontend unit test suite -don't look for doctests in files that start with .
    git-svn-id: svn://test.kernel.org/autotest/trunk@1415 592f7852-d20e-0410-864c-8624ca9c26a4

--- frontend/afe/test.py
+++ frontend/afe/test.py
@@ -16,21 +16,27 @@ from frontend import settings, afe
 app_name = 'afe'
 doctest_dir = 'doctests'
 doctest_paths = [os.path.join(doctest_dir, filename) for filename
-		 in os.listdir(os.path.join(app_name, doctest_dir))]
+		 in os.listdir(os.path.join(app_name, doctest_dir))
+		 if not filename.startswith('.')]
 
-modules = []
-module_names = [os.path.basename(filename)[:-3]
-		for filename in glob.glob(app_name + '/*.py')
-		if '__init__' not in filename and 'test.py' not in filename]
-for module_name in module_names:
-	__import__('frontend.afe', globals(), locals(), [module_name])
-	modules.append(getattr(afe, module_name))
+def get_modules():
+	modules = []
+	module_names = [os.path.basename(filename)[:-3]
+			for filename in glob.glob(app_name + '/*.py')
+			if '__init__' not in filename
+			and 'test.py' not in filename]
+	for module_name in module_names:
+		__import__('frontend.afe', globals(), locals(), [module_name])
+		modules.append(getattr(afe, module_name))
+	return modules
+
 
 print_after = 'Ran %d tests from %s'
 
 
 def run_tests(module_list, verbosity=1):
-	total_errors = run_pylint()
+	modules = get_modules()
+	total_errors = 0
 	old_db = settings.DATABASE_NAME
 	django.test.utils.setup_test_environment()
 	django.test.utils.create_test_db(verbosity)
@@ -51,38 +57,3 @@ def run_tests(module_list, verbosity=1):
 		print 'OK'
 	else:
 		print 'FAIL: %d errors' % total_errors
-
-
-pylint_opts = {
-    'models': ['--disable-msg=E0201'],
-    'rpc_client_lib': ['--disable-msg=E0611'],
-}
-
-pylint_base_opts = ['--disable-msg-cat=warning,refactor,convention',
-		    '--reports=no']
-
-pylint_exclude = ['management']
-
-
-def run_pylint():
-	try:
-		import pylint.lint
-	except ImportError:
-		print 'pylint not installed'
-		return
-	original_dir = os.getcwd()
-	os.chdir('..')
-	total_errors = 0
-	for module in modules:
-		module_name = module.__name__
-		module_basename = module_name.split('.')[-1]
-		if module_basename in pylint_exclude:
-			continue
-		print 'Checking ' + module_name
-		opts = (pylint_base_opts + pylint_opts.get(module_basename, []))
-		full_args = opts + [module_name]
-		result = pylint.lint.Run(full_args)
-		errors = result.linter.stats['error']
-		total_errors += errors
-	os.chdir(original_dir)
-	return total_errors
