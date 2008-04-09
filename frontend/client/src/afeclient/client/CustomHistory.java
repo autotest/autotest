0d5e5a44ca1cc33a24089e18ee8874221e12d6a0
Steve Howard <showard@google.com>
2008-04-09 22:09:58 +0000

    Forgot to add files.

---
+++ frontend/client/src/afeclient/client/CustomHistory.java
@@ -0,0 +1,48 @@
+package afeclient.client;
+
+import com.google.gwt.user.client.History;
+import com.google.gwt.user.client.HistoryListener;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Wrapper around gwt.user.client.History that won't call onHistoryChanged for
+ * programmatically-generated history items.
+ *
+ */
+public class CustomHistory implements HistoryListener {
+    protected static CustomHistory theInstance = new CustomHistory();
+    
+    protected List listeners = new ArrayList();
+    protected boolean ignoreNextChange = false;
+    
+    protected CustomHistory() {
+        History.addHistoryListener(this);
+    }
+    
+    public void onHistoryChanged(String historyToken) {
+        if (ignoreNextChange) {
+            ignoreNextChange = false;
+            return;
+        }
+
+        for (Iterator i = listeners.iterator(); i.hasNext(); )
+            ((HistoryListener) i.next()).onHistoryChanged(historyToken);
+    }
+
+    public static void addHistoryListener(HistoryListener listener) {
+        theInstance.listeners.add(listener);
+    }
+    
+    public static void removeHistoryListener(HistoryListener listener) {
+        theInstance.listeners.remove(listener);
+    }
+    
+    public static void newItem(String historyToken) {
+        if (!History.getToken().equals(historyToken))
+            theInstance.ignoreNextChange = true;
+        History.newItem(historyToken);
+    }
+}
---
+++ frontend/client/src/afeclient/client/table/MultipleListFilter.java
@@ -0,0 +1,38 @@
+package afeclient.client.table;
+
+import com.google.gwt.json.client.JSONArray;
+import com.google.gwt.json.client.JSONString;
+import com.google.gwt.json.client.JSONValue;
+
+public class MultipleListFilter extends ListFilter {
+    protected int maxVisibleSize;
+    
+    public MultipleListFilter(String fieldName, int maxVisibleSize) {
+        super(fieldName);
+        this.maxVisibleSize = maxVisibleSize;
+        select.setMultipleSelect(true);
+    }
+
+    public JSONValue getMatchValue() {
+        JSONArray labels = new JSONArray();
+        // skip first item ("all values")
+        for (int i = 1; i < select.getItemCount(); i++) {
+            if (select.isItemSelected(i)) {
+                labels.set(labels.size(), 
+                           new JSONString(getItemText(i)));
+            }
+        }
+        return labels;
+    }
+
+    public boolean isActive() {
+        return true;
+    }
+
+    public void setChoices(String[] choices) {
+        super.setChoices(choices);
+        int visibleSize = Math.min(select.getItemCount(), maxVisibleSize);
+        select.setVisibleItemCount(visibleSize);
+        select.setSelectedIndex(0);
+    }
+}
