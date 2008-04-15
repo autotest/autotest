dfe83ab1fac27395a2d650ab74071dfc92acd051
Steve Howard <showard@google.com>
2008-04-15 17:32:20 +0000

    -fix bug when headers is not passed to get_proxy -print tracebacks from RPC proxy -remove obsolete comment

--- frontend/afe/rpc_client_lib.py
+++ frontend/afe/rpc_client_lib.py
@@ -5,9 +5,6 @@ the afe RPC server.
 
 __author__ = 'showard@google.com (Steve Howard)'
 
-# we currently use xmlrpclib, but we modify it slightly to support keyword args
-# in function calls (with corresponding support in the server)
-
 from json_rpc import proxy
 
 def get_proxy(*args, **kwargs):
