934510764ba342faafc4db967a644f5f9cf8d5f7
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2008-04-17 15:39:22 +0000

    Add missing files from svn
    Signed-off-by: Martin J. Bligh <mbligh@google.com>
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@1450 592f7852-d20e-0410-864c-8624ca9c26a4

---
+++ tko/models.py
@@ -0,0 +1,46 @@
+class job(object):
+	def __init__(self, dir, user, label, machine, queued_time,
+		     started_time, finished_time, machine_owner):
+		self.dir = dir
+		self.tests = []
+		self.user = user
+		self.label = label
+		self.machine = machine
+		self.queued_time = queued_time
+		self.started_time = started_time
+		self.finished_time = finished_time
+		self.machine_owner = machine_owner
+
+
+class kernel(object):
+	def __init__(self, base, patches, kernel_hash):
+		self.base = base
+		self.patches = patches
+		self.kernel_hash = kernel_hash
+
+
+class test(object):
+	def __init__(self, subdir, testname, status, reason, test_kernel,
+		     machine, finished_time, iterations, attributes):
+		self.subdir = subdir
+		self.testname = testname
+		self.status = status
+		self.reason = reason
+		self.kernel = test_kernel
+		self.machine = machine
+		self.finished_time = finished_time
+		self.iterations = iterations
+		self.attributes = attributes
+
+
+class patch(object):
+	def __init__(self, spec, reference, hash):
+		self.spec = spec
+		self.reference = reference
+		self.hash = hash
+
+
+class iteration(object):
+	def __init__(self, index, keyval):
+		self.index = index
+		self.keyval = keyval
