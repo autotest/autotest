2d3572bfac8c386a056d7c09e385e1814b252ca3
Martin J. Bligh <mbligh@google.com>
2008-04-17 15:40:51 +0000

    Add more missing files from previous commits. Did I mention I hate svn?
    Signed-off-by: Martin J. Bligh <mbligh@google.com>

---
+++ tko/status_lib.py
@@ -0,0 +1,65 @@
+import collections
+
+
+class status_stack(object):
+	statuses = ["ABORT", "ERROR", "FAIL", "WARN", "GOOD", "ALERT",
+		    "NOSTATUS"]
+
+
+	def __init__(self):
+		self.status_stack = [self.statuses[-1]]
+
+
+	def current_status(self):
+		return self.status_stack[-1]
+
+
+	def update(self, new_status):
+		if new_status not in self.statuses:
+			return
+		old = self.statuses.index(self.current_status())
+		new = self.statuses.index(new_status)
+		if new < old:
+			self.status_stack[-1] = new_status
+
+
+	def start(self):
+		self.status_stack.append(self.statuses[-1])
+
+
+	def end(self):
+		result = self.status_stack.pop()
+		if len(self.status_stack) == 0:
+			self.status_stack.append(self.statuses[-1])
+		return result
+
+
+	def size(self):
+		return len(self.status_stack) - 1
+
+
+class line_buffer(object):
+	def __init__(self):
+		self.buffer = collections.deque()
+
+
+	def get(self):
+		return self.buffer.pop()
+
+
+	def put(self, line):
+		self.buffer.appendleft(line)
+
+
+	def put_back(self, line):
+		self.buffer.append(line)
+
+
+	def size(self):
+		return len(self.buffer)
+
+
+def parser(version):
+	library = "autotest_lib.tko.parsers.version_%d" % version
+	module = __import__(library, globals(), locals(), ["parser"])
+	return module.parser()
