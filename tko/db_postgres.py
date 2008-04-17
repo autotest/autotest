1b308d58329d8769999eaff4f63d8d7d359bb38b
John Admanski <jadmanski@google.com>
2008-04-17 15:25:38 +0000

    Convert the parser into an embeddable library. This includes a major re-organization of the existing code.
    Signed-off-by: John Admanski <jadmanski@google.com>

--- tko/db_postgres.py
+++ tko/db_postgres.py
@@ -1,7 +1,7 @@
-import psycopg2.psycopg1 as postgres
+import psycopg2.psycopg1 as driver
 import db
 
 class db_postgres(db.db_sql):
 	def connect(self, host, database, user, password):
-		return postgres.connect("dbname=%s user=%s password=%s" % \
-				(database, user, password))
+		return driver.connect("dbname=%s user=%s password=%s" % \
+				      (database, user, password))
