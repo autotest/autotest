d5c5c17637fa52b93b178641484acc23166df684
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2008-05-06 20:41:37 +0000

    Provides a fraimwork for forming n of m tuples of machines.
    From: travis miller
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@1488 592f7852-d20e-0410-864c-8624ca9c26a4

---
+++ server/utils_unittest.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python2.4
+
+__author__ = 'raphtee@google.com (Travis Miller)'
+
+import unittest
+import common
+from autotest_lib.server import utils
+
+
+class UtilsTest(unittest.TestCase):
+	
+	def setUp(self):
+		# define out machines here
+		self.machines = ['mach1', 'mach2', 'mach3', 'mach4', 'mach5', 
+					'mach6', 'mach7']
+		
+		self.ntuples = [['mach1', 'mach2'], ['mach3', 'mach4'],
+				['mach5', 'mach6']]
+		self.failures = []
+		self.failures.append(('mach7', "machine can not be tupled"))
+
+
+	def test_form_cell_mappings(self):
+		(ntuples, failures) = utils.form_ntuples_from_machines(self.machines)
+		self.assertEquals(self.ntuples, ntuples)
+		self.assertEquals(self.failures, failures)
+
+
+if __name__ == "__main__":
+	unittest.main()
