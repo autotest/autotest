276c44846eb5c782024e19f1eb99d272ab08b66f
Martin J. Bligh <mbligh@google.com>
2008-05-09 20:01:18 +0000

    Remove duplicated code in unittest_suite.py
    Signed-off-by: Martin J. Bligh <mbligh@google.com>

--- unittest_suite.py
+++ unittest_suite.py
@@ -24,29 +24,3 @@ if __name__ == "__main__":
 	alltests = unittest.TestSuite(suites)
 	runner = unittest.TextTestRunner(verbosity=2)
         runner.run(alltests)
-#!/usr/bin/python2.4
-
-import os, sys
-import unittest
-
-# ensure the root is where it should be
-root = os.path.dirname(__file__)
-sys.path.append(root)
-
-
-suites = []
-def lister(dummy, dirname, files):
-	loader = unittest.TestLoader()
-	for f in files:
-		if f.endswith('_unittest.py'):
-			temp = os.path.join(dirname, f).strip('.py')
-			mod = temp[1:].replace('/', '.')
-			suite = loader.loadTestsFromName(mod)
-			suites.append(suite)
-
-
-if __name__ == "__main__":
-	os.path.walk(root, lister, None)
-	alltests = unittest.TestSuite(suites)
-	runner = unittest.TextTestRunner(verbosity=2)
-        runner.run(alltests)
