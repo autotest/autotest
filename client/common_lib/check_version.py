f7fb4ecc488f59f30e06267d5e2d611bc59ec348
Jeremy Orlow <jorlow@google.com>
2008-05-14 20:17:09 +0000

    Check version should support the scenario where a user does not have python 2.4 installed, but does have some later version installed.
    Signed-off-by: Jeremy Orlow <jorlow@google.com>

--- client/common_lib/check_version.py
+++ client/common_lib/check_version.py
@@ -1,4 +1,36 @@
-import sys, string, os
+import sys, string, os, glob, re
+
+
+def extract_version(path):
+	match = re.search(r'/python(\d+)\.(\d+)$', path)
+	if match:
+		return (int(match.group(1)), int(match.group(2)))
+	else:
+		return None
+
+
+def find_newest_python():
+	pythons = []
+	pythons.extend(glob.glob('/usr/bin/python*'))
+	pythons.extend(glob.glob('/usr/local/bin/python*'))
+
+	best_python = (0, 0), ''
+	for python in pythons:
+		version = extract_version(python)
+		if version > best_python[0] and version >= (2, 4):
+			best_python = version, python
+
+	if best_python[0] == (0, 0):
+		raise ValueError('Python 2.4 or newer is needed')
+	return best_python[1]
+	
+
+def restart():
+	python = find_newest_python()
+	sys.argv.insert(0, '-u')
+	sys.argv.insert(0, python)
+	os.execv(sys.argv[0], sys.argv)
+
 
 def check_python_version():
 	version = None
@@ -7,9 +39,4 @@ def check_python_version():
 	except AttributeError:
 		pass # pre 2.0, no neat way to get the exact number
 	if not version or version < (2, 4):
-                for new in ('/usr/bin/python2.4', '/usr/local/bin/python2.4'):
-                        if os.path.exists(new):
-                                sys.argv.insert(0, '-u')
-                                sys.argv.insert(0, new)
-                                os.execv(sys.argv[0], sys.argv)
-		raise ValueError('Python 2.4 or newer is needed')
+		restart()
