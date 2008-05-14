1862701469ff34f54d849475edc2782360b10de8
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2008-05-14 20:52:51 +0000

    setup_modules duplicates the old check_version code.  It seems to me that check_version should probably remain in its own module, so I've changed setup_modules to import check_version the old (nasty) way rather than calling its own copy of the code (which is now out of date).
    Signed-off-by: Jeremy Orlow <jorlow@google.com>
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@1516 592f7852-d20e-0410-864c-8624ca9c26a4

--- client/setup_modules.py
+++ client/setup_modules.py
@@ -95,16 +95,10 @@ def setup(base_path, root_module_name=""):
 	_setup_common_library(root_module_name)
 
 
-## make sure we're running python 2.4 or later
-version = None
-try:
-	version = sys.version_info[0:2]
-except AttributeError:
-	pass # pre 2.0, no neat way to get the exact number
-if not version or version < (2, 4):
-	for new in ('/usr/bin/python2.4', '/usr/local/bin/python2.4'):
-		if os.path.exists(new):
-			sys.argv.insert(0, '-u')
-			sys.argv.insert(0, new)
-			os.execv(sys.argv[0], sys.argv)
-	raise ValueError('Python 2.4 or newer is needed')
+# This must run on Python versions less than 2.4.
+dirname = os.path.dirname(sys.modules[__name__].__file__)
+common_dir = os.path.abspath(os.path.join(dirname, "common_lib"))
+sys.path.insert(0, common_dir)
+import check_version
+sys.path.pop(0)
+check_version.check_python_version()
