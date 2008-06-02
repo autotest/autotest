3e688edb27ab5fc9095fdf00a0b94b7724e40495
Travis Miller <raphtee@google.com>
2008-06-02 19:34:25 +0000

    Factored parser out into a singleton that can be accessed anywhere.  Also allowed the possiblity to define additional site specific usage info and still have that info displayed via autoserv -h. Will define site specific usage in future submissions.
    Note: This is patch 1 of 2 patches that should be applied together. 
    
    Signed-off-by: Travis Miller <raphtee@google.com>

---
+++ server/site_autoserv_parser.py
@@ -0,0 +1,14 @@
+__author__ = "raphtee@google.com (Travis Miller)"
+
+import common
+from autotest_lib.server.autoserv_parser import base_autoserv_parser
+
+
+add_usage = """\
+"""
+
+
+class site_autoserv_parser(base_autoserv_parser):
+	def get_usage(self):
+		usage = super(site_autoserv_parser, self).get_usage()
+		return usage+add_usage
