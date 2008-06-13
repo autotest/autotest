3c37f014cb45316d6ff915ce3177e65846154e45
Steve Howard <showard@google.com>
2008-06-13 20:49:25 +0000

    -change frontend_unittest to execute the unittests directly (as opposed to executing them through manage.py in a separate process).  this should get us coverage data.  the unit tests could still be better integrated into the whole suite, but this is an improvement. -some fixes to doctests related to tab-space conversion

--- frontend/frontend_unittest.py
+++ frontend/frontend_unittest.py
@@ -5,16 +5,17 @@ This file provides a unittest.TestCase wrapper around the Django unit test
 runner.
 """
 
-import unittest, os
-import common
+import unittest
+from django.core import management
+import settings
 
 
 class FrontendTest(unittest.TestCase):
     def test_all(self):
-        manage_dir = os.path.dirname(__file__)
-        result = os.system("cd %s && ./manage.py test" % (manage_dir))
-        self.assert_(result == 0)
-
+        management.setup_environ(settings)
+        from frontend.afe import test
+        errors = test.run_tests()
+        self.assert_(errors == 0, '%s failures in frontend unit tests' % errors)
 
 if __name__ == '__main__':
     unittest.main()
