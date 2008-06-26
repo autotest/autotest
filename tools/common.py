6b94c2adb40eb5062153832edd7c483096d0e861
Lucas Meneghel Rodrigues <lucasmr@br.ibm.com>
2008-06-26 23:42:18 +0000

    When applying Changeset 1731 (http://test.kernel.org/trac-autotest/changeset/1731) the patch was misapplied - actually the patch was meant to create a utils directory at toplevel dir, however the files were applied at the autotest root directory, altering the contents of the /common.py file and thus rendering run_pylint.py not able to run. This patch fix this issue by:
    1 - Restoring the original contents of /common.py
    2 - Removing run_pylint.py from toplevel dir to the tools dir;
    3 - Creating a new entry point inside of tools
    
    I've moved the scripts to tools instead of the initially proposed utils
    toplevel dir because IMHO is not needed to have both a tools and a utils
    at toplevel.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lucasmr@br.ibm.com>

--- tools/common.py
+++ tools/common.py
@@ -1,6 +1,6 @@
 import os, sys
 dirname = os.path.dirname(sys.modules[__name__].__file__)
-autotest_dir = os.path.abspath(dirname)
+autotest_dir = os.path.abspath(os.path.join(dirname, ".."))
 client_dir = os.path.join(autotest_dir, "client")
 sys.path.insert(0, client_dir)
 import setup_modules
