7aa5ac5e057616505f1567cb86c54867d5ff1092
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2008-07-01 01:49:22 +0000

    Changes to coverage_suite.py which exclude certain directories from coverage analysis.
    From: Travis Miller
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@1752 592f7852-d20e-0410-864c-8624ca9c26a4

--- coverage_suite.py
+++ coverage_suite.py
@@ -9,12 +9,18 @@ root = os.path.abspath(os.path.dirname(__file__))
 
 
 def is_valid_directory(dirpath):
-	if dirpath.find('client/tests') >= 0:
-		return False
-	elif dirpath.find('server/tests') >= 0:
-		return False
-	else:
-		return True
+    if dirpath.find('client/tests') >= 0:
+        return False
+    elif dirpath.find('client/site_tests') >= 0:
+        return False
+    elif dirpath.find('tko/migrations') >= 0:
+        return False
+    elif dirpath.find('server/tests') >= 0:
+        return False
+    elif dirpath.find('server/site_tests') >= 0:
+        return False
+    else:
+        return True
 
 
 def is_valid_filename(f):
@@ -36,15 +42,15 @@ def is_valid_filename(f):
 def main():
 	coverage = os.path.join(root, "contrib/coverage.py")
 	unittest_suite = os.path.join(root, "unittest_suite.py")
-	
+
 	# remove preceeding coverage data
 	cmd = "%s -e" % (coverage)
 	utils.system_output(cmd)
-	
+
 	# run unittest_suite through coverage analysis
 	cmd = "%s -x %s" % (coverage, unittest_suite)
 	utils.system_output(cmd)
-	
+
 	# now walk through directory grabbing lits of files
 	module_strings = []
 	for dirpath, dirnames, files in os.walk(root):
@@ -53,7 +59,7 @@ def main():
 				if is_valid_filename(f):
 					temp = os.path.join(dirpath, f)
 					module_strings.append(temp)
-	
+
 	# analyze files
 	cmd = "%s -r -m %s" % (coverage, " ".join(module_strings))
 	utils.system(cmd)
