c06fa51d8a08fe9265aa57b798b00689cbeb8443
showard <showard@592f7852-d20e-0410-864c-8624ca9c26a4>
2008-07-10 00:41:36 +0000

    Added better ACL group support. More checks are now performed to ensure that users cannot create jobs against hosts that are not visible, and that users cannot abort other users' jobs.
    Note: If applying this patch, you must also apply the "move logged-in user
    tracking to separate thread_local module" patch.
    
    Risk: medium (disallowing actions may have unintended side effects)
    Visibility: medium (new errors will occur if disallowed actions are
    attempted)
    
    Signed-off-by: James Ren <jamesren@google.com>
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@1797 592f7852-d20e-0410-864c-8624ca9c26a4

---
+++ frontend/make_superuser.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+import common
+import MySQLdb
+import sys
+from autotest_lib.client.common_lib import global_config
+
+if (len(sys.argv) < 2 or
+    [arg for arg in sys.argv[1:] if arg.startswith('-')]):
+    print "Usage: %s username [username ...]" %sys.argv[0]
+    sys.exit(1)
+
+config = global_config.global_config
+section = 'AUTOTEST_WEB'
+host = config.get_config_value(section, "host")
+db_name = config.get_config_value(section, "database")
+user = config.get_config_value(section, "user")
+password = config.get_config_value(section, "password")
+
+con = MySQLdb.connect(host=host, user=user,
+                      passwd=password, db=db_name)
+cur = con.cursor()
+
+for username in sys.argv[1:]:
+    cur.execute("""
+        SELECT access_level
+        FROM users
+        WHERE login = %s""", username)
+    row = cur.fetchone()
+
+    if row is None:
+        print "User %s does not exist. Creating..." % username
+        cur.execute("""
+            INSERT INTO users (login, access_level)
+            VALUES (%s, 100)""", username)
+        print "    Done"
+    else:
+        print "Updating user %s..." % username
+        cur.execute("""
+            UPDATE users
+            SET access_level = 100
+            WHERE login = %s""", username)
+        if (cur.rowcount == 1):
+            print "    Done"
+        else:
+            print "    %s is already a superuser!" % username
+
+cur.close()
+con.commit()
+con.close()
