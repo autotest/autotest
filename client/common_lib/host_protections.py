38469ccfe96a59099f2019ae2a919a597d9f495f
James Ren <jamesren@google.com>
2008-07-11 21:24:21 +0000

    Rename host protection category "Filesystem only" to "Repair filesystem only".
    Signed-off-by: James Ren <jamesren@google.com>

--- client/common_lib/host_protections.py
+++ client/common_lib/host_protections.py
@@ -1,9 +1,11 @@
 import enum, global_config
 
-Protection = enum.Enum('No protection',   # Repair can do anything to this host
-                       'Filesystem only', # Repair should only try to
-                                          # recover the file system
-                       'Do not repair'    # Repair should not touch this host
+Protection = enum.Enum('No protection',          # Repair can do anything to
+                                                 # this host.
+                       'Repair filesystem only', # Repair should only try to
+                                                 # recover the file system.
+                       'Do not repair'           # Repair should not touch this
+                                                 # host.
                        )
 
 default = Protection.get_value(
