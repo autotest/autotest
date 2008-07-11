0631cdd2bd4fc5c68da96915494e230c71a5f7b2
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2008-07-11 17:58:02 +0000

    Better parsing of the output of the unittest runs so we properly report the errors.
    Signed-off-by: Travis Miller <raphtee@google.com>
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@1815 592f7852-d20e-0410-864c-8624ca9c26a4

--- unittest_suite.py
+++ unittest_suite.py
@@ -6,27 +6,41 @@ from autotest_lib.client.common_lib import utils
 
 root = os.path.abspath(os.path.dirname(__file__))
 
+passes = []
 failures = []
+output_file = None
+
+
+def search_after_line(lines, i):
+    for j in range(i+1, len(lines)):
+        if "ok" in lines[j]:
+            return (False, j)
+        elif "FAIL" in lines[j]:
+            return ("FAIL", j)
+        elif "ERROR" in lines[j]:
+            return ("ERROR", j)
+
 
 def parse_output(output):
     lines = output.splitlines()
-    i = 0;
+    i = 0
     while i < len(lines):
-        if lines[i].startswith('FAIL:'):
-            failures.append(lines[i])
-            i += 1
-            if i == len(lines):
-                break
-            if lines[i].startswith('-'):
+        if lines[i].startswith("test_"):
+            # we found a test line so check output
+            if lines[i].endswith("... ok"):
+                # success!
+                passes.append(lines[i])
+            elif lines[i].endswith("... ERROR") or lines[i].endswith("... FAIL"):
                 failures.append(lines[i])
-                while True:
-                    i += 1
-                    if lines[i].startswith('-'):
-                        break
-                    else:
-                        failures.append(lines[i])
-        else:
-            i += 1
+            else:
+                # annoying stdout got in the way so we need to search
+                (rt, j) = search_after_line(lines, i)
+                if rt == False:
+                    passes.append(lines[i] + " ... ok")
+                else:
+                    failures.append(lines[i] + " ... %s" % rt)
+                i = j
+        i += 1
 
 
 def run_unittests(dummy, dirname, files):
@@ -35,8 +49,12 @@ def run_unittests(dummy, dirname, files):
             testfile = os.path.abspath(os.path.join(dirname, f))
             print "running test %s" % testfile
             test = "%s -v" % testfile
-            output = utils.system_output(test, ignore_status=True)
-            parse_output(output)
+            result = utils.run(test, ignore_status=True)
+            output.write("==================================================\n")
+            output.write("Unittest file=%s\n" % testfile)
+            output.write("==================================================\n")
+            output.write(result.stderr)
+            parse_output(result.stderr)
 
 
 if __name__ == "__main__":
@@ -45,10 +63,20 @@ if __name__ == "__main__":
     else:
         start = root
 
+    # open a file for dumping the output
+    output = open(os.path.join(root, 'unittest_suite_output.txt'), 'w')
+
     os.path.walk(start, run_unittests, None)
 
     print "\n\n"
+    for line in passes:
+        print line
+
+    print "\n\n"
     if len(failures):
+        numfail = len(failures)
+        numpass = len(passes)
+        print "%s out of %s tests failed" % (numfail, numfail+numpass)
         print "Failures---------------------------------------------"
         for line in failures:
             print line
