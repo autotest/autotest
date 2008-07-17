85a2b280130560400393714a4f0014a2ce3d42bb
James Ren <jamesren@google.com>
2008-07-17 17:04:12 +0000

    Adding new columns "locked_by_id" and "lock_time" to the hosts table, to indicate who locked a host and when.
    Risk: low
    Visibility: low
    
    Signed-off-by: James Ren <jamesren@google.com>

---
+++ frontend/migrations/015_add_locked_by_and_lock_time.py
@@ -0,0 +1,12 @@
+def migrate_up(manager):
+    manager.execute("""ALTER TABLE hosts
+                       ADD COLUMN locked_by_id
+                       INT(11) DEFAULT NULL""")
+    manager.execute("""ALTER TABLE hosts
+                       ADD COLUMN lock_time
+                       DATETIME DEFAULT NULL""")
+
+
+def migrate_down(manager):
+    manager.execute('ALTER TABLE hosts DROP COLUMN locked_by')
+    manager.execute('ALTER TABLE hosts DROP COLUMN lock_time')
