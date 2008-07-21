fc5d36660ba601bf2fb9d1c3c38e896ef2b46bb7
Steve Howard <showard@google.com>
2008-07-21 20:23:02 +0000

    -unified code for table actions (select all/none and table-wide actions menu) between AFE and new TKO  -extracted non-TKO-specific code for ContextMenu into common.ui.ContextMenu  -moved TableActionsPanel to common.ui  -made JobListView use TableActionsPanel instead of its old private version of it  -made AfeClient module use standard.css stylesheet, since TableActionsPanel depends on it -added checkbox to host tables to enable/disable filtering on ACL-accessible hosts, and got rid of now-unnecessary RpcHostTable class

--- frontend/client/src/autotest/afe/HostTableDecorator.java
+++ frontend/client/src/autotest/afe/HostTableDecorator.java
@@ -3,22 +3,61 @@ package autotest.afe;
 import autotest.common.StaticDataRepository;
 import autotest.common.Utils;
 import autotest.common.table.BooleanFilter;
+import autotest.common.table.FieldFilter;
 import autotest.common.table.ListFilter;
 import autotest.common.table.SearchFilter;
 import autotest.common.table.TableDecorator;
 
 import com.google.gwt.json.client.JSONArray;
+import com.google.gwt.json.client.JSONValue;
+import com.google.gwt.user.client.ui.CheckBox;
+import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.ui.Widget;
 
-public class HostTableDecorator extends TableDecorator {
+class HostTableDecorator extends TableDecorator {
+    SearchFilter hostnameFilter;
+    LabelFilter labelFilter;
+    ListFilter statusFilter;
+    BooleanFilter lockedFilter;
+    AclAccessibleFilter aclFilter;
     
-    
-    protected SearchFilter hostnameFilter;
-    protected LabelFilter labelFilter;
-    protected ListFilter statusFilter;
-    protected BooleanFilter lockedFilter;
+    static class AclAccessibleFilter extends FieldFilter implements ClickListener {
+        private CheckBox checkBox = new CheckBox("ACL accessible only");
+        private JSONValue username;
+        
+        public AclAccessibleFilter() {
+            super("acl_group__users__login");
+            username = StaticDataRepository.getRepository().getData("user_login");
+            checkBox.addClickListener(this);
+        }
+        
+        public void onClick(Widget sender) {
+            notifyListeners();
+        }
+
+        @Override
+        public JSONValue getMatchValue() {
+            return username;
+        }
+
+        @Override
+        public Widget getWidget() {
+            return checkBox;
+        }
+
+        @Override
+        public boolean isActive() {
+            return checkBox.isChecked();
+        }
+        
+        public void setActive(boolean active) {
+            checkBox.setChecked(active);
+        }
+    }
     
     public HostTableDecorator(HostTable table, int rowsPerPage) {
         super(table);
+        table.sortOnColumn("Hostname");
         table.setRowsPerPage(rowsPerPage);
         addPaginators();
         
@@ -32,11 +71,13 @@ public class HostTableDecorator extends TableDecorator {
         statusFilter = new ListFilter("status");
         statusFilter.setChoices(statusStrings);
         lockedFilter = new BooleanFilter("locked");
+        aclFilter = new AclAccessibleFilter();
         
         addFilter("Hostname", hostnameFilter);
         addControl("Platform", labelFilter.getPlatformWidget());
         addFilter("Label", labelFilter);
         addFilter("Status", statusFilter);
         addFilter("Locked", lockedFilter);
+        addFilter("ACL accessible only", aclFilter);
     }
 }
