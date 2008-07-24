5d14d411293cfde020c0372e0dc308dad219cab2
James Ren <jamesren@google.com>
2008-07-24 16:35:02 +0000

    Added templates for 404 and 500 errors. 404 displays a generic "page not found" error. 500 displays the exception type and value, along with a traceback that can be revealed.
    Risk: low
    Visibility: medium (changing the look and feel of errors)
    
    Signed-off-by: James Ren <jamesren@google.com>

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,6 +3,8 @@ from django.conf import settings
 
 RE_PREFIX = '^' + settings.URL_PREFIX
 
+handler500 = 'frontend.afe.views.handler500'
+
 urlpatterns = patterns('',
     (RE_PREFIX + r'admin/', include('django.contrib.admin.urls')),
     (RE_PREFIX, include('frontend.afe.urls')),
