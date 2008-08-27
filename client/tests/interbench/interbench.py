77bee8f71cffb5bc21c9050c7cb447753c20aafa
Martin J. Bligh <mbligh@google.com>
2008-08-27 16:13:21 +0000

    make interbench use run_once

--- client/tests/interbench/interbench.py
+++ client/tests/interbench/interbench.py
@@ -8,6 +8,7 @@ class interbench(test.test):
 
     def initialize(self):
         self.job.require_gcc()
+        self.iteration = 0
 
 
     # http://www.kernel.org/pub/linux/kernel/people/ck/apps/interbench/interbench-0.30.tar.bz2
@@ -15,24 +16,12 @@ class interbench(test.test):
         tarball = utils.unmap_url(self.bindir, tarball, self.tmpdir)
         autotest_utils.extract_tarball_to_dir(tarball, self.srcdir)
         os.chdir(self.srcdir)
-
         utils.system('make')
 
 
-    def execute(self, iterations = 1, args = ''):
+    def run_once(self, args = ''):
         os.chdir(self.tmpdir)
         args += " -c"
-
-        profilers = self.job.profilers
-        if not profilers.only():
-            for i in range(iterations):
-                utils.system("%s/interbench -m 'run #%s' %s" % \
-                        (self.srcdir, i, args))
-
-        # Do a profiling run if necessary
-        if profilers.present():
-            profilers.start(self)
-            utils.system("%s/interbench -m 'profile run' %s" % \
-                    (self.srcdir, args))
-            profilers.stop(self)
-            profilers.report(self)
+        self.iteration += 1
+        utils.system("%s/interbench -m 'run #%s' %s" % (self.srcdir, 
+                                                        self.iteration, args))
