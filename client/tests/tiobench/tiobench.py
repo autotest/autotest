12b225b4a877d3f74c82de8adaa8c2c76e674e5f
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2008-08-28 18:19:55 +0000

    Converting tiobench test to run_once.
    Part of the patch series to make tests using profiling transparently
    
    Risk: low
    Visibility: low - users of tiobench test
    
    Signed-off-by: Lucas Meneghel Rodrigues <lucasmr@br.ibm.com>
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@2070 592f7852-d20e-0410-864c-8624ca9c26a4

--- client/tests/tiobench/tiobench.py
+++ client/tests/tiobench/tiobench.py
@@ -14,20 +14,20 @@ class tiobench(test.test):
 
         utils.system('make')
 
-    def execute(self, dir = None, iterations=1, args = None):
-        if not dir:
-            dir = self.tmpdir
-        os.chdir(self.srcdir)
-        if not args:
-            args = '--block=4096 --block=8192 --threads=10 --size=1024 --numruns=2'
-        profilers = self.job.profilers
-        if not profilers.only():
-            for i in range(iterations):
-                utils.system('./tiobench.pl --dir %s %s' %(dir, args))
 
-        # Do a profiling run if necessary
-        if profilers.present():
-            profilers.start(self)
-            utils.system('./tiobench.pl --dir %s %s' %(dir, args))
-            profilers.stop(self)
-            profilers.report(self)
+    def initialize(self):
+        self.job.require_gcc()
+
+
+    def run_once(self, dir = None, args = None):
+        if not dir:
+            self.dir = self.tmpdir
+        else:
+            self.dir = dir
+        if not args:
+            self.args = '--block=4096 --block=8192 --threads=10 --size=1024 --numruns=2'
+        else:
+            self.args = args
+
+        os.chdir(self.srcdir)
+        utils.system('./tiobench.pl --dir %s %s' %(self.dir, self.args))
