df7cb3f7ad17dcba2714cfcdd716e554fd71075f
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2008-08-28 17:36:21 +0000

    Convert sleeptest to use run_once
    git-svn-id: svn://test.kernel.org/autotest/trunk@2064 592f7852-d20e-0410-864c-8624ca9c26a4

--- client/tests/sleeptest/sleeptest.py
+++ client/tests/sleeptest/sleeptest.py
@@ -4,9 +4,5 @@ from autotest_lib.client.bin import test
 class sleeptest(test.test):
     version = 1
 
-    def execute(self, seconds = 1):
-        profilers = self.job.profilers
-        profilers.start(self)
+    def run_once(self, seconds=1):
         time.sleep(seconds)
-        profilers.stop(self)
-        profilers.report(self)
