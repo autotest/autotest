9fbd45dff0ea7759116a51f388f0709e757fa99e
Martin J. Bligh <mbligh@google.com>
2008-09-13 16:36:39 +0000

    Fix parallel_dd to clean up after itself
    Signed-off-by: Martin J. Bligh <mbligh@google.com>

--- client/tests/parallel_dd/parallel_dd.py
+++ client/tests/parallel_dd/parallel_dd.py
@@ -1,10 +1,10 @@
 import os, re, time, subprocess, sys
 from autotest_lib.client.bin import test, autotest_utils
-from autotest_lib.client.common_lib import utils
+from autotest_lib.client.common_lib import utils, error
 
 
 class parallel_dd(test.test):
-    version = 1
+    version = 2
 
     def initialize(self, fs, fstype = 'ext2', megabytes = 1000, streams = 2):
         self.megabytes = megabytes
@@ -14,13 +14,19 @@ class parallel_dd(test.test):
         self.fstype = fstype
         self.streams = streams
 
+        self.old_fstype = self._device_to_fstype('/etc/mtab')
+        if not self.old_fstype:
+            self.old_fstpye = self._device_to_fstype('/etc/fstab')
+        if not self.old_fstype:
+            self.old_fstype = self.fstype
+
         print 'Dumping %d megabytes across %d streams' % (megabytes, streams)
 
 
     def raw_write(self):
         print "Timing raw write of %d megabytes" % self.megabytes
         sys.stdout.flush()
-        dd = 'dd if=/dev/zero of=%s bs=4K count=%d' % \
+        dd = 'dd if=/dev/zero of=%s bs=4k count=%d' % \
                                         (self.fs.device, self.blocks)
         print dd
         utils.system(dd + ' > /dev/null')
@@ -29,7 +35,7 @@ class parallel_dd(test.test):
     def raw_read(self):
         print "Timing raw read of %d megabytes" % self.megabytes
         sys.stdout.flush()
-        dd = 'dd if=%s of=/dev/null bs=4K count=%d' % \
+        dd = 'dd if=%s of=/dev/null bs=4k count=%d' % \
                                         (self.fs.device, self.blocks)
         print dd
         utils.system(dd + ' > /dev/null')
@@ -41,7 +47,7 @@ class parallel_dd(test.test):
         for i in range(self.streams):
             file = 'poo%d' % (i+1)
             file = os.path.join(self.job.tmpdir, file)
-            dd = 'dd if=/dev/zero of=%s bs=4K count=%d' % \
+            dd = 'dd if=/dev/zero of=%s bs=4k count=%d' % \
                                     (file, self.blocks_per_file)
             print dd
             p.append(subprocess.Popen(dd + ' > /dev/null', shell=True))
@@ -58,12 +64,31 @@ class parallel_dd(test.test):
     def fs_read(self):
         for i in range(self.streams):
             file = os.path.join(self.job.tmpdir, 'poo%d' % (i+1))
-            dd = 'dd if=%s of=/dev/null bs=4K count=%d' % \
+            dd = 'dd if=%s of=/dev/null bs=4k count=%d' % \
                                     (file, self.blocks_per_file)
             utils.system(dd + ' > /dev/null')
 
 
+    def _device_to_fstype(self, file):
+        device = self.fs.device
+        try:
+            line = utils.system_output('egrep ^%s %s' % (device, file))
+            print line
+            fstype = line.split()[2]
+            print 'Found %s is type %s from %s' % (device, fstype, file)
+            return fstype
+        except error.CmdError, e:
+            print 'No %s found in %s' % (device, file)
+            return None
+
+
     def run_once(self):
+        try:
+            self.fs.unmount()
+        except error.CmdError, e:
+            pass
+
+        print '----------------- Timing raw operations ----------------------'
         start = time.time()
         self.raw_write()
         self.raw_write_rate = self.megabytes / (time.time() - start)
@@ -72,33 +97,33 @@ class parallel_dd(test.test):
         self.raw_read()
         self.raw_read_rate = self.megabytes / (time.time() - start)
 
+        # Set up the filesystem
         self.fs.mkfs(self.fstype)
         self.fs.mount()
+
+        print '----------------- Timing fs operations ----------------------'
         start = time.time()
-        try:
-            self.fs_write()
-        except:
-            try:
-                self.fs.unmount()
-            finally:
-                raise
-        self.fs.unmount()
+        self.fs_write()
         self.fs_write_rate = self.megabytes / (time.time() - start)
+        self.fs.unmount()
 
         self.fs.mount()
         start = time.time()
-        try:
-            self.fs_read()
-        except:
-            try:
-                self.fs.unmount()
-            finally:
-                raise
+        self.fs_read()
         self.fs_read_rate = self.megabytes / (time.time() - start)
-        self.fs.unmount()
         
         self.write_perf_keyval({
             'raw_write' : self.raw_write_rate,
             'raw_read'  : self.raw_read_rate,
             'fs_write'  : self.fs_write_rate,
-       	    'fs_read'   : self.fs_read_rate })
+            'fs_read'   : self.fs_read_rate })
+
+
+    def cleanup(self):
+        try:
+            self.fs.unmount()
+        except error.CmdError, e:
+            pass
+        print '\nFormatting %s back to type %s\n' % (self.fs, self.old_fstype)
+        self.fs.mkfs(self.old_fstype)
+        self.fs.mount()
