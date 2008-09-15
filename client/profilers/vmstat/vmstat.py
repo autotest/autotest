ab6039cd9c90ec700b971a32ba76d93c299ef6c6
John Admanski <jadmanski@google.com>
2008-09-15 15:44:17 +0000

    Clean up vmstat formatting.
    Signed-off-by: John Admanski <jadmanski@google.com>

--- client/profilers/vmstat/vmstat.py
+++ client/profilers/vmstat/vmstat.py
@@ -1,19 +1,23 @@
-# sets up a subprocess to cat a file on a specified interval
-# really, really ought to autoswitch on a list of files or individual file
+"""
+Runs vmstat X where X is the interval in seconds
+
+Defaults options:
+job.profilers.add('vmstat', interval=1)
+"""
 import profiler,time,os,subprocess
 
 class vmstat(profiler.profiler):
     version = 1
 
-    def initialize(self, interval=1):
+    def initialize(self, interval = 1):
         self.interval = interval
 
 
     def start(self, test):
         cmd = "/usr/bin/vmstat %d" % self.interval
         logfile = open(os.path.join(test.profdir, "vmstat"), 'w')
-        p = subprocess.Popen(cmd, shell=True, stdout=logfile,
-                                                       stderr=subprocess.STDOUT)
+        p = subprocess.Popen(cmd, shell=True, stdout=logfile, \
+                                        stderr=subprocess.STDOUT)
         self.pid = p.pid
 
 
