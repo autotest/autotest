18fea7ad46064344db8aab5cd0ab95ea9e36bfbe
John Admanski <jadmanski@google.com>
2008-10-03 16:15:59 +0000

    Fix up a whole ton of test documentation, as well as some minor style changes. Also adds a few missing self.job.require_gcc() calls, etc.
    Signed-off-by: John Admanski <jadmanski@google.com>

--- client/tests/raisetest/raisetest.py
+++
@@ -1,9 +0,0 @@
-from autotest_lib.client.bin import test
-from autotest_lib.client.common_lib import error
-
-
-class raisetest(test.test):
-    version = 1
-
-    def execute(self):
-        raise error.TestError('Arrrrrrrrggggh. You are DOOOMED')
