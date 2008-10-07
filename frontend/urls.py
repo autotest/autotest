38e5c3b3bb7633bd7777a9e790eabedbcbc4348f
Steve Howard <showard@google.com>
2008-10-07 11:06:17 +0000

    add debug urlconf to frontend/urls.py to make development shell work smoother

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,7 +5,19 @@ RE_PREFIX = '^' + settings.URL_PREFIX
 
 handler500 = 'frontend.afe.views.handler500'
 
-urlpatterns = patterns('',
+pattern_list = (
     (RE_PREFIX + r'admin/', include('django.contrib.admin.urls')),
     (RE_PREFIX, include('frontend.afe.urls')),
 )
+
+debug_pattern_list = (
+    # redirect /tko and /results to local apache server
+    (r'^(?P<path>(tko|results)/.*)$',
+     'frontend.afe.views.redirect_with_extra_data',
+     {'url': 'http://%(server_name)s/%(path)s?%(getdata)s'}),
+)
+
+if settings.DEBUG:
+  pattern_list += debug_pattern_list
+
+urlpatterns = patterns('', *pattern_list)
