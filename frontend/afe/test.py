a4d1b041c12927f3bddbda35e560722d91706a84
showard <showard@592f7852-d20e-0410-864c-8624ca9c26a4>
2008-10-08 19:15:45 +0000

    -refactor frontend/afe/test.py to make it reusable by new_tko -make frontend_unittest.py use code in django_test_utils instead of its own copy
    git-svn-id: svn://test.kernel.org/autotest/trunk@2255 592f7852-d20e-0410-864c-8624ca9c26a4

--- frontend/afe/test.py
+++ frontend/afe/test.py
@@ -1,7 +1,7 @@
 import common
 import os, doctest, glob, sys
 import django.test.utils, django.test.simple
-from autotest_lib.frontend import settings, afe
+from django.conf import settings
 
 # doctest takes a copy+paste log of a Python interactive session, runs a Python
 # interpreter, and replays all the inputs from the log, checking that the
@@ -14,54 +14,65 @@ from autotest_lib.frontend import settings, afe
 # We use django.test.utils to run the tests against a fresh test database every
 # time.
 
-app_dir = os.path.dirname(__file__)
-doctest_dir = os.path.join(app_dir, 'doctests')
-doctest_paths = [os.path.join(doctest_dir, filename) for filename
-                 in os.listdir(doctest_dir)
-                 if not filename.startswith('.')
-                 if not filename.endswith('~')]
-doctest_paths.sort()
+class DoctestRunner(object):
+    _PRINT_AFTER = 'Ran %d tests from %s'
 
-def get_modules():
-    modules = []
-    module_names = [os.path.basename(filename)[:-3]
-                    for filename in glob.glob(os.path.join(app_dir, '*.py'))
-                    if '__init__' not in filename
-                    and 'test.py' not in filename]
-    for module_name in module_names:
-        mod = common.setup_modules.import_module(module_name, 'frontend.afe')
-        modules.append(mod)
-    return modules
+    def __init__(self, app_dir, app_module_name):
+        self._app_dir = app_dir
+        self._app_module_name = app_module_name
 
 
-print_after = 'Ran %d tests from %s'
+    def _get_doctest_paths(self):
+        doctest_dir = os.path.join(self._app_dir, 'doctests')
+        doctest_paths = [os.path.join(doctest_dir, filename) for filename
+                         in os.listdir(doctest_dir)
+                         if not filename.startswith('.')
+                         if not filename.endswith('~')]
+        return sorted(doctest_paths)
 
 
-def run_tests(module_list=[], verbosity=1):
-    """
-    module_list is ignored - we're just required to have this signature as a
-    Django test runner.
-    """
-    modules = get_modules()
-    total_errors = 0
-    old_db = settings.DATABASE_NAME
-    django.test.utils.setup_test_environment()
-    django.test.utils.create_test_db(verbosity)
-    try:
-        for module in modules:
-            failures, test_count = doctest.testmod(module)
-            print print_after % (test_count, module.__name__)
-            total_errors += failures
-        for path in doctest_paths:
-            failures, test_count = doctest.testfile(path, module_relative=False)
-            print print_after % (test_count, path)
-            total_errors += failures
-    finally:
-        django.test.utils.destroy_test_db(old_db)
-        django.test.utils.teardown_test_environment()
-    print
-    if total_errors == 0:
-        print 'OK'
-    else:
-        print 'FAIL: %d errors' % total_errors
-    return total_errors
+    def _get_modules(self):
+        modules = []
+        module_names = [os.path.basename(filename)[:-3]
+                        for filename
+                        in glob.glob(os.path.join(self._app_dir, '*.py'))
+                        if '__init__' not in filename
+                        and 'test.py' not in filename]
+        # TODO: use common.setup_modules.import_module()
+        app_module = __import__(self._app_module_name, globals(), locals(),
+                                module_names)
+        for module_name in module_names:
+            modules.append(getattr(app_module, module_name))
+        return modules
+
+
+    def run_tests(self):
+        """
+        module_list is ignored - we're just required to have this signature as a
+        Django test runner.
+        """
+        doctest_paths = self._get_doctest_paths()
+        modules = self._get_modules()
+        total_errors = 0
+        old_db = settings.DATABASE_NAME
+        django.test.utils.setup_test_environment()
+        django.test.utils.create_test_db()
+        try:
+            for module in modules:
+                failures, test_count = doctest.testmod(module)
+                print self._PRINT_AFTER % (test_count, module.__name__)
+                total_errors += failures
+            for path in doctest_paths:
+                failures, test_count = doctest.testfile(path,
+                                                        module_relative=False)
+                print self._PRINT_AFTER % (test_count, path)
+                total_errors += failures
+        finally:
+            django.test.utils.destroy_test_db(old_db)
+            django.test.utils.teardown_test_environment()
+        print
+        if total_errors == 0:
+            print 'OK'
+        else:
+            print 'FAIL: %d errors' % total_errors
+        return total_errors
