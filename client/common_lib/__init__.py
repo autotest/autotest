52eeed7bc270955a83d0514073a05d492894878a
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2008-10-09 19:46:13 +0000

    Adding a library with debug functions to client/common_lib to help with the logging process.
    This library has functions to read and write information on a file,
    debug.ini. This file is supposed to contain debugging information for
    the 3 autotest code sessions:
    
     * Server
     * Client
     * Tests
    
    So we can choose different levels of logging for each one of these
    levels. This patch is supposed to be applied after the patch that
    renames the autotest logger module to log.
    
    This patch addresses the comments made by Steve:
    
     * Now we parse the configuration file once. Steve, I really did try to
    implement the lazy load like you described, but I just couldn't get it
    to work. So I represented the debug parser as a class and used a design
    patterns that ensures that we read the parser only once at each autotest
    or autoserv execution.
     * No more delegates, just a function that returns the root logger
    properly configured (even though I still think the delegates would be
    useful convenience functions).
    
    I hope it looks good now. I've tested and it produces the wanted
    results. I'll send a last patch with some suggestions on how to use the
    library and make the verbosity on a default autotest/autoserv run a lot
    smaller, with only 2 print conversions.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lucasmr@br.ibm.com>
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@2260 592f7852-d20e-0410-864c-8624ca9c26a4

--- client/common_lib/__init__.py
+++ client/common_lib/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ['error', 'log', 'barrier', 'check_version', 'test', 'utils',
-           'global_config', 'mail']
+           'global_config', 'mail', 'debug']
 
 import site_libraries
 __all__.extend(site_libraries.libraries)
