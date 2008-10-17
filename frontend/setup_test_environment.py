2567197deb06bbd144847d4f862e9cb72ff36b1e
Steve Howard <showard@google.com>
2008-10-17 02:01:16 +0000

    Refactor the basic environment setup code out of django_test_utils.py into setup_django_environment.py, and rename django_test_utils.py to setup_test_environment.py.  Also changed the environment setup code to run at import time.  This makes it easy for scripts, both test and non-test, to use Django models without running through manage.py.  The idea is that scripts will import setup_django_environment before importing Django code (somewhat akin to common.py), and test code will subsequently import setup_test_environment.

--- frontend/setup_test_environment.py
+++ frontend/setup_test_environment.py
@@ -1,22 +1,19 @@
 import tempfile, shutil, os
 from django.core import management
 from django.conf import settings
-# can't import any django code that depends on the environment setup
 import common
 
-def setup_test_environ():
-    from autotest_lib.frontend import settings
-    management.setup_environ(settings)
-    from django.conf import settings
-    # django.conf.settings.LazySettings is buggy and requires us to get
-    # something from it before we set stuff on it
-    getattr(settings, 'DATABASE_ENGINE')
-    settings.DATABASE_ENGINE = 'sqlite3'
-    settings.DATABASE_NAME = ':memory:'
+# we need to set DATABASE_ENGINE now, at import time, before the Django database
+# system gets initialized.
+# django.conf.settings.LazySettings is buggy and requires us to get something
+# from it before we set stuff on it.
+getattr(settings, 'DATABASE_ENGINE')
+settings.DATABASE_ENGINE = 'sqlite3'
+settings.DATABASE_NAME = ':memory:'
 
+from django.db import connection
 
 def set_test_database(database):
-    from django.db import connection
     settings.DATABASE_NAME = database
     connection.close()
 
@@ -29,7 +26,6 @@ def backup_test_database():
 
 
 def restore_test_database(backup_path):
-    from django.db import connection
     connection.close()
     shutil.copyfile(backup_path, settings.DATABASE_NAME)
 
