3d3c0815a5bb4a75d31c26f80c5f038d59bde054
Jeremy Orlow <jorlow@google.com>
2008-11-05 21:47:54 +0000

    The server side sleeptest should be importing server.test not client.test!
    Signed-off-by: Jeremy Orlow <jorlow@google.com>

--- server/tests/sleeptest/sleeptest.py
+++ server/tests/sleeptest/sleeptest.py
@@ -1,8 +1,8 @@
 import time
-from autotest_lib.client.bin import test
+from autotest_lib.server import test
 
 class sleeptest(test.test):
     version = 1
 
-    def execute(self, seconds = 1):
+    def execute(self, seconds=1):
         time.sleep(seconds)
