837c2b36f1160d5d33904ef2f87d839bf94c2261
jadmanski <jadmanski@592f7852-d20e-0410-864c-8624ca9c26a4>
2008-11-19 17:10:32 +0000

    This depends on Martin's "run autoserv without a results dir" patch to be applied cleanly.
    A rather large refactoring and stubbing change working towards
    client-side profilers from the server side. This makes a few major
    changes:
      - refactors almost all of client/bin/profilers.py into a common lib
        profiler_manager class; except for the code for actually loading
        a profiler object, all of that code is already completely generic
      - add a server-side profiler_proxy class that will act as a proxy
        on the server for using a client-side profiler, this doesn't
        actually do anything useful right now but it basically just a
        stub for later changes
      - add a server-side profiler_manager implementation that creates a
        profiler_proxy class instead of actually loading a real profiler
    
    The intended changes still in the pipeline that will build on this are:
      - add code to the profiler_proxy for actually making sure the
        profiler is set up and installed on the remote host(s)
      - add a mechanism for client-side profilers to deal with reboots
    
    Risk: Medium
    Visibility: Adds a bunch of stubs that don't actually do anything yet
    but will do things soon.
    
    Signed-off-by: John Admanski <jadmanski@google.com>
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@2447 592f7852-d20e-0410-864c-8624ca9c26a4

--- client/common_lib/profiler_manager_unittest.py
+++ client/common_lib/profiler_manager_unittest.py
@@ -2,7 +2,7 @@
 
 import unittest
 import common
-from autotest_lib.client.bin import profilers
+from autotest_lib.client.common_lib import profiler_manager
 
 
 # simple job stub for using in tests
@@ -24,45 +24,40 @@ class stub_profiler(object):
         cls.started -= 1
 
 
-# replace profilers._load_profiler with a simple stub
-def stub_load(p):
-    def _load_profiler(profiler, args, dargs):
+# replace profiler_manager.load_profiler with a simple stub
+class stub_manager(profiler_manager.profiler_manager):
+    def load_profiler(self, profiler, args, dargs):
         return stub_profiler(profiler)
-    p._load_profiler = _load_profiler
 
 
-class TestProfilers(unittest.TestCase):
+class TestProfilerManager(unittest.TestCase):
     def test_starts_with_no_profilers(self):
-        p = profilers.profilers(stub_job)
+        p = stub_manager(stub_job)
         self.assertEqual(set(), p.current_profilers())
 
 
     def test_single_add(self):
-        p = profilers.profilers(stub_job)
-        stub_load(p)
+        p = stub_manager(stub_job)
         p.add("prof1")
         self.assertEqual(set(["prof1"]), p.current_profilers())
 
 
     def test_duplicate_adds(self):
-        p = profilers.profilers(stub_job)
-        stub_load(p)
+        p = stub_manager(stub_job)
         p.add("prof1")
         p.add("prof1")
         self.assertEqual(set(["prof1"]), p.current_profilers())
 
 
     def test_multiple_adds(self):
-        p = profilers.profilers(stub_job)
-        stub_load(p)
+        p = stub_manager(stub_job)
         p.add("prof1")
         p.add("prof2")
         self.assertEqual(set(["prof1", "prof2"]), p.current_profilers())
 
 
     def test_add_and_delete(self):
-        p = profilers.profilers(stub_job)
-        stub_load(p)
+        p = stub_manager(stub_job)
         p.add("prof1")
         p.add("prof2")
         p.delete("prof1")
@@ -70,28 +65,25 @@ class TestProfilers(unittest.TestCase):
 
 
     def test_present_with_no_profilers(self):
-        p = profilers.profilers(stub_job)
+        p = stub_manager(stub_job)
         self.assertEqual(False, p.present())
 
 
     def test_present_after_add(self):
-        p = profilers.profilers(stub_job)
-        stub_load(p)
+        p = stub_manager(stub_job)
         p.add("prof1")
         self.assertEqual(True, p.present())
 
 
     def test_present_after_add_and_remove(self):
-        p = profilers.profilers(stub_job)
-        stub_load(p)
+        p = stub_manager(stub_job)
         p.add("prof1")
         p.delete("prof1")
         self.assertEqual(False, p.present())
 
 
     def test_started(self):
-        p = profilers.profilers(stub_job)
-        stub_load(p)
+        p = stub_manager(stub_job)
         p.add("prof1")
         p.add("prof2")
         started = stub_profiler.started
@@ -102,8 +94,7 @@ class TestProfilers(unittest.TestCase):
 
 
     def test_stop(self):
-        p = profilers.profilers(stub_job)
-        stub_load(p)
+        p = stub_manager(stub_job)
         p.add("prof1")
         p.add("prof2")
         started = stub_profiler.started
---
+++ server/profilers.py
@@ -0,0 +1,13 @@
+import os, sys
+import common
+
+from autotest_lib.client.common_lib import utils, packages, profiler_manager
+from autotest_lib.server import profiler
+
+
+class profilers(profiler_manager.profiler_manager):
+    def load_profiler(self, profiler_name, args, dargs):
+        newprofiler = profiler.profiler_proxy(self.job, profiler_name)
+        newprofiler.initialize(*args, **dargs)
+        newprofiler.setup(*args, **dargs) # lazy setup is done client-side
+        return newprofiler
