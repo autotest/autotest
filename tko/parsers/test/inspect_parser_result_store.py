a9e8c1f1815669c6cfbe501ac01e95ab173d46be
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2008-11-27 00:41:54 +0000

    Add option to inspect script to open store with write access. Also fix timezone diff problem between results store and parser harness.
    Signed-off-by: Cary Hull <chull@google.com>
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@2521 592f7852-d20e-0410-864c-8624ca9c26a4

--- tko/parsers/test/inspect_parser_result_store.py
+++ tko/parsers/test/inspect_parser_result_store.py
@@ -13,6 +13,8 @@ from autotest_lib.tko.parsers.test import scenario_base
 
 usage = 'usage: %prog [options] scenario_dirpath'
 parser = optparse.OptionParser(usage=usage)
+parser.add_option("-w", action="store_true", dest="open_for_write")
+
 (options, args) = parser.parse_args()
 if len(args) < 1:
     parser.print_help()
@@ -24,4 +26,5 @@ if not path.exists(scenario_dirpath) or not path.isdir(scenario_dirpath):
     parser.print_help()
     sys.exit(1)
 
-sto = scenario_base.load_parser_result_store(scenario_dirpath, 'c')
+sto = scenario_base.load_parser_result_store(
+    scenario_dirpath, options.open_for_write)
