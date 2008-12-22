73019c8c5a5b9e73c5147309d609f03c0967a6bd
Andrew G. Morgan <agm@google.com>
2008-12-22 14:51:48 +0000

    Update libcap to 2.16.
    The update ensures that we don't need to apply a patch before building
    the library (without file-setting capability support). Its also more
    robust in the face of apparently 'really old' versions of sed.
    
    Reviewed-by: Martin Bligh <mbligh@google.com>
    Signed-off-by: Andrew G. Morgan <agm@google.com>

--- client/deps/libcap/libcap.py
+++ client/deps/libcap/libcap.py
@@ -4,17 +4,14 @@ import os, common
 from autotest_lib.client.common_lib import utils
 from autotest_lib.client.bin import autotest_utils
 
-version = 1
+version = 2
 
-def setup(srcdir, tarball='libcap-2.14.tar.gz'):
+def setup(srcdir, tarball='libcap-2.16.tar.gz'):
     topdir = os.getcwd()
     autotest_utils.extract_tarball_to_dir(tarball, srcdir)
-    os.chdir(os.path.join(srcdir, 'libcap'))
-    utils.system('patch -p0 < ../../libcap.patch')
     os.chdir(srcdir)
-    utils.system('make -C libcap')
+    utils.system('make -C libcap LIBATTR=no')
     os.chdir(topdir)
-
 
 srcdir = os.path.abspath('./src')
 utils.update_version(srcdir, False, version, setup, srcdir)
