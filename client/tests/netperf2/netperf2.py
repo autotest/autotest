6c2b503fcd81b4b3ce49fe998ec9bc92fb4b1117
Martin J. Bligh <mbligh@google.com>
2009-01-13 19:31:08 +0000

    Fix broken imports on netperf2
    Summary: The client/bin/autotest_utils.py rename required a fairly large
    patch to fix out the tests due to the API breakage. This patch fixes a
    broken import leftover from the process.
    
    Visibility: Small (only users of the netperf2 test)
    
    Impact: Small (only users of the netpef2 test, small fix)
    
    From: lmr

--- client/tests/netperf2/netperf2.py
+++ client/tests/netperf2/netperf2.py
@@ -1,6 +1,6 @@
 import os,time
-from autotest_lib.client.bin import test, autotest_utils
-from autotest_lib.client.common_lib import utils, error
+from autotest_lib.client.bin import test, utils
+from autotest_lib.client.common_lib import error
 
 
 class netperf2(test.test):
