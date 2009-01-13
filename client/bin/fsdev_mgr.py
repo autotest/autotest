53f43857fa92d94b8abaa66369b1c008f14c9b7a
Mihai Rusu <dizzy@google.com>
2009-01-13 00:37:11 +0000

    Created a client/common_lib/utils/import_site_class function to take care of site specific importing of classes. Modified code to use it.
    Signed-off-by: Mihai Rusu <dizzy@google.com>

--- client/bin/fsdev_mgr.py
+++ client/bin/fsdev_mgr.py
@@ -6,6 +6,8 @@ implementation of the 'fsdev' helper API; site specific extensions
 to any of these methods should inherit this class.
 """
 
+from autotest_lib.client.bin import utils
+
 class BaseFsdevManager(object):
 
     def __init__(self):
@@ -21,17 +23,10 @@ class BaseFsdevManager(object):
         return partname
 
 
-# site_fsdev.py may be non-existent or empty - make sure that an appropriate
-# SiteFsdevManager class is created irregardless
-try:
-    from site_fsdev import SiteFsdevManager
-except ImportError:
-    # No SiteFsdevManager class defined - supply an empty one, then
-    class SiteFsdevManager(BaseFsdevManager):
-        pass
+SiteFsdevManager = utils.import_site_class(
+    __file__, "autotest_lib.client.bin.site_fsdev", "SiteFsdevManager",
+    BaseFsdevManager)
 
 # Wrap whatever SiteFsdevManager class we've found above in a class
 class FsdevManager(SiteFsdevManager):
-    def __init__(self):
-        SiteFsdevManager.__init__(self)
-
+    pass
--- client/bin/sysinfo.py
+++ client/bin/sysinfo.py
@@ -1,14 +1,9 @@
+from autotest_lib.client.common_lib import utils
 from autotest_lib.client.bin import base_sysinfo
-try:
-    from autotest_lib.client.bin import site_sysinfo
-except ImportError:
-    # no site_sysinfo, just make a class using the base version
-    class sysinfo(base_sysinfo.base_sysinfo):
-        pass
-else:
-    # otherwise, use the site version (should inherit from the base)
-    class sysinfo(site_sysinfo.site_sysinfo):
-        pass
+
+sysinfo = utils.import_site_class(__file__,
+                                  "autotest_lib.client.bin.site_sysinfo",
+                                  "site_sysinfo", base_sysinfo.base_sysinfo)
 
 # pull in some data stucture stubs from base_sysinfo, for convenience
 logfile = base_sysinfo.logfile
