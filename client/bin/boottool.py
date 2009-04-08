ac44ecffbf9e0f42537c317101100d207e19ffe0
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2009-04-08 20:54:46 +0000

    Remove duplicate kernel boot parameters given to boottool.add_kernel.
    Signed-off-by: Mihai Rusu <dizzy@google.com>
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@2974 592f7852-d20e-0410-864c-8624ca9c26a4

--- client/bin/boottool.py
+++ client/bin/boottool.py
@@ -16,6 +16,28 @@ class boottool(object):
             raise error.AutotestError('Failed to set boottool_exec')
 
 
+    def _remove_duplicate_args(self, args):
+        """
+        Remove the duplicate entries in args making sure that the first
+        duplicate occurances are the ones removed and the last one remains
+        (this is in order to not change the semantics of the "console"
+        parameter where the last occurance has special meaning)
+
+        @param args: a space separate list of kernel boot parameters
+            (ex. 'console=ttyS0,57600n8 nmi_watchdog=1')
+        @return: a space separated list of kernel boot parameters without
+            duplicates
+        """
+        copied = set()
+        new_args = []
+
+        for arg in reversed(args.split()):
+            if arg not in copied:
+                new_args.insert(0, arg)
+                copied.add(arg)
+        return ' '.join(new_args)
+
+
     def run_boottool(self, params):
         return utils.system_output('%s %s' % (self.boottool_exec, params))
 
@@ -27,15 +49,15 @@ class boottool(object):
     def architecture(self):
         return self.run_boottool('--arch-probe')
 
-        
+
     def __get_key_values_from_entries(self, key):
-        """ 
+        """
         Helper method to get key values from boot entries
-        
-            @param key: one of the keys in boot entry. 
+
+            @param key: one of the keys in boot entry.
                          e.g. index, title, args, kernel, root
         """
-        
+
         regex = re.compile('^%s\s*:\s*(.*)' % key)
         lines = self.run_boottool('--info all').splitlines()
         key_values = []
@@ -43,10 +65,10 @@ class boottool(object):
             match = regex.match(line)
             if match:
                 key_values.append(match.group(1))
-                
+
         return key_values
-    
-    
+
+
     def get_titles(self):
         """ Returns a list of boot entries titles """
         return self.__get_key_values_from_entries("title")
@@ -56,7 +78,7 @@ class boottool(object):
         """ Returns a list of boot entries indexes """
         return self.__get_key_values_from_entries("index")
 
-    
+
     def get_default_title(self):
         default = int(self.get_default())
         return self.get_titles()[default]
@@ -80,18 +102,18 @@ class boottool(object):
 
 
     def print_entry(self, index):
-        """ 
-        Prints entry to stdout as returned by perl boottool 
-        
+        """
+        Prints entry to stdout as returned by perl boottool
+
             @deprecated: use get_entry instead
         """
         print self.run_boottool('--info=%s' % index)
 
 
     def get_entries(self):
-        """ 
-        Get all entries 
-        
+        """
+        Get all entries
+
             @rtype: dict
         """
         raw = "\n" + self.run_boottool('--info all')
@@ -104,15 +126,15 @@ class boottool(object):
 
         return entries
 
-            
+
     def get_entry(self, kernel):
-        """ 
-        Get entry 
-        
+        """
+        Get entry
+
         NOTE: if entry is "fallback" and bootloader is grub
         use index instead of kernel title ("fallback") as fallback is
         a special option in grub
-        
+
             @param kernel: can be a position number (index) or title
             @rtype: dict
         """
@@ -133,7 +155,7 @@ class boottool(object):
             name = line[:line.find(":")]
             value = line[line.find(":") + 1:]
             entry[name.strip()] = value.strip()
-        
+
         return entry
 
 
@@ -158,13 +180,13 @@ class boottool(object):
 
 
     def add_args(self, kernel, args):
-        """ 
+        """
         Add specified argument
-        
+
             @param kernel: can be a position number (index) or title
             @param args: argument to be added to the current list of args
         """
-        
+
         parameters = '--update-kernel="%s" --args="%s"' % (kernel, args)
 
         #add parameter if this is a Xen entry
@@ -179,13 +201,13 @@ class boottool(object):
 
 
     def remove_args(self, kernel, args):
-        """ 
+        """
         Removes specified argument
-        
+
             @param kernel: can be a position number (index) or title
             @param args: argument to be removed of the current list of args
         """
-        
+
         parameters = '--update-kernel="%s" --remove-args="%s"' % (kernel, args)
 
         #add parameter if this is a Xen entry
@@ -202,7 +224,7 @@ class boottool(object):
 
     def add_kernel(self, path, title='autotest', initrd='', xen_hypervisor='',
                    args=None, root=None, position='end', force=False):
-        
+
         parameters = ""
         if force:
             parameters += " --force "
@@ -220,7 +242,7 @@ class boottool(object):
                 parameters += ' --xenhyper=%s' % xen_hypervisor
 
         if args:
-            parameters += ' --args="%s"' % args
+            parameters += ' --args="%s"' % self._remove_duplicate_args(args)
         if root:
             parameters += ' --root="%s"' % root
         if position:
