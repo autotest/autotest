1616f975de43c7cbf9847a857530d9c5889254bd
Anton Blanchard <anton@samba.org>
2009-04-17 22:11:37 +0000

    Add dejagnu as a dependency
    Signed-off-by: Anton Blanchard <anton@samba.org>

---
+++ client/deps/dejagnu/dejagnu.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import os
+import common
+from autotest_lib.client.bin import utils
+
+version = 1
+
+def setup(tarball, topdir):
+    srcdir = os.path.join(topdir, 'src')
+    utils.extract_tarball_to_dir(tarball, 'src')
+    os.chdir(srcdir)
+    utils.system ('./configure --prefix=%s/dejagnu' % topdir)
+    utils.system('make')
+    utils.system('make install')
+
+    os.chdir(topdir)
+
+pwd = os.getcwd()
+# http://ftp.gnu.org/pub/gnu/dejagnu/dejagnu-1.4.4.tar.gz
+tarball = os.path.join(pwd, 'dejagnu-1.4.4.tar.bz2')
+utils.update_version(pwd+'/src', False, version, setup, tarball, pwd)
