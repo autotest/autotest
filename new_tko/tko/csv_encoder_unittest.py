ae41209d660dac88f2d412ba7395c07a146d6a08
Steve Howard <showard@google.com>
2009-05-08 02:30:20 +0000

    CSV support for TKO table view. * make RpcDataSource record the last RPC call params, since that's much easier than reconstructing it as I was doing for SpreadsheetVIew * make TableDecorator support an Export CSV link, and make TableView use it * add onExportCsv to Table View, plus some minor refactorings there that happened along the way * generalize CSV code from SpreadsheetView and extract to TkoUtils * add new CsvEncoder classes for table view * change logic to decide which CsvEncoder to use --  simply dispatching off of method name isn't good enough, since both spreadsheet and table views use get_status_counts * add new unit tests for the three kinds of table view
    Signed-off-by: Steve Howard <showard@google.com>

--- new_tko/tko/csv_encoder_unittest.py
+++ new_tko/tko/csv_encoder_unittest.py
@@ -7,8 +7,11 @@ from autotest_lib.frontend import setup_test_environment
 from autotest_lib.new_tko.tko import csv_encoder
 
 class CsvEncodingTest(unittest.TestCase):
-    def _make_request(self, method):
-        return dict(method=method)
+    def _make_request(self, method, columns=None):
+        request = dict(method=method)
+        if columns:
+            request['columns'] = columns
+        return request
 
 
     def _make_group(self, header_indices, pass_count, complete_count,
@@ -18,10 +21,11 @@ class CsvEncodingTest(unittest.TestCase):
                     incomplete_count=incomplete_count)
 
 
-    def _encode_and_check_result(self, request, result, expected_csv):
+    def _encode_and_check_result(self, request, result, *expected_csv_rows):
         encoder = csv_encoder.encoder(request, result)
         response = encoder.encode()
         csv_result = response.content
+        expected_csv = '\r\n'.join(expected_csv_rows) + '\r\n'
         self.assertEquals(csv_result, expected_csv)
 
 
@@ -35,17 +39,61 @@ class CsvEncodingTest(unittest.TestCase):
                                 self._make_group((1, 2), 3, 4, 5)]}
 
         self._encode_and_check_result(request, response,
-                                      ',col1/sub1,col1/sub2,col2/sub1\r\n'
-                                      'row1,1 / 2,,\r\n'
-                                      'row2,,,3 / 4 (5 incomplete)\r\n'
-                                      '"comma,header",,,\r\n')
+                                      ',col1/sub1,col1/sub2,col2/sub1',
+                                      'row1,1 / 2,,',
+                                      'row2,,,3 / 4 (5 incomplete)',
+                                      '"comma,header",,,')
+
+
+    def test_table_encoder(self):
+        request = self._make_request('get_test_views', [['col1', 'Column 1'],
+                                                        ['col2', 'Column 2']])
+        response = [{'col1' : 'foo', 'col2' : 'bar'},
+                    {'col1' : 'baz', 'col2' : 'asdf'}]
+        self._encode_and_check_result(request, response,
+                                      'Column 1,Column 2',
+                                      'foo,bar',
+                                      'baz,asdf')
+
+
+    def test_grouped_table_encoder(self):
+        request = self._make_request('get_group_counts',
+                                     [['col1', 'Column 1'],
+                                      ['group_count', 'Count in group']])
+        response = {'header_values' : 'unused',
+                    'groups' : [{'col1' : 'foo', 'group_count' : 1},
+                                {'col1' : 'baz', 'group_count' : 3}]}
+        self._encode_and_check_result(request, response,
+                                      'Column 1,Count in group',
+                                      'foo,1',
+                                      'baz,3')
+
+
+    def _status_count_dict(self, col1_value, pass_count, complete_count,
+                                  incomplete_count):
+        return dict(col1=col1_value, pass_count=pass_count,
+                    complete_count=complete_count,
+                    incomplete_count=incomplete_count)
+
+
+    def test_status_count_table_encoder(self):
+        request = self._make_request('get_status_counts',
+                                     [['col1', 'Column 1'],
+                                      ['_unused_', 'Test pass rate']])
+        response = {'header_values' : 'unused',
+                    'groups' : [self._status_count_dict('foo', 1, 2, 0),
+                                self._status_count_dict('baz', 4, 5, 6)]}
+        self._encode_and_check_result(request, response,
+                                      'Column 1,Test pass rate',
+                                      'foo,1 / 2',
+                                      'baz,4 / 5 (6 incomplete)')
 
 
     def test_unhandled_method(self):
         request = self._make_request('foo')
         self._encode_and_check_result(request, None,
                                       'Unhandled method foo (this indicates a '
-                                      'bug)\n')
+                                      'bug)')
 
 
 if __name__ == '__main__':
