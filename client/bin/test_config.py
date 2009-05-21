c582a4f7bb9f4705bdd50115a4182113b6459c98
Martin J. Bligh <mbligh@google.com>
2009-05-21 01:41:29 +0000

    Hi Team,
    I wrote some methods that extends the test_config.py. These methods are
    to check the parameters in a config file. 
    
    Today we did not had this functionality in autotest and need write some
    functions inside our test to check if the test got a valid ip address
    from config file or if the parameter got was a string or int.
    
    To use this proposal, we will use a new format to describe the
    parameters, this format is type.parameter, example:
    
    ib.server = 192.168.1.1
    str.password = mypass
    int.mtu = 2048
    
    This method can be extended to support new types, helping in the test
    development, because the developer only needs use the method
    test_cfg.check() to validate the config file, example:
    
    test_cfg.check('test config section')
    
    Regards,

--- client/bin/test_config.py
+++ client/bin/test_config.py
@@ -7,7 +7,7 @@ __author__ = 'rsalveti@linux.vnet.ibm.com (Ricardo Salveti de Araujo)'
 from ConfigParser import ConfigParser
 from StringIO import StringIO
 from os import path
-import types
+import types, re, string
 from autotest_lib.client.common_lib import utils
 
 __all__ = ['config_loader']
@@ -89,3 +89,82 @@ class config_loader:
             self.parser.write(fileobj)
         finally:
             fileobj.close()
+
+
+    def check(self, section):
+        """
+        Check if the config file has valid values
+        """
+        if not self.parser.has_section(section):
+            return False, "Section not found: %s"%(section)
+
+        options = self.parser.items(section)
+        for i in range(options.__len__()):
+            param = options[i][0]
+            aux = string.split(param, '.')
+
+            if aux.__len__ < 2:
+                return False, "Invalid parameter syntax at %s"%(param)
+
+            if not self.check_parameter(aux[0], options[i][1]):
+                return False, "Invalid value at %s"%(param)
+
+        return True, None
+
+
+    def check_parameter(self, param_type, parameter):
+        """
+        Check if a option has a valid value
+        """
+        if parameter == '' or parameter == None:
+            return False
+        elif param_type == "ip" and self.__isipaddress(parameter):
+            return True
+        elif param_type == "int" and self.__isint(parameter):
+            return True
+        elif param_type == "float" and self.__isfloat(parameter):
+            return True
+        elif param_type == "str" and self.__isstr(parameter):
+            return True
+
+        return False
+
+
+    def __isipaddress(self, parameter):
+        """
+        Verify if the ip address is valid
+
+        @param ip String: IP Address
+        @return True if a valid IP Address or False
+        """
+        octet1 = "([1-9][0-9]{,1}|1[0-9]{2}|2[0-4][0-9]|25[0-5])"
+        octet = "([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5])"
+        pattern = "^" + octet1 + "\.(" + octet + "\.){2}" + octet + "$"
+        if re.match(pattern, parameter) == None:
+            return False
+        else:
+            return True
+
+
+    def __isint(self, parameter):
+        try:
+            int(parameter)
+        except Exception, e_stack:
+            return False
+        return True
+
+
+    def __isfloat(self, parameter):
+        try:
+            float(parameter)
+        except Exception, e_stack:
+            return False
+        return True
+
+
+    def __isstr(self, parameter):
+        try:
+            str(parameter)
+        except Exception, e_stack:
+            return False
+        return True
