85942242b3ccab015add48a0bc81982f6e7bd42b
Martin J. Bligh <mbligh@google.com>
2009-06-15 21:28:53 +0000

    updates for blktrace
    Signed-off-by: Martin J. Bligh <mbligh@google.com>

--- client/profilers/blktrace/blktrace.py
+++ client/profilers/blktrace/blktrace.py
@@ -1,13 +1,12 @@
 import os
-from autotest_lib.client.bin import profiler, utils
+from autotest_lib.client.bin import profiler, utils, error
 
 
 class blktrace(profiler.profiler):
-    version = 1
+    version = 2
 
-    def initialize(self, disk):
+    def initialize(self, **dargs):
         self.mountpoint = '/sys/kernel/debug'
-        self.disk = disk
         self.blktrace = os.path.join(self.srcdir, 'blktrace')
         self.blkparse = os.path.join(self.srcdir, 'blkparse')
         self.blktrace_job = None
@@ -17,13 +16,27 @@ class blktrace(profiler.profiler):
         ldflags = '-L ' + self.job.autodir + '/deps/libaio/lib'
         cflags = '-I ' + self.job.autodir + '/deps/libaio/include'
         self.gcc_flags = ldflags + ' ' + cflags
+        self.device = dargs.get('device', None)
 
 
     def setup(self, tarball='blktrace.tar.bz2', **dargs):
+        # v1.0.1, pulled from git, 2009/06/10
+        # commit 1e09f6e9012826fca69fa07222b7bc53c3e629ee
         self.tarball = utils.unmap_url(self.bindir, tarball, self.tmpdir)
         utils.extract_tarball_to_dir(self.tarball, self.srcdir)
         os.chdir(self.srcdir)
         utils.system('make ' + '"CFLAGS=' + self.gcc_flags + '"')
+
+
+    def get_device(self, test):
+        if getattr(test, 'device', None):
+            device = test.device
+        else:
+            if self.device:
+                device=self.device
+            else:
+                raise error.TestWarn('No device specified for blktrace')
+        return device
 
 
     def start(self, test):
@@ -31,16 +44,13 @@ class blktrace(profiler.profiler):
                               ignore_status=True)
         if result:
             utils.system('mount -t debugfs debugfs /sys/kernel/debug')
-        if getattr(test, 'disk', None):
-            disk = test.disk
-        else:
-            disk = self.disk
-        self.blktrace_job = utils.BgJob('%s /dev/%s' % (self.blktrace, disk))
+        device = self.get_device(test)
+        self.blktrace_job = utils.BgJob('%s /dev/%s' % (self.blktrace, device))
 
 
     def stop(self, test):
         if self.blktrace_job is not None:
-            utils.nuke_subprocess(self.blktrace.sp)
+            utils.nuke_subprocess(self.blktrace_job.sp)
         self.blktrace_job = None
 
 
@@ -48,4 +58,5 @@ class blktrace(profiler.profiler):
         output_file = os.path.join(test.profdir, 'blktrace')
         if getattr(test, 'profile_tag', None):
             output_file += '.' + test.profile_tag
-        utils.system('%s %s > %s' % (self.blkparse, self.disk, output_file))
+        device = self.get_device(test)
+        utils.system('%s %s > %s' % (self.blkparse, device, output_file))
