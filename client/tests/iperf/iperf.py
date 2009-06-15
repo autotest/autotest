dc922cffa78641d46d2f63f718d7ace073d60d09
Martin J. Bligh <mbligh@google.com>
2009-06-15 21:52:16 +0000

    This patch changes iperf.py to make use of client.bin.net.net_utils to configure the device used for the iperf test.  This allows for testing hardware that has more than one ethernet device.   Also we make sure to call self.job.require_gcc() since this test performs a compile of source code. The call to 'sync' insures that all is ready for when the test starts. In addition the test makes sure to add mpstat to the cmd line that is executed so that this data will show up in the logs for later analysis. Also the mpstat data is used to calculate additional keyvals relating to CPU stats.  This also allows for a calulation of the keyval Efficiency_C.
    Travis Miller

--- client/tests/iperf/iperf.py
+++ client/tests/iperf/iperf.py
@@ -1,24 +1,27 @@
 import os, re, socket, time, logging
 from autotest_lib.client.bin import test, utils
+from autotest_lib.client.bin.net import net_utils
 from autotest_lib.client.common_lib import error
 
+MPSTAT_IX = 0
+IPERF_IX = 1
 
 class iperf(test.test):
     version = 1
 
     # http://downloads.sourceforge.net/iperf/iperf-2.0.4.tar.gz
     def setup(self, tarball = 'iperf-2.0.4.tar.gz'):
+        self.job.require_gcc()
         tarball = utils.unmap_url(self.bindir, tarball, self.tmpdir)
         utils.extract_tarball_to_dir(tarball, self.srcdir)
         os.chdir(self.srcdir)
 
         utils.system('./configure')
         utils.system('make')
+        utils.system('sync')
 
 
     def initialize(self):
-        self.job.require_gcc()
-
         self.SERVER_PORT = '5001'
 
         # The %%s is to add extra args later
@@ -32,10 +35,12 @@ class iperf(test.test):
 
         self.results = []
         self.actual_times = []
+        self.netif = ''
+        self.network_utils = net_utils.network_utils()
 
 
     def run_once(self, server_ip, client_ip, role, udp=False,
-                 bidirectional=False, test_time=10, stream_list=[1]):
+                 bidirectional=False, test_time=10, dev='', stream_list=[1]):
         self.role = role
         self.udp = udp
         self.bidirectional = bidirectional
@@ -45,6 +50,13 @@ class iperf(test.test):
         server_tag = server_ip + '#iperf-server'
         client_tag = client_ip + '#iperf-client'
         all = [server_tag, client_tag]
+
+        # If a specific device has been requested, configure it.
+        if dev:
+            if role == 'server':
+                self.configure_interface(dev, server_ip)
+            else:
+                self.configure_interface(dev, client_ip)
 
         for num_streams in stream_list:
             if self.role == 'server':
@@ -78,6 +90,20 @@ class iperf(test.test):
             else:
                 raise error.TestError('invalid role specified')
 
+        self.restore_interface()
+
+
+    def configure_interface(self, dev, ip_addr):
+        self.netif = net_utils.netif(dev)
+        self.netif.up()
+        if self.netif.get_ipaddr() != ip_addr:
+            self.netif.set_ipaddr(ip_addr)
+
+
+    def restore_interface(self):
+        if self.netif:
+            self.netif.restore()
+
 
     def server_start(self):
         args = ''
@@ -100,14 +126,23 @@ class iperf(test.test):
         if self.bidirectional:
             args += '-d '
 
-        cmd = self.client_path % (server_ip, args)
         try:
+            cmds = []
+            # Get 5 mpstat samples. Since tests with large number of streams
+            # take a long time to start up all the streams, we'll toss out the
+            # first and last sample when recording results
+            interval = max(1, test_time / 5)
+            cmds.append('mpstat -P ALL %s 5' % interval)
+
+            # Add the iperf command
+            cmd = self.client_path % (server_ip, args)
+            cmds.append(cmd)
+
             t0 = time.time()
-            self.results.append(utils.get_cpu_percentage(
-                utils.system_output, cmd, timeout=test_time+60,
-                retain_output=True))
+            out = utils.run_parallel(cmds, timeout = test_time + 60)
             t1 = time.time()
 
+            self.results.append(out)
             self.actual_times.append(t1 - t0)
 
         except error.CmdError, e:
@@ -120,8 +155,7 @@ class iperf(test.test):
             works for now"""
 
             if 'within' in e.additional_text:
-                # Results are cpu%, output
-                self.results.append((0, None))
+                self.results.append(None)
                 self.actual_times.append(1)
             else:
                 raise
@@ -165,24 +199,35 @@ class iperf(test.test):
             for i, streams in enumerate(self.stream_list):
                 attr = {'stream_count':streams}
                 keyval = {}
-                keyval['CPU_C'], output  = self.results[i]
-                runs = {'Bandwidth_S2C':[], 'Bandwidth_C2S':[],
-                        'Jitter_S2C':[], 'Jitter_C2S':[]}
 
                 # Short circuit to handle errors due to client timeouts
-                if output is None:
+                if not self.results[i] or not self.results[i][IPERF_IX].stdout:
                     self.write_iteration_keyval(attr, keyval)
                     continue
+
+                mpstat_out = self.results[i][MPSTAT_IX].stdout
+                iperf_out = self.results[i][IPERF_IX].stdout
+
+                # Process mpstat output
+                cpu_stats = self.network_utils.process_mpstat(mpstat_out, 5)
+                keyval['CPU_C'] = 100 - cpu_stats['idle']
+                keyval['CPU_C_SYS'] = cpu_stats['sys']
+                keyval['CPU_C_HI'] = cpu_stats['irq']
+                keyval['CPU_C_SI'] = cpu_stats['soft']
+                keyval['INTRS_C'] = cpu_stats['intr/s']
+
+                runs = {'Bandwidth_S2C':[], 'Bandwidth_C2S':[],
+                        'Jitter_S2C':[], 'Jitter_C2S':[]}
 
                 if self.udp:
                     regex = udp_regex
                     keys = udp_keys
                     # Ignore the first line
-                    stdout = output.split('\n',1)[1]
+                    stdout = iperf_out.split('\n',1)[1]
                 else:
                     regex = tcp_regex
                     keys = tcp_keys
-                    stdout = output
+                    stdout = iperf_out
 
                 # This will not find the average lines because the 'id' field
                 # is negative and doesn't match the patterns -- this is good
@@ -217,7 +262,10 @@ class iperf(test.test):
                         keyval[key] = keyval[key] * scale
                         total_bw = total_bw + keyval[key]
 
-                keyval['Efficiency_C'] = total_bw/keyval['CPU_C']
+                if keyval['CPU_C']:
+                    keyval['Efficiency_C'] = total_bw/keyval['CPU_C']
+                else:
+                    keyval['Efficiency_C'] = total_bw
                 self.write_iteration_keyval(attr, keyval)
         else:
             # This test currently does not produce a keyval file on the
