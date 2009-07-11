0f7649aa868fb1c29437c9ea9cab9e8f71b416f7
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2009-07-11 00:34:18 +0000

    This patch adds the wrapper for ebizzy into autotest. here is the link to get a copy of the test tarball. http://sourceforge.net/project/platformdownload.php?group_id=202378&sel_platform=3809
    Please review the patch and provide your comments.
    
    Signed-off-by: Sudhir Kumar <skumar@linux.vnet.ibm.com>
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@3399 592f7852-d20e-0410-864c-8624ca9c26a4

---
+++ client/tests/ebizzy/ebizzy.py
@@ -0,0 +1,31 @@
+import os
+from autotest_lib.client.bin import utils, test
+from autotest_lib.client.common_lib import error
+
+class ebizzy(test.test):
+    version = 3
+
+    def initialize(self):
+        self.job.require_gcc()
+
+
+    # http://sourceforge.net/project/downloading.php?group_id=202378&filename=ebizzy-0.3.tar.gz
+    def setup(self, tarball='ebizzy-0.3.tar.gz'):
+        tarball = utils.unmap_url(self.bindir, tarball, self.tmpdir)
+        utils.extract_tarball_to_dir(tarball, self.srcdir)
+        os.chdir(self.srcdir)
+
+        utils.system('[ -x configure ] && ./configure')
+        utils.system('make')
+
+
+    # Note: default we use always mmap()
+    def run_once(self, args='', num_chunks=1000, chunk_size=512000,
+                 seconds=100, num_threads=100):
+
+        #TODO: Write small functions which will choose many of the above
+        # variables dynamicaly looking at guest's total resources
+        logfile = os.path.join(self.resultsdir, 'ebizzy.log')
+        args2 = '-m -n %s -P -R -s %s -S %s -t %s' % (num_chunks, chunk_size,
+                                                      seconds, num_threads)
+        args = args + ' ' + args2
