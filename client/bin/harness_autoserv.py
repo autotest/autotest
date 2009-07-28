dfaca61dc486abaf5f1409988e8f1c85222ae7d6
John Admanski <jadmanski@google.com>
2009-07-28 23:29:04 +0000

    Does some refactoring of the harness_autoserv class so that later when I need to create a packaging tie in I can reuse some of the server signalling code.
    Risk: Low
    Visibility: Mostly just extracts a bunch of code out into a separate
    helper method.
    
    Signed-off-by: John Admanski <jadmanski@google.com>

--- client/bin/harness_autoserv.py
+++ client/bin/harness_autoserv.py
@@ -1,5 +1,6 @@
-import os, tempfile
+import os
 
+from autotest_lib.client.common_lib import autotemp
 from autotest_lib.client.bin import harness
 
 
@@ -21,34 +22,43 @@ class harness_autoserv(harness.harness):
         self.status = os.fdopen(3, 'w', 0)
 
 
+    def _send_and_wait(self, title, *args):
+        """Send a message to the autoserv and wait for it to signal
+        completion.
+
+        @param title: An alphanumeric string to title the message.
+        @param *args: Additional arbitrary alphanumeric arguments to pass
+                to the server.
+        """
+        # create a named pipe for us to recieve a signal on
+        fifo_dir = autotemp.tempdir(suffix='-fifo', unique_id='harness',
+                                    dir=self.job.tmpdir)
+        try:
+            fifo_path = os.path.join(fifo_dir.name, 'autoserv.fifo')
+            os.mkfifo(fifo_path)
+
+            # send signal to the server as title[:args]:path
+            msg = ':'.join([title] + list(args) + [fifo_path]) + '\n'
+            self.status.write(msg)
+
+            # wait for the server to signal back to us
+            fifo = open(fifo_path)
+            fifo.read(1)
+            fifo.close()
+        finally:
+            fifo_dir.clean()
+
+
     def run_test_complete(self):
         """A test run by this job is complete, signal it to autoserv and
         wait for it to signal to continue"""
-        # create a named pipe for us to receive a signal on
-        fifo_dir = tempfile.mkdtemp(suffix="-fifo", dir=self.job.tmpdir)
-        fifo_path = os.path.join(fifo_dir, "autoserv.fifo")
-        os.mkfifo(fifo_path)
-
-        # signal test completion to the server as
-        # AUTOTEST_TEST_COMPLETE:path
-        msg = "AUTOTEST_TEST_COMPLETE:%s\n"
-        msg %= fifo_path
-        self.status.write(msg)
-
-        # wait for the server to signal back to us
-        fifo = open(fifo_path)
-        fifo.read(1)
-        fifo.close()
-
-        # clean up the named pipe
-        os.remove(fifo_path)
-        os.rmdir(fifo_dir)
+        self._send_and_wait('AUTOTEST_TEST_COMPLETE')
 
 
     def test_status(self, status, tag):
         """A test within this job is completing"""
         for line in status.split('\n'):
             # sent status messages with AUTOTEST_STATUS:tag:message
-            msg = "AUTOTEST_STATUS:%s:%s\n"
+            msg = 'AUTOTEST_STATUS:%s:%s\n'
             msg %= (tag, line)
             self.status.write(msg)
