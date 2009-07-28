cfd853a264c0a1e28818a0b306e6a67d152efe91
Steve Howard <showard@google.com>
2009-07-28 20:00:32 +0000

    Added support for proper subprocess killing to parallel.py, as parallel.fork_nuke_subprocess().  This involved a simple refactoring of parallel.fork_waitfor().
    I also fixed a bug in parallel.fork_start() with the writing of exception pickles.
    
    Signed-off-by: Steve Howard <showard@google.com>

--- client/bin/parallel.py
+++ client/bin/parallel.py
@@ -2,8 +2,8 @@
 
 __author__ = """Copyright Andy Whitcroft 2006"""
 
-import sys, os, pickle, logging
-from autotest_lib.client.common_lib import error
+import sys, logging, os, pickle, traceback
+from autotest_lib.client.common_lib import error, utils
 
 def fork_start(tmp, l):
     sys.stdout.flush()
@@ -23,10 +23,19 @@ def fork_start(tmp, l):
     except Exception, detail:
         try:
             try:
-                logging.error("child process failed")
+                logging.error('child process failed')
+                # logging.exception() uses ERROR level, but we want DEBUG for
+                # the traceback
+                logging.debug(traceback.format_exc())
             finally:
-                ename = tmp + "/debug/error-%d" % (os.getpid())
+                # note that exceptions originating in this block won't make it
+                # to the logs
+                output_dir = os.path.join(tmp, 'debug')
+                if not os.path.exists(output_dir):
+                    os.makedirs(output_dir)
+                ename = os.path.join(output_dir, "error-%d" % os.getpid())
                 pickle.dump(detail, open(ename, "w"))
+
                 sys.stdout.flush()
                 sys.stderr.flush()
         finally:
@@ -39,12 +48,21 @@ def fork_start(tmp, l):
             os._exit(0)
 
 
-def fork_waitfor(tmp, pid):
-    (pid, status) = os.waitpid(pid, 0)
-
-    ename = tmp + "/debug/error-%d" % pid
+def _check_for_subprocess_exception(temp_dir, pid):
+    ename = temp_dir + "/debug/error-%d" % pid
     if os.path.exists(ename):
         raise pickle.load(file(ename, 'r'))
 
+
+def fork_waitfor(tmp, pid):
+    (pid, status) = os.waitpid(pid, 0)
+
+    _check_for_subprocess_exception(tmp, pid)
+
     if status:
         raise error.TestError("Test subprocess failed rc=%d" % (status))
+
+
+def fork_nuke_subprocess(tmp, pid):
+    utils.nuke_pid(pid)
+    _check_for_subprocess_exception(tmp, pid)
