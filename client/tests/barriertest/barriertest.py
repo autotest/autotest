f8d023a64b10f56128275e9dc37def27b6ae0a0a
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2009-08-03 16:46:50 +0000

    Use lower ports for profiling. old: 63100 new: 11920 old: 63001, 63101 new: 11921 old: 63002, 63000 new: 11922
    Signed-off-by: Rachel Kroll <rkroll@google.com>
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@3483 592f7852-d20e-0410-864c-8624ca9c26a4

--- client/tests/barriertest/barriertest.py
+++ client/tests/barriertest/barriertest.py
@@ -10,11 +10,11 @@ class barriertest(test.test):
         profilers = self.job.profilers
 
         b0 = self.job.barrier(hostid, "sync_profilers", timeout_start,
-                              port=63100)
+                              port=11920)
         b0.rendezvous_servers(masterid, hostid)
 
         b1 = self.job.barrier(hostid, "start_profilers", timeout_start,
-                              port=63100)
+                              port=11920)
         b1.rendezvous_servers(masterid, hostid)
 
         b2 = self.job.barrier(hostid, "local_sync_profilers", timeout_sync)
@@ -23,7 +23,7 @@ class barriertest(test.test):
         profilers.start(self)
 
         b3 = self.job.barrier(hostid, "stop_profilers", timeout_stop,
-                              port=63100)
+                              port=11920)
         b3.rendezvous_servers(masterid, hostid)
 
         profilers.stop(self)
