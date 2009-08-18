356b85aaed718a858cd0394251fb0d9adc54cc5f
Michael Goldish <mgoldish@redhat.com>
2009-08-18 15:17:08 +0000

    KVM test: Batch script that prepares a Windows guest for remote access
    setuprss.bat copies rss.exe to C:\, makes it run at startup, disables the
    firewall, sets a known password for the administrator account, makes it logon
    automatically, and does some Vista/2008 specific things.
    
    The script requires administrator privileges.
    
    Usage: setuprss.bat [full path to executable]
    e.g.: setuptrss.bat D:\rss.exe
    
    If used without parameters, rss.exe will be copied from the directory in which
    setuprss.bat resides.
    
    Signed-off-by: Michael Goldish <mgoldish@redhat.com>

---
+++ client/tests/kvm/deps/setuprss.bat
@@ -0,0 +1,19 @@
+set rsspath=%1
+if [%1]==[] set rsspath=%~dp0\rss.exe
+copy %rsspath% C:\rss.exe
+
+net user Administrator /active:yes
+net user Administrator 1q2w3eP
+netsh firewall set opmode disable
+
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Run" /v "Remote Shell Server" /d "C:\rss.exe 22" /t REG_SZ /f
+reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\winlogon" /v "AutoAdminLogon" /d "1" /t REG_SZ /f
+reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\winlogon" /v "DefaultUserName" /d "Administrator" /t REG_SZ /f
+reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\winlogon" /v "DefaultPassword" /d "1q2w3eP" /t REG_SZ /f
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v "EnableLUA" /d "0" /t REG_DWORD /f
+reg add "HKLM\Software\Policies\Microsoft\Windows NT\Reliability" /v "ShutdownReasonOn" /d "0" /t REG_DWORD /f
+
+rem Just in case reg.exe is missing (e.g. Windows 2000):
+regedit /s %~dp0\rss.reg
+
+start /B C:\rss.exe
