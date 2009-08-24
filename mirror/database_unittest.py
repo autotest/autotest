d53807ec13ffbee9e9ca55a48b0c651033e590ad
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2009-08-24 22:01:03 +0000

    Improved reliability when saving "mirror" dict_database contents by employing a fcntl.flock() based process synchronization to protect against 2 or more processes updating the db at the same time. Also switched to using a temporary file to protect against write failures (like disk full) situations from corrupting the existent data. Updated unittest.
    Signed-off-by: Mihai Rusu <dizzy@google.com>
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@3596 592f7852-d20e-0410-864c-8624ca9c26a4

--- mirror/database_unittest.py
+++ mirror/database_unittest.py
@@ -15,15 +15,18 @@ class dict_database_unittest(unittest.TestCase):
         'file2': database.item('file2', 20, 20000),
         }
 
-    class _file_instance:
-        pass
-
     def setUp(self):
         self.god = mock.mock_god()
 
         self.god.stub_function(database.cPickle, 'load')
         self.god.stub_function(database.cPickle, 'dump')
-        self.open_mock = self.god.create_mock_function('open')
+        self.god.stub_function(database.tempfile, 'mkstemp')
+        self.god.stub_function(database.os, 'fdopen')
+        self.god.stub_function(database.os, 'close')
+        self.god.stub_function(database.os, 'rename')
+        self.god.stub_function(database.os, 'unlink')
+        self._open_mock = self.god.create_mock_function('open')
+        self._file_instance = self.god.create_mock_class(file, 'file')
 
 
     def tearDown(self):
@@ -32,35 +35,37 @@ class dict_database_unittest(unittest.TestCase):
 
     def test_get_dictionary_no_file(self):
         # record
-        (self.open_mock.expect_call(self._path, 'rb')
+        (self._open_mock.expect_call(self._path, 'rb')
             .and_raises(IOError('blah')))
 
         # playback
         db = database.dict_database(self._path)
-        self.assertEqual(db.get_dictionary(_open_func=self.open_mock), {})
+        self.assertEqual(db.get_dictionary(_open_func=self._open_mock), {})
 
         self.god.check_playback()
 
 
     def test_get_dictionary(self):
         # record
-        (self.open_mock.expect_call(self._path, 'rb')
+        (self._open_mock.expect_call(self._path, 'rb')
             .and_return(self._file_instance))
         (database.cPickle.load.expect_call(self._file_instance)
             .and_return(self._db_contents))
+        self._file_instance.close.expect_call()
 
         # playback
         db = database.dict_database(self._path)
-        self.assertEqual(db.get_dictionary(_open_func=self.open_mock),
+        self.assertEqual(db.get_dictionary(_open_func=self._open_mock),
                          self._db_contents)
 
         self.god.check_playback()
 
 
-    def test_merge_dictionary(self):
+    def _setup_merge_dictionary(self):
         # setup
         db = database.dict_database(self._path)
         self.god.stub_function(db, 'get_dictionary')
+        self.god.stub_function(db, '_aquire_lock')
 
         new_files = {
             'file3': database.item('file3', 30, 30000),
@@ -70,15 +75,41 @@ class dict_database_unittest(unittest.TestCase):
         all_files.update(new_files)
 
         # record
+        db._aquire_lock.expect_call().and_return(3)
         db.get_dictionary.expect_call().and_return(self._db_contents)
+        (database.tempfile.mkstemp.expect_call(prefix=self._path, dir='')
+                .and_return((4, 'tmpfile')))
+        database.os.fdopen.expect_call(4, 'wb').and_return(self._file_instance)
 
-        (self.open_mock.expect_call(self._path, 'wb')
-            .and_return(self._file_instance))
+        return db, new_files, all_files
+
+
+    def test_merge_dictionary(self):
+        db, new_files, all_files = self._setup_merge_dictionary()
+
         database.cPickle.dump.expect_call(all_files, self._file_instance,
-                                               protocol=2)
+                protocol=database.cPickle.HIGHEST_PROTOCOL)
+        self._file_instance.close.expect_call()
+        database.os.rename.expect_call('tmpfile', self._path)
+        database.os.close.expect_call(3)
 
         # playback
-        db.merge_dictionary(new_files, _open_func=self.open_mock)
+        db.merge_dictionary(new_files)
+        self.god.check_playback()
+
+
+    def test_merge_dictionary_disk_full(self):
+        err = Exception('fail')
+        db, new_files, all_files = self._setup_merge_dictionary()
+
+        database.cPickle.dump.expect_call(all_files, self._file_instance,
+                protocol=database.cPickle.HIGHEST_PROTOCOL).and_raises(err)
+        self._file_instance.close.expect_call().and_raises(err)
+        database.os.unlink.expect_call('tmpfile')
+        database.os.close.expect_call(3)
+
+        # playback
+        self.assertRaises(Exception, db.merge_dictionary, new_files)
         self.god.check_playback()
 
 
