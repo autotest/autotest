3ee142d0009af69502d8ffa78adc389e1322c006
James Ren <jamesren@google.com>
2009-09-18 21:20:51 +0000

    Set invalid host_id references to a new __missing_host__ host before adding the foreign key to host_queue_entries.
    Signed-off-by: James Ren <jamesren@google.com>

--- frontend/migrations/040_add_foreign_keys.py
+++ frontend/migrations/040_add_foreign_keys.py
@@ -9,6 +9,14 @@ ADD CONSTRAINT host_queue_entries_job_id_fk FOREIGN KEY
 (job_id) REFERENCES jobs(id)
 ON DELETE NO ACTION;
 
+INSERT INTO hosts (hostname, invalid, protection, dirty)
+VALUES ('__missing_host__', 1, 0, 1);
+
+UPDATE host_queue_entries AS hqe
+    LEFT OUTER JOIN hosts ON (hqe.host_id = hosts.id)
+SET hqe.host_id = (SELECT id FROM hosts WHERE hostname = '__missing_host__')
+WHERE hqe.host_id IS NOT NULL AND hosts.id IS NULL;
+
 ALTER TABLE host_queue_entries
 ADD CONSTRAINT host_queue_entries_host_id_fk FOREIGN KEY
 (host_id) REFERENCES hosts(id)
