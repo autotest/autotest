1db14c3fd782135dc2c760c1e2de260a3231ef76
Martin J. Bligh <mbligh@google.com>
2009-10-14 20:04:15 +0000

    update crackerjack URL
    Signed-off-by: Martin J. Bligh <mbligh@google.com>

--- tko/display.py
+++ tko/display.py
@@ -330,7 +330,7 @@ text-align: left
     print '&nbsp&nbsp&nbsp'
     print '<a href="machine_benchmark.cgi">Performance</a>'
     print '&nbsp&nbsp&nbsp'
-    print '<a href="http://crackerjack.good-day.net/cjk/compare_results?col=!d&row=!d">Crackerjack</a>'
+    print '<a href="http://ossipedia.ipa.go.jp/crackerjack/compare_results.html">Crackerjack</a>'
     print '&nbsp&nbsp&nbsp'
     print '<a href="http://autotest.kernel.org">[About Page]</a>'
     print '</h2><p>'
