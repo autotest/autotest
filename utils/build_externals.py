c5f4ebcd1f96460224d3b0098b8ed7dbd5ad9aec
showard <showard@592f7852-d20e-0410-864c-8624ca9c26a4>
2009-10-28 19:53:10 +0000

    Upgrade to Django 1.0.4.  In the course of normal development I ran into this Django bug:
    http://code.djangoproject.com/ticket/9188
    
    It seems to have been fixed between 1.0.2 and 1.0.4.  For reference, it can be reproduced on 1.0.2 with our AFE DB using this simple script:
    
    import common
    from autotest_lib.frontend import setup_django_environment
    from autotest_lib.frontend import setup_test_environment
    from autotest_lib.frontend.afe import models
    
    setup_test_environment.set_up()
    
    # this will raise an OperationalError if the bug is present
    print models.SpecialTask.objects.exclude(host__hostqueueentry__active=True)
    
    Signed-off-by: Steve Howard <showard@google.com>
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@3886 592f7852-d20e-0410-864c-8624ca9c26a4

--- utils/build_externals.py
+++ utils/build_externals.py
@@ -638,11 +638,10 @@ class MySQLdbPackage(ExternalPackage):
 
 
 class DjangoPackage(ExternalPackage):
-    version = '1.0.2'
-    local_filename = 'Django-%s-final.tar.gz' % version
-    urls = ('http://media.djangoproject.com/releases/%s/%s'
-            % (version, local_filename),)
-    hex_sum = 'f2d9088f17aff47ea17e5767740cab67b2a73b6b'
+    version = '1.0.4'
+    local_filename = 'Django-%s.tar.gz' % version
+    urls = ('http://www.djangoproject.com/download/%s/tarball/' % version,)
+    hex_sum = 'a8b9f33adb007146b8b57f22f0f38467f0d2693a'
 
     _build_and_install = ExternalPackage._build_and_install_from_package
     _build_and_install_current_dir = (
