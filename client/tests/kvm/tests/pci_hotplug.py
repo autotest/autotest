99743d2868bf5e1724d1798c9a1da2e85b3216c3
lmr <lmr@592f7852-d20e-0410-864c-8624ca9c26a4>
2009-11-10 16:56:27 +0000

    KVM test: minor pci_hotplug fixes
    - Put the PCI device removal code in a finally clause.
    - Use kvm_vm.get_image_filename() instead of os.path.join().
    It's a bit cleaner because if we ever change the names of image parameters
    we'll only have to change the code in one place.
    Also, the way os.path.join() was used lead to image filenames being prefixed
    with 'images/' twice, e.g. 'images/images/foo.qcow2'.
    - Make some failure messages clearer.
    - Remove 'only Fedora Ubuntu Windows' from the fmt_raw variant.
    'only' works for things that have already been defined, but the guests are
    defined later.
    - Remove unused 'modprobe_acpiphp' parameter.
    - Change 'online disk' to 'online' in pci_test_cmd for Windows ('online disk'
    doesn't seem to work).
    - Remove the unneeded telnet/ssh/guest_port parameters from the Windows
    block_hotplug parameters.
    
    Changes from previous version:
    - When deleting the PCI device compare the output of 'info pci' rather than the
    output of the pci_del command.
    - If an exception is raised in the 'try' block, do not raise an exception in
    the device deletion code, because it will mask the first exception.
    - After pci_del, give the device some time to disappear from 'info pci'.
    
    Signed-off-by: Michael Goldish <mgoldish@redhat.com>
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@3938 592f7852-d20e-0410-864c-8624ca9c26a4

--- client/tests/kvm/tests/pci_hotplug.py
+++ client/tests/kvm/tests/pci_hotplug.py
@@ -1,6 +1,6 @@
 import logging, os
 from autotest_lib.client.common_lib import error
-import kvm_subprocess, kvm_test_utils, kvm_utils
+import kvm_subprocess, kvm_test_utils, kvm_utils, kvm_vm
 
 
 def run_pci_hotplug(test, params, env):
@@ -21,8 +21,8 @@ def run_pci_hotplug(test, params, env):
     session = kvm_test_utils.wait_for_login(vm)
 
     # Modprobe the module if specified in config file
-    if params.get("modprobe_module"):
-        module = params.get("modprobe_module")
+    module = params.get("modprobe_module")
+    if module:
         if session.get_command_status("modprobe %s" % module):
             raise error.TestError("Modprobe module '%s' failed" % module)
 
@@ -38,61 +38,73 @@ def run_pci_hotplug(test, params, env):
     if test_type == "nic":
         pci_add_cmd = "pci_add pci_addr=auto nic model=%s" % tested_model
     elif test_type == "block":
-        image_name = params.get("image_name_stg")
-        image_filename = "%s.%s" % (image_name, params.get("image_format_stg"))
-        image_dir = os.path.join(test.bindir, "images")
-        storage_name = os.path.join(image_dir, image_filename)
+        image_params = kvm_utils.get_sub_dict(params, "stg")
+        image_filename = kvm_vm.get_image_filename(image_params, test.bindir)
         pci_add_cmd = ("pci_add pci_addr=auto storage file=%s,if=%s" %
-                                    (storage_name, tested_model))
+                       (image_filename, tested_model))
 
     # Implement pci_add
     s, add_output = vm.send_monitor_cmd(pci_add_cmd)
     if not "OK domain" in add_output:
         raise error.TestFail("Add device failed. Hypervisor command is: %s. "
-                             "Output: %s" % (pci_add_cmd, add_output))
-
-    # Compare the output of 'info pci'
+                             "Output: %r" % (pci_add_cmd, add_output))
     s, after_add = vm.send_monitor_cmd("info pci")
-    if after_add == info_pci_ref:
-        raise error.TestFail("No new PCI device shown after executing "
-                             "hypervisor command: 'info pci'")
 
-    # Define a helper function to compare the output
-    def new_shown():
-        o = session.get_command_output(params.get("reference_cmd"))
-        if reference == o:
-            return False
-        return True
+    # Define a helper function to delete the device
+    def pci_del(ignore_failure=False):
+        slot_id = "0" + add_output.split(",")[2].split()[1]
+        cmd = "pci_del pci_addr=%s" % slot_id
+        vm.send_monitor_cmd(cmd)
 
-    secs = int(params.get("wait_secs_for_hook_up"))
-    if not kvm_utils.wait_for(new_shown, 30, secs, 3):
-        raise error.TestFail("No new device shown in output of command "
-                             "executed inside the guest: %s" %
-                             params.get("reference_cmd"))
+        def device_removed():
+            s, after_del = vm.send_monitor_cmd("info pci")
+            return after_del != after_add
 
-    # Define a helper function to catch PCI device string
-    def find_pci():
-        output = session.get_command_output(params.get("find_pci_cmd"))
-        if not params.get("match_string") in output:
-            return False
-        return True
+        if (not kvm_utils.wait_for(device_removed, 10, 0, 1)
+            and not ignore_failure):
+            raise error.TestFail("Failed to hot remove PCI device: %s. "
+                                 "Hypervisor command: %s" % (tested_model,
+                                                             cmd))
 
-    if not kvm_utils.wait_for(find_pci, 30, 3, 3):
-        raise error.TestFail("PCI model not found: %s. Command is: %s" %
-                             (tested_model, params.get("find_pci_cmd")))
+    try:
+        # Compare the output of 'info pci'
+        if after_add == info_pci_ref:
+            raise error.TestFail("No new PCI device shown after executing "
+                                 "hypervisor command: 'info pci'")
 
-    # Test the newly added device
-    s, o = session.get_command_status_output(params.get("pci_test_cmd"))
-    if s != 0:
-        raise error.TestFail("Check for %s device failed after PCI hotplug. "
-                             "Output: %s" % (test_type, o))
+        # Define a helper function to compare the output
+        def new_shown():
+            o = session.get_command_output(params.get("reference_cmd"))
+            return o != reference
 
-    # Delete the added pci device
-    slot_id = "0" + add_output.split(",")[2].split()[1]
-    cmd = "pci_del pci_addr=%s" % slot_id
-    s, after_del = vm.send_monitor_cmd(cmd)
-    if after_del == after_add:
-        raise error.TestFail("Failed to hot remove PCI device: %s. "
-                             "Hypervisor command: %s" % (tested_model, cmd))
+        secs = int(params.get("wait_secs_for_hook_up"))
+        if not kvm_utils.wait_for(new_shown, 30, secs, 3):
+            raise error.TestFail("No new device shown in output of command "
+                                 "executed inside the guest: %s" %
+                                 params.get("reference_cmd"))
 
-    session.close()
+        # Define a helper function to catch PCI device string
+        def find_pci():
+            o = session.get_command_output(params.get("find_pci_cmd"))
+            return params.get("match_string") in o
+
+        if not kvm_utils.wait_for(find_pci, 30, 3, 3):
+            raise error.TestFail("PCI %s %s device not found in guest. "
+                                 "Command was: %s" %
+                                 (tested_model, test_type,
+                                  params.get("find_pci_cmd")))
+
+        # Test the newly added device
+        s, o = session.get_command_status_output(params.get("pci_test_cmd"))
+        if s != 0:
+            raise error.TestFail("Check for %s device failed after PCI "
+                                 "hotplug. Output: %r" % (test_type, o))
+
+        session.close()
+
+    except:
+        pci_del(ignore_failure=True)
+        raise
+
+    else:
+        pci_del()
