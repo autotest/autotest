a6a9e03a6f50f7803e01764b61ec1627863d42c8
Michael Goldish <mgoldish@redhat.com>
2009-11-10 16:30:39 +0000

    KVM test: add -o PreferredAuthentications=password to SSH options
    This speeds up SSH logins because by default other time consuming
    authentication methods are attempted first.
    
    Signed-off-by: Michael Goldish <mgoldish@redhat.com>

--- client/tests/kvm/kvm_utils.py
+++ client/tests/kvm/kvm_utils.py
@@ -551,7 +551,8 @@ def scp_to_remote(host, port, username, password, local_path, remote_path,
 
     @return: True on success and False on failure.
     """
-    command = ("scp -o UserKnownHostsFile=/dev/null -r -P %s %s %s@%s:%s" %
+    command = ("scp -o UserKnownHostsFile=/dev/null "
+               "-o PreferredAuthentications=password -r -P %s %s %s@%s:%s" %
                (port, local_path, username, host, remote_path))
     return remote_scp(command, password, timeout)
 
@@ -571,7 +572,8 @@ def scp_from_remote(host, port, username, password, remote_path, local_path,
 
     @return: True on success and False on failure.
     """
-    command = ("scp -o UserKnownHostsFile=/dev/null -r -P %s %s@%s:%s %s" %
+    command = ("scp -o UserKnownHostsFile=/dev/null "
+               "-o PreferredAuthentications=password -r -P %s %s@%s:%s %s" %
                (port, username, host, remote_path, local_path))
     return remote_scp(command, password, timeout)
 
@@ -589,7 +591,8 @@ def ssh(host, port, username, password, prompt, linesep="\n", timeout=10):
 
     @return: kvm_spawn object on success and None on failure.
     """
-    command = ("ssh -o UserKnownHostsFile=/dev/null -p %s %s@%s" %
+    command = ("ssh -o UserKnownHostsFile=/dev/null "
+               "-o PreferredAuthentications=password -p %s %s@%s" %
                (port, username, host))
     return remote_login(command, password, prompt, linesep, timeout)
 
