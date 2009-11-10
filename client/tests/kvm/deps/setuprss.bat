8fe679d5bc1318d6d8121a503edcf1c2ad428f2a
lmr <lmr@592f7852-d20e-0410-864c-8624ca9c26a4>
2009-11-10 16:23:06 +0000

    KVM test: disable password prompt on resume for Windows guests
    Add a command to setuprss.bat that disables the password prompt on resuming
    from hibernation.
    Note that for this change to take effect the local winutils.iso should be
    updated as well.
    
    Signed-off-by: Michael Goldish <mgoldish@redhat.com>
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@3926 592f7852-d20e-0410-864c-8624ca9c26a4

--- client/tests/kvm/deps/setuprss.bat
+++ client/tests/kvm/deps/setuprss.bat
@@ -5,6 +5,7 @@ copy %rsspath% C:\rss.exe
 net user Administrator /active:yes
 net user Administrator 1q2w3eP
 netsh firewall set opmode disable
+powercfg /G OFF /OPTION RESUMEPASSWORD
 
 reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Run" /v "Remote Shell Server" /d "C:\rss.exe 22" /t REG_SZ /f
 reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\winlogon" /v "AutoAdminLogon" /d "1" /t REG_SZ /f
