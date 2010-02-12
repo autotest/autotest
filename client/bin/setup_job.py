a8131be2ce3c39eb6b7cd2782046912de0a6a73f
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2010-02-12 20:19:05 +0000

    The new flag will prebuild all autotest client tests/site_tests on the server side, when the dev tool chain is not available on the client, by directly invoke the setup method() on the test.
    If this flag is not provided on the command line (default behavior), there should be no regression changes to existing usages.
    
    If provided:
      --setup=all will prebuild all client tests/site_tests.
      --setup=A,B,C,.. will only build the comma separated list of tests supplied.
    
    This new flag does not work with any control file, instead, it works on test classes.
    
    
    from:ericli@google.com
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@4243 592f7852-d20e-0410-864c-8624ca9c26a4

--- client/bin/setup_job.py
+++ client/bin/setup_job.py
@@ -2,67 +2,153 @@
 #
 # Eric Li <ericli@google.com>
 
-import logging, os, pickle, shutil, sys
-from autotest_lib.client.bin import utils
+import logging, os, pickle, sys
+import common
 
-def initialize(client_job):
-    cwd = os.getcwd()
-    os.chdir(client_job.autodir)
-    os.system('tools/make_clean')
-    os.chdir(cwd)
-
-    sys.path.insert(0, client_job.bindir)
-
-    os.environ['AUTODIR'] = client_job.autodir
-    os.environ['AUTODIRBIN'] = client_job.bindir
-    os.environ['PYTHONPATH'] = client_job.bindir
+from autotest_lib.client.bin import client_logging_config
+from autotest_lib.client.bin import job as client_job
+from autotest_lib.client.common_lib import base_job
+from autotest_lib.client.common_lib import error
+from autotest_lib.client.common_lib import logging_manager
+from autotest_lib.client.common_lib import packages
 
 
-# This function was inspired from runtest() on client/common_lib/test.py.
-# Same logic to instantiate a client test object.
-def setup_test(testname, client_job):
-    logging.info('setup %s.' % testname)
+class setup_job(client_job.job):
+    """
+    setup_job is a job which runs client test setup() method at server side.
+
+    This job is used to pre-setup client tests when development toolchain is not
+    available at client.
+    """
+
+    def __init__(self, options):
+        """
+        Since setup_job is a client job but run on a server, it takes no control
+        file as input. So client_job.__init__ is by-passed.
+
+        @param options: an object passed in from command line OptionParser.
+                        See all options defined on client/bin/autotest.
+        """
+        base_job.base_job.__init__(self, options=options)
+        logging_manager.configure_logging(
+            client_logging_config.ClientLoggingConfig(),
+            results_dir=self.resultdir,
+            verbose=options.verbose)
+        self._cleanup_results_dir()
+        self.pkgmgr = packages.PackageManager(
+            self.autodir, run_function_dargs={'timeout':3600})
+
+
+def load_all_client_tests(options):
+    """
+    Load and instantiate all client tests.
+
+    This function is inspired from runtest() on client/common_lib/test.py.
+
+    @param options: an object passed in from command line OptionParser.
+                    See all options defined on client/bin/autotest.
+    """
 
     local_namespace = locals().copy()
     global_namespace = globals().copy()
 
-    outputdir = os.path.join(client_job.resultdir, testname)
-    try:
-        os.makedirs(outputdir)
-    except OSError, oe:
-        print oe
+    all_tests = []
+    for test_base_dir in ['tests', 'site_tests']:
+        testdir = os.path.join(os.environ['AUTODIR'], test_base_dir)
+        for test_name in os.listdir(testdir):
+            job = setup_job(options=options)
+            testbindir = os.path.join(testdir, test_name)
+            local_namespace['testbindir'] = testbindir
 
-    local_namespace['job'] = client_job
-    local_namespace['outputdir'] = outputdir
+            outputdir = os.path.join(job.resultdir, test_name)
+            try:
+                os.makedirs(outputdir)
+            except OSError:
+                pass
 
-    # if the test is local, it can be found in either testdir or site_testdir.
-    # tests in site_testdir override tests defined in testdir.
-    testdir = os.path.join(client_job.autodir, 'site_tests')
-    testbindir = os.path.join(testdir, testname)
-    if not os.path.exists(testbindir):
-        testdir = os.path.join(client_job.autodir, 'tests')
-        testbindir = os.path.join(testdir, testname)
-    local_namespace['testbindir'] = testbindir
-    sys.path.insert(0, testbindir)
+            local_namespace['job'] = job
+            local_namespace['outputdir'] = outputdir
 
-    try:
-        exec("import %s" % testname, local_namespace, global_namespace)
-        exec("auto_test = %s.%s(job, testbindir, outputdir)" %
-              (testname, testname), local_namespace, global_namespace)
-    finally:
-        sys.path.pop(0) # pop up testbindir
+            sys.path.insert(0, testbindir)
+            try:
+                try:
+                    exec("import %s" % test_name, local_namespace,
+                         global_namespace)
+                    exec("auto_test = %s.%s(job, testbindir, outputdir)" %
+                         (test_name, test_name), local_namespace,
+                         global_namespace)
+                    client_test = global_namespace['auto_test']
+                    all_tests.append(client_test)
+                except ImportError:
+                    # this test has only control file but no python.
+                    pass
+            finally:
+                sys.path.pop(0) # pop up testbindir
+    return all_tests
 
-    pwd = os.getcwd()
-    os.chdir(outputdir)
 
-    try:
-        auto_test = global_namespace['auto_test']
-        auto_test.setup()
+def setup_tests(options):
+    """
+    Load and instantiate all client tests.
 
-        # touch .version file under src to prevent further setup on client host.
-        # see client/common_lib/utils.py update_version()
-        versionfile = os.path.join(auto_test.srcdir, '.version')
-        pickle.dump(auto_test.version, open(versionfile, 'w'))
-    finally:
-        os.chdir(pwd)
-        shutil.rmtree(auto_test.tmpdir, ignore_errors=True)
+    This function is inspired from runtest() on client/common_lib/test.py.
+
+    @param options: an object passed in from command line OptionParser.
+                    See all options defined on client/bin/autotest.
+    """
+
+    assert options.client_test_setup, 'Specify prebuild client tests on the ' \
+                                      'command line.'
+
+    requested_tests = options.client_test_setup.split(',')
+    candidates = load_all_client_tests(options)
+
+    if options.client_test_setup.lower().find('all') >= 0:
+        need_to_setup = candidates
+    else:
+        need_to_setup = []
+        for candidate in candidates:
+            if candidate.__class__.__name__ in requested_tests:
+                need_to_setup.append(candidate)
+
+    if need_to_setup:
+        cwd = os.getcwd()
+        os.chdir(need_to_setup[0].job.clientdir)
+        os.system('tools/make_clean')
+        os.chdir(cwd)
+
+    failed_tests = []
+    for setup_test in need_to_setup:
+        test_name = setup_test.__class__.__name__
+        try:
+            outputdir = os.path.join(setup_test.job.resultdir, test_name)
+            try:
+                os.makedirs(outputdir)
+                os.chdir(outputdir)
+            except OSError:
+                pass
+            logging.info('setup %s.' % test_name)
+            setup_test.setup()
+            # Touch .version file under src to prevent further setup on client
+            # host. See client/common_lib/utils.py update_version()
+            if os.path.exists(setup_test.srcdir):
+                versionfile = os.path.join(setup_test.srcdir, '.version')
+                pickle.dump(setup_test.version, open(versionfile, 'w'))
+        except Exception, err:
+            logging.error(err)
+            failed_tests.append(test_name)
+
+    logging.info('############################# SUMMARY '
+                 '#############################')
+
+    # Print out tests that failed
+    if failed_tests:
+        logging.info('Finished setup -- The following tests failed')
+        for failed_test in failed_tests:
+            logging.info(failed_test)
+    else:
+        logging.info('Finished setup -- All tests built successfully')
+    logging.info('######################### END SUMMARY '
+                 '##############################')
+    if failed_tests:
+        raise error.AutoservError('Finished setup with errors.')
