35d9eabfd762c30168dbaeda3cf45b6997f0d686
Martin J. Bligh <mbligh@google.com>
2010-02-18 16:14:22 +0000

    Patch for dbench.py for cross compiling.  The purpose of this change is to make dbench cross-compilable for Chromium OS arm build.
    Should not impact any existing dbench testers.
    
    from: ericli@google.com

--- client/tests/dbench/dbench.py
+++ client/tests/dbench/dbench.py
@@ -12,7 +12,14 @@ class dbench(test.test):
         os.chdir(self.srcdir)
 
         utils.system('patch -p1 < ../dbench_startup.patch')
-        utils.system('./configure')
+        # CBUILD and CHOST is env vars used inside Chromium OS build environment
+        # for cross compiling.
+        if 'CBUILD' in os.environ and 'CHOST' in os.environ:
+            config_params = '--build=%s --host=%s' % (os.environ['CBUILD'],
+                                                      os.environ['CHOST'])
+        else:
+            config_params = ''
+        utils.system('./configure %s' % config_params)
         utils.system('make')
 
 
