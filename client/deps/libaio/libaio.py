926217667aef53dce9b7dafba93df9ad290efcb6
Darin Petkov <petkov@google.com>
2010-02-23 01:38:06 +0000

    Add a patch file to the libaio package so that it builds on ARM. Patch is pulled from debian.
    From: petkov@google.com

--- client/deps/libaio/libaio.py
+++ client/deps/libaio/libaio.py
@@ -10,8 +10,9 @@ def setup(tarball, topdir):
     srcdir = os.path.join(topdir, 'src')
     utils.extract_tarball_to_dir(tarball, srcdir)
     os.chdir(srcdir)
-    utils.system ('make')
-    utils.system ('make prefix=%s install' % topdir)
+    utils.system('patch -p1 < ../00_arches.patch')
+    utils.system('make')
+    utils.system('make prefix=%s install' % topdir)
     os.chdir(topdir)
 
 
@@ -19,6 +20,8 @@ def setup(tarball, topdir):
 # http://www.kernel.org/pub/linux/kernel/people/bcrl/aio/libaio-0.3.92.tar.bz2
 # now grabbing from debian
 # http://ftp.debian.org/debian/pool/main/liba/libaio/libaio_0.3.106.orig.tar.gz
+# architecture patch from here
+# http://git.hadrons.org/?p=debian/pkgs/libaio.git;a=tree;f=debian/patches
 
 pwd = os.getcwd()
 tarball = os.path.join(pwd, 'libaio_0.3.106.orig.tar.gz')
