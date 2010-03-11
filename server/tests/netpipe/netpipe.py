f379806e67273b82e0a0a50a2bea5c5704c02b6b
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2010-03-11 17:42:46 +0000

    add missing files from previous commit
    Signed-off-by: Martin J. Bligh <mbligh@google.com>
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@4291 592f7852-d20e-0410-864c-8624ca9c26a4

---
+++ server/tests/netpipe/netpipe.py
@@ -0,0 +1,50 @@
+from autotest_lib.server import autotest, hosts, subcommand, test
+from autotest_lib.server import utils
+
+class netpipe(test.test):
+    version = 2
+
+    def run_once(self, pair, buffer, upper_bound, variance):
+        print "running on %s and %s\n" % (pair[0], pair[1])
+
+        # Designate a platform label for the server side of tests.
+        server_label = 'net_server'
+
+        server = hosts.create_host(pair[0])
+        client = hosts.create_host(pair[1])
+
+        # If client has the server_label, then swap server and client.
+        platform_label = client.get_platform_label()
+        if platform_label == server_label:
+            (server, client) = (client, server)
+
+        # Disable IP Filters if they are enabled.
+        for m in [client, server]:
+            status = m.run('iptables -L')
+            if not status.exit_status:
+                m.disable_ipfilters()
+
+        server_at = autotest.Autotest(server)
+        client_at = autotest.Autotest(client)
+
+        template = ''.join(["job.run_test('netpipe', server_ip='%s', ",
+                            "client_ip='%s', role='%s', bidirectional=True, ",
+                            "buffer_size=%d, upper_bound=%d,"
+                            "perturbation_size=%d)"])
+
+        server_control_file = template % (server.ip, client.ip, 'server',
+                                          buffer, upper_bound, variance)
+        client_control_file = template % (server.ip, client.ip, 'client',
+                                          buffer, upper_bound, variance)
+
+        server_command = subcommand.subcommand(server_at.run,
+                                    [server_control_file, server.hostname])
+        client_command = subcommand.subcommand(client_at.run,
+                                    [client_control_file, client.hostname])
+
+        subcommand.parallel([server_command, client_command])
+
+        for m in [client, server]:
+            status = m.run('iptables -L')
+            if not status.exit_status:
+                m.enable_ipfilters()
