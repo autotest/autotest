6c65e6fa9593a12e4473a61b5d5d0cfc289b41dd
mbligh <mbligh@592f7852-d20e-0410-864c-8624ca9c26a4>
2010-03-11 18:26:13 +0000

    I've split IPerf out into a control file and an iperf.py file, to avoid redundancy when writing multiple IPerf tests. I'm also using a couple of new functions recently added to disable IP filters and to check for a specific platform label.
    From:kdlucas@google.com
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@4297 592f7852-d20e-0410-864c-8624ca9c26a4

---
+++ server/tests/iperf/iperf.py
@@ -0,0 +1,52 @@
+from autotest_lib.server import autotest, hosts, subcommand, test
+from autotest_lib.server import utils
+
+class iperf(test.test):
+    version = 1
+
+    def run_once(self, pair, udp, bidirectional, time, stream_list):
+        print "running on %s and %s\n" % (pair[0], pair[1])
+
+        # Designate a lable for the server side tests.
+        server_label = 'net_server'
+
+        tagname = "%s_%s" % (pair[0], pair[1])
+        server = hosts.create_host(pair[0])
+        client = hosts.create_host(pair[1])
+
+        # Ensure the client doesn't have the server label.
+        platform_label = client.get_platform_label()
+        if platform_label == server_label:
+            (server, client) = (client, server)
+
+        # Disable IPFilters if they are present.
+        for m in [client, server]:
+            status = m.run('iptables -L')
+            if not status.exit_status:
+                m.disable_ipfilters()
+
+        server_at = autotest.Autotest(server)
+        client_at = autotest.Autotest(client)
+
+        template = ''.join(["job.run_test('iperf', server_ip='%s', client_ip=",
+                            "'%s', role='%s', udp=%s, bidirectional=%s,",
+                            "test_time=%d, stream_list=%s, tag='%s')"])
+
+        server_control_file = template % (server.ip, client.ip, 'server', udp,
+                                          bidirectional, time, stream_list,
+                                          tagname)
+        client_control_file = template % (server.ip, client.ip, 'client', udp,
+                                          bidirectional, time, stream_list,
+                                          tagname)
+
+        server_command = subcommand.subcommand(server_at.run,
+                         [server_control_file, server.hostname])
+        client_command = subcommand.subcommand(client_at.run,
+                         [client_control_file, client.hostname])
+
+        subcommand.parallel([server_command, client_command])
+
+        for m in [client, server]:
+            status = m.run('iptables -L')
+            if not status.exit_status:
+                m.enable_ipfilters()
