e72c455d85590e0fde938f155dc8a0c096174009
Mihai Rusu <dizzy@google.com>
2010-03-19 20:08:00 +0000

    Moved binding/listening on the server socket from barrier.barrier.run_server() to a separate class (barrier.listen_server). Added a keyword argument to barrier to pass a listen_server instance to use it when it needs access to the server socket. When a listen_server instance is not given to barrier it will create one in run_server() (preserving the old semantics). Updated barriertest (used by server side profiler support code) to reuse a listen_server instance in almost all its barrier instances (thus hopefully fixing an issue where we rebind too fast on the same port when packets are in transit from an old connection and get a ECONNRESET on a new connection because of this). Updated unittests.
    Signed-off-by: Mihai Rusu <dizzy@google.com>

--- client/tests/barriertest/barriertest.py
+++ client/tests/barriertest/barriertest.py
@@ -1,5 +1,6 @@
 import time
 from autotest_lib.client.bin import test
+from autotest_lib.client.common_lib import barrier
 
 class barriertest(test.test):
     version = 1
@@ -9,12 +10,13 @@ class barriertest(test.test):
                     hostid, masterid, all_ids):
         profilers = self.job.profilers
 
+        barrier_server = barrier.listen_server(port=11920)
         b0 = self.job.barrier(hostid, "sync_profilers", timeout_start,
-                              port=11920)
+                              listen_server=barrier_server)
         b0.rendezvous_servers(masterid, hostid)
 
         b1 = self.job.barrier(hostid, "start_profilers", timeout_start,
-                              port=11920)
+                              listen_server=barrier_server)
         b1.rendezvous_servers(masterid, hostid)
 
         b2 = self.job.barrier(hostid, "local_sync_profilers", timeout_sync)
@@ -23,12 +25,14 @@ class barriertest(test.test):
         profilers.start(self)
 
         b3 = self.job.barrier(hostid, "stop_profilers", timeout_stop,
-                              port=11920)
+                              listen_server=barrier_server)
         b3.rendezvous_servers(masterid, hostid)
 
         profilers.stop(self)
         profilers.report(self)
 
         b4 = self.job.barrier(hostid, "finish_profilers", timeout_stop,
-                              port=11920)
+                              listen_server=barrier_server)
         b4.rendezvous_servers(masterid, hostid)
+
+        barrier_server.close()
