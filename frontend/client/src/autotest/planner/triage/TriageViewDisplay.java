27fe1a4fef4f6c2ed08810964cca6247b109d720
James Ren <jamesren@google.com>
2010-05-03 20:52:39 +0000

    Refactor Test Planner GWT code. Factoring out common functionality in presenter, display, and tab classes for reuse with other Test Planner tabs.
    Signed-off-by: James Ren <jamesren@google.com>

--- frontend/client/src/autotest/planner/triage/TriageViewDisplay.java
+++ frontend/client/src/autotest/planner/triage/TriageViewDisplay.java
@@ -1,6 +1,7 @@
 package autotest.planner.triage;
 
 import autotest.common.ui.NotifyManager;
+import autotest.planner.TestPlannerDisplay;
 
 import com.google.gwt.event.dom.client.HasClickHandlers;
 import com.google.gwt.event.logical.shared.ResizeEvent;
@@ -12,44 +13,46 @@ import com.google.gwt.user.client.ui.ScrollPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 
-public class TriageViewDisplay implements TriageViewPresenter.Display, ResizeHandler {
-    
+public class TriageViewDisplay implements TestPlannerDisplay,
+        TriageViewPresenter.Display, ResizeHandler {
+
     private static final int HEIGHT_FUDGE = 300;
-    
+
     private VerticalPanel container = new VerticalPanel();
     private ScrollPanel scroll = new ScrollPanel(container);
     private Button triage = new Button("Triage");
-    
+
+    @Override
     public void initialize(HTMLPanel htmlPanel) {
         container.setSpacing(25);
         container.setWidth("90%");
-        
+
         scroll.setSize("100%", getHeightParam(Window.getClientHeight()));
         scroll.setVisible(false);
         triage.setVisible(false);
-        
+
         htmlPanel.add(scroll, "triage_failure_tables");
         htmlPanel.add(triage, "triage_button");
-        
+
         Window.addResizeHandler(this);
     }
-    
+
     @Override
     public void setLoading(boolean loading) {
         NotifyManager.getInstance().setLoading(loading);
         scroll.setVisible(!loading);
-        
+
         scroll.setVisible(true);
         triage.setVisible(true);
     }
-    
+
     @Override
     public FailureTable.Display generateFailureTable(String group, String[] columnNames) {
         FailureTableDisplay display = new FailureTableDisplay(group, columnNames);
         container.add(display);
         return display;
     }
-    
+
     @Override
     public TriagePopup.Display generateTriagePopupDisplay() {
         return new TriagePopupDisplay();
@@ -69,7 +72,7 @@ public class TriageViewDisplay implements TriageViewPresenter.Display, ResizeHan
     public void onResize(ResizeEvent event) {
         scroll.setHeight(getHeightParam(event.getHeight()));
     }
-    
+
     private String getHeightParam(int height) {
         return (height - HEIGHT_FUDGE) + "px";
     }
--- frontend/client/src/autotest/planner/triage/TriageViewTab.java
+++ frontend/client/src/autotest/planner/triage/TriageViewTab.java
@@ -1,35 +1,37 @@
 package autotest.planner.triage;
 
-import autotest.common.ui.TabView;
 import autotest.planner.TestPlanSelector;
+import autotest.planner.TestPlannerDisplay;
+import autotest.planner.TestPlannerPresenter;
+import autotest.planner.TestPlannerTab;
 
-import com.google.gwt.user.client.ui.HTMLPanel;
 
+public class TriageViewTab extends TestPlannerTab {
 
-public class TriageViewTab extends TabView {
-    
-    private TriageViewPresenter presenter;
+    private TriageViewPresenter presenter = new TriageViewPresenter();
     private TriageViewDisplay display = new TriageViewDisplay();
-    
+
     public TriageViewTab(TestPlanSelector selector) {
-        presenter = new TriageViewPresenter(selector, this);
+        super(selector);
     }
-    
+
     @Override
     public String getElementId() {
         return "triage_view";
     }
-    
+
     @Override
-    public void initialize() {
-        super.initialize();
-        display.initialize((HTMLPanel) getWidget());
-        presenter.bindDisplay(display);
+    protected TestPlannerDisplay getDisplay() {
+        return display;
     }
-    
+
     @Override
-    public void refresh() {
-        super.refresh();
-        presenter.refresh();
+    protected TestPlannerPresenter getPresenter() {
+        return presenter;
+    }
+
+    @Override
+    protected void bindDisplay() {
+        presenter.bindDisplay(display);
     }
 }
