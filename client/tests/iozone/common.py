888019bf5dfe21525f551e1fdaa0ce3723687d31
Lucas Meneghel Rodrigues <lmr@redhat.com>
2010-05-04 13:56:05 +0000

    IOzone test: Introduce postprocessing module
    This module contains code to postprocess IOzone data
    in a convenient way so we can generate performance graphs
    and condensed data. The graph generation part depends
    on gnuplot, but if the utility is not present,
    functionality will gracefully degrade.
    
    Use the postprocessing module introduced on the previous
    patch, use it to analyze results and write performance
    graphs and performance tables.
    
    Also, in order for other tests to be able to use the
    postprocessing code, added the right __init__.py
    files, so a simple
    
    from autotest_lib.client.tests.iozone import postprocessing
    
    will work
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

---
+++ client/tests/iozone/common.py
@@ -0,0 +1,8 @@
+import os, sys
+dirname = os.path.dirname(sys.modules[__name__].__file__)
+client_dir = os.path.abspath(os.path.join(dirname, "..", ".."))
+sys.path.insert(0, client_dir)
+import setup_modules
+sys.path.pop(0)
+setup_modules.setup(base_path=client_dir,
+                    root_module_name="autotest_lib.client")
