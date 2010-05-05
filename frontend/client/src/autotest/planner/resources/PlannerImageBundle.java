e0db95dd5a0c732c0518b81d166c486a3cf0430c
jamesren <jamesren@592f7852-d20e-0410-864c-8624ca9c26a4>
2010-05-05 22:56:20 +0000

    Remove usage of ImageBundle (deprecated in GWT 2.0)
    Signed-off-by: James Ren <jamesren@google.com>
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@4471 592f7852-d20e-0410-864c-8624ca9c26a4

--- frontend/client/src/autotest/planner/resources/PlannerImageBundle.java
+++
@@ -1,8 +0,0 @@
-package autotest.planner.resources;
-
-import com.google.gwt.user.client.ui.AbstractImagePrototype;
-import com.google.gwt.user.client.ui.ImageBundle;
-
-public interface PlannerImageBundle extends ImageBundle {
-    public AbstractImagePrototype close();
-}
