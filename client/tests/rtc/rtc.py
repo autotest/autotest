7404898907c16ee41460fb3f795758fadfc43508
lmr <lmr@592f7852-d20e-0410-864c-8624ca9c26a4>
2010-05-25 23:39:21 +0000

    Adding rtctest as client test
    This new autotest module implements a simple test for the driver of realtime
    clock. It do the testing of interrupt, date reading, alarm and
    frequency.
    
    Please refer the Documentation/rtc.txt for details.
    
    Signed-off-by: Jason Wang <jasowang@redhat.com>
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@4560 592f7852-d20e-0410-864c-8624ca9c26a4

---
+++ client/tests/rtc/rtc.py
@@ -0,0 +1,23 @@
+import os
+from autotest_lib.client.bin import test, utils
+from autotest_lib.client.common_lib import error
+
+class rtc(test.test):
+    version = 1
+    preserve_srcdir = True
+
+    def setup(self):
+        os.chdir(self.srcdir)
+        utils.system('make clobber')
+        utils.system('make')
+
+
+    def initialize(self):
+        self.job.require_gcc()
+
+
+    def run_once(self, def_rtc="/dev/rtc0", maxfreq=64):
+        if not os.path.exists(def_rtc):
+            raise error.TestNAError("RTC device %s does not exist" % def_rtc)
+        os.chdir(self.srcdir)
+        utils.system('./rtctest %s %s' % (def_rtc, maxfreq))
