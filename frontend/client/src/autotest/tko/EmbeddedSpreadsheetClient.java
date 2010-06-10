6df983bf79dde4ea6bd47b0445fcd052fba40b4e
jamesren <jamesren@592f7852-d20e-0410-864c-8624ca9c26a4>
2010-06-10 22:32:11 +0000

    Add a listener to the embedded spreadsheet, to open the full TKO results interface upon click.
    Signed-off-by: James Ren <jamesren@google.com>
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@4596 592f7852-d20e-0410-864c-8624ca9c26a4

--- frontend/client/src/autotest/tko/EmbeddedSpreadsheetClient.java
+++ frontend/client/src/autotest/tko/EmbeddedSpreadsheetClient.java
@@ -3,9 +3,13 @@ package autotest.tko;
 import autotest.common.JsonRpcCallback;
 import autotest.common.JsonRpcProxy;
 import autotest.common.spreadsheet.Spreadsheet;
+import autotest.common.spreadsheet.Spreadsheet.CellInfo;
+import autotest.common.spreadsheet.Spreadsheet.SpreadsheetListener;
+import autotest.tko.TkoSpreadsheetUtils.DrilldownType;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.dom.client.Element;
+import com.google.gwt.http.client.URL;
 import com.google.gwt.json.client.JSONNumber;
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.json.client.JSONValue;
@@ -15,8 +19,9 @@ import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 
 import java.util.Collections;
+import java.util.List;
 
-public class EmbeddedSpreadsheetClient implements EntryPoint {
+public class EmbeddedSpreadsheetClient implements EntryPoint, SpreadsheetListener {
     private static class SimpleHeaderField extends HeaderField {
         protected SimpleHeaderField(String name) {
             super(name, name);
@@ -24,7 +29,7 @@ public class EmbeddedSpreadsheetClient implements EntryPoint {
 
         @Override
         public String getSqlCondition(String value) {
-            throw new UnsupportedOperationException("No SQL condition");
+            return getSimpleSqlCondition(name, value);
         }
     }
 
@@ -32,19 +37,30 @@ public class EmbeddedSpreadsheetClient implements EntryPoint {
     public static final String COLUMN_HEADER = "test_name";
     private static final String NO_RESULTS = "There are no results for this query (yet?)";
 
+    public static final String DRILLDOWN_ROW_HEADER = "job_tag";
+    public static final String DRILLDOWN_COLUMN_HEADER = "subdir";
+
     private Label noResults = new Label(NO_RESULTS);
     private Spreadsheet spreadsheet = new Spreadsheet();
     private SpreadsheetDataProcessor spreadsheetProcessor =
             new SpreadsheetDataProcessor(spreadsheet);
+    private List<HeaderField> rowHeader =
+            Collections.singletonList((HeaderField) new SimpleHeaderField(ROW_HEADER));
+    private List<HeaderField> columnHeader =
+            Collections.singletonList((HeaderField) new SimpleHeaderField(COLUMN_HEADER));
+
+    private String afeJobIdStr;
+    private JSONObject condition;
 
     @Override
     public void onModuleLoad() {
         JsonRpcProxy.setDefaultBaseUrl(JsonRpcProxy.TKO_BASE_URL);
-        checkAndRender(Window.Location.getParameter("afe_job_id"));
+        afeJobIdStr = Window.Location.getParameter("afe_job_id");
+        checkAndRender();
     }
 
-    private void checkAndRender(String afeJobIdStr) {
-        final JSONObject condition = getFilterCondition(afeJobIdStr);
+    private void checkAndRender() {
+        condition = getFilterCondition(afeJobIdStr);
         if (condition == null) {
             showNoResults();
         }
@@ -85,11 +101,9 @@ public class EmbeddedSpreadsheetClient implements EntryPoint {
 
     private void renderSpreadsheet(JSONObject condition) {
         spreadsheetProcessor.setDataSource(TestGroupDataSource.getStatusCountDataSource());
-        spreadsheetProcessor.setHeaders(
-                Collections.singletonList(new SimpleHeaderField(ROW_HEADER)),
-                Collections.singletonList(new SimpleHeaderField(COLUMN_HEADER)),
-                new JSONObject());
+        spreadsheetProcessor.setHeaders(rowHeader, columnHeader, new JSONObject());
 
+        spreadsheet.setListener(this);
         RootPanel.get().add(spreadsheet);
         spreadsheetProcessor.refresh(condition, new Command() {
             public void execute() {
@@ -105,4 +119,54 @@ public class EmbeddedSpreadsheetClient implements EntryPoint {
     private native void notifyParent(int width, int height) /*-{
         $wnd.parent.postMessage(width + 'px ' + height + 'px', '*');
     }-*/;
+
+    @Override
+    public void onCellClicked(CellInfo cellInfo, boolean isRightClick) {
+        TestSet testSet = TkoSpreadsheetUtils.getTestSet(
+                cellInfo, condition, rowHeader, columnHeader);
+
+        if (testSet.isSingleTest()) {
+            openTestDetailView(testSet.getTestIndex());
+        } else {
+            openSpreadsheetView(testSet.getPartialSqlCondition(),
+                    TkoSpreadsheetUtils.getDrilldownType(cellInfo));
+        }
+    }
+
+    private void openTestDetailView(int testIdx) {
+        openUrl("/new_tko/#tab_id=test_detail_view&object_id=" + testIdx);
+    }
+
+    private void openSpreadsheetView(String extraCondition, DrilldownType drilldownType) {
+        String drilldownPath;
+
+        switch (drilldownType) {
+        case DRILLDOWN_ROW:
+            drilldownPath = generatePath(DRILLDOWN_ROW_HEADER, COLUMN_HEADER, extraCondition);
+            break;
+        case DRILLDOWN_COLUMN:
+            drilldownPath = generatePath(ROW_HEADER, DRILLDOWN_COLUMN_HEADER, extraCondition);
+            break;
+        case DRILLDOWN_BOTH:
+            drilldownPath = generatePath(
+                    DRILLDOWN_ROW_HEADER, DRILLDOWN_COLUMN_HEADER, extraCondition);
+            break;
+        default:
+            throw new UnsupportedOperationException(
+                    "DrilldownType " + drilldownType + " not supported");
+        }
+
+        openUrl(drilldownPath);
+    }
+
+    private String generatePath(String rowHeader, String columnHeader, String extraCondition) {
+        String condition = "afe_job_id = " + afeJobIdStr + " AND " + extraCondition;
+
+        return "/new_tko/#tab_id=spreadsheet_view&row=" + rowHeader + "&column=" + columnHeader +
+                "&condition=" + URL.encodeComponent(condition, true) + "&show_incomplete=true";
+    }
+
+    private void openUrl(String url) {
+        Window.open(url, "_blank", null);
+    }
 }
