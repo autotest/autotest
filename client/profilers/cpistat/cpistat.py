3512b4afe6aeec1e556dbbbad8cde2c764a08db4
Arun Sharma <aruns@google.com>
2010-07-01 00:30:08 +0000

    Add perf_events based counting/sampling profilers to autotest
    Counting profiler:
    
    Counts cycles, instructions and computes CPI
    
    Sampling profiler:
    
    Uses perf record/report on a per-CPU basis
    
    Signed-off-by: Arun Sharma <aruns@google.com>

---
+++ client/profilers/cpistat/cpistat.py
@@ -0,0 +1,28 @@
+"""
+Uses perf_events to count cycles and instructions
+
+Defaults options:
+job.profilers.add('cpistat', interval=1)
+"""
+import time, os, subprocess
+from autotest_lib.client.bin import profiler
+
+class cpistat(profiler.profiler):
+    version = 1
+
+    def initialize(self, interval = 1):
+        self.interval = interval
+
+
+    def start(self, test):
+        cmd = os.path.join(self.bindir, 'site_cpistat')
+        if not os.path.exists(cmd):
+           cmd = os.path.join(self.bindir, 'cpistat')
+        logfile = open(os.path.join(test.profdir, "cpistat"), 'w')
+        p = subprocess.Popen(cmd, stdout=logfile,
+                             stderr=subprocess.STDOUT)
+        self.pid = p.pid
+
+
+    def stop(self, test):
+        os.kill(self.pid, 15)
---
+++ client/profilers/perf/perf.py
@@ -0,0 +1,53 @@
+"""
+perf is a tool included in the linux kernel tree that
+supports functionality similar to oprofile and more.
+
+@see: http://lwn.net/Articles/310260/
+"""
+
+import time, os, subprocess, signal
+from autotest_lib.client.bin import profiler, os_dep, utils
+
+
+class perf(profiler.profiler):
+    version = 1
+
+    def initialize(self, events="cycles,instructions"):
+        self.events = events
+        self.perf_bin = os_dep.command('perf')
+        perf_help = utils.run('%s report help' % self.perf_bin,
+                              ignore_status=True).stderr
+        self.sort_keys = None
+        for line in perf_help.split('\n'):
+            a = "sort by key(s):"
+            if a in line:
+                line = line.strip(a)
+                self.sort_keys = [k.rstrip(",") for k in line.split() if
+                                  k.rstrip(",") != 'dso']
+        if not self.sort_keys:
+            self.sort_keys = ['comm', 'cpu']
+
+
+    def start(self, test):
+        self.logfile = os.path.join(test.profdir, "perf")
+        cmd = ("%s record -a -o %s -e %s" %
+               (self.perf_bin, self.logfile, self.events))
+        self._process = subprocess.Popen(cmd, shell=True,
+                                         stderr=subprocess.STDOUT)
+
+
+    def stop(self, test):
+        os.kill(self._process.pid, signal.SIGINT)
+        self._process.wait()
+
+
+    def report(self, test):
+        for key in self.sort_keys:
+            reportfile = os.path.join(test.profdir, '%s.comm' % key)
+            cmd = ("%s report -i %s --sort %s,dso" % (self.perf_bin,
+                                                      self.logfile,
+                                                      key))
+            outfile = open(reportfile, 'w')
+            p = subprocess.Popen(cmd, shell=True, stdout=outfile,
+                                 stderr=subprocess.STDOUT)
+            p.wait()
