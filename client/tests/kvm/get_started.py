49474ac725f68deafe713929a8f1325c2e2acb54
Lucas Meneghel Rodrigues <lmr@redhat.com>
2010-07-08 23:20:54 +0000

    KVM test: Add new control.unittests control file
    With this control file, it is possible to run the KVM
    unittests available for a given KVM build.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- client/tests/kvm/get_started.py
+++ client/tests/kvm/get_started.py
@@ -69,7 +69,7 @@ if __name__ == "__main__":
     kvm_test_dir = os.path.dirname(sys.modules[__name__].__file__)
     kvm_test_dir = os.path.abspath(kvm_test_dir)
     config_file_list = ["address_pools.cfg", "build.cfg", "cdkeys.cfg",
-                        "tests_base.cfg", "tests.cfg"]
+                        "tests_base.cfg", "tests.cfg", "unittests.cfg"]
     for config_file in config_file_list:
         src_file = os.path.join(kvm_test_dir, "%s.sample" % config_file)
         dst_file = os.path.join(kvm_test_dir, config_file)
