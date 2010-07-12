f18238826adb8910b74b6f097ccedf5c94dbe349
jamesren <jamesren@592f7852-d20e-0410-864c-8624ca9c26a4>
2010-07-12 21:21:03 +0000

    This file was missing from the patch.
    Signed-off-by: James Ren <jamesren@google.com>
    
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@4719 592f7852-d20e-0410-864c-8624ca9c26a4

--- frontend/client/src/autotest/tko/TestSet.java
+++ frontend/client/src/autotest/tko/TestSet.java
@@ -3,7 +3,7 @@ package autotest.tko;
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.json.client.JSONString;
 
-abstract class TestSet {
+public abstract class TestSet {
     /**
      * Get the full condition args for this test set.
      */
@@ -14,10 +14,10 @@ abstract class TestSet {
     public abstract String getPartialSqlCondition();
     public abstract boolean isSingleTest();
     public abstract int getTestIndex();
-    
+
     public JSONObject getCondition() {
         JSONObject condition = getInitialCondition();
-        String sqlCondition = TkoUtils.getSqlCondition(condition); 
+        String sqlCondition = TkoUtils.getSqlCondition(condition);
         sqlCondition = TkoUtils.joinWithParens(" AND ", sqlCondition, getPartialSqlCondition());
         condition.put("extra_where", new JSONString(sqlCondition));
         return condition;
