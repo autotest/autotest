07779d26300f5785de93c5a86b9dfef32b129973
lmr <lmr@592f7852-d20e-0410-864c-8624ca9c26a4>
2010-08-25 00:29:49 +0000

    Add a helpful function to log.py
    Add a helpful "is_failure" function to log.py for checking if a test status
    should be considered a failure. Also added some comments and whitespace to make
    the related code a little clearer.
    
    Signed-off-by: John Admanski <jadmanski@google.com>
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@4758 592f7852-d20e-0410-864c-8624ca9c26a4

--- client/common_lib/log.py
+++ client/common_lib/log.py
@@ -1,15 +1,27 @@
 import sys, re, traceback
 
-
+# these statuses are ordered such that a status earlier in the list will
+# override a status later in a list (e.g. ERROR during a test will override
+# prior GOOD results, but WARN will not override a FAIL)
 job_statuses = ["TEST_NA", "ABORT", "ERROR", "FAIL", "WARN", "GOOD", "ALERT",
                 "RUNNING", "NOSTATUS"]
 
 def is_valid_status(status):
-    if not re.match(r'(START|INFO|(END )?('+'|'.join(job_statuses)+'))$',
+    if not re.match(r'(START|INFO|(END )?(' + '|'.join(job_statuses) + '))$',
                     status):
         return False
     else:
         return True
+
+
+def is_failure(status):
+    if not is_valid_status(status):
+        return False
+    if status in ('START', 'INFO'):
+        return False
+    if status.startswith('END '):
+        status = status[len('END '):]
+    return job_statuses.index(status) <= job_statuses.index("FAIL")
 
 
 def record(fn):
