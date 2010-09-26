7112e084b02a9fc825da4c011e57bcce197d1f0c
Lucas Meneghel Rodrigues <lmr@redhat.com>
2010-09-26 17:15:16 +0000

    server/git_kernel.py - Use the correct base directory
    The original code of git_kernel.py was attributing an
    incorrect path to the source code directory in the
    client. Fixing that.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- server/git_kernel.py
+++ server/git_kernel.py
@@ -93,18 +93,16 @@ class GitKernel(git.InstallableGitRepo):
             logging.info('Checked out revision: %s', self._revision)
 
         if not builddir:
-            self._build = os.path.join(host.get_tmp_dir(),"build")
+            self._build = os.path.join(host.get_tmp_dir(), "build")
             logging.warning('Builddir %s is not persistent (it will be erased '
                             'in future jobs)', self._build)
 
         # push source to host for install
         logging.info('Pushing %s to host', self.source_material)
         host.send_file(self.source_material, self._build)
-        remote_source_material= os.path.join(self._build,
-                                        os.path.basename(self.source_material))
 
         # use a source_kernel to configure, patch, build and install.
-        sk = source_kernel.SourceKernel(remote_source_material)
+        sk = source_kernel.SourceKernel(self._build)
 
         if build:
             # apply patches
