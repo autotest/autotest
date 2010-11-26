a07892641bac68f5b5e3aa005576e6a1affb554f
Feng Yang <fyang@redhat.com>
2010-11-26 20:07:35 +0000

    KVM-test: Add a new bonding subtest
    In this test, execute bonding_setup.py to setup bonding of guest, do
    basic transfer test, and repeatedly put down/up interfaces by set_link for
    testing high availability.
    
    Changes from v1:
    - Add file transfer test
    - Setup bonding by serial
    - Fix bonding_setup.py
    
    Changes from v2:
    - Drop ping test
    - Assign mac of nic1 to bond0
    - Do some basic test before testing high availability
    - Repeatedly down/up link by monitor cmd
    
    Changes from v3:
    - Fix indentation issue in config file
    - Return 1 when not assign macaddr to bonding_setup.py
    
    Signed-off-by: Feng Yang <fyang@redhat.com>
    Signed-off-by: Amos Kong <akong@redhat.com>

---
+++ client/tests/kvm/tests/nic_bonding.py
@@ -0,0 +1,57 @@
+import logging, time, threading
+from autotest_lib.client.common_lib import error
+from tests import file_transfer
+import kvm_test_utils, kvm_utils
+
+def run_nic_bonding(test, params, env):
+    """
+    Nic bonding test in guest.
+
+    1) Start guest with four nic models.
+    2) Setup bond0 in guest by script bonding_setup.py.
+    3) Execute file transfer test between guest and host.
+    4) Repeatedly put down/up interfaces by set_link
+    5) Execute file transfer test between guest and host.
+
+    @param test: Kvm test object.
+    @param params: Dictionary with the test parameters.
+    @param env: Dictionary with test environment.
+    """
+    def control_link_loop(vm, termination_event):
+        logging.info("Repeatedly put down/up interfaces by set_link")
+        while True:
+            for i in range(len(params.get("nics").split())):
+                linkname = "%s.%s" % (params.get("nic_model"), i)
+                cmd = "set_link %s down" % linkname
+                vm.monitor.cmd(cmd)
+                time.sleep(1)
+                cmd = "set_link %s up" % linkname
+                vm.monitor.cmd(cmd)
+            if termination_event.isSet():
+                break
+
+    timeout = int(params.get("login_timeout", 1200))
+    vm = kvm_test_utils.get_living_vm(env, params.get("main_vm"))
+    session_serial = kvm_test_utils.wait_for_login(vm, 0, timeout, 0, 2,
+                                                   serial=True)
+    script_path = kvm_utils.get_path(test.bindir, "scripts/bonding_setup.py")
+    vm.copy_files_to(script_path, "/tmp/bonding_setup.py")
+    cmd = "python /tmp/bonding_setup.py %s" % vm.get_mac_address()
+    s, o = session_serial.get_command_status_output(cmd)
+    if s != 0:
+        logging.debug(o)
+        raise error.TestFail("Fail to setup bonding")
+
+    termination_event = threading.Event()
+    t = threading.Thread(target=control_link_loop,
+                         args=(vm, termination_event))
+    try:
+        logging.info("Do some basic test before testing high availability")
+        file_transfer.run_file_transfer(test, params, env)
+        t.start()
+        logging.info("Do file transfer testing")
+        file_transfer.run_file_transfer(test, params, env)
+    finally:
+        termination_event.set()
+        t.join(10)
+        session_serial.close()
