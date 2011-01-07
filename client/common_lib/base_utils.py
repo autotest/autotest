91886dd849273dbadc97515775e9c1bd9fadc265
Shaun Ruffell <sruffell@digium.com>
2011-01-07 02:25:53 +0000

    base_utils: Simpler version of fix present on r5074
    Signed-off-by: Shaun Ruffell <sruffell@digium.com>

--- client/common_lib/base_utils.py
+++ client/common_lib/base_utils.py
@@ -245,7 +245,7 @@ def matrix_to_string(matrix, header=None):
         for column in header:
             lengths.append(len(column))
     for row in matrix:
-        for i, column in zip(range(len(row)), row):
+        for i, column in enumerate(row):
             cl = len(column)
             try:
                 ml = lengths[i]
