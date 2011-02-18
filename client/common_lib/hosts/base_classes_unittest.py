c96e2e301092ebe45fe42e930d16c8d102243cce
Gregory P. Smith <gps@google.com>
2011-02-18 01:16:59 +0000

    Fix check_diskspace to work at a granularity less than 1GB
    This was breaking a check we have that there be 100MB free
    on / by making it always appear true as it could no longer
    measure a value that low.
    
    The previous change inadvertently limited the granularity of the disk
    space to check to whole integer GB rather than fractional.  This gives
    it a granularity of MB so that a test for gb=0.1 works properly again.
    
    Signed-off-by: Gregory Smith <gps@google.com>

--- client/common_lib/hosts/base_classes_unittest.py
+++ client/common_lib/hosts/base_classes_unittest.py
@@ -3,6 +3,7 @@
 import unittest
 import common
 
+from autotest_lib.client.common_lib import error, utils
 from autotest_lib.client.common_lib.test_utils import mock
 from autotest_lib.client.common_lib.hosts import base_classes
 
@@ -19,6 +20,23 @@ class test_host_class(unittest.TestCase):
     def test_run_output_notimplemented(self):
         host = base_classes.Host()
         self.assertRaises(NotImplementedError, host.run_output, "fake command")
+
+
+    def test_check_diskspace(self):
+        self.god.stub_function(base_classes.Host, 'run')
+        host = base_classes.Host()
+        host.hostname = 'unittest-host'
+        test_df_tail = ('/dev/sda1                    1061       939'
+                        '       123      89% /')
+        fake_cmd_status = utils.CmdResult(exit_status=0, stdout=test_df_tail)
+        host.run.expect_call('df -PB 1000000 /foo | tail -1').and_return(
+                fake_cmd_status)
+        self.assertRaises(error.AutoservDiskFullHostError,
+                          host.check_diskspace, '/foo', 0.2)
+        host.run.expect_call('df -PB 1000000 /foo | tail -1').and_return(
+                fake_cmd_status)
+        host.check_diskspace('/foo', 0.1)
+        self.god.check_playback()
 
 
 if __name__ == "__main__":
