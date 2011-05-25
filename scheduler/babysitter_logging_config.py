5a7ccbfa4495d45ddd380945b18f393fcf639d34
James Laska <jlaska@redhat.com>
2011-05-25 20:12:12 +0000

    scheduler monitor (monitor_db_babysitter): Add --background option
    The init script libraries for Red Hat based systems don't contain
    the start-stop-daemon function present on /lib/lsb/init-functions,
    commonly present on debian/ubuntu boxes. So, in order to implement
    a init script for RH-based systems, introduce the --background
    option for the scheduler monitor, which will fork it and detach it
    from the parent terminal. In order to do that, defer the logging
    configuration a little, also modify the interfaces for the logging
    config functions of this module a little.
    
    Signed-off-by: James Laska <jlaska@redhat.com>
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- scheduler/babysitter_logging_config.py
+++ scheduler/babysitter_logging_config.py
@@ -3,8 +3,13 @@ import logging
 from autotest_lib.client.common_lib import logging_config
 
 class BabysitterLoggingConfig(logging_config.LoggingConfig):
+    def __init__(self, use_console=True):
+        self.use_console = use_console
+        super(BabysitterLoggingConfig, self).__init__()
+
     def configure_logging(self):
-        super(BabysitterLoggingConfig, self).configure_logging(use_console=True)
+        super(BabysitterLoggingConfig, self).configure_logging(
+                                                  use_console=self.use_console)
 
         self.add_file_handler(self.get_timestamped_log_name('babysitter'),
                               logging.DEBUG,
