31089aebb88df5fc4b477dca3a12cc3b5c1f65b3
Lucas Meneghel Rodrigues <lmr@redhat.com>
2011-06-22 21:37:21 +0000

    Add Afe special SQLite based DB backend
    For the database unittests, the same change made to support
    the method get_from_clause on the MySQL backend (see 5408)
    has to be made for the SQLite backend, as the unittests run
    under this very database engine.
    
    So we had to end up adding a new engine that supports this
    additional method on compiler, so we can fix the unittest
    
    autotest_lib.frontend.afe.rpc_interface_unittest
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

---
+++ frontend/db/backends/afe_sqlite/base.py
@@ -0,0 +1,15 @@
+"""
+Autotest frontend SQLite based compiler.
+"""
+from django.db.backends.sqlite3.base import DatabaseOperations as SQLiteDatabaseOperations
+from django.db.backends.sqlite3.base import DatabaseWrapper as SQLiteDatabaseWrapper
+
+
+class DatabaseOperations(SQLiteDatabaseOperations):
+    compiler_module = "autotest_lib.frontend.db.backends.afe_sqlite.compiler"
+
+
+class DatabaseWrapper(SQLiteDatabaseWrapper):
+    def __init__(self, *args, **kwargs):
+        super(DatabaseWrapper, self).__init__(*args, **kwargs)
+        self.ops = DatabaseOperations()
---
+++ frontend/db/backends/afe_sqlite/creation.py
@@ -0,0 +1,1 @@
+from django.db.backends.sqlite3.creation import *
