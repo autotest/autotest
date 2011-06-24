a2d3c15afba201d2862174dbc4693a8dd841fa6d
lmr <lmr@592f7852-d20e-0410-864c-8624ca9c26a4>
2011-06-24 18:34:45 +0000

    Fix bug #55: 'FsdevManager' object has no attribute 'check_mount_point'
    This fixes bug 55, found by Shrirang Phansalkar when trying to use an
    API present on fsdev_disks.py: Some needed methods were missing from
    the base implementation present on fsdev_mgr, map_drive_name and
    check_mount_point. This patch adds them.
    
    Signed-off-by: Gregory P. Smith <gps@google.com>
    
    
    git-svn-id: svn://test.kernel.org/autotest/trunk@5449 592f7852-d20e-0410-864c-8624ca9c26a4

--- client/bin/fsdev_mgr.py
+++ client/bin/fsdev_mgr.py
@@ -12,13 +12,34 @@ class BaseFsdevManager(object):
         pass
 
 
-    def include_partition(self, partname):
+    def include_partition(self, part_name):
         # Client to fill in logic that will pick the right partitions
         return False
 
 
-    def map_drive_name(self, partname):
-        return partname
+    def map_drive_name(self, part_name):
+        return part_name
+
+
+    def check_mount_point(self, part_name, mount_point):
+        """
+        @param part_name: A partition name such as 'sda3' or similar.
+        @param mount_point: A mount point such as '/usr/local' or an empty
+                string if no mount point is known.
+
+        @returns The expected mount point for part_name or a false value
+                (None or '') if the client should not mount this partition.
+        """
+        return mount_point
+
+
+    def use_partition(self, part_name):
+        """
+        @param part_name: A partition name such as 'sda3' or similar.
+
+        @returns bool, should we use this partition for testing?
+        """
+        return True
 
 
 SiteFsdevManager = utils.import_site_class(
