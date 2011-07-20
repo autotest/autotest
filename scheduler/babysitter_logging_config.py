e500a6d166d10ec71e5298658381b34df20782e6
James Laska <jlaska@redhat.com>
2011-07-20 02:35:07 +0000

    scheduler: Fix console logging when running babysitter as daemon
    Prior to this fix, output from 'monitor_babysitter_db --background' still goes
    to stdout.  This later results in InputError failures when the calling tty
    exits and stdout no longer exists.
    
    Signed-off-by: James Laska <jlaska@redhat.com>

--- scheduler/babysitter_logging_config.py
+++ scheduler/babysitter_logging_config.py
@@ -4,8 +4,7 @@ from autotest_lib.client.common_lib import logging_config
 
 class BabysitterLoggingConfig(logging_config.LoggingConfig):
     def __init__(self, use_console=True):
-        self.use_console = use_console
-        super(BabysitterLoggingConfig, self).__init__()
+        super(BabysitterLoggingConfig, self).__init__(use_console=use_console)
 
     def configure_logging(self):
         super(BabysitterLoggingConfig, self).configure_logging(
