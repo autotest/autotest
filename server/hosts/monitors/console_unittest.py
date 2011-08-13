55414e4f8a78183db3df9278e51823d2722af2ef
Gregory P. Smith <gps@google.com>
2011-08-13 23:34:50 +0000

    Compress /var/log/messages during client test sysinfo collection
    Compress /var/log/messages during client test sysinfo collection.
    Compress all log messages saved by the autoserv logfile_monitor
    system before writing them to disk.
    
    Both of these help alleviate the problem of an insane amount of
    disk space being used by copies of kernel logs that occur during
    some stress tests that are intentionally triggering verbose error
    logs from the kernel.
    
    Side effect: there will be multiple logfile_monitor.log.nnnn-nnn.gz
    files instead of a single logfile_monitor.log file as the logfile
    monitor is stopped and started a few times during some autoserv runs.
    
    Signed-off-by: Gregory Smith <gps@google.com>

---
+++ server/hosts/monitors/console_unittest.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+"""Tests for console.py"""
+
+import os, shutil, signal, StringIO, tempfile
+
+import common
+from autotest_lib.client.common_lib.test_utils import mock
+from autotest_lib.client.common_lib.test_utils import unittest
+from autotest_lib.server.hosts.monitors import console
+
+class console_test(unittest.TestCase):
+    def setUp(self):
+        self.god = mock.mock_god()
+        self.tempdir = tempfile.mkdtemp()
+
+
+    def tearDown(self):
+        shutil.rmtree(self.tempdir)
+
+
+    def test_open_logfile(self):
+        path = os.path.join(self.tempdir, 'its-log-log')
+        fun_log = console._open_logfile(path)
+        fun_log.write("it's big it's heavy it's wood.\n")
+        fun_log.close()
+
+        # Open it again to ensure the original gets renamed.
+        fun_log = console._open_logfile(path)
+        fun_log.write("it's better than bad, it's good!\n")
+        fun_log.close()
+
+        log_file_list = os.listdir(self.tempdir)
+        self.assertEqual(2, len(log_file_list))
+        for name in log_file_list:
+            self.assertTrue(name.startswith('its-log-log.'),
+                            'unexpected name %s' % name)
+            self.assertTrue(name.endswith('.gz'), 'unexpected name %s' % name)
+
+
+    def test_logfile_close_signal_handler(self):
+        self.god.stub_function(os, 'exit')
+        os.exit.expect_call(1)
+        logfile = StringIO.StringIO()
+        console._set_logfile_close_signal_handler(logfile)
+        try:
+            self.assertFalse(logfile.closed)
+            os.kill(os.getpid(), signal.SIGTERM)
+        finally:
+            console._unset_signal_handler()
+        self.god.check_playback()
+        self.assertTrue(logfile.closed)
+
+
+if __name__ == '__main__':
+    unittest.main()
