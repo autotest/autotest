955ffeec0f24f1391ccbc73074b18ce12b458a51
Mike Truty <truty@chromium.org>
2011-08-30 18:23:51 +0000

    frontend: Eliminate Django 1.3 load_template_source DeprecationWarning
    Message that no longer emits:
    django.template.loaders.filesystem.load_template_source' is deprecated; use
    'django.template.loaders.filesystem.Loader
    
    TEST=ran local dash gen that uses templates and no longer see warning message.
    
    Signed-off-by: Mike Truty <truty@chromium.org>

--- frontend/settings.py
+++ frontend/settings.py
@@ -81,9 +81,9 @@ SECRET_KEY = 'pn-t15u(epetamdflb%dqaaxw+5u&2#0u-jah70w1l*_9*)=n7'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-#     'django.template.loaders.eggs.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
