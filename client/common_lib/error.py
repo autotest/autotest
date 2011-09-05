7373388dec28538230588a0fa8b61b45455e100b
Lucas Meneghel Rodrigues <lmr@redhat.com>
2011-09-05 21:53:05 +0000

    error: Make InstallTimeoutError to inherit from JobError
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- client/common_lib/error.py
+++ client/common_lib/error.py
@@ -311,6 +311,7 @@ class AutotestTimeoutError(AutotestError):
     """This exception is raised when an autotest test exceeds the timeout
     parameter passed to run_timed_test and is killed.
     """
+    pass
 
 
 class HostRunErrorMixIn(Exception):
@@ -329,7 +330,7 @@ class HostRunErrorMixIn(Exception):
         return self.description + '\n' + repr(self.result_obj)
 
 
-class HostInstallTimeoutError(Exception):
+class HostInstallTimeoutError(JobError):
     """
     Indicates the machine failed to be installed after the predetermined
     timeout.
