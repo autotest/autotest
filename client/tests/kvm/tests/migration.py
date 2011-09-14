0c3a40f752163a6a087e034c1b1a8f05803420f5
Lucas Meneghel Rodrigues <lmr@redhat.com>
2011-09-14 17:13:53 -0300

    KVM test: migration subtest - Introduce migration_living_guest param
    In order to test a special guest configuration (special blkdebug
    config files to inject errors on the block subsystem), added a
    migration_living_guest param. If the param is set to "no", no
    attempt to have a session on the guest will be made, since the
    guest block subsystem will stop when the first error is injected.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- client/tests/kvm/tests/migration.py
+++ client/tests/kvm/tests/migration.py
@@ -36,75 +36,82 @@ def run_migration(test, params, env):
         mig_speed = params.get("mig_speed", "1G")
         return vm.monitor.migrate_set_speed(mig_speed)
 
-    vm = env.get_vm(params["main_vm"])
-    vm.verify_alive()
-    timeout = int(params.get("login_timeout", 360))
-    session = vm.wait_for_login(timeout=timeout)
-
     mig_timeout = float(params.get("mig_timeout", "3600"))
     mig_protocol = params.get("migration_protocol", "tcp")
     mig_cancel_delay = int(params.get("mig_cancel") == "yes") * 2
     offline = params.get("offline", "no") == "yes"
     check = params.get("vmstate_check", "no") == "yes"
+    living_guest_os = params.get("migration_living_guest", "yes") == "yes"
 
-    # Get the output of migration_test_command
-    test_command = params.get("migration_test_command")
-    reference_output = session.cmd_output(test_command)
+    vm = env.get_vm(params["main_vm"])
+    vm.verify_alive()
 
-    # Start some process in the background (and leave the session open)
-    background_command = params.get("migration_bg_command", "")
-    session.sendline(background_command)
-    time.sleep(5)
-
-    # Start another session with the guest and make sure the background
-    # process is running
-    session2 = vm.wait_for_login(timeout=timeout)
-
-    try:
-        check_command = params.get("migration_bg_check_command", "")
-        session2.cmd(check_command, timeout=30)
-        session2.close()
-
-        # run some functions before migrate start.
-        pre_migrate = get_functions(params.get("pre_migrate"), locals())
-        for func in pre_migrate:
-            func()
-
-        # Migrate the VM
-        vm.migrate(mig_timeout, mig_protocol, mig_cancel_delay, offline, check)
-
-        # run some functions after migrate finish.
-        post_migrate = get_functions(params.get("post_migrate"), locals())
-        for func in post_migrate:
-            func()
-
-        # Log into the guest again
-        logging.info("Logging into guest after migration...")
-        session2 = vm.wait_for_login(timeout=30)
-        logging.info("Logged in after migration")
-
-        # Make sure the background process is still running
-        session2.cmd(check_command, timeout=30)
+    if living_guest_os:
+        timeout = int(params.get("login_timeout", 360))
+        session = vm.wait_for_login(timeout=timeout)
 
         # Get the output of migration_test_command
-        output = session2.cmd_output(test_command)
+        test_command = params.get("migration_test_command")
+        reference_output = session.cmd_output(test_command)
 
-        # Compare output to reference output
-        if output != reference_output:
-            logging.info("Command output before migration differs from "
-                         "command output after migration")
-            logging.info("Command: %s", test_command)
-            logging.info("Output before:" +
-                         virt_utils.format_str_for_message(reference_output))
-            logging.info("Output after:" +
-                         virt_utils.format_str_for_message(output))
-            raise error.TestFail("Command '%s' produced different output "
-                                 "before and after migration" % test_command)
+        # Start some process in the background (and leave the session open)
+        background_command = params.get("migration_bg_command", "")
+        session.sendline(background_command)
+        time.sleep(5)
 
-    finally:
-        # Kill the background process
-        if session2 and session2.is_alive():
-            session2.cmd_output(params.get("migration_bg_kill_command", ""))
+        # Start another session with the guest and make sure the background
+        # process is running
+        session2 = vm.wait_for_login(timeout=timeout)
 
-    session2.close()
-    session.close()
+        try:
+            check_command = params.get("migration_bg_check_command", "")
+            session2.cmd(check_command, timeout=30)
+            session2.close()
+
+            # run some functions before migrate start.
+            pre_migrate = get_functions(params.get("pre_migrate"), locals())
+            for func in pre_migrate:
+                func()
+
+            # Migrate the VM
+            vm.migrate(mig_timeout, mig_protocol, mig_cancel_delay, offline,
+                       check)
+
+            # run some functions after migrate finish.
+            post_migrate = get_functions(params.get("post_migrate"), locals())
+            for func in post_migrate:
+                func()
+
+            # Log into the guest again
+            logging.info("Logging into guest after migration...")
+            session2 = vm.wait_for_login(timeout=30)
+            logging.info("Logged in after migration")
+
+            # Make sure the background process is still running
+            session2.cmd(check_command, timeout=30)
+
+            # Get the output of migration_test_command
+            output = session2.cmd_output(test_command)
+
+            # Compare output to reference output
+            if output != reference_output:
+                logging.info("Command output before migration differs from "
+                             "command output after migration")
+                logging.info("Command: %s", test_command)
+                logging.info("Output before:" +
+                            virt_utils.format_str_for_message(reference_output))
+                logging.info("Output after:" +
+                             virt_utils.format_str_for_message(output))
+                raise error.TestFail("Command '%s' produced different output "
+                                    "before and after migration" % test_command)
+
+        finally:
+            # Kill the background process
+            if session2 and session2.is_alive():
+                session2.cmd_output(params.get("migration_bg_kill_command", ""))
+
+        session2.close()
+        session.close()
+    else:
+        # Just migrate without depending on a living guest OS
+        vm.migrate(mig_timeout, mig_protocol, mig_cancel_delay, offline, check)
