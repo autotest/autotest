a7ad43f64ed757d543068891c3e87cb64f68cc94
Lucas Meneghel Rodrigues <lmr@redhat.com>
2011-10-04 20:34:45 -0300

    client.tests: Introducing libvirt test
    Introduce libvirt test. The small amount of actual code
    on this test is made possible by code reuse, made possible
    by the kvm refactor patchset + the previous patches on
    this set.
    
    The obvious difference is that some new parameters had
    to be introduced to accomplish libvirt testing. A more
    complete description of such parameters will be present
    on our online documentation (wiki).
    
    Signed-off-by: Martin Jenner <mjenner@redhat.com>
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

---
+++ client/tests/libvirt/libvirt.py
@@ -0,0 +1,9 @@
+from autotest_lib.client.virt import virt_test
+
+class libvirt(virt_test.virt_test):
+    """
+    Suite of libvirt virtualization functional tests.
+
+    @copyright: Red Hat 2011
+    """
+    pass
