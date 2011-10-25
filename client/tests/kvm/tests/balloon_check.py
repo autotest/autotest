0a3bd4474c1b27d17e48608878b67e0cb395cf61
Yiqiao Pu <ypu@redhat.com>
2011-10-25 11:41:51 +0800

    KVM-test: Balloon check test case improve and subtest add
    This modify does following changes:
      - Make the range of memory change from (0.65, 0.9) to
        (Free memory， Total memory)
      - Add subtests as migrate when memory change
      - Add offset for memory read inside geust. Otherwise it will
        cause a test failed
      - Add monitor cmdline support for both qmp and human monitor
    
    Changes from v1:
      - Fix the parameters problem and signed off author
      - Add subtest shutdown
    Changes from v2:
      - Change the run_sub_test to run_virt_sub_test
    Changes from v3:
      - Make base test default in configure files
      - Delete lines commented out
    
    Signed-off-by: Feng Yang <fyang@redhat.com>
    Signed-off-by: Yiqiao Pu <ypu@redhat.com>

--- client/tests/kvm/tests/balloon_check.py
+++ client/tests/kvm/tests/balloon_check.py
@@ -1,13 +1,13 @@
 import re, logging, random, time
 from autotest_lib.client.common_lib import error
-from autotest_lib.client.virt import kvm_monitor
-
+from autotest_lib.client.virt import kvm_monitor, virt_test_utils, aexpect
 
 def run_balloon_check(test, params, env):
     """
     Check Memory ballooning:
     1) Boot a guest
-    2) Change the memory between 60% to 95% of memory of guest using ballooning
+    2) Change the memory between MemFree to Assigned memory of memory
+       of guest using ballooning
     3) check memory info
 
     @param test: kvm test object
@@ -27,11 +27,10 @@ def run_balloon_check(test, params, env):
         except kvm_monitor.MonitorError, e:
             logging.error(e)
             fail += 1
-            return 0
-        return int(re.findall("\d+", output)[0]), fail
+            return 0, fail
+        return int(re.findall("\d+", str(output))[0]), fail
 
-
-    def balloon_memory(new_mem):
+    def balloon_memory(new_mem, offset):
         """
         Baloon memory to new_mem and verifies on both qemu monitor and
         guest OS if change worked.
@@ -40,9 +39,12 @@ def run_balloon_check(test, params, env):
         @return: Number of failures occurred during operation.
         """
         fail = 0
+        cur_mem, fail = check_ballooned_memory()
+        if params.get("monitor_type") == "qmp":
+            new_mem = new_mem * 1024 * 1024
         logging.info("Changing VM memory to %s", new_mem)
         # This should be replaced by proper monitor method call
-        vm.monitor.cmd("balloon %s" % new_mem)
+        vm.monitor.send_args_cmd("balloon value=%s" % new_mem)
         time.sleep(20)
 
         ballooned_mem, cfail = check_ballooned_memory()
@@ -55,7 +57,10 @@ def run_balloon_check(test, params, env):
 
         # Verify whether the guest OS reports the correct new memory
         current_mem_guest = vm.get_current_memory_size()
-
+        fail += cfail
+        current_mem_guest = current_mem_guest + offset
+        if params.get("monitor_type") == "qmp":
+            current_mem_guest = current_mem_guest * 1024 * 1024
         # Current memory figures will allways be a little smaller than new
         # memory. If they are higher, ballooning failed on guest perspective
         if current_mem_guest > new_mem:
@@ -64,12 +69,12 @@ def run_balloon_check(test, params, env):
             fail += 1
         return fail
 
-
     fail = 0
     vm = env.get_vm(params["main_vm"])
     vm.verify_alive()
     timeout = int(params.get("login_timeout", 360))
     session = vm.wait_for_login(timeout=timeout)
+    balloon_chk_cmd = params.get("balloon_chk_cmd")
 
     # Upper limit that we can raise the memory
     vm_assigned_mem = int(params.get("mem"))
@@ -82,7 +87,6 @@ def run_balloon_check(test, params, env):
         logging.error("    Assigned to VM: %s", vm_assigned_mem)
         logging.error("    Reported by guest OS at boot: %s", boot_mem)
         fail += 1
-
     # Check if info balloon works or not
     current_vm_mem, cfail = check_ballooned_memory()
     if cfail:
@@ -90,15 +94,54 @@ def run_balloon_check(test, params, env):
     if current_vm_mem:
         logging.info("Current VM memory according to ballooner: %s",
                      current_vm_mem)
+    # Get the offset of memory report by guest system
+    guest_memory = vm.get_current_memory_size()
+    offset = vm_assigned_mem - guest_memory
 
-    # Reduce memory to random size between 60% to 95% of max memory size
-    percent = random.uniform(0.6, 0.95)
-    new_mem = int(percent * vm_assigned_mem)
-    fail += balloon_memory(new_mem)
+    # Reduce memory to random size between Free memory
+    # to max memory size
+    s, o = session.cmd_status_output("cat /proc/meminfo")
+    if s != 0:
+        raise error.TestError("Can not get guest memory information")
 
+    vm_mem_free = int(re.findall('MemFree:\s+(\d+).*', o)[0]) / 1024
+
+    new_mem = int(random.uniform(vm_assigned_mem - vm_mem_free, vm_assigned_mem))
+    fail += balloon_memory(new_mem, offset)
+    # Run option test after evict memory
+    if params.has_key('sub_balloon_test_evict'):
+        balloon_test = params['sub_balloon_test_evict']
+        virt_test_utils.run_virt_sub_test(test, params, env, sub_type=balloon_test)
+        if balloon_test == "shutdown" :
+            logging.info("Guest shutdown normally after balloon")
+            exit(0)
     # Reset memory value to original memory assigned on qemu. This will ensure
     # we won't trigger guest OOM killer while running multiple iterations
-    fail += balloon_memory(vm_assigned_mem)
+    fail += balloon_memory(vm_assigned_mem, offset)
+
+    # Run sub test after enlarge memory
+    if params.has_key('sub_balloon_test_enlarge'):
+        balloon_test = params['sub_balloon_test_enlarge']
+        virt_test_utils.run_virt_sub_test(test, params, env, sub_type=balloon_test)
+        if balloon_test == "shutdown" :
+            logging.info("Guest shutdown normally after balloon")
+            exit(0)
+
+    #Check memory after sub test running
+    logging.info("Check memory after tests")
+    boot_mem = vm.get_memory_size()
+    if boot_mem != vm_assigned_mem:
+        fail += 1
+    # Check if info balloon works or not
+    current_vm_mem, cfail = check_ballooned_memory()
+    if params.get("monitor_type") == "qmp":
+        current_vm_mem = current_vm_mem / 1024 / 1024
+    if current_vm_mem != vm_assigned_mem:
+        fail += 1
+    logging.error("Memory size after tests:")
+    logging.error("    Assigned to VM: %s", vm_assigned_mem)
+    logging.error("    Reported by guest OS: %s", boot_mem)
+    logging.error("    Reported by monitor: %s", current_vm_mem)
 
     # Close stablished session
     session.close()
