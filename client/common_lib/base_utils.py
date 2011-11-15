41407b8989cd6bac39492a276d00cd176c8dbae4
David Greenberg <david.greenberg@twosigma.com>
2011-11-15 18:08:26 +0000

    Added async job to client
    Logs are no longer delayed--they're written immediately. Also, the overridable kill function now takes no arguments
    
    Implemented asynchronous job mixin for running on hosts
    
    Changed nuke_subprocess to a plain os.kill() to avoid the sleep(1) in nuke_subprocess
    
    example: autotest/server/tests/async/async.py
    from autotest_lib.server import hosts, test
    
    class async(test.test):
        version = 1
    
        def run_once(self, machine):
            # Designate a label for the server side tests.
            server = hosts.create_host(machine)
    
            job = server.run_async("sleep 1 && echo \"test1\" &&"
                                   " sleep 3 && echo \"test2\"")
    
            print job.wait_for()
    
    Signed-off-by: Jiří Župka <jzupka@redhat.com>
    Signed-off-by: David Greenberg <dsg123456789@gmail.com>

--- client/common_lib/base_utils.py
+++ client/common_lib/base_utils.py
@@ -4,7 +4,7 @@
 import os, pickle, random, re, resource, select, shutil, signal, StringIO
 import socket, struct, subprocess, sys, time, textwrap, traceback, urlparse
 import warnings, smtplib, logging, urllib2
-from threading import Thread, Event
+from threading import Thread, Event, Lock
 try:
     import hashlib
 except ImportError:
@@ -118,6 +118,118 @@ class BgJob(object):
     def _reset_sigpipe(self):
         signal.signal(signal.SIGPIPE, signal.SIG_DFL)
 
+class AsyncJob(BgJob):
+    def __init__(self, command, stdout_tee=None, stderr_tee=None, verbose=True,
+                 stdin=None, stderr_level=DEFAULT_STDERR_LEVEL, kill_func=None):
+        super(AsyncJob, self).__init__(command, stdout_tee=stdout_tee,
+            stderr_tee=stderr_tee, verbose=verbose, stdin=stdin,
+            stderr_level=stderr_level)
+
+        #start time for CmdResult
+        self.start_time = time.time()
+
+        if kill_func is None:
+            self.kill_func = self._kill_self_process
+        else:
+            self.kill_func = kill_func
+
+        #we're going to make some threads to drain the stdout and stderr
+        def drainer(input, output, lock):
+            """
+            input is a pipe and output is file-like. if lock is non-None, then
+            we assume output isn't threadsafe
+            """
+            try:
+                while True:
+                    tmp = os.read(input.fileno(), 1024)
+                    if tmp == '':
+                        break
+                    if lock is not None:
+                        lock.acquire()
+                    for f in filter(lambda x: x is not None, output):
+                        f.write(tmp)
+                    if lock is not None:
+                        lock.release()
+            except:
+                pass
+
+        self.stdout_lock = Lock()
+        self.stdout_file = StringIO.StringIO()
+        self.stdout_thread = Thread(target=drainer, name=("%s-stdout"%command),
+                 args=(self.sp.stdout, (self.stdout_file, self.stdout_tee),
+                       self.stdout_lock))
+        self.stdout_thread.daemon = True
+
+        self.stderr_lock = Lock()
+        self.stderr_file = StringIO.StringIO()
+        self.stderr_thread = Thread(target=drainer, name=("%s-stderr"%command),
+                 args=(self.sp.stderr, (self.stderr_file, self.stderr_tee),
+                       self.stderr_lock))
+        self.stderr_thread.daemon = True
+
+        self.stdout_thread.start()
+        self.stderr_thread.start()
+
+    def output_prepare(self, stdout_file=None, stderr_file=None):
+        raise NotImplementedError("This object automatically prepares its own "
+            "output")
+
+    def process_output(self, stdout=True, final_read=False):
+        raise NotImplementedError("This object has background threads "
+            "automatically polling the process. Use the locked accessors")
+
+    def get_stdout(self):
+        self.stdout_lock.acquire()
+        tmp = self.stdout_file.getvalue()
+        self.stdout_lock.release()
+        return tmp
+
+    def get_stderr(self):
+        self.stderr_lock.acquire()
+        tmp = self.stderr_file.getvalue()
+        self.stderr_lock.release()
+        return tmp
+
+    def cleanup(self):
+        raise NotImplementedError("This must be waited for to get a result")
+
+    def _kill_self_process(self):
+        try:
+            os.kill(self.sp.pid, signal.SIGTERM)
+        except OSError:
+            pass #don't care if the process is already gone, since that was the goal
+
+    def wait_for(self, timeout=None):
+        if timeout is None:
+            self.sp.wait()
+
+        if timeout > 0:
+            start_time = time.time()
+            while time.time() - start_time < timeout:
+                self.result.exit_status = self.sp.poll()
+                if self.result.exit_status is not None:
+                    break
+        #first need to kill the threads and process, then no more locking
+        #issues for superclass's cleanup function
+        self.kill_func()
+
+        #we need to fill in parts of the result that aren't done automatically
+        try:
+            pid, self.result.exit_status = os.waitpid(self.sp.pid, 0)
+        except OSError:
+            self.result.exit_status = self.sp.poll()
+        self.result.duration = time.time() - self.start_time
+        assert self.result.exit_status is not None
+
+        #make sure we've got stdout and stderr
+        self.stdout_thread.join(1)
+        self.stderr_thread.join(1)
+        assert not self.stdout_thread.is_alive()
+        assert not self.stderr_thread.is_alive()
+
+        super(AsyncJob, self).cleanup()
+
+        return self.result
 
 def ip_to_long(ip):
     # !L is a long in network byte order
