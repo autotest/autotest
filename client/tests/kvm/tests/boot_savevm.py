d3c43df47836090d8cbd0a56efbd5c92ec303895
Lucas Meneghel Rodrigues <lmr@redhat.com>
2011-11-18 03:38:47 -0200

    KVM test: boot_savevm: Cleanups and simplification
    1) Removed unneeded debug statements.
    2) Removed unused variable.
    3) Replaced vm.login with wait_for_login(), which
       already has wait_for logic.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- client/tests/kvm/tests/boot_savevm.py
+++ client/tests/kvm/tests/boot_savevm.py
@@ -6,9 +6,10 @@ from autotest_lib.client.virt import kvm_monitor
 def run_boot_savevm(test, params, env):
     """
     KVM boot savevm test:
-    1) Start guest
-    2) Periodically savevm/loadvm
-    4) Log into the guest to verify it's up, fail after timeout seconds
+
+    1) Start guest.
+    2) Periodically savevm/loadvm.
+    4) Log into the guest to verify it's up, fail after timeout seconds.
 
     @param test: kvm test object
     @param params: Dictionary with the test parameters
@@ -18,19 +19,15 @@ def run_boot_savevm(test, params, env):
     vm.verify_alive()
     savevm_delay = float(params.get("savevm_delay"))
     savevm_login_delay = float(params.get("savevm_login_delay"))
-    logging.info("savevm_delay = %f", savevm_delay)
-    login_expire = time.time() + savevm_login_delay
     end_time = time.time() + float(params.get("savevm_timeout"))
 
     while time.time() < end_time:
         time.sleep(savevm_delay)
-
         try:
             vm.monitor.cmd("stop")
         except kvm_monitor.MonitorError, e:
             logging.error(e)
         try:
-            # This should be replaced by a proper monitor method call
             vm.monitor.cmd("savevm 1")
         except kvm_monitor.MonitorError, e:
             logging.error(e)
@@ -39,7 +36,6 @@ def run_boot_savevm(test, params, env):
         except kvm_monitor.MonitorError, e:
             logging.error(e)
         try:
-            # This should be replaced by a proper monitor method call
             vm.monitor.cmd("loadvm 1")
         except kvm_monitor.MonitorError, e:
             logging.error(e)
@@ -48,13 +44,12 @@ def run_boot_savevm(test, params, env):
         except kvm_monitor.MonitorError, e:
             logging.error(e)
 
-        # Log in
-        if (time.time() > login_expire):
-            login_expire = time.time() + savevm_login_delay
-            logging.info("Logging in after loadvm...")
-            session = vm.login()
-            logging.info("Logged in to guest!")
+        try:
+            vm.wait_for_login(timeout=savevm_login_delay)
             break
+        except Exception, detail:
+            logging.debug(detail)
 
     if (time.time() > end_time):
-        raise error.TestFail("fail: timeout")
+        raise error.TestFail("Not possible to log onto the vm after %s s" %
+                             params.get("savevm_timeout"))
