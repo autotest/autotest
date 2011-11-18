1305d7f9a92c8e36fecfcced1b13e227544ee87a
Amos Kong <akong@redhat.com>
2011-11-18 17:06:35 +0800

    virt: Introduce regression testing infrastructure
    > regression.py:
    Usage: python regression.py $testname $dir1 $dir2 $configfile
     'regression' module is used to compare the test results
     of two jobs, we can use it (regression.compare()) at
     the end of control file, this script can also be used directly.
    
    Example:
    | # python regression.py netperf /result1-dir /result2-dir perf.conf
    
    > analyzer.py:
    Usage: python analyzer.py "$results list1" "$results list2" $log_file
     It's used to compute average, standard deviation, augment rate, etc,
     and compare two test results (standard format).
    
     It can be used directly, example:
    | # python analyzer.py "result-v1-1.RHS result-v1-2.RHS" \
    |   "result-v2-1.RHS result-v2-2.RHS result-v2-3.RHS" log.txt
    | Thu Jan  5 10:17:24 2012
    |
    | == Avg1 SD Augment Rate ==========================
    |     TCP_STREAM
    |     size|sessions|throughput|   cpu|normalize| ...
    |     2048|       2|  14699.17| 31.73|   463.19| ...
    | %SD     |     0.0|       0.6|   0.0|      0.8| ...
    |     2048|       4|  15935.68| 34.30|   464.66| ...
    | %SD     |     0.0|       0.3|   1.7|      1.5| ...
    | ...
    |
    | == AvgS Augment Rate =============================
    |     TCP_STREAM
    |     size|sessions|throughput|   cpu|normalize| ...
    |     2048|       2|   7835.61| 31.66|   247.36| ...
    |     2048|       2|   8757.03| 31.94|   274.14| ...
    | %       |    +0.0|     +11.8|  +0.9|    +10.8| ...
    |     2048|       4|  12000.65| 32.38|   370.62| ...
    |     2048|       4|  13641.20| 32.27|   423.29| ...
    | %       |    +0.0|     +13.7|  -0.3|    +14.2| ...
    |
    
    > perf.conf:
     config test related parameters.
    
    perf regression guide:
    https://github.com/autotest/autotest/wiki/KVMAutotest-Networking
    
    Changes from v1:
    - refactor analysis code
    - add standard deviation percent
    - only provide mechanism to user, user can use tools directly or use the lib in scripts
    
    Signed-off-by: Amos Kong <akong@redhat.com>

---
+++ client/tools/regression.py
@@ -0,0 +1,24 @@
+import ConfigParser, sys, commands, os
+import analyzer
+
+def compare(testname, olddir, curdir, config_file='perf.conf', output_dir="./result.txt"):
+    config = ConfigParser.ConfigParser()
+    config.read(config_file)
+    result_file_pattern = config.get(testname, "result_file_pattern")
+
+    def search_files(dir):
+        cmd = 'find %s|grep "%s.*/%s"' % (dir, testname, result_file_pattern)
+        print cmd
+        return commands.getoutput(cmd)
+
+    oldlist = search_files(olddir)
+    newlist = search_files(curdir)
+    if oldlist != "" or newlist != "":
+        analyzer.analyze(oldlist, newlist, output_dir)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print 'Usage: python %s $testname $dir1 $dir2 $configfile' % sys.argv[0]
+        sys.exit(1)
+    compare(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
