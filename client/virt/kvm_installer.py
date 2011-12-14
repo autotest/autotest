autotest/autotest

commit eda2ffce5c2ec514f404719010106bad1400d2bb

KVM test: Make tests aware of the qemu-io path
As it is important for qemu-iotests that this particular
program is used.

Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

client/virt/kvm_installer.py
@@ -32,6 +32,7 @@ class KVMBaseInstaller(base_installer.BaseInstaller):
     #
     QEMU_BIN = 'qemu'
     QEMU_IMG_BIN = 'qemu-img'
+    QEMU_IO_BIN = 'qemu-io'
 
 
     def _kill_qemu_processes(self):
@@ -131,6 +132,23 @@ class KVMBaseInstaller(base_installer.BaseInstaller):
             return None
 
 
+    def _qemu_io_bin_exists_at_prefix(self):
+        '''
+        Attempts to find the qemu-io binary at the installation prefix
+
+        @return: full path of qemu-io binary or None if not found
+        '''
+        qemu_io_bin_name = os.path.join(self.install_prefix,
+                                         'bin', self.QEMU_IO_BIN)
+        if os.path.isfile(qemu_io_bin_name):
+            logging.debug('Found qemu-io binary at %s', qemu_io_bin_name)
+            return qemu_io_bin_name
+        else:
+            logging.debug('Could not find qemu-img binary at prefix %s',
+                          self.install_prefix)
+            return None
+
+
     def _create_symlink_qemu(self):
         """
         Create symbolic links for qemu and qemu-img commands on test bindir
@@ -141,6 +159,7 @@ class KVMBaseInstaller(base_installer.BaseInstaller):
 
         qemu_dst = os.path.join(self.test_bindir, self.QEMU_BIN)
         qemu_img_dst = os.path.join(self.test_bindir, self.QEMU_IMG_BIN)
+        qemu_io_dst = os.path.join(self.test_bindir, self.QEMU_IO_BIN)
 
         qemu_bin = self._qemu_bin_exists_at_prefix()
         if qemu_bin is not None:
@@ -154,6 +173,11 @@ class KVMBaseInstaller(base_installer.BaseInstaller):
         else:
             raise error.TestError('Invalid qemu-img path')
 
+        qemu_io_bin = self._qemu_io_bin_exists_at_prefix()
+        if qemu_img_bin is not None:
+            os.symlink(qemu_io_bin, qemu_io_dst)
+        else:
+            raise error.TestError('Invalid qemu-img path')
 
     def _install_phase_init(self):
         '''

client/tests/kvm/base.cfg.sample
@@ -3,6 +3,7 @@
 # Absolute paths and/or names of binaries (default path is /usr/bin)
 qemu_binary = qemu
 qemu_img_binary = qemu-img
+qemu_io_binary = qemu-io
 
 # List of virtual machine object names (whitespace seperated)
 vms = vm1

client/tests/kvm/tests.cfg.sample
@@ -60,6 +60,7 @@ variants:
         # We want qemu-kvm for this run
         qemu_binary = /usr/bin/qemu-kvm
         qemu_img_binary = /usr/bin/qemu-img
+        qemu_io_binary = /usr/bin/qemu-io
         # Only qcow2 file format
         only qcow2
         # Only rtl8139 for nw card (default on qemu-kvm)
@@ -82,6 +83,7 @@ variants:
         # We want qemu for this run
         qemu_binary = /usr/bin/qemu
         qemu_img_binary = /usr/bin/qemu-img
+        qemu_io_binary = /usr/bin/qemu-io
         only qcow2
         # The default nw card for qemu is e1000
         only e1000
@@ -100,6 +102,7 @@ variants:
         # We want qemu-kvm for this run
         qemu_binary = /usr/bin/qemu-kvm
         qemu_img_binary = /usr/bin/qemu-img
+        qemu_io_binary = /usr/bin/qemu-io
         only qcow2
         only rtl8139
         only ide
@@ -117,6 +120,7 @@ variants:
         # We want qemu-kvm for this run
         qemu_binary = /usr/bin/qemu-kvm
         qemu_img_binary = /usr/bin/qemu-img
+        qemu_io_binary = /usr/bin/qemu-io
         only qcow2
         only rtl8139
         only ide

commit 3d521ba3fcb5955df74c14659e8cf4ad9d04bea0

virt.kvm_installer: Fix typo
Introduced with commit eda2ffce5c2ec514f404719010106bad1400d2bb.

Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

client/virt/kvm_installer.py
@@ -174,7 +174,7 @@ class KVMBaseInstaller(base_installer.BaseInstaller):
             raise error.TestError('Invalid qemu-img path')
 
         qemu_io_bin = self._qemu_io_bin_exists_at_prefix()
-        if qemu_img_bin is not None:
+        if qemu_io_bin is not None:
             os.symlink(qemu_io_bin, qemu_io_dst)
         else:
             raise error.TestError('Invalid qemu-img path')