d62de2e22b15fba1c772bc29c57d9796ae76daf1
Chris Evich <cevich@redhat.com>
2011-12-15 14:56:50 -0500

    virt/test/boot_savevm: Make KVM version common
    Added use of tempfile module to help generate
    never-clashing VM state files. If test writer needs
    these files to end up under a specific place in the
    filesystem, they may simply define the config key
    "savevm_statedir" - which is checked by this test.
    Otherwise, it just uses /tmp for the files, but
    tries to clean them up frequently.
    
    I unwrapped the calls to pause/save/restore/resume
    from individual try..except clauses because libvirt
    doesn't have a proper monitor class and/or similar
    exceptions to check for.  Either way, part of the
    requirements of this test are successful
    saves/restores, so if they break, some noise should
    be made about it.
    
    Note that the removal of the VM state files is w/in
    the try..except clause because some virt backends
    might automatically remove the file on restore.
    This avoids unnecessary OSError's but helps keep
    the temp directory clean with frequent removals.
    
    For simplicity sake, the same save file is re-used
    for each iteration. This does introduce a very small
    chance for a name-clash problem, should another
    process/thread use the file.  This problem exists
    in the small window between the os.unlink(), and
    a subsequent vm.save_to_file() on next loop iteration.
    
    Otherwise, the logic of the former kvm/boot_savevm
    test should be indentical except for these changes.
    This similarity includes a small bug with respect
    to an assumed initial VM state of "booting".
    Further changes are needed within the vm classes
    to standardize on a start() method behavior before
    this problem can be corrected.
    
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- client/virt/tests/boot_savevm.py
+++ client/virt/tests/boot_savevm.py
@@ -1,63 +1,47 @@
-import logging, time
+import logging, time, tempfile, os.path
 from autotest_lib.client.common_lib import error
-from autotest_lib.client.virt import kvm_monitor
-
+from autotest_lib.client.virt import virt_utils
 
 def run_boot_savevm(test, params, env):
     """
-    KVM boot savevm test:
+    libvirt boot savevm test:
 
-    1) Start guest.
-    2) Periodically savevm/loadvm.
-    4) Log into the guest to verify it's up, fail after timeout seconds.
+    1) Start guest booting
+    2) Periodically savevm/loadvm while guest booting
+    4) Stop test when able to login, or fail after timeout seconds.
 
-    @param test: kvm test object
+    @param test: test object
     @param params: Dictionary with the test parameters
     @param env: Dictionary with test environment.
     """
     vm = env.get_vm(params["main_vm"])
-    vm.verify_alive()
+    vm.verify_alive() # This shouldn't require logging in to guest
     savevm_delay = float(params.get("savevm_delay"))
     savevm_login_delay = float(params.get("savevm_login_delay"))
     savevm_login_timeout = float(params.get("savevm_timeout"))
+    savevm_statedir = params.get("savevm_statedir", tempfile.gettempdir())
+    fd, savevm_statefile = tempfile.mkstemp(suffix='.img', prefix=vm.name+'-',
+                                            dir=savevm_statedir)
+    os.close(fd) # save_to_file doesn't need the file open
     start_time = time.time()
-
     cycles = 0
 
     successful_login = False
     while (time.time() - start_time) < savevm_login_timeout:
-        logging.info("Save/load cycle %d", cycles + 1)
+        logging.info("Save/Restore cycle %d", cycles + 1)
         time.sleep(savevm_delay)
-        try:
-            vm.monitor.cmd("stop")
-        except kvm_monitor.MonitorError, e:
-            logging.error(e)
-        try:
-            vm.monitor.cmd("savevm 1")
-        except kvm_monitor.MonitorError, e:
-            logging.error(e)
-        try:
-            vm.monitor.cmd("system_reset")
-        except kvm_monitor.MonitorError, e:
-            logging.error(e)
-        try:
-            vm.monitor.cmd("loadvm 1")
-        except kvm_monitor.MonitorError, e:
-            logging.error(e)
-        try:
-            vm.monitor.cmd("cont")
-        except kvm_monitor.MonitorError, e:
-            logging.error(e)
-
-        vm.verify_kernel_crash()
-
+        vm.pause()
+        vm.save_to_file(savevm_statefile) # Re-use same filename
+        vm.restore_from_file(savevm_statefile)
+        vm.resume() # doesn't matter if already running or not
+        vm.verify_kernel_crash() # just in case
         try:
             vm.wait_for_login(timeout=savevm_login_delay)
-            successful_login = True
+            successful_login = True # not set if timeout expires
+            os.unlink(savevm_statefile) # don't let these clutter disk
             break
         except:
-            pass
-
+            pass # loop until successful login or time runs out
         cycles += 1
 
     time_elapsed = int(time.time() - start_time)
