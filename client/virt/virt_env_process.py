e5e1245c4cfe19cd126a1edf3f8c26a6b933e931
Amos Kong <akong@redhat.com>
2011-12-20 14:56:53 +0000

    virt_env_process.py: fix a typo
    It should be "DHCP"
    
    Signed-off-by: Amos Kong <akong@redhat.com>

--- client/virt/virt_env_process.py
+++ client/virt/virt_env_process.py
@@ -445,7 +445,7 @@ def _update_address_cache(address_cache, line):
         if matches and address_cache.get("last_seen"):
             mac_address = matches[0].lower()
             if time.time() - address_cache.get("time_%s" % mac_address, 0) > 5:
-                logging.debug("(address cache) DCHP lease OK: %s --> %s",
+                logging.debug("(address cache) DHCP lease OK: %s --> %s",
                               mac_address, address_cache.get("last_seen"))
             address_cache[mac_address] = address_cache.get("last_seen")
             address_cache["time_%s" % mac_address] = time.time()
