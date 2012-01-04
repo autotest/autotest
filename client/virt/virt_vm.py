cc14cda0cf27bf72656027199c9fc57bcc5765b5
Qingtang Zhou <qzhou@redhat.com>
2012-01-04 20:45:02 -0200

    virt: kvm_vm.py: Update get_peer method
    There is no command for query networking
    status in QMP monitor, and the human monitor
    'info network' doesn't return peer parameter
    in some version of qemu. Thus this method
    'get_peer' can't work correctly in some
    versions of host.
    
    This patch get net device and tap device
    pair directly from the output of human
    monitor command 'info network', make it
    work under both qmp and human monitor.
    
    Regex for get peer device for a net device.
    This regex is for Fedora host (with qemu-kvm 0.15.*),
    netdev_peer_re = "\s{2,}(.*?): .*?\\\s(.*?):"
    for RHEL5 host, the regex should be:
    netdev_peer_re = "\s{2,}(.*?): .*?\s{2,}(.*?):"
    for RHEL6 host, the regex should be:
    netdev_peer_re = "\s{2,}(.*?):.*?peer=(.*?)\n"
    
    Signed-off-by: Qingtang Zhou <qzhou@redhat.com>

--- client/virt/virt_vm.py
+++ client/virt/virt_vm.py
@@ -32,6 +32,16 @@ class VMStartError(VMError):
         return msg
 
 
+class VMConfigMissingError(VMError):
+    def __init__(self, name, config):
+        VMError.__init(self, name, config)
+        self.name = name
+        self.config = config
+
+    def __str__(self):
+        return "Missing config '%s' for VM %s" % (self.config, self.name)
+
+
 class VMHashMismatchError(VMError):
     def __init__(self, actual, expected):
         VMError.__init__(self, actual, expected)
