6a1ddec9045372c187fd64a98731066ca0dcf378
Chris Evich <cevich@redhat.com>
2012-01-14 07:02:58 +0000

    virt: Update and enhance save_restore test
    * Cleaned up subtest config comments and lifecycle varients
    
    * Fixed a race condition between ssh session recovery and host tcp
      recovery.  Moral: Don't leave ssh sessions open during save/restore.
    
    * Enhanced test to run backgrounded command on guest that dirties
      memory and keeps at least one CPU 99% busy in guest. Attempt
      made to stop background job when testing done. Warning issued if it
      will not be possible to kill the background process.
    
    Note: Smoke-tested on f16 host fine, there's a problem/bug on
          RHEL hosts which I'm still tracking down.
    
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- client/virt/tests/save_restore.py
+++ client/virt/tests/save_restore.py
@@ -32,6 +32,7 @@ def run_save_restore(test, params, env):
         os.close(fd)
         return filename
 
+
     def nuke_filename(filename):
         """
         Try to unlink filename, ignore any os errors.
@@ -41,41 +42,75 @@ def run_save_restore(test, params, env):
         except OSError:
             pass
 
+
+    def check_system(vm, timeout):
+        """
+        Raise TestFail if system is not in expected state
+        """
+        session = None
+        try:
+            session = vm.wait_for_login(timeout=timeout)
+            result = session.is_responsive(timeout=timeout/10.0)
+            if not result:
+                logging.warning("Login session established, but non-responsive")
+                # assume guest is just busy with stuff
+        except:
+            raise error.TestFail("VM check timed out and/or VM non-responsive")
+        finally:
+            del session
+
+
     vm = env.get_vm(params["main_vm"])
-    # TODO: Verify initial VM state
-    session = vm.wait_for_login()
-    # FIXME: If VM already running, it gets paused for some reason.
+    session = vm.wait_for_login(timeout=600)
+
     start_delay = float(params.get("save_restore_start_delay", "10.0"))
     restore_delay = float(params.get("save_restore_delay", "0.0"))
+    save_restore_duration = float(params.get("save_restore_duration", "60.0"))
+    repeat = int(params.get("save_restore_repeat","1"))
+
     path = os.path.abspath(params.get("save_restore_path", "/tmp"))
     file_pfx = vm.name+'-'
+    save_file = get_save_filename(path, file_pfx)
+
+    save_restore_bg_command = params.get("save_restore_bg_command")
+    if save_restore_bg_command:
+        session.cmd(save_restore_bg_command + ' &')
+        try:
+            # assume sh-like shell, try to get background process's pid
+            bg_command_pid = int(session.cmd('jobs -rp'))
+        except ValueError:
+            logging.warning("Background guest command 'job -rp' output not PID")
+            bg_command_pid = none
+    del session # don't leave stray ssh session lying around over save/restore
+
     start_time = time.time()
-    now = time_to_stop = (start_time +
-                          float(params.get("save_restore_duration", "60.0")))
-    repeat = int(params.get("save_restore_repeat","1"))
+    # 'now' needs outside scope for error.TestFail() at end
+    # especially if exception thrown in loop before completion
+    now = time_to_stop = (start_time + save_restore_duration)
     while True:
         try:
-            if not session.is_responsive():
-                raise error.TestFail("Guest shell session is non-responsive")
+            vm.verify_kernel_crash()
+            check_system(vm,120) # networking needs time to recover
             logging.info("Save/restores left: %d (or %0.4f more seconds)" %
                          (repeat, (time_to_stop - time.time())))
-            # TODO: Start some background test or load within VM
             if start_delay:
                 logging.debug("Sleeping %0.4f seconds start_delay" %
                               start_delay)
                 time.sleep(start_delay)
             vm.pause()
+            vm.verify_kernel_crash()
             save_file = get_save_filename(path, file_pfx)
             vm.save_to_file(save_file)
+            vm.verify_kernel_crash()
             if restore_delay:
                 logging.debug("Sleeping %0.4f seconds restore_delay" %
                               restore_delay)
                 time.sleep(restore_delay)
             vm.restore_from_file(save_file)
+            vm.verify_kernel_crash()
             vm.resume() # make sure some work gets done
             vm.verify_kernel_crash()
             now = time.time()
-            # TODO: Examine background test/load completion/success status
         finally:
             if save_file:
                 nuke_filename(save_file) # make sure these are cleaned up
@@ -83,9 +118,18 @@ def run_save_restore(test, params, env):
         repeat -= 1
         if (now >= time_to_stop) or (repeat <= 0):#TODO: or BG test status==foo
             break
+        save_file = get_save_filename(path, file_pfx)
+    # Check the final save/restore cycle
+    check_system(vm,120) # networking needs time to recover
     logging.info("Save/Restore itteration(s) complete.")
+    if save_restore_bg_command and bg_command_pid:
+        session = vm.wait_for_login(timeout=120)
+        status = session.cmd_status('kill %d' % bg_command_pid)
+        if status != 0:
+            logging.warning("Background guest command kill %d failed" %\
+                            bg_command_pid)
+        del session
     if repeat > 0: # time_to_stop reached but itterations didn't complete
         raise error.TestFail("Save/Restore save_restore_duration"
                              " exceeded by %0.4f seconds with %d itterations"
                              " remaining." % (now-time_to_stop, repeat+1))
-    # TODO: Check for any other failure condition
