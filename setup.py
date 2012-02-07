87df5e0c87f867e8aec57b5ea5a434c4a5360640
Lucas Meneghel Rodrigues <lmr@redhat.com>
2012-02-06 22:35:43 -0200

    Autotest: Introducing setup.py script to install autotest files v2
    In order to support packaging, we need the ability to determine
    which file goes where and which ones get packaged.
    
    I decided to use the standard setup.py approach to accomplish
    this. This is an initial first cut and should be reviewed and
    tweaked as needed.
    
    Changes from v1:
     * Starting the tweaks - Change references to author
     * Remove unneeded glob imports
    
    Signed-off-by: Don Zickus <dzickus@redhat.com>

---
+++ setup.py
@@ -0,0 +1,11 @@
+# High level way of installing each autotest component
+import client.setup
+import frontend.setup
+import cli.setup
+import server.setup
+import scheduler.setup
+import database.setup
+import tko.setup
+import utils.setup
+import mirror.setup
+
