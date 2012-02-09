5555db2327063757be68b4a28029e2f6468f5f94
Chris Evich <cevich@redhat.com>
2012-02-08 21:33:10 -0500

    libvirt: Enhance virsh_migrate test
    Updated *migrate() functions and test code to support flexable
    multi-iteration bi-directional migration testing.  By default
    it just reverses source and destination, but it also supports
    specifying reversal options for more advanced testing.
    
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- client/tests/libvirt/tests/virsh_migrate.py
+++ client/tests/libvirt/tests/virsh_migrate.py
@@ -7,37 +7,45 @@ def run_virsh_migrate(test, params, env):
     Test the migrate command with parameter --live.
     """
 
-    vm_name = params.get("main_vm")
-    dest_host = params.get("virsh_migrate_desthost")
-    protocol = params.get("virsh_migrate_proto", "qemu+ssh")
-    options = params.get("virsh_migrate_options")
-    extra = params.get("virsh_migrate_extra")
-    destuser = params.get("virsh_migrate_destuser", "root")
-    destpwd = params.get("virsh_migrate_destpwd", "")
-    dly = int(params.get("virsh_migrate_delay", 10))
+    def do_migration(dly, vm, dest_uri, options, extra):
+        logging.info("Sleeping %d seconds before migration" % dly)
+        time.sleep(dly)
+        # Migrate the guest.
+        successful = vm.migrate(dest_uri, options, extra)
+        if not successful:
+            raise error.TestFail("Migration failed for %s." % vm_name)
 
+        if vm.is_alive(): # vm.connect_uri was updated
+            logging.info("Alive guest found on destination %s." % dest_uri)
+        else:
+            raise error.TestFail("VM not running on destination %s" % dest_uri)
+        # FIXME: This needs to be tested, but won't work currently
+        # vm.verify_kernel_crash()
+
+    vm_name = params.get("main_vm")
     vm = env.get_vm(params["main_vm"])
     vm.verify_alive()
-    logging.info("Sleeping %d seconds before migration" % dly)
-    time.sleep(dly)
-    # Migrate the guest.
-    successful = vm.migrate(dest_host, protocol, options, extra)
-    if not successful:
-        raise error.TestFail("Migration failed for %s." % vm_name)
 
-    if vm.is_alive():
-        raise error.TestFail("VM %s still running on %s after migration" %
-                             (vm_name, vm.connect_uri))
-    # libvirt guaranteed to be running on remote uri, exploit for verification
-    # and optional destruction or subsequent tests with same VM instance
-    orig_uri = vm.connect_uri
-    vm.connect_uri = "%s://%s/system" % (protocol,dest_host)
-    if vm.is_alive():
-        logging.info("Alive guest %s found on destination %s." %
-                     (vm_name, dest_host))
-    else:
-        vm.connect_uri = orig_uri
-        raise error.TestFail("VM %s is not running on destination %s" %
-                             (vm_name, dest_host))
-    # FIXME: This needs to be tested, but won't work currently
-    # vm.verify_kernel_crash()
+    src_uri = vm.connect_uri
+    dest_uri = params.get("virsh_migrate_desturi")
+    options = params.get("virsh_migrate_options")
+    extra = params.get("virsh_migrate_extra")
+    # FIXME: libvirt.vm should support other remote access styles
+    # destuser = params.get("virsh_migrate_destuser", "root")
+    # destpwd = params.get("virsh_migrate_destpwd", "")
+    dly = int(params.get("virsh_migrate_delay", 10))
+
+    
+    do_migration(dly, vm, dest_uri, options, extra)
+    # Repeat the migration with a recursive call and guaranteed exit
+    if params.get("virsh_migrate_back", "no") == 'yes':
+        back_dest_uri = params.get("virsh_migrate_back_desturi", 'default')
+        back_options = params.get("virsh_migrate_back_options", 'default')
+        back_extra = params.get("virsh_migrate_back_extra", 'default')
+        if back_dest_uri == 'default':
+            back_dest_uri = src_uri
+        if back_options == 'default':
+            back_options = options
+        if back_extra == 'default':
+            back_extra = extra
+        do_migration(dly, vm, back_dest_uri, back_options, back_extra)
