703abd4acf36d589fc6ab26cc8611f352744af2b
guyanhua <guyanhua-fnst@cn.fujitsu.com>
2012-02-17 13:42:09 +0000

    libvirt: Introducing 'virsh version' test
    This patch adds three test cases for "virsh version" command.
    
    Use three cases:(1) virsh version
                    (2) virsh version with a unexpected option
                    (3) virsh version with libvirtd service stop
    
    Signed-off-by: Gu Yanhua <guyanhua-fnst@cn.fujitsu.com>

---
+++ client/tests/libvirt/tests/virsh_version.py
@@ -0,0 +1,45 @@
+import logging
+from autotest_lib.client.common_lib import utils, error
+from autotest_lib.client.virt import libvirt_vm
+
+def run_virsh_version(test, params, env):
+    """
+    Test the command virsh version
+
+    (1) Call virsh version
+    (2) Call virsh version with an unexpected option
+    (3) Call virsh version with libvirtd service stop
+    """
+    def virsh_version(option):
+        cmd = "virsh version  %s" % option
+        cmd_result = utils.run(cmd, ignore_status=True)
+        logging.debug("Output: %s", cmd_result.stdout.strip())
+        logging.debug("Error: %s", cmd_result.stderr.strip())
+        logging.debug("Status: %d", cmd_result.exit_status)
+        return cmd_result.exit_status
+
+    # Prepare libvirtd service
+    check_libvirtd = params.has_key("libvirtd")
+    if check_libvirtd:
+        libvirtd = params.get("libvirtd")
+        if libvirtd == "off":
+            libvirt_vm.libvirtd_stop()
+
+    # Run test case
+    option = params.get("options")
+    status = virsh_version(option)
+
+    # Recover libvirtd service start
+    if libvirtd == "off":
+        libvirt_vm.libvirtd_start()
+
+    # Check status_error
+    status_error = params.get("status_error")
+    if status_error == "yes":
+        if status == 0:
+            raise error.TestFail("Command 'virsh version %s' succeeded "
+                                 "(incorrect command)" % option)
+    elif status_error == "no":
+        if status != 0:
+            raise error.TestFail("Command 'virsh version %s' failed "
+                                 "(correct command)" % option)
