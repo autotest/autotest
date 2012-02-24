0a50bafa7d8d5c8deb20305fe2b71b0036580aec
Yiqiao Pu <ypu@redhat.com>
2012-02-24 16:13:31 +0800

    Virt tests: Move image_func outside the loop of vms
    Update process put image_func outside the loop of vms to make the
    image_func can work for cases don't need to boot up vm. Such as some
    tests related with qcow2.
    
    And fix the missing docstring for vm_first.
    
    Signed-off-by: Yiqiao Pu <ypu@redhat.com>

--- client/virt/virt_env_process.py
+++ client/virt/virt_env_process.py
@@ -205,32 +205,39 @@ def process(test, params, env, image_func, vm_func, vm_first=False):
     @param env: The environment (a dict-like object).
     @param image_func: A function to call for each image.
     @param vm_func: A function to call for each VM.
+    @param vm_first: Call vm_func first or not.
     """
-    # Get list of VMs specified for this test
-    for vm_name in params.objects("vms"):
-        vm_params = params.object_params(vm_name)
-        vm = env.get_vm(vm_name)
-        if not vm_first:
-            # Get list of images specified for this VM
-            for image_name in vm_params.objects("images"):
-                image_params = vm_params.object_params(image_name)
-                # Call image_func for each image
-                if vm is not None and vm.is_alive():
-                    vm.pause()
-                image_func(test, image_params)
-                if vm is not None and vm.is_alive():
-                    vm.resume()
-            # Call vm_func for each vm
+    def _call_vm_func():
+        for vm_name in params.objects("vms"):
+            vm_params = params.object_params(vm_name)
+            vm = env.get_vm(vm_name)
             vm_func(test, vm_params, env, vm_name)
+
+    def _call_image_func():
+        if params.objects("vms"):
+            for vm_name in params.objects("vms"):
+                vm_params = params.object_params(vm_name)
+                vm = env.get_vm(vm_name)
+                for image_name in vm_params.objects("images"):
+                    image_params = vm_params.object_params(image_name)
+                    # Call image_func for each image
+                    if vm is not None and vm.is_alive():
+                        vm.pause()
+                    image_func(test, image_params)
+                    if vm is not None and vm.is_alive():
+                        vm.resume()
         else:
-            vm_func(test, vm_params, env, vm_name)
-            for image_name in vm_params.objects("images"):
-                image_params = vm_params.object_params(image_name)
-                if vm is not None and vm.is_alive():
-                    vm.pause()
+            for image_name in params.objects("images"):
+                image_params = params.object_params(image_name)
                 image_func(test, image_params)
-                if vm is not None and vm.is_alive():
-                    vm.resume()
+
+    if not vm_first:
+        _call_image_func()
+
+    _call_vm_func()
+
+    if vm_first:
+        _call_image_func()
 
 
 @error.context_aware
