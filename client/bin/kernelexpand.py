eb7f4bf77ca8a005dffb824185d2fcf4d1344516
Nishanth Aravamudan <nacc@us.ibm.com>
2012-03-29 14:44:27 -0700

    client/kernelexpand: add support for kernel mirror at  configuration level
    Allow system administrators to define kernel mirrors (defaulting at
    install time to kernel.org itself) for jobs.
    
    Signed-off-by: Nishanth Aravamudan <nacc@us.ibm.com>

--- client/bin/kernelexpand.py
+++ client/bin/kernelexpand.py
@@ -12,48 +12,54 @@ http://www.kernel.org/pub/linux/kernel/v3.x/linux-3.1.tar.bz2
 @license: GPL v2
 @see: Inspired by kernelexpand by Martin J. Bligh, 2003
 """
+try:
+    import autotest.common as common
+except ImportError:
+    import common
+
+from autotest_lib.client.common_lib import global_config
 import sys, re, os, urllib2
 
+GLOBAL_CONFIG = global_config.global_config
 
-KERNEL_BASE_URL = 'http://www.kernel.org/pub/linux/kernel/'
+KERNEL_BASE_URL = GLOBAL_CONFIG.get_config_value('CLIENT', 'kernel_mirror', default='')
 
 MAPPINGS_2X = [
-        [ r'^\d+\.\d+$', '', True, [
-                KERNEL_BASE_URL + 'v%(major)s/linux-%(full)s.tar.bz2'
-        ]],
-        [ r'^\d+\.\d+\.\d+$', '', True, [
-                KERNEL_BASE_URL + 'v%(major)s/linux-%(full)s.tar.bz2'
-        ]],
-        [ r'^\d+\.\d+\.\d+\.\d+$', '', True, [
-                KERNEL_BASE_URL + 'v%(major)s/linux-%(full)s.tar.bz2'
-        ]],
-        [ r'-rc\d+$', '%(minor-prev)s', True, [
-                KERNEL_BASE_URL + 'v%(major)s/testing/v%(minor)s/linux-%(full)s.tar.bz2',
-                KERNEL_BASE_URL + 'v%(major)s/testing/linux-%(full)s.tar.bz2',
-        ]],
-        [ r'-(git|bk)\d+$', '%(base)s', False, [
-                KERNEL_BASE_URL + 'v%(major)s/snapshots/old/patch-%(full)s.bz2',
-                KERNEL_BASE_URL + 'v%(major)s/snapshots/patch-%(full)s.bz2',
-        ]],
-        [ r'-mm\d+$', '%(base)s', False, [
-                KERNEL_BASE_URL + 'people/akpm/patches/' +
-                        '%(major)s/%(base)s/%(full)s/%(full)s.bz2'
-        ]],
-        [ r'-mjb\d+$', '%(base)s', False, [
-                KERNEL_BASE_URL + 'people/mbligh/%(base)s/patch-%(full)s.bz2'
-        ]]
+        [ r'^\d+\.\d+$', '', True,
+                map(lambda x : x + 'v%(major)s/linux-%(full)s.tar.bz2', KERNEL_BASE_URL.split())
+        ],
+        [ r'^\d+\.\d+\.\d+$', '', True,
+                map(lambda x : x + 'v%(major)s/linux-%(full)s.tar.bz2', KERNEL_BASE_URL.split())
+        ],
+        [ r'^\d+\.\d+\.\d+\.\d+$', '', True,
+                map(lambda x : x + 'v%(major)s/linux-%(full)s.tar.bz2', KERNEL_BASE_URL.split())
+        ],
+        [ r'-rc\d+$', '%(minor-prev)s', True,
+                map(lambda x : x + 'v%(major)s/testing/v%(minor)s/linux-%(full)s.tar.bz2', KERNEL_BASE_URL.split()) +
+                map(lambda x : x + 'v%(major)s/testing/linux-%(full)s.tar.bz2', KERNEL_BASE_URL.split())
+        ],
+        [ r'-(git|bk)\d+$', '%(base)s', False,
+                map(lambda x : x + 'v%(major)s/snapshots/old/patch-%(full)s.bz2', KERNEL_BASE_URL.split()) +
+                map(lambda x : x + 'v%(major)s/snapshots/patch-%(full)s.bz2', KERNEL_BASE_URL.split())
+        ],
+        [ r'-mm\d+$', '%(base)s', False,
+                map(lambda x : x + 'people/akpm/patches/' + '%(major)s/%(base)s/%(full)s/%(full)s.bz2', KERNEL_BASE_URL.split())
+        ],
+        [ r'-mjb\d+$', '%(base)s', False,
+                map(lambda x : x + 'people/mbligh/%(base)s/patch-%(full)s.bz2', KERNEL_BASE_URL.split())
+        ],
 ];
 
 MAPPINGS_POST_2X = [
-        [ r'^\d+\.\d+$', '', True, [
-                KERNEL_BASE_URL + 'v%(major)s/linux-%(full)s.tar.bz2'
-        ]],
-        [ r'^\d+\.\d+\.\d+$', '', True, [
-                KERNEL_BASE_URL + 'v%(major)s/linux-%(full)s.tar.bz2'
-        ]],
-        [ r'-rc\d+$', '%(minor-prev)s', True, [
-                KERNEL_BASE_URL + 'v%(major)s/testing/linux-%(full)s.tar.bz2',
-        ]],
+        [ r'^\d+\.\d+$', '', True,
+                map(lambda x : x + 'v%(major)s/linux-%(full)s.tar.bz2', KERNEL_BASE_URL.split())
+        ],
+        [ r'^\d+\.\d+\.\d+$', '', True,
+                map(lambda x : x + 'v%(major)s/linux-%(full)s.tar.bz2', KERNEL_BASE_URL.split())
+        ],
+        [ r'-rc\d+$', '', True,
+                map(lambda x : x + 'v%(major)s/testing/linux-%(full)s.tar.bz2', KERNEL_BASE_URL.split())
+        ],
 ];
 
 
