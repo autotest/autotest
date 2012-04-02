f481e84e9966ac383755d1c9ca5a4aed2b0473a1
Lucas Meneghel Rodrigues <lmr@redhat.com>
2012-04-02 11:13:03 -0300

    client/tools/scan_results.py: List results from all job tags
    As well pointed by Chris Evich <cevich@redhat.com>, scan_results
    could get results from all job tags instead of just default.
    The fix is easy enough, though.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- client/tools/scan_results.py
+++ client/tools/scan_results.py
@@ -93,7 +93,7 @@ if __name__ == "__main__":
     import sys, glob, os
     dirname = os.path.dirname(sys.modules[__name__].__file__)
     client_dir = os.path.abspath(os.path.join(dirname, ".."))
-    resfiles = glob.glob(os.path.join(client_dir, 'results', 'default', 'status*'))
+    resfiles = glob.glob(os.path.join(client_dir, 'results', '*', 'status*'))
 
     if len(sys.argv) > 1:
         if sys.argv[1] == "-h" or sys.argv[1] == "--help":
