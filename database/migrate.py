3e6016e5e4b8c0ec4983a437fcfd3a309f009196
Cleber Rosa <crosa@redhat.com>
2012-04-15 21:01:10 -0300

    database: rebase initial schema to latest version
    As planned and discused on github issue #266.
    
    Also, do not manually set the database version,
    as the schema file already contains the migrate_info
    table and value.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- database/migrate.py
+++ database/migrate.py
@@ -205,13 +205,10 @@ class MigrationManager(object):
         self.confirm_initialization()
 
         migration_script = utils.read_file(
-                os.path.join(os.path.dirname(__file__), 'schema_051.sql'))
+                os.path.join(os.path.dirname(__file__), 'schema_068.sql'))
         migration_script = migration_script % (
                 dict(username=self._database.get_database_info()['username']))
         self.execute_script(migration_script)
-
-        self.create_migrate_table()
-        self.set_db_version(51)
 
 
     def confirm_initialization(self):
