9109065ec55c2ab0f20f9725a1d6474772134b89
Jiří Župka <jzupka@redhat.com>
2012-05-11 12:08:24 +0200

    virt: Repair bug in speed limiting of scp.
    Signed-off-by: Jiří Župka <jzupka@redhat.com>

--- client/virt/virt_vm.py
+++ client/virt/virt_vm.py
@@ -507,14 +507,15 @@ class BaseVM(object):
 
 
     @error.context_aware
-    def copy_files_to(self, host_path, guest_path, nic_index=0, verbose=False,
-                      timeout=COPY_FILES_TIMEOUT):
+    def copy_files_to(self, host_path, guest_path, nic_index=0, limit="",
+                      verbose=False, timeout=COPY_FILES_TIMEOUT):
         """
         Transfer files to the remote host(guest).
 
         @param host_path: Host path
         @param guest_path: Guest path
         @param nic_index: The index of the NIC to connect to.
+        @param limit: Speed limit of file transfer.
         @param verbose: If True, log some stats using logging.debug (RSS only)
         @param timeout: Time (seconds) before giving up on doing the remote
                 copy.
@@ -529,12 +530,12 @@ class BaseVM(object):
                         (self.name, address,
                         virt_utils.generate_random_string(4)))
         virt_utils.copy_files_to(address, client, username, password, port,
-                                host_path, guest_path, log_filename, verbose,
-                                timeout)
+                                 host_path, guest_path, limit, log_filename,
+                                 verbose, timeout)
 
 
     @error.context_aware
-    def copy_files_from(self, guest_path, host_path, nic_index=0,
+    def copy_files_from(self, guest_path, host_path, nic_index=0, limit="",
                         verbose=False, timeout=COPY_FILES_TIMEOUT):
         """
         Transfer files from the guest.
@@ -542,6 +543,7 @@ class BaseVM(object):
         @param host_path: Guest path
         @param guest_path: Host path
         @param nic_index: The index of the NIC to connect to.
+        @param limit: Speed limit of file transfer.
         @param verbose: If True, log some stats using logging.debug (RSS only)
         @param timeout: Time (seconds) before giving up on doing the remote
                 copy.
@@ -556,8 +558,8 @@ class BaseVM(object):
                         (self.name, address,
                         virt_utils.generate_random_string(4)))
         virt_utils.copy_files_from(address, client, username, password, port,
-                                  guest_path, host_path, log_filename,
-                                  verbose, timeout)
+                                   guest_path, host_path, limit, log_filename,
+                                   verbose, timeout)
 
 
     @error.context_aware
