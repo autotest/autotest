autotest/autotest

commit f541fd43601f64205c990765db15870cdc86461c

contrib/install-autotest-server.sh: Fix locale and help msg
1) Set en.US locale
2) Fix slightly misleading help message
3) Set the autotest user $HOME to be /usr/local/autotest

Signed-off-by: Matěj Cepl <mcepl@redhat.com>

contrib/install-autotest-server.sh
@@ -7,6 +7,7 @@ LOG="/tmp/$BASENAME-$DATETIMESTAMP.log"
 ATPASSWD=
 MYSQLPW=
 INSTALL_PACKAGES_ONLY=0
+export LANG=en_US.utf8
 
 print_log() {
 echo $(date "+%H:%M:%S") $1 '|' $2 | tee -a $LOG
@@ -22,7 +23,7 @@ Currently supported systems: Fedora 16 and RHEL 6.2.
 GENERAL OPTIONS:
    -h      Show this message
    -u      Autotest user password
-   -d      Autotest MySQL database password
+   -d      MySQL password (both mysql root and autotest_web db)
 
 INSTALLATION STEP SELECTION:
    -p      Only install packages
@@ -226,7 +227,7 @@ print_log "INFO" "Installing autotest"
 if [ "$(grep "^autotest:" /etc/passwd)" = "" ]
 then
     print_log "INFO" "Adding user autotest"
-    useradd autotest
+    useradd -b /usr/local autotest
     print_log "INFO" "Setting autotest user password"
     echo "$ATPASSWD
 $ATPASSWD" | passwd --stdin autotest >> $LOG

commit 973d504f75fca2b327af07fdcf02bc3abc44eb45

contrib/install-autotest-server.sh: Fetch autotest instead of cloning it
Ray Spear <ray.spear@hp.com> reported that
f541fd43601f64205c990765db15870cdc86461c introduced a
regression when installing the autotest server from
scratch:

15:14:48 INFO | Setting autotest user password
15:14:48 INFO | Cloning autotest repo in /usr/local
15:14:48 INFO | Trying clone via git
fatal: destination path 'autotest' already exists and is not an empty
directory.

So, instead of just clone the tree using git clone, init
a git repo in there and fetch from upstream. Later on if
people want, we could make the tree/branch configurable.

Thanks to Ray for having reported the problem.

Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

contrib/install-autotest-server.sh
@@ -237,8 +237,10 @@ mkdir -p /usr/local
 if [ ! -e $ATHOME/.git/config ]
 then
     print_log "INFO" "Cloning autotest repo in $ATHOME"
-    cd /usr/local
-    git clone git://github.com/autotest/autotest.git
+    cd $ATHOME
+    git init
+    git fetch -f -u -t git://github.com/autotest/autotest.git master:master
+    git checkout master
 else
     print_log "INFO" "Updating autotest repo in $ATHOME"
     cd $ATHOME