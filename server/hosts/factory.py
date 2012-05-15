4eb3cd4ad1962130608ac0c9d6596d9294035b76
Nishanth Aravamudan <nacc@us.ibm.com>
2012-05-15 16:55:00 -0300

    hosts: add installable_host class
    Each host in a job may have a profile associated with it. This does not
    seem to be really a component of what a SSHHost is, so create a new
    sub-class, InstallableHost, of SSHHost which just adds a profile to the
    parent object.
    
    If the global_config specifies an install server URL, then we use
    InstallableHosts rather than SSHHosts.
    
    Signed-off-by: Nishanth Aravamudan <nacc@us.ibm.com>

--- server/hosts/factory.py
+++ server/hosts/factory.py
@@ -1,6 +1,6 @@
 from autotest.client.shared import utils, error, global_config
 from autotest.server import autotest_remote, utils as server_utils
-from autotest.server.hosts import site_factory, ssh_host, serial
+from autotest.server.hosts import site_factory, installable_host, ssh_host, serial, remote
 from autotest.server.hosts import logfile_monitor
 
 DEFAULT_FOLLOW_PATH = '/var/log/kern.log'
@@ -20,7 +20,11 @@ def create_host(
         from autotest.server.hosts import paramiko_host
         classes = [paramiko_host.ParamikoHost]
     elif SSH_ENGINE == 'raw_ssh':
-        classes = [ssh_host.SSHHost, ssh_host.AsyncSSHMixin]
+        if remote.install_server_is_configured():
+            classes = [installable_host.InstallableHost]
+        else:
+            classes = [ssh_host.SSHHost]
+        classes.append(ssh_host.AsyncSSHMixin)
     else:
         raise error.AutoServError("Unknown SSH engine %s. Please verify the "
                                   "value of the configuration key 'ssh_engine' "
@@ -68,7 +72,7 @@ def create_host(
     site_factory.postprocess_classes(classes, hostname,
                                      auto_monitor=auto_monitor, **args)
 
-    hostname, args['user'], args['password'], args['port'] = \
+    hostname, args['user'], args['password'], args['port'], args['profile'] = \
             server_utils.parse_machine(hostname, ssh_user, ssh_pass, ssh_port)
 
     # create a custom host class for this machine and return an instance of it
