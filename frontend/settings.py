d61b580d677c82a4a4eb8adf9752daee54fb3f25
Lucas Meneghel Rodrigues <lmr@redhat.com>
2012-05-31 16:51:38 -0300

    frontend.settings: Add MessageMiddleware among the middleware classes
    So we can effectively show up popup messages in the Django
    admin view. As this class was introduced in 1.2, this change
    should also allow to use Django 1.3 as a supported dep.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- frontend/settings.py
+++ frontend/settings.py
@@ -92,6 +92,7 @@ TEMPLATE_LOADERS = (
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'frontend.apache_auth.ApacheAuthMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.doc.XViewMiddleware',
