2380f5f5fc64e70443ba3e6003e659e6dbf5254d
Lucas Meneghel Rodrigues <lmr@redhat.com>
2012-05-31 15:37:51 -0300

    frontend.db: Pass connection to DatabaseOperations
    In Django 1.4, the DatabaseOperations constructor takes
    a mandatory connection argument, so let's handle the
    TypeError, and make the application compatible with both
    Django 1.3 and 1.4.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- frontend/db/backends/afe_sqlite/base.py
+++ frontend/db/backends/afe_sqlite/base.py
@@ -12,4 +12,7 @@ class DatabaseOperations(SQLiteDatabaseOperations):
 class DatabaseWrapper(SQLiteDatabaseWrapper):
     def __init__(self, *args, **kwargs):
         super(DatabaseWrapper, self).__init__(*args, **kwargs)
-        self.ops = DatabaseOperations()
+        try:
+            self.ops = DatabaseOperations()
+        except TypeError:
+            self.ops = DatabaseOperations(connection=kwargs.get('connection'))
