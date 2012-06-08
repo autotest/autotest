2243c4f7e908969a33c4ad795f905e2d1c869171
Lucas Meneghel Rodrigues <lmr@redhat.com>
2012-06-08 17:17:57 -0300

    frontend: Change RPC server timezone to UTC
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- frontend/settings.py
+++ frontend/settings.py
@@ -53,7 +53,7 @@ TKO_URL_PREFIX = 'new_tko/server/'
 # although not all variations may be possible on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Los_Angeles'
+TIME_ZONE = 'UTC'
 
 # Language code for this installation. All choices can be found here:
 # http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
