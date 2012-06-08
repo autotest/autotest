f58fc5f0e73efc427a727d9d43933240030edb38
Lucas Meneghel Rodrigues <lmr@redhat.com>
2012-06-08 17:15:18 -0300

    frontend: Disable i18n for rpc server
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- frontend/settings.py
+++ frontend/settings.py
@@ -64,7 +64,7 @@ SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
-USE_I18N = True
+USE_I18N = False
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
