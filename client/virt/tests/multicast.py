3544d7be243dc00d0b44a3162687424e3cf7a833
Qingtang Zhou <qzhou@redhat.com>
2012-06-11 21:42:23 +0000

    virt test: multicast: Correct multicast_guest.py script path
    Signed-off-by: Qingtang Zhou <qzhou@redhat.com>

--- client/virt/tests/multicast.py
+++ client/virt/tests/multicast.py
@@ -53,7 +53,7 @@ def run_multicast(test, params, env):
     prefix = re.findall("\d+.\d+.\d+", mcast)[0]
     suffix = int(re.findall("\d+", mcast)[-1])
     # copy python script to guest for joining guest to multicast groups
-    mcast_path = os.path.join(test.bindir, "scripts/multicast_guest.py")
+    mcast_path = os.path.join(test.virtdir, "scripts/multicast_guest.py")
     vm.copy_files_to(mcast_path, "/tmp")
     output = session.cmd_output("python /tmp/multicast_guest.py %d %s %d" %
                                 (mgroup_count, prefix, suffix))
