6291967865cb2bfd57a87ace15c7d230bf2426f7
Chris Evich <cevich@redhat.com>
2012-06-20 14:12:00 -0400

    Client: Include ElementTree and wrapper module
    Signed-off-by: Chris Evich <cevich@redhat.com>

---
+++ client/shared/xml_utils.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+Wrapper module to load native or included ElementTree module.
+"""
+
+try:
+    import autotest.common as common
+except ImportError:
+    import common
+
+try:
+      from xml.etree.ElementTree import *
+except ImportError:
+      from autotest_lib.client.shared.ElementTree import *
