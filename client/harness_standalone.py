54638ab6f29e19e8131a2270f40ebc7c76420227
Lucas Meneghel Rodrigues <lmr@redhat.com>
2012-06-20 00:40:26 -0300

    client.job: Consolidate tests output options in one
    Turn client/[tests|site-tests] into read only directories,
    make all the output that required those dirs to be
    read write to go to a configurable location. Consolidate
    all previous options that dealt with test output:
    
    * test_tmp_dir
    * pkg_dir
    * state_dir
    
    Into a single one:
    
    * test_output_dir
    
    If that variable is not set, use the client temporary
    directory to write those files into.
    
    Signed-off-by: Don Zickus <dzickus@redhat.com>
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- client/harness_standalone.py
+++ client/harness_standalone.py
@@ -5,9 +5,11 @@ The default interface as required for the standalone reboot helper.
 
 __author__ = """Copyright Andy Whitcroft 2007"""
 
-from autotest.client.shared import error
+from autotest.client.shared import error, global_config
 from autotest.client import utils
 import os, harness, shutil, logging
+
+GLOBAL_CONFIG = global_config.global_config
 
 class harness_standalone(harness.harness):
     """The standalone server harness
@@ -25,8 +27,12 @@ class harness_standalone(harness.harness):
         self.autodir = os.path.abspath(os.environ['AUTODIR'])
         self.setup(job)
 
+        tmpdir = os.path.join(self.autodir, 'tmp')
+        tests_dir = GLOBAL_CONFIG.get_config_value('COMMON', 'test_output_dir',
+                                                   default=tmpdir)
+
         src = job.control_get()
-        dest = os.path.join(self.autodir, 'control')
+        dest = os.path.join(tests_dir, 'control')
         if os.path.abspath(src) != os.path.abspath(dest):
             shutil.copyfile(src, dest)
             job.control_set(dest)
