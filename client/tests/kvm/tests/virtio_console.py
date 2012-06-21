b7f090f8e29e605ea2c9bea9238ff980af6eb4fc
Lukáš Doktor <ldoktor@redhat.com>
2012-06-21 17:38:00 +0200

    virt: Move kvm_virtio_port related helpers from test to library
    * This patch moves virtio_port related helpers from virtio_console test
      to kvm_virtio_port library.
    * It also changes the way of VM handling and avoids unnecessarily VM
      recreation.
    
    Signed-off-by: Lukáš Doktor <ldoktor@redhat.com>

--- client/tests/kvm/tests/virtio_console.py
+++ client/tests/kvm/tests/virtio_console.py
@@ -197,257 +197,6 @@ def run_virtio_console(test, params, env):
             return self._gen_res(lambda str: self.result_to_string(str))
 
 
-    class ThSend(Thread):
-        """
-        Random data sender thread.
-        """
-        def __init__(self, port, data, event, quiet=False):
-            """
-            @param port: Destination port.
-            @param data: The data intend to be send in a loop.
-            @param event: Exit event.
-            @param quiet: If true don't raise event when crash.
-            """
-            Thread.__init__(self)
-            self.port = port
-            # FIXME: socket.send(data>>127998) without read blocks thread
-            if len(data) > 102400:
-                data = data[0:102400]
-                logging.error("Data is too long, using only first %d bytes",
-                              len(data))
-            self.data = data
-            self.exitevent = event
-            self.idx = 0
-            self.quiet = quiet
-
-
-        def run(self):
-            logging.debug("ThSend %s: run", self.getName())
-            try:
-                while not self.exitevent.isSet():
-                    self.idx += self.port.send(self.data)
-                logging.debug("ThSend %s: exit(%d)", self.getName(),
-                              self.idx)
-            except Exception, ints:
-                if not self.quiet:
-                    raise ints
-                logging.debug(ints)
-
-
-    class ThSendCheck(Thread):
-        """
-        Random data sender thread.
-        """
-        def __init__(self, port, event, queues, blocklen=1024):
-            """
-            @param port: Destination port
-            @param event: Exit event
-            @param queues: Queues for the control data (FIFOs)
-            @param blocklen: Block length
-            """
-            Thread.__init__(self)
-            self.port = port
-            self.queues = queues
-            # FIXME: socket.send(data>>127998) without read blocks thread
-            if blocklen > 102400:
-                blocklen = 102400
-                logging.error("Data is too long, using blocklen = %d",
-                              blocklen)
-            self.blocklen = blocklen
-            self.exitevent = event
-            self.idx = 0
-
-
-        def run(self):
-            logging.debug("ThSendCheck %s: run", self.getName())
-            too_much_data = False
-            while not self.exitevent.isSet():
-                # FIXME: workaround the problem with qemu-kvm stall when too
-                # much data is sent without receiving
-                for queue in self.queues:
-                    while not self.exitevent.isSet() and len(queue) > 1048576:
-                        too_much_data = True
-                        time.sleep(0.1)
-                ret = select.select([], [self.port.sock], [], 1.0)
-                if ret[1]:
-                    # Generate blocklen of random data add them to the FIFO
-                    # and send them over virtio_console
-                    buf = ""
-                    for i in range(self.blocklen):
-                        ch = "%c" % random.randrange(255)
-                        buf += ch
-                        for queue in self.queues:
-                            queue.append(ch)
-                    target = self.idx + self.blocklen
-                    while not self.exitevent.isSet() and self.idx < target:
-                        try:
-                            idx = self.port.sock.send(buf)
-                        except Exception, inst:
-                            # Broken pipe
-                            if inst.errno == 32:
-                                logging.debug("ThSendCheck %s: Broken pipe "
-                                              "(migration?), reconnecting",
-                                              self.getName())
-                                attempt = 10
-                                while (attempt > 1
-                                       and not self.exitevent.isSet()):
-                                    self.port.is_open = False
-                                    self.port.open()
-                                    try:
-                                        idx = self.port.sock.send(buf)
-                                    except Exception:
-                                        attempt += 1
-                                        time.sleep(10)
-                                    else:
-                                        attempt = 0
-                        buf = buf[idx:]
-                        self.idx += idx
-            logging.debug("ThSendCheck %s: exit(%d)", self.getName(),
-                          self.idx)
-            if too_much_data:
-                logging.error("ThSendCheck: working around the 'too_much_data'"
-                              "bug")
-
-
-    class ThRecv(Thread):
-        """
-        Recieves data and throws it away.
-        """
-        def __init__(self, port, event, blocklen=1024, quiet=False):
-            """
-            @param port: Data source port.
-            @param event: Exit event.
-            @param blocklen: Block length.
-            @param quiet: If true don't raise event when crash.
-            """
-            Thread.__init__(self)
-            self.port = port
-            self._port_timeout = self.port.gettimeout()
-            self.port.settimeout(0.1)
-            self.exitevent = event
-            self.blocklen = blocklen
-            self.idx = 0
-            self.quiet = quiet
-
-
-        def run(self):
-            logging.debug("ThRecv %s: run", self.getName())
-            try:
-                while not self.exitevent.isSet():
-                    # TODO: Workaround, it didn't work with select :-/
-                    try:
-                        self.idx += len(self.port.recv(self.blocklen))
-                    except socket.timeout:
-                        pass
-                self.port.settimeout(self._port_timeout)
-                logging.debug("ThRecv %s: exit(%d)", self.getName(), self.idx)
-            except Exception, ints:
-                if not self.quiet:
-                    raise ints
-                logging.debug(ints)
-
-
-    class ThRecvCheck(Thread):
-        """
-        Random data receiver/checker thread.
-        """
-        def __init__(self, port, buffer, event, blocklen=1024, sendlen=0):
-            """
-            @param port: Source port.
-            @param buffer: Control data buffer (FIFO).
-            @param length: Amount of data we want to receive.
-            @param blocklen: Block length.
-            @param sendlen: Block length of the send function (on guest)
-            """
-            Thread.__init__(self)
-            self.port = port
-            self.buffer = buffer
-            self.exitevent = event
-            self.blocklen = blocklen
-            self.idx = 0
-            self.sendlen = sendlen + 1  # >=
-
-
-        def run(self):
-            logging.debug("ThRecvCheck %s: run", self.getName())
-            attempt = 10
-            sendidx = -1
-            minsendidx = self.sendlen
-            while not self.exitevent.isSet():
-                ret = select.select([self.port.sock], [], [], 1.0)
-                if ret[0] and (not self.exitevent.isSet()):
-                    buf = self.port.sock.recv(self.blocklen)
-                    if buf:
-                        # Compare the received data with the control data
-                        for ch in buf:
-                            ch_ = self.buffer.popleft()
-                            if ch == ch_:
-                                self.idx += 1
-                            else:
-                                # TODO BUG: data from the socket on host can
-                                # be lost during migration
-                                while ch != ch_:
-                                    if sendidx > 0:
-                                        sendidx -= 1
-                                        ch_ = self.buffer.popleft()
-                                    else:
-                                        self.exitevent.set()
-                                        logging.error("ThRecvCheck %s: "
-                                                      "Failed to recv %dth "
-                                                      "character",
-                                                      self.getName(), self.idx)
-                                        logging.error("ThRecvCheck %s: "
-                                                      "%s != %s",
-                                                      self.getName(),
-                                                      repr(ch), repr(ch_))
-                                        logging.error("ThRecvCheck %s: "
-                                                      "Recv = %s",
-                                                      self.getName(), repr(buf))
-                                        # sender might change the buffer :-(
-                                        time.sleep(1)
-                                        ch_ = ""
-                                        for buf in self.buffer:
-                                            ch_ += buf
-                                            ch_ += ' '
-                                        logging.error("ThRecvCheck %s: "
-                                                      "Queue = %s",
-                                                      self.getName(), repr(ch_))
-                                        logging.info("ThRecvCheck %s: "
-                                                    "MaxSendIDX = %d",
-                                                    self.getName(),
-                                                    (self.sendlen - sendidx))
-                                        raise error.TestFail("ThRecvCheck %s: "
-                                                             "incorrect data" %
-                                                             self.getName())
-                        attempt = 10
-                    else:   # ! buf
-                        # Broken socket
-                        if attempt > 0:
-                            attempt -= 1
-                            logging.debug("ThRecvCheck %s: Broken pipe "
-                                          "(migration?), reconnecting. ",
-                                          self.getName())
-                            # TODO BUG: data from the socket on host can be lost
-                            if sendidx >= 0:
-                                minsendidx = min(minsendidx, sendidx)
-                                logging.debug("ThRecvCheck %s: Previous data "
-                                              "loss was %d.",
-                                              self.getName(),
-                                              (self.sendlen - sendidx))
-                            sendidx = self.sendlen
-                            self.port.is_open = False
-                            self.port.open()
-            if sendidx >= 0:
-                minsendidx = min(minsendidx, sendidx)
-            if (self.sendlen - minsendidx):
-                logging.error("ThRecvCheck %s: Data loss occured during socket"
-                              "reconnection. Maximal loss was %d per one "
-                              "migration.", self.getName(),
-                              (self.sendlen - minsendidx))
-            logging.debug("ThRecvCheck %s: exit(%d)", self.getName(),
-                          self.idx)
-
-
     def process_stats(stats, scale=1.0):
         """
         Process and print the stats.
@@ -623,7 +372,8 @@ def run_virtio_console(test, params, env):
         on_guest("print 'PASS: nothing'", vm, 10)
 
 
-    def _vm_create(no_console=3, no_serialport=3, spread=True):
+    def _vm_create(no_console=3, no_serialport=3, spread=True, quiet=False):
+        # TODO: Add option to suppress logging.warning (for max_console tests)
         """
         Creates the VM and connects the specified number of consoles and serial
         ports.
@@ -641,30 +391,43 @@ def run_virtio_console(test, params, env):
                 guest informations = [vm, session, tmp_dir, kcrash]
                 consoles informations = [consoles[], serialports[]]
         """
-        params['virtio_ports'] = ""
-        if spread is True:  # Compatibility with the original default value
-            params['virtio_port_spread'] = 2
-        elif spread:
-            params['virtio_port_spread'] = spread
-        else:
-            params['virtio_port_spread'] = 0
+        # check the number of running VM's consoles
+        vm = env.get_vm(params.get("main_vm"))
 
-        for i in xrange(no_console):
-            name = "console-%d" % i
-            params['virtio_ports'] += " %s" % name
-            params['virtio_port_type_%s' % name] = "console"
+        _no_serialports = 0
+        _no_consoles = 0
+        for port in vm.virtio_ports:
+            if isinstance(port, kvm_virtio_port.VirtioSerial):
+                _no_serialports += 1
+            else:
+                _no_consoles += 1
+        # If not enough ports, modify params and recreate VM
+        if _no_serialports < no_serialport or _no_consoles < no_console:
+            logging.warning("cfg: not enough virtio_ports specified, increase "
+                            "the number of virtio_ports in config.")
 
-        for i in xrange(no_serialport):
-            name = "serialport-%d" % i
-            params['virtio_ports'] += " %s" % name
-            params['virtio_port_type_%s' % name] = "serialport"
+            params['virtio_ports'] = ""
+            if spread is True:  # Compatibility with the original default value
+                params['virtio_port_spread'] = 2
+            elif spread:
+                params['virtio_port_spread'] = spread
+            else:
+                params['virtio_port_spread'] = 0
 
-        consoles = []
-        serialports = []
+            for i in xrange(no_console):
+                name = "console-%d" % i
+                params['virtio_ports'] += " %s" % name
+                params['virtio_port_type_%s' % name] = "console"
+
+            for i in xrange(no_serialport):
+                name = "serialport-%d" % i
+                params['virtio_ports'] += " %s" % name
+                params['virtio_port_type_%s' % name] = "serialport"
 
         (vm, session, sserial) = _restore_vm()
 
-        # connect the sockets
+        consoles = []
+        serialports = []
         for port in vm.virtio_ports:
             if isinstance(port, kvm_virtio_port.VirtioSerial):
                 serialports.append(port)
@@ -673,7 +436,7 @@ def run_virtio_console(test, params, env):
 
         kcrash = False
 
-        # FIXME: 3rd parameter was tmp_dir which is handled by kvm_vm now
+        # FIXME: 3rd parameter was console tmp_dir which is handled by kvm_vm
         return [vm, session, None, sserial, kcrash], [consoles, serialports]
 
 
@@ -687,20 +450,7 @@ def run_virtio_console(test, params, env):
 
         vm = env.get_vm(params.get("main_vm"))
 
-        kernel_bug = None
-        try:
-            session = virt_test_utils.wait_for_login(vm, 0,
-                                    float(params.get("boot_timeout", 100)),
-                                    0, 2)
-        except (error.TestFail):
-            kernel_bug = _search_kernel_crashlog(vm.serial_console, 10)
-            if kernel_bug is not None:
-                logging.error(kernel_bug)
-            raise
-
-        kernel_bug = _search_kernel_crashlog(vm.serial_console, 10)
-        if kernel_bug is not None:
-            logging.error(kernel_bug)
+        vm.verify_kernel_crash()
 
         sserial = virt_test_utils.wait_for_login(vm, 0,
                                          float(params.get("boot_timeout", 20)),
@@ -1227,13 +977,13 @@ def run_virtio_console(test, params, env):
         exit_event = threading.Event()
 
         # TEST
-        thread = ThSendCheck(send_pt, exit_event, queues,
+        thread = kvm_virtio_port.ThSendCheck(send_pt, exit_event, queues,
                              parms[2])
         thread.start()
         threads.append(thread)
 
         for i in range(len(recv_pts)):
-            thread = ThRecvCheck(recv_pts[i], queues[i], exit_event,
+            thread = kvm_virtio_port.ThRecvCheck(recv_pts[i], queues[i], exit_event,
                                  parms[3], sendlen=sendlen)
             thread.start()
             threads.append(thread)
@@ -1457,8 +1207,8 @@ def run_virtio_console(test, params, env):
                  "virt.LOOP_POLL)" % (consoles[0][0].name,
                                       consoles[1][0].name), vm, 10)
         exit_event = threading.Event()
-        send = ThSend(consoles[0][0].sock, "Data", exit_event, quiet=True)
-        recv = ThRecv(consoles[1][0].sock, exit_event, quiet=True)
+        send = kvm_virtio_port.ThSend(consoles[0][0].sock, "Data", exit_event, quiet=True)
+        recv = kvm_virtio_port.ThRecv(consoles[1][0].sock, exit_event, quiet=True)
         send.start()
         time.sleep(2)
         recv.start()
@@ -1626,13 +1376,13 @@ def run_virtio_console(test, params, env):
             exit_event = threading.Event()
 
             # TEST
-            thread = ThSendCheck(send_pt, exit_event, queues,
+            thread = kvm_virtio_port.ThSendCheck(send_pt, exit_event, queues,
                                    buf_len[0])
             thread.start()
             threads.append(thread)
 
             for i in range(len(recv_pts)):
-                thread = ThRecvCheck(recv_pts[i], queues[i], exit_event,
+                thread = kvm_virtio_port.ThRecvCheck(recv_pts[i], queues[i], exit_event,
                                        buf_len[i + 1])
                 thread.start()
                 threads.append(thread)
@@ -1701,7 +1451,7 @@ def run_virtio_console(test, params, env):
             # HOST -> GUEST
             on_guest('virt.loopback(["%s"], [], %d, virt.LOOP_NONE)' %
                      (port.name, buf_len), vm, 10)
-            thread = ThSend(port.sock, data, exit_event)
+            thread = kvm_virtio_port.ThSend(port.sock, data, exit_event)
             stats = array.array('f', [])
             loads = utils.SystemLoad([(os.getpid(), 'autotest'),
                                       (vm[0].get_pid(), 'VM'), 0])
@@ -1741,7 +1491,7 @@ def run_virtio_console(test, params, env):
             stats = array.array('f', [])
             on_guest("virt.send_loop_init('%s', %d)" % (port.name, buf_len),
                      vm, 30)
-            thread = ThRecv(port.sock, exit_event, buf_len)
+            thread = kvm_virtio_port.ThRecv(port.sock, exit_event, buf_len)
             thread.start()
             loads.start()
             on_guest("virt.send_loop()", vm, 10)
@@ -1995,11 +1745,6 @@ def run_virtio_console(test, params, env):
 
 
     # INITIALIZE
-    if "extra_params" in params:
-        standard_extra_params = params['extra_params']
-    else:
-        standard_extra_params = ""
-
     tsmoke_params = params.get('virtio_console_smoke', '')
     tloopback_params = params.get('virtio_console_loopback', '')
     tperf_params = params.get('virtio_console_perf', '')
@@ -2078,5 +1823,4 @@ def run_virtio_console(test, params, env):
 
 
     # CLEANUP
-    vm[1].close()
-    vm[0].destroy(gracefully=False)
+    vm[1].close()   # Close the socket
