0877bb05dfc111e0b677656193039af195283484
Alex Jia <ajia@redhat.com>
2012-07-10 01:29:15 +0800

    virt.tests: Introducing virt-v2v test
    Introduce virt-v2v test. The small amount of actual code on this test is made
    possible by code reuse, made possible by the kvm/libvirt refactor patchset + the
    previous patches on this set.
    
    The obvious difference is that some new parameters had to be introduced to
    accomplish virt-v2v testing. A more complete description of such parameters will
    be present on our online documentation (wiki) later.
    
    v2:
     - Update some *.sample files.
    
    Signed-off-by: Alex Jia <ajia@redhat.com>
    Signed-off-by: Wayne Sun <gsun@redhat.com>

---
+++ client/tests/virt_v2v/virt_v2v.py
@@ -0,0 +1,12 @@
+from autotest.client.virt import virt_test
+
+class virt_v2v(virt_test.virt_test):
+    """
+    Suite of virt-v2v virtualization functional tests.
+
+    @copyright: Red Hat 2008-2012
+
+    @author: Alex Jia (ajia@redhat.com)
+    @author: Wayne Sun (gsun@redhat.com)
+    """
+    pass
