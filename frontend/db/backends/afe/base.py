autotest/autotest

commit ee3cced1afb6bc45216c7038472827ad4033c3f5

frontend: Replaces mod_python by mod_wsgi apache module
This patch also repairs bug with readonly_connection to
mysql database.

Signed-off-by: Jiří Župka <jzupka@redhat.com>

frontend/db/backends/afe/base.py
@@ -23,3 +23,14 @@ class DatabaseWrapper(MySQLDatabaseWrapper):
         except TypeError:
             self.ops = DatabaseOperations(connection=kwargs.get('connection'))
         self.introspection = MySQLIntrospection(self)
+
+    def _valid_connection(self):
+        if self.connection is not None:
+            if self.connection.open:
+                try:
+                    self.connection.ping()
+                    return True
+                except DatabaseError:
+                    self.connection.close()
+                    self.connection = None
+        return False

apache/conf/django-directives
@@ -33,14 +33,17 @@ RewriteRule /media/(css|img|js)(.*) /usr/lib/python2.7/site-packages/django/cont
 RewriteCond /usr/lib/python2.7/site-packages/django/contrib/admin/static/admin -d
 RewriteRule /media/(css|img|js)(.*) /usr/lib/python2.7/site-packages/django/contrib/admin/static/admin/$1/$2
 
-<Location ~ "/(afe|new_tko)/server">
-    SetHandler python-program
-    PythonHandler django.core.handlers.modpython
-    SetEnv DJANGO_SETTINGS_MODULE frontend.settings
-    PythonDebug On
-    # Force our own site-packages to be loaded by mod_python prior
-    # to mod_python's system python site-packages directory.
-    # This way our code can depend on library versions other than
-    # those available as packages on various OS distributions.
-    PythonPath "['/usr/local/autotest/site-packages', '/usr/local/autotest', '/usr/lib/python2.7/site-packages/autotest', '/usr/lib/python2.6/site-packages/autotest', '/usr/lib/python2.5/site-packages/autotest', '/usr/lib/python2.4/site-packages/autotest'] + sys.path"
-</Location>
+# Configuration for mod_wsgi
+RewriteRule ^/(afe|new_tko)/server(.*)$ /$1/server$2 [QSA,PT,L]
+
+WSGISocketPrefix run/wsgi
+
+WSGIDaemonProcess apache processes=10 threads=1
+WSGIProcessGroup apache
+
+WSGIScriptAliasMatch /(afe|new_tko)/server.* /usr/local/autotest/frontend/frontend.wsgi
+
+<Directory /usr/local/autotest/frontend/>
+    Order allow,deny
+    Allow from all
+</Directory>

frontend/frontend.wsgi
@@ -1,0 +1,22 @@
+import os, sys
+
+path_list = ['/usr/local/autotest/site-packages', '/usr/local/autotest',
+            '/usr/lib/python2.7/site-packages/autotest',
+            '/usr/lib/python2.6/site-packages/autotest',
+            '/usr/lib/python2.5/site-packages/autotest',
+            '/usr/lib/python2.4/site-packages/autotest']
+
+for p in path_list:
+   if os.path.isdir(p):
+       sys.path.append(p)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'frontend.settings'
+
+import django.core.handlers.wsgi
+
+_application = django.core.handlers.wsgi.WSGIHandler()
+
+def application(environ, start_response):
+    environ['DJANGO_USE_POST_REWRITE'] = "yes"
+    environ['PATH_INFO'] = environ['SCRIPT_NAME'] + environ['PATH_INFO']
+    return _application(environ, start_response)

commit 1c25a2387f8a7e1844ce28f315cb04233469979f

frontend.db.backends: Fix undefined name error in _valid_connection
We've experienced some problems in a newer instance of
our autotest server, when we discovered that one exception
handling in the django db code (introduced by
ee3cced1afb6bc45216c7038472827ad4033c3f5) had the wrong
namespace for the DatabaseError. Without the appropriate
path, the exception handling would itself throw a NameError
exception, making the function return None.

This is expected to fix errors like:

[Tue Jul 10 10:35:04 2012] [error] [client 1.2.3.4]     return
self.connection.rollback(), referer:
http://foo.com/afe

[Tue Jul 10 10:35:04 2012] [error] [client 1.2.3.4] OperationalError:
(2006, 'MySQL server has gone away'), referer:
http://foo.com/afe/

Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

frontend/db/backends/afe/base.py
@@ -30,7 +30,7 @@ class DatabaseWrapper(MySQLDatabaseWrapper):
                 try:
                     self.connection.ping()
                     return True
-                except DatabaseError:
+                except Database.DatabaseError:
                     self.connection.close()
                     self.connection = None
         return False