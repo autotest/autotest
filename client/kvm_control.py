2b5898121a4fa67b3e8897d0c31ce090f4ad425c
Liu Sheng <liusheng@linux.vnet.ibm.com>
2012-07-23 22:07:55 -0300

    client.kvm_control: fix kvm load and unload issue on power based system
    on power system, kvm only have kvm.ko module. this patch adjects
    load_kvm and unload_kvm functions when finding the cpu is power7
    
    Signed-off-by: Liu Sheng <liusheng@linux.vnet.ibm.com>
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- client/kvm_control.py
+++ client/kvm_control.py
@@ -20,20 +20,26 @@ def get_kvm_arch():
                 arch_type = "kvm_amd"
             elif "GenuineIntel" in line:
                 arch_type = "kvm_intel"
+            elif "POWER7" in line:
+                arch_type = "kvm_power7"
+                return arch_type
         elif "flags" in line:
             if arch_type == "kvm_amd" and "svm" in line:
                 return arch_type
             if arch_type == "kvm_intel" and "vmx" in line:
                 return arch_type
-    raise error.TestError("CPU Must be AMD or Intel, and must be KVM ready.")
+    raise error.TestError("CPU Must be AMD or Intel or POWER7, and must be KVM ready.")
 
 
 def load_kvm():
     """
     Loads the appropriate KVM kernel modules
     """
+    kvm_arch = get_kvm_arch()
     kvm_status = utils.system('modprobe kvm')
-    kvm_amdintel_status = utils.system("modprobe " + get_kvm_arch())
+    if kvm_arch == "kvm_power7":
+        return kvm_status
+    kvm_amdintel_status = utils.system("modprobe " + kvm_arch)
     if kvm_status:
         return kvm_status
     else:
@@ -44,8 +50,12 @@ def unload_kvm():
     """
     Unloads the appropriate KVM kernel modules
     """
-    kvm_amdintel_status = utils.system("rmmod " + get_kvm_arch())
+    kvm_arch = get_kvm_arch()
+    if kvm_arch != "kvm_power7":
+        kvm_amdintel_status = utils.system("rmmod " + kvm_arch)
     kvm_status = utils.system('rmmod kvm')
+    if kvm_arch == "kvm_power7":
+        return kvm_status
     if kvm_status:
         return kvm_status
     else:
