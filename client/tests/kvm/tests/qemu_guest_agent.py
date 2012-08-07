c14a23c3201ba81c55183ea1165b520695f34828
Qingtang Zhou <qzhou@redhat.com>
2012-08-07 16:23:07 +0800

    KVM Test: Add qemu guest agent test
    This test installs qemu guest agent package in guest and
    verify it work in guest.
    
    Because there is no pre-build guest agent binary for windows
    guest (qemu-ga.exe), just limit this test on Fedora guests.
    
    Changes from v2:
     - Rename GuestAgent to QemuAgent.
    
    Changes from v1:
     - Use new GuestAgent interface arguments.
     - Put guest agent name in config file.
    
    Signed-off-by: Qingtang Zhou <qzhou@redhat.com>

---
+++ client/tests/kvm/tests/qemu_guest_agent.py
@@ -0,0 +1,36 @@
+import logging
+from autotest.client.shared import error
+from autotest.client.virt import virt_agent
+
+
+def run_qemu_guest_agent(test, params, env):
+    """
+    Test qemu guest agent, this case will:
+    1) Start VM with virtio serial port.
+    2) Install qemu-guest-agent package in guest.
+    3) Create QemuAgent object and test if virt agent works.
+
+    @param test: kvm test object
+    @param params: Dictionary with the test parameters
+    @param env: Dictionary with test environmen.
+    """
+    vm = env.get_vm(params["main_vm"])
+    vm.verify_alive()
+
+    session = vm.wait_for_login(timeout=int(params.get("login_timeout", 360)))
+
+    # Try to install 'qemu-guest-agent' package.
+    gagent_install_cmd = params.get("gagent_install_cmd")
+    s = session.cmd_status(gagent_install_cmd)
+    session.close()
+    if s != 0:
+        raise error.TestError("Could not install qemu-guest-agent package")
+
+    gagent_name = params.get("gagent_name", "org.qemu.guest_agent.0")
+    gagent_file_name = vm.get_virtio_port_filename(gagent_name)
+    gagent = virt_agent.QemuAgent(vm, gagent_name, gagent_file_name,
+                                   get_supported_cmds=True)
+
+    # Check if guest agent work.
+    gagent.verify_responsive()
+    logging.info(gagent.cmd("guest-info"))
