9284c9319ce65683f9c19c262e12aba244b28b32
Qingtang Zhou <qzhou@redhat.com>
2012-08-21 10:19:27 -0300

    virt.kvm_vm: Make sure boot=on\off is added
    Commit 52f94e55 attempts to search the boot option to
    a given drive if qemu command line supports the syntax,
    but it turns out it was slightly incorrect. This patch
    fixes it.
    
    Signed-off-by: Qingtang Zhou <qzhou@redhat.com>

--- client/virt/kvm_vm.py
+++ client/virt/kvm_vm.py
@@ -550,7 +550,7 @@ class VM(virt_vm.BaseVM):
             cmd += _add_option("werror", werror)
             cmd += _add_option("serial", serial)
             cmd += _add_option("snapshot", snapshot, bool)
-            if has_option(help, "boot=on\|off"):
+            if re.search("boot=on\|off", help, re.MULTILINE):
                 cmd += _add_option("boot", boot, bool)
             cmd += _add_option("id", name)
             cmd += _add_option("readonly", readonly, bool)
