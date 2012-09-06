5de453fa0b01f68bb7636eed39af077d7b14866d
Lucas Meneghel Rodrigues <lmr@redhat.com>
2012-09-06 16:40:10 -0300

    virt: Add v2v get started script
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- client/tests/virt/v2v/common.py
+++ client/tests/virt/v2v/common.py
@@ -4,10 +4,12 @@ try:
     client_dir = os.path.dirname(setup_modules.__file__)
 except ImportError:
     dirname = os.path.dirname(sys.modules[__name__].__file__)
-    client_dir = os.path.abspath(os.path.join(dirname, ".."))
+    client_dir = os.path.abspath(os.path.join(dirname, "..", "..", ".."))
     sys.path.insert(0, client_dir)
     import setup_modules
     sys.path.pop(0)
+    virt_test_dir = os.path.abspath(os.path.join(dirname, ".."))
+    sys.path.insert(0, virt_test_dir)
 
 setup_modules.setup(base_path=client_dir,
                     root_module_name="autotest.client")
