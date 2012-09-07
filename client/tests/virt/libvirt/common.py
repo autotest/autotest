b3a75ec5a95be4a41828d80cc1fe0414f1d203ec
Lucas Meneghel Rodrigues <lmr@redhat.com>
2012-09-07 19:02:30 -0300

    virt: Modify common.py files for standalone dir mode
    When testing outside the autotest tree, we had failures
    executing the get_started scripts. Fix this by leveraging
    the AUTOTEST_PATH environment dir.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- client/tests/virt/libvirt/common.py
+++ client/tests/virt/libvirt/common.py
@@ -1,11 +1,31 @@
 import os, sys
-dirname = os.path.dirname(sys.modules[__name__].__file__)
-client_dir = os.path.abspath(os.path.join(dirname, "..", "..", ".."))
-sys.path.insert(0, client_dir)
-import setup_modules
-sys.path.pop(0)
-virt_test_dir = os.path.abspath(os.path.join(dirname, ".."))
-sys.path.insert(0, virt_test_dir)
 
-setup_modules.setup(base_path=client_dir,
-                    root_module_name="autotest.client")
+def load_setup_modules(client_dir):
+    try:
+        sys.path.insert(0, client_dir)
+        import setup_modules
+    finally:
+        sys.path.pop(0)
+    return setup_modules
+
+try:
+    import autotest.client.setup_modules as setup_modules
+    client_dir = os.path.dirname(setup_modules.__file__)
+    sm = setup_modules
+except ImportError:
+    dirname = os.path.dirname(sys.modules[__name__].__file__)
+    try:
+        client_dir = os.path.abspath(os.path.join(dirname, "..", "..", ".."))
+        sm = load_setup_modules(client_dir)
+    except:
+        try:
+            client_dir = os.path.join(os.environ['AUTOTEST_PATH'], 'client')
+        except KeyError:
+            print("Environment variable $AUTOTEST_PATH not set. "
+                  "please set it to a path containing an autotest checkout")
+            sys.exit(1)
+        sm = load_setup_modules(client_dir)
+    virt_test_dir = os.path.abspath(os.path.join(dirname, ".."))
+    sys.path.insert(0, virt_test_dir)
+
+sm.setup(base_path=client_dir, root_module_name="autotest.client")
