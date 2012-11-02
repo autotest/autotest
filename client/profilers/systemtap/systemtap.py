bbcface6ebdd8f862505e851282bd6c4f178ffba
Yunping Zheng <yunzheng@redhat.com>
2012-11-02 18:06:25 -0200

    client.profilers: Add systemtap profiler
    This patch adds a systemtap profiler to autotest. Now systemtap
    can run during your test, providing useful debug data.
    
    When you use this profiler on virt tests, you should add
        'profilers = systemtap'
    and
        'stap_script_file = file_path_systemtap_script'
    
    On your test cfg file. If the 'file_path_systemtap_script' you
    assigned is a relative path,the script will try to find the file
    under 'test.autodir/profilers/systemtap/scripts'.
    
    Before using this patch make sure you host is support "stap",you can
    run  "stap -e 'probe begin { log(\"Support\") exit() }'" to have a
    check, if you host not suppor stap,please install systemtap and
    related packages.
    
    Signed-off-by: Yunping Zheng <yunzheng@redhat.com>

---
+++ client/profilers/systemtap/systemtap.py
@@ -0,0 +1,82 @@
+"""
+Autotest systemtap profiler.
+"""
+import logging, os, re, subprocess
+from autotest.client import profiler, os_dep
+from autotest.client.shared import utils, error
+
+
+class systemtap(profiler.profiler):
+    """
+    Tracing test process using systemtap tools.
+    """
+    version = 1
+
+    def initialize(self, **dargs):
+        self.is_enabled = False
+
+        stap_installed = False
+        try:
+            self.stap_path = os_dep.command('stap')
+            stap_installed = True
+        except ValueError:
+            logging.error('Command stap not present')
+
+        if stap_installed:
+            self.is_enabled = True
+            self.script_name = dargs.get('stap_script_file')
+            stap_support_cmd = "stap -e 'probe begin { log(\"Support\") exit() }'"
+            if not re.findall("Support", utils.system_output(stap_support_cmd)):
+                logging.warning("Seems your host does not support systemtap")
+                self.is_enabled = False
+            if not self.script_name:
+                logging.warning("You should assign a script file")
+                self.is_enabled = False
+
+
+    def _get_stap_script_name(self, test):
+        try:
+            if os.path.isabs(self.script_name):
+                return self.script_name
+            else:
+                return os.path.join(test.autodir, "profilers/systemtap/scripts", self.script_name)
+        except AttributeError:
+            return self.script_name
+
+
+    def start(self, test):
+        if self.is_enabled:
+            stap_script = self._get_stap_script_name(test)
+            if os.path.isfile(stap_script):
+                cmd = "stap %s"  % (stap_script)
+                logfile = open(os.path.join(test.profdir, "systemtap.log"), 'w')
+                p = subprocess.Popen(cmd, shell=True, stdout=logfile,
+                                     stderr=subprocess.STDOUT)
+                self.pid = p.pid
+            else:
+                logging.warning("Asked for systemtap profiling, but no script "
+                                "file %s not found", stap_script)
+                self.is_enabled = False
+        else:
+            logging.warning("Asked for systemtap profiling, but it couldn't "
+                            "be initialized")
+
+
+    def stop(self, test):
+        if self.is_enabled:
+            try:
+                term_profiler = "kill -15 %d" % self.pid
+                # send SIGTERM to iostat and give it a 5-sec timeout
+                utils.system(term_profiler, timeout=5)
+            except error.CmdError: # probably times out
+                pass
+            # do a ps again to see if iostat is still there
+            ps_cmd = "ps -p %d | grep stap" % self.pid
+            out = utils.system_output(ps_cmd, ignore_status=True)
+            if out != '':
+                kill_profiler = 'kill -9 %d' % self.pid
+                utils.system(kill_profiler, ignore_status=True)
+
+
+    def report(self, test):
+        return None
