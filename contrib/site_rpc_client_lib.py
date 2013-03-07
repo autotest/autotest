2739d8a3c612f32d679c62e79dacf741422b8927
Nishanth Aravamudan <nacc@linux.vnet.ibm.com>
2013-03-07 21:54:56 +0000

    contrib/site_rpc_client_lib: provide authorization_headers that allow authentication
    In my environment, I am authenticating users against an LDAP server. The CLI
    does not currently allow users to authenticate, though, so provide an example
    site_rpc_client_lib that does.
    
    Changes from v1:
         * Moved authorship info from __author__ attribute to docstring.
    
    Signed-off-by: Nishanth Aravamudan <nacc@linux.vnet.ibm.com>

---
+++ contrib/site_rpc_client_lib.py
@@ -0,0 +1,27 @@
+"""
+This module provides site-local authorization headers for Apache.
+It asks the end-user for a password, rather than assuming no password
+is necessary.
+To actually use this, put it in the cli/ directory.
+
+@author: Nishanth Aravamudan <nacc@linux.vnet.ibm.com>
+"""
+
+import getpass, os, base64
+
+
+def authorization_headers(username, server):
+    """
+    Ask the user for their password, rather than assuming they don't
+    need one.
+
+    @returns A dictionary of authorization headers to pass in to get_proxy().
+    """
+    if not username:
+        if 'AUTOTEST_USER' in os.environ:
+            username = os.environ['AUTOTEST_USER']
+        else:
+            username = getpass.getuser()
+    password = getpass.getpass('Enter the password for %s: ' % username)
+    base64string = base64.encodestring('%s:%s' % (username,password))[:-1]
+    return {'AUTHORIZATION' : 'Basic %s' % base64string}
