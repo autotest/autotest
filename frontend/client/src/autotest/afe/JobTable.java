605794f56567ba92b6a06d5b7155edc32534fabf
Julius Gawlas <julius.gawlas@hp.com>
2013-03-19 06:37:37 +0000

    fixing sorting for Jobs table
    - make created sortable field
    - prevent sorting on status, display friendly message instead of error
    
    Signed-off-by: Julius Gawlas <julius.gawlas@hp.com>

--- frontend/client/src/autotest/afe/JobTable.java
+++ frontend/client/src/autotest/afe/JobTable.java
@@ -3,6 +3,7 @@ package autotest.afe;
 import autotest.common.table.DynamicTable;
 import autotest.common.table.RpcDataSource;
 import autotest.common.table.DataSource.SortDirection;
+import autotest.common.ui.NotifyManager;
 
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.json.client.JSONString;
@@ -17,8 +18,9 @@ public class JobTable extends DynamicTable {
     private static final String[][] JOB_COLUMNS = { {CLICKABLE_WIDGET_COLUMN, "Select"}, 
             { "id", "ID" }, { "owner", "Owner" }, { "name", "Name" },
             { "priority", "Priority" }, { "control_type", "Client/Server" },
-            { CREATED_TEXT, "Created" }, { HOSTS_SUMMARY, "Status" } };
-
+            { "created_on", "Created" }, { HOSTS_SUMMARY, "Status" } };
+    private static final int STATUS_COLUMN = JOB_COLUMNS.length-1; 
+    
     public JobTable() {
         super(JOB_COLUMNS, new RpcDataSource("get_jobs_summary", "get_num_jobs"));
         sortOnColumn("id", SortDirection.DESCENDING);
@@ -31,6 +33,19 @@ public class JobTable extends DynamicTable {
         row.put(HOSTS_SUMMARY, new JSONString(countString));
         
         // remove seconds from created time
+        if ( row.containsKey(CREATED_TEXT) )
+            row.put("created_on", row.get(CREATED_TEXT));        
         AfeUtils.removeSecondsFromDateField(row, "created_on", CREATED_TEXT);
     }
+
+    @Override
+    protected void onCellClicked(int row, int cell, boolean isRightClick) {
+        if (row == headerRow && cell == STATUS_COLUMN) { 
+            // prevent sorting error on status columns
+            NotifyManager.getInstance().showMessage("Sorting is not supported for this column.");
+            return;
+        }
+        super.onCellClicked(row, cell, isRightClick);
+    }
+    
 }
