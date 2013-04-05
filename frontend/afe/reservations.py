32cc07be251ffdcf153131373bc71f8235567299
Julius Gawlas <julius.gawlas@hp.com>
2013-04-05 05:42:38 -0700

    frontend/afe/reservations.py: utilities for reserve/release
    Simple utilities for managing reservations. The implementation is
    based on setting and removing ACLs, that will prevent other users
    from having access to machines that are reserved by others.
    
    Changes from v1:
     * docstrings content and format
     * general style changes
     * created method to get default user
    
    Signed-off-by: Julius Gawlas <julius.gawlas@hp.com>
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

---
+++ frontend/afe/reservations.py
@@ -0,0 +1,105 @@
+#
+# Copyright 2013 Hewlett-Packard Development Company, L.P.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of version 2 of the GNU General Public License
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to:
+#   Free Software Foundation, Inc.
+#   51 Franklin Street, Fifth Floor
+#   Boston, MA 02110-1301, USA.
+#
+
+
+__author__ = "Julius Gawlas <julius.gawlas@hp.com>"
+
+
+from autotest.frontend.afe import models
+
+
+__all__ = ['create', 'release']
+
+
+#
+# Implementation notes:
+#
+# Reservations are implemented using ACL mechanism to minimize changes to the
+# core. If we reserve a system for a user ACL is created with the same name as
+# username and host is placed in that ACL, release simple removes the system
+# from that ACL
+#
+
+
+def get_user(username=None):
+    '''
+    Get the specificed user object or the current user if none is specified
+
+    :param username: login of the user reserving hosts
+    :type username: str
+    :returns: the user object for the given username
+    :rtype: :class:`models.User`
+    '''
+    if username:
+        user = models.User.objects.get(login=username)
+    else:
+        user = models.User.current_user()
+    return user
+
+
+def create(hosts_to_reserve, username=None):
+    """
+    Reserve hosts for user
+
+    :param hosts_to_reserve: strings or idents for hosts to reserve
+    :type hosts_to_reserve: list
+    :param username: login of the user reserving hosts
+    :type username: str
+    :raises: AclAccessViolation if user cannot reserve all specified hosts
+    """
+    hosts = models.Host.smart_get_bulk(hosts_to_reserve)
+    if not hosts:
+        raise Exception("At least one host must be specified")
+    # check if this user can access specified hosts
+    user = get_user(username)
+    models.AclGroup.check_for_acl_violation_hosts(hosts, user.login)
+    user_acl, created = models.AclGroup.objects.get_or_create(name=user.login)
+    if created:
+        user_acl.users = [user]
+        user_acl.save()
+    for host in hosts:
+        # remove host from other acls
+        user_acl.hosts.clear()
+        host.aclgroup_set.add(user_acl)
+        # and add to reservation acl
+        user_acl.hosts.add(*hosts)
+        user_acl.on_host_membership_change()
+
+
+def release(hosts_to_release, username=None):
+    """
+    Release a collection of hosts from user
+
+    It's OK if user does not own these systems, in which case this does nothing
+
+    :param hosts_to_release: strings or idents for hosts to release
+    :type hosts_to_release: list
+    :param username: login of the user reserving hosts
+    :type username: str
+    """
+    hosts = models.Host.smart_get_bulk(hosts_to_release)
+    if not hosts:
+        raise Exception("At least one host must be specified")
+    user = get_user(username)
+    acls = models.AclGroup.objects.filter(name=user.login)
+    if acls:
+        user_acl = acls[0]
+        user_acl.hosts.remove(*hosts)
+        user_acl.on_host_membership_change()
---
+++ frontend/afe/reservations_unittest.py
@@ -0,0 +1,141 @@
+#!/usr/bin/python
+
+#
+# Copyright 2013 Hewlett-Packard Development Company, L.P.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of version 2 of the GNU General Public License
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to:
+#   Free Software Foundation, Inc.
+#   51 Franklin Street, Fifth Floor
+#   Boston, MA 02110-1301, USA.
+#
+
+
+__author__ = "Julius Gawlas <julius.gawlas@hp.com>"
+
+
+import unittest
+
+try:
+    import autotest.common as common
+except ImportError:
+    import common
+from autotest.frontend import setup_django_environment
+from autotest.frontend.afe import frontend_test_utils
+from autotest.frontend.afe import models, reservations
+from autotest.frontend import thread_local
+
+
+class ReservationsTest(unittest.TestCase,
+                       frontend_test_utils.FrontendTestMixin):
+    def setUp(self):
+        self._frontend_common_setup()
+        self._create_basic_reservation_data()
+
+
+    def tearDown(self):
+        self._frontend_common_teardown()
+
+
+    def _create_basic_reservation_data(self):
+        '''
+        Creates the basic data used on tests
+        '''
+        self.reservation_hosts = []
+        for hostname in ('reshost1', 'reshost2', 'reshost3', 'reshost4'):
+            host = models.Host.objects.create(hostname=hostname)
+            self.reservation_hosts.append(host)
+
+        self.user1 = models.User.objects.create(login='resuser1')
+        self.user2 = models.User.objects.create(login='resuser2')
+        reservations.create(['reshost4'], self.user2.login)
+
+
+    def test_unreservable_already_reserved(self):
+        '''
+        Tests a reservation attempt on a already explicitly reserved host
+
+        Host 'reshost4' is already reserved to user2 at basic data creation
+        '''
+        self.assertRaises(models.AclAccessViolation,
+                          reservations.create,
+                          ['reshost4'],
+                          self.user1.login)
+
+
+    def test_unreservable_in_another_acl(self):
+        '''
+        Tests unreservable hosts in another acl
+        '''
+        models.AclGroup.check_for_acl_violation_hosts(
+                        self.reservation_hosts[2:3],
+                        self.user2.login)
+
+
+    def test_normal_reserve_lifecycle(self):
+        '''
+        Tests the normal reservation lifecycle
+
+        A few hosts will be reserved for a given user, and then a second
+        user tries to access the host.
+        '''
+        #reserve few hosts
+        host_names = [h.hostname for h in self.reservation_hosts[0:2]]
+        reservations.create(host_names, self.user1.login)
+
+        #second user cannot access it
+        self.assertRaises(models.AclAccessViolation,
+                          models.AclGroup.check_for_acl_violation_hosts,
+                          self.reservation_hosts[0:2],
+                          self.user2.login)
+
+        reservations.release(host_names, self.user1.login)
+        models.AclGroup.check_for_acl_violation_hosts(
+            self.reservation_hosts[0:2], self.user2.login)
+
+
+    def test_nop_release(self):
+        '''
+        Tests releasing hosts from a user that doesn't have a host reserved
+
+        This should perform no action, and the user should continue to have
+        no access to the host
+        '''
+        reservations.release(['reshost4'], self.user1.login)
+        self.assertRaises(models.AclAccessViolation,
+                          reservations.create,
+                          ['reshost4'],
+                          self.user1.login)
+
+
+    def test_reservations_default_user(self):
+        '''
+        Tests if we handle default logged in user
+        '''
+        thread_local.set_user(self.user1)
+        self.assertRaises(models.AclAccessViolation,
+                          reservations.create,
+                          ['reshost4'])
+
+
+    def test_reservations_no_hosts(self):
+        '''
+        Tests creating reservations for non-existing hosts
+        '''
+        self.assertRaises(Exception,
+                          reservations.create,
+                          ['blah', 'woof'])
+
+
+if __name__ == '__main__':
+    unittest.main()
