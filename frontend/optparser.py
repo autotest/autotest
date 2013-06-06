393a92d4b9bf98555ad65e13a0030974083c701c
Cleber Rosa <crosa@redhat.com>
2013-06-06 19:14:32 -0300

    frontend/optparser.py: use values from global_config.ini by default
    This saves users of apps that manipulate the database via the frontend
    model interface from always manually entering options.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- frontend/optparser.py
+++ frontend/optparser.py
@@ -5,6 +5,15 @@ functionality, that is, direct database access via frontend models.
 
 import optparse
 
+from autotest.client.shared import settings
+
+
+def get_config_db_value(key):
+    '''
+    Return the value from the default autotest database config section
+    '''
+    return settings.settings.get_value('AUTOTEST_WEB', key)
+
 
 # pylint: disable=I0011,R0904
 class OptionParser(optparse.OptionParser):
@@ -16,11 +25,15 @@ class OptionParser(optparse.OptionParser):
 
         database = optparse.OptionGroup(self, 'DATABASE ACCESS')
         database.add_option("-s", "--database-hostname", metavar="HOSTNAME",
+                            default=get_config_db_value('host'),
                             help="Database server hostname")
         database.add_option("-u", "--database-username", metavar="USERNAME",
+                            default=get_config_db_value('user'),
                             help="Database username")
         database.add_option("-p", "--database-password", metavar="PASSWORD",
+                            default=get_config_db_value('password'),
                             help="Database password")
         database.add_option("-d", "--database-name", metavar="NAME",
+                            default=get_config_db_value('database'),
                             help="Database name")
         self.add_option_group(database)
