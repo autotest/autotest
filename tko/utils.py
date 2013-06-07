145b25f9a34b00b67d747753272330ef6f3c3eb8
Cleber Rosa <crosa@redhat.com>
2013-06-07 01:24:54 -0300

    tko: replace custom debug print routine with logging.*
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- tko/utils.py
+++ tko/utils.py
@@ -1,16 +1,6 @@
 import os, sys, datetime, re
 
 
-_debug_logger = sys.stderr
-def dprint(msg):
-    print >> _debug_logger, msg
-
-
-def redirect_parser_debugging(ostream):
-    global _debug_logger
-    _debug_logger = ostream
-
-
 def get_timestamp(mapping, field):
     val = mapping.get(field, None)
     if val is not None:
