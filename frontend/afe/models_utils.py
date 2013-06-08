c8696093301fcac6400853634c586a131c2aa9cc
Cleber Rosa <crosa@redhat.com>
2013-06-08 19:27:45 -0300

    frontend: introduce utility functions modules
    For both AFE and TKO. These are supposed to wrap common used data
    manipulation idioms in easier to use functions.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

---
+++ frontend/afe/models_utils.py
@@ -0,0 +1,24 @@
+"""
+Simple utility wrappers around the AFE models
+"""
+
+from autotest.frontend.afe.models import Job
+
+
+def job_get_by_id(job_id):
+    '''
+    Return a job based on its id
+    '''
+    try:
+        job = Job.objects.get(pk=job_id)
+    except Job.DoesNotExist:
+        return None
+    return job
+
+
+def job_delete_by_id(job_id):
+    '''
+    Deletes a job entry based on its tag
+    '''
+    Job.objects.get(pk=job_id).delete()
+    return job_get_by_id(job_id) is None
