a0f27d28fc33dce01486ad788bdb65efe704d395
Cleber Rosa <crosa@redhat.com>
2013-06-13 15:58:41 -0300

    tko: remove legacy database layer
    Now that all the users of the legacy database layer have been either
    rewritten or removed, it should be safe to completely remove it.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- tko/db_postgres.py
+++
@@ -1,7 +0,0 @@
-import psycopg2.psycopg1 as driver
-import db
-
-class db_postgres(db.db_sql):
-    def connect(self, host, database, user, password):
-        return driver.connect("dbname=%s user=%s password=%s" % \
-                              (database, user, password))
