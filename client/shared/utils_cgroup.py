06eac6b299ec41da477b7d5376f264fab06ea063
Pavel Hrdina <phrdina@redhat.com>
2013-09-14 10:11:52 +0200

    utils_cgroup: Strip the slash from cgroup path
    We have to strip the slash from cgroup path because the "join" function
    takes the last element starting with slash as the root path.
    
    Signed-off-by: Pavel Hrdina <phrdina@redhat.com>

--- client/shared/utils_cgroup.py
+++ client/shared/utils_cgroup.py
@@ -684,7 +684,7 @@ def resolve_task_cgroup_path(pid, controller):
         proc_file.close()
 
     mount_path = re.findall(r":%s:(\S*)\n" % controller, proc_cgroup_txt)
-    return os.path.join(root_path, mount_path[0])
+    return os.path.join(root_path, mount_path[0].strip("/"))
 
 
 def service_cgconfig_control(action):
