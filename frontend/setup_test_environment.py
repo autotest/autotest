4efbc7cd42eec74fc6a3bd7746a876ada049dbca
Cleber Rosa <crosa@redhat.com>
2013-12-19 13:58:36 -0300

    frontend/setup_test_environment.py: use a test MySQL database
    For running unittests. This will now DROP by default an existing
    database if one exists with name "autotest_web_unittest_run".
    
    Since it's highly unlikely that a user will host a MySQL database
    with that name for other purposes, we consider it's safe enough
    to drop it before each database related unittests.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- frontend/setup_test_environment.py
+++ frontend/setup_test_environment.py
@@ -6,6 +6,12 @@ except ImportError:
     import common
 
 #
+# This is a database name that will be created and dropped on the go
+#
+DATABASE_TEST_NAME = 'autotest_web_unittest_run'
+settings.DATABASES['default']['NAME'] = DATABASE_TEST_NAME
+
+#
 # Enabling the DEBUG setting is the most straightforward way to prevent Django
 # from enforcing conditions that should be enforced during production, but not
 # necessarily under a test environment (such as when running unittests. One
@@ -25,18 +31,12 @@ from autotest.installation_support import database_manager
 # PLEASE READ:
 #
 # Previously the unittests would run only on the memory based SQLite
-# database, but now that it can also run on other backends such as MySQL
-# there's no easy way to make the test environment decide which one to
-# use.
+# database, but now the primary testing target is MySQL, which is
+# the only currently supported backend.
 #
-# Now *developers* can change the settings.DATABASES configurations (or
-# comment them out to use the configuration on global_config.ini) to run
-# the unittests on other backends.
+# Now, by default, a test database is created and droped for unittests.
 #
-# WARNING: if you change the following variable, and have a database
-# with data, you WILL LOSE IT. Only set the following variable to true and
-# run the unittests on a development system and database.
-COMPLETELY_DESTROY_THE_DATABASE = False
+COMPLETELY_DESTROY_THE_TEST_DATABASE = True
 
 
 def run_syncdb(verbosity=0):
@@ -54,8 +54,7 @@ def destroy_test_database():
     # the real connection ourselves.
     # Note this depends on Django internals and will likely need to be changed
     # when we move to Django 1.x.
-    if ((not settings.DATABASES['default']['ENGINE'].endswith('sqlite')) and
-            COMPLETELY_DESTROY_THE_DATABASE):
+    if COMPLETELY_DESTROY_THE_TEST_DATABASE:
         database_name = settings.DATABASES['default']['NAME']
         cur = connection.cursor()
         cur.execute('DROP DATABASE IF EXISTS %s' % database_name)
@@ -73,6 +72,7 @@ def setup_database_via_manager():
     engine = settings.DATABASES['default']['ENGINE']
     rdbms_type = database_manager.engine_to_rdbms_type(engine)
     manager = database_manager.get_manager_from_config(rdbms_type=rdbms_type)
+    manager.name = DATABASE_TEST_NAME
     manager.create_instance()
     manager.grant_privileges()
     run_syncdb()
@@ -80,7 +80,7 @@ def setup_database_via_manager():
 
 
 def set_up():
-    if COMPLETELY_DESTROY_THE_DATABASE:
+    if COMPLETELY_DESTROY_THE_TEST_DATABASE:
         setup_database_via_manager()
 
     readonly_connection.ReadOnlyConnection.set_globally_disabled(True)
