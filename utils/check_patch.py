dc8a3418640a73f66e97b9b150cacfcd0ac4e6a1
Lucas Meneghel Rodrigues <lmr@redhat.com>
2014-02-07 13:12:43 -0200

    tools/check_patch: Remove reference to codespell submodule
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- utils/check_patch.py
+++ utils/check_patch.py
@@ -45,7 +45,6 @@ import run_pep8
 
 UTILS_DIRNAME = os.path.dirname(sys.modules[__name__].__file__)
 TOP_LEVEL_DIRNAME = os.path.abspath(os.path.dirname(UTILS_DIRNAME))
-CODESPELL_PATH = os.path.join(UTILS_DIRNAME, 'codespell', 'codespell.py')
 
 # Hostname of patchwork server to use
 PWHOST = "patchwork.virt.bos.redhat.com"
@@ -375,11 +374,6 @@ class GitBackend(object):
             logging.error("git tree update failed: %s" % e)
 
 
-def run_codespell(path):
-    cmd = CODESPELL_PATH + " -w " + path
-    utils.system(CODESPELL_PATH, ignore_status=True)
-
-
 class FileChecker(object):
 
     """
@@ -531,20 +525,6 @@ class FileChecker(object):
 
         return success
 
-    def _check_codespell(self):
-        """
-        Verifies the file with codespell.
-        """
-        success = True
-        for exc in self.check_exceptions:
-            if re.search(exc, self.path):
-                return success
-
-        path = self._get_checked_filename()
-
-        run_codespell(path)
-
-        return success
 
     def _check_unittest(self):
         """
@@ -617,8 +597,6 @@ class FileChecker(object):
             if not self._check_code():
                 success = False
             if not self._check_pep8():
-                success = False
-            if not self._check_codespell():
                 success = False
             if not skip_unittest:
                 if not self._check_unittest():
@@ -805,10 +783,6 @@ if __name__ == "__main__":
     if full_check:
         failed_paths = []
         run_pylint.set_verbosity(False)
-        logging.info("%s spell check", PROJECT_NAME)
-        logging.info("")
-        run_codespell(TOP_LEVEL_DIRNAME)
-
         logging.info("%s full tree check", PROJECT_NAME)
         logging.info("")
 
