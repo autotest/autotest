dfaac1c9814c340de090a9ddc0be0957f98cd7de
Plamen Dimitrov <pdimitrov@pevogam.com>
2014-03-06 21:25:36 +0200

    Handle cases where no operations were executed in the HTML report library
    Signed-off-by: Plamen Dimitrov <pdimitrov@pevogam.com>

--- client/shared/report.py
+++ client/shared/report.py
@@ -200,8 +200,11 @@ def parse_results_dir(results_dir, relative_links=True):
     # Now we will account the number of operations and PASS rate
     job_data['operations_executed'] = (job_data['operations_passed'] +
                                        job_data['operations_failed'])
-    job_data['operations_pass_rate'] = float(100 *
-                                             job_data['operations_passed'] / job_data['operations_executed'])
+    if job_data['operations_executed'] == 0:
+        job_data['operations_pass_rate'] = 100.0
+    else:
+        job_data['operations_pass_rate'] = float(100 * job_data['operations_passed'] /
+                                                 job_data['operations_executed'])
 
     return job_data
 
